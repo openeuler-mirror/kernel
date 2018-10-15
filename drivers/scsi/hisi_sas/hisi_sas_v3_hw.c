a9cc7cd9ecc843e5cc77a350cfe76ae24421064d
chenxiang <chenxiang66@hisilicon.com>
2018-10-15 11:41:07 +0800

    scsi: hisi_sas: add a separate host attribute for every host adapter
    Currently all the three host adapter (v1/v2/v3 hw) share the same host
    attribute. Actually one host attribute may has own host attribute which
    is not supported by other host adapter. So add a separate host attribute
    for every host adapter.
    
    Signed-off-by: Xiang Chen <chenxiang66@hisilicon.com>
    Signed-off-by: John Garry <john.garry@huawei.com>

--- drivers/scsi/hisi_sas/hisi_sas_v3_hw.c
+++ drivers/scsi/hisi_sas/hisi_sas_v3_hw.c
@@ -2125,6 +2125,11 @@ static void wait_cmds_complete_timeout_v3_hw(struct hisi_hba *hisi_hba,
 	dev_dbg(dev, "wait commands complete %dms\n", time);
 }
 
+struct device_attribute *host_attrs_v3_hw[] = {
+	&dev_attr_phy_event_threshold,
+	NULL
+};
+
 static struct scsi_host_template sht_v3_hw = {
 	.name			= DRV_NAME,
 	.module			= THIS_MODULE,
@@ -2143,7 +2148,7 @@ static struct scsi_host_template sht_v3_hw = {
 	.eh_target_reset_handler = sas_eh_target_reset_handler,
 	.target_destroy		= sas_target_destroy,
 	.ioctl			= sas_ioctl,
-	.shost_attrs		= host_attrs,
+	.shost_attrs		= host_attrs_v3_hw,
 	.host_reset             = hisi_sas_host_reset,
 	.tag_alloc_policy	= BLK_TAG_ALLOC_RR,
 };
