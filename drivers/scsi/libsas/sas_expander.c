08d00f461057b15cc11fb878324f713f6768e841
Jason Yan <yanaijie@huawei.com>
2018-12-28 10:56:45 +0800

    scsi: libsas: do not clear phy_state and attached_dev_type
    euler inclusion
    category: bugfix
    bugzilla: 5457
    CVE: NA
    
    When we check the sata pathway to the root phy, we need to know if the
    expander phy has sata device attached. So that we can have the chance to
    set the min linkrate for the sata device.
    
    Signed-off-by: Jason Yan <yanaijie@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- drivers/scsi/libsas/sas_expander.c
+++ drivers/scsi/libsas/sas_expander.c
@@ -1113,8 +1113,6 @@ static int sas_ex_discover_dev(struct domain_device *dev, int phy_id)
 		 * will not treat the phy as flutter in the next
 		 * revalidation
 		 */
-		ex_phy->phy_state = PHY_VACANT;
-		ex_phy->attached_dev_type = SAS_PHY_UNUSED;
 		memset(ex_phy->attached_sas_addr, 0, SAS_ADDR_SIZE);
 	}
 
