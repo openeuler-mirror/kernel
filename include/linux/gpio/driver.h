7c7db3d1ff954d82e71c69f5f9241217d4ae109b
Johan Hovold <johan@kernel.org>
2019-02-13 18:40:12 +0800

    gpio: drop broken to_gpio_irq_chip() helper
    mainline inclusion
    from mainline-5.0-rc1
    commit eee3919c5f2949a8b7b1e9fa239d153be1538656
    category: bugfix
    bugzilla: 5548
    CVE: NA
    
    ---------------------------
    
    Drop the broken to_gpio_irq_chip() container_of() helper, which would
    break the build for anyone who tries to use it.
    
    Specifically, struct gpio_irq_chip only holds a pointer to a struct
    irq_chip so using container_of() on an irq-chip pointer makes no sense.
    
    Fixes: da80ff81a8f5 ("gpio: Move irqchip into struct gpio_irq_chip")
    Cc: Thierry Reding <treding@nvidia.com>
    Cc: Grygorii Strashko <grygorii.strashko@ti.com>
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Reviewed-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    
    Signed-off-by: Jason Yan <yanaijie@huawei.com>
    Reviewed-by: zhengbin <zhengbin13@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- include/linux/gpio/driver.h
+++ include/linux/gpio/driver.h
@@ -146,11 +146,6 @@ struct gpio_irq_chip {
 	 */
 	unsigned int first;
 };
-
-static inline struct gpio_irq_chip *to_gpio_irq_chip(struct irq_chip *chip)
-{
-	return container_of(chip, struct gpio_irq_chip, chip);
-}
 #endif
 
 /**
