874e7071a4a7a1648cedcdbc1e0360d047a0cca7
Andrey Konovalov <andreyknvl@google.com>
2019-03-05 00:17:33 +0800

    kasan, slab: remove redundant kasan_slab_alloc hooks
    mainline inclusion
    from mainline-v5.0-rc7
    commit 557ea25383a2
    category: bugfix
    bugzilla: 10810
    CVE: NA
    
    -------------------------------------------------
    
    kasan_slab_alloc() calls in kmem_cache_alloc() and kmem_cache_alloc_node()
    are redundant as they are already called via slab_alloc/slab_alloc_node()->
    slab_post_alloc_hook()->kasan_slab_alloc().  Remove them.
    
    Link: http://lkml.kernel.org/r/4ca1655cdcfc4379c49c50f7bf80f81c4ad01485.1550602886.git.andreyknvl@google.com
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>
    Tested-by: Qian Cai <cai@lca.pw>
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Evgeniy Stepanov <eugenis@google.com>
    Cc: Kostya Serebryany <kcc@google.com>
    Cc: Vincenzo Frascino <vincenzo.frascino@arm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: zhong jiang <zhongjiang@huawei.com>
    Reviewed-by: Jing Xiangfeng <jingxiangfeng@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- mm/slab.c
+++ mm/slab.c
@@ -3553,7 +3553,6 @@ void *kmem_cache_alloc(struct kmem_cache *cachep, gfp_t flags)
 {
 	void *ret = slab_alloc(cachep, flags, _RET_IP_);
 
-	kasan_slab_alloc(cachep, ret, flags);
 	trace_kmem_cache_alloc(_RET_IP_, ret,
 			       cachep->object_size, cachep->size, flags);
 
@@ -3643,7 +3642,6 @@ void *kmem_cache_alloc_node(struct kmem_cache *cachep, gfp_t flags, int nodeid)
 {
 	void *ret = slab_alloc_node(cachep, flags, nodeid, _RET_IP_);
 
-	kasan_slab_alloc(cachep, ret, flags);
 	trace_kmem_cache_alloc_node(_RET_IP_, ret,
 				    cachep->object_size, cachep->size,
 				    flags, nodeid);
