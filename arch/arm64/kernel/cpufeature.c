dcb1ba68f748e1552681f0f1785ba03fb18d9e89
Hanjun Guo <hanjun.guo@linaro.org>
2019-03-27 16:00:49 +0800

    arm64: kpti: Whitelist HiSilicon Taishan v110 CPUs
    mainline inclusion
    from mainline-5.1-rc2
    commit 0ecc471a2cb7d4d386089445a727f47b59dc9b6e
    category: bugfix
    bugzilla: NA
    CVE: NA
    
    please see: https://patchwork.kernel.org/cover/10839459/
    ---------------------------------------------------
    
    HiSilicon Taishan v110 CPUs didn't implement CSV3 field of the
    ID_AA64PFR0_EL1 and are not susceptible to Meltdown, so whitelist
    the MIDR in kpti_safe_list[] table.
    
    Signed-off-by: Hanjun Guo <hanjun.guo@linaro.org>
    Reviewed-by: John Garry <john.garry@huawei.com>
    Reviewed-by: Zhangshaokun <zhangshaokun@hisilicon.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    
    Conflicts:
    	arch/arm64/kernel/cpufeature.c
    
    Signed-off-by: Hanjun Guo <guohanjun@huawei.com>
    Reviewed-by: Yang Yingliang <yangyingliang@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- arch/arm64/kernel/cpufeature.c
+++ arch/arm64/kernel/cpufeature.c
@@ -899,6 +899,7 @@ static bool unmap_kernel_at_el0(const struct arm64_cpu_capabilities *entry,
 	static const struct midr_range kpti_safe_list[] = {
 		MIDR_ALL_VERSIONS(MIDR_CAVIUM_THUNDERX2),
 		MIDR_ALL_VERSIONS(MIDR_BRCM_VULCAN),
+		MIDR_ALL_VERSIONS(MIDR_HISI_TSV110),
 		{ /* sentinel */ }
 	};
 	char const *str = "command line option";
