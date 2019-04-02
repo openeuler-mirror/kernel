0199ed2c91cd7a812450b07317e6e0cd3ef29264
shenjian <shenjian15@huawei.com>
2019-04-02 16:17:44 +0800

    net: hns3: add dma_rmb() for rx description
    driver inclusion
    category: bugfix
    bugzilla: NA
    CVE: NA
    
    HW can not guarantee complete write desc->rx.size, even though
    HNS3_RXD_VLD_B has been set. Driver needs to add dma_rmb()
    instruction to make sure desc->rx.size is always valid.
    
    Signed-off-by: shenjian (K) <shenjian15@huawei.com>
    Reviewed-by: lipeng <lipeng321@huawei.com>
    Reviewed-by: Xie XiuQi <xiexiuqi@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- drivers/net/ethernet/hisilicon/hns3/hns3_enet.c
+++ drivers/net/ethernet/hisilicon/hns3/hns3_enet.c
@@ -2663,7 +2663,7 @@ static int hns3_add_frag(struct hns3_enet_ring *ring, struct hns3_desc *desc,
 		desc_cb = &ring->desc_cb[ring->next_to_clean];
 		bd_base_info = le32_to_cpu(desc->rx.bd_base_info);
 		/* make sure HW write desc complete */
-		rmb();
+		dma_rmb();
 		if (!(bd_base_info & BIT(HNS3_RXD_VLD_B)))
 			return -ENXIO;
 
