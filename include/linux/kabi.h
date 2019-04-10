d430ea6da1e16406fd3d3f6b75835239b1707bd6
Tan Xiaojun <tanxiaojun@huawei.com>
2019-04-10 18:34:49 +0800

    kabi: Add generic kabi macros
    hulk inclusion
    category: feature
    bugzilla: 13276
    CVE: NA
    
    -------------------------------
    
    Introduce kabi macro for hulk.
    
    Signed-off-by: Tan Xiaojun <tanxiaojun@huawei.com>
    Reviewed-by: Xie XiuQi <xiexiuqi@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

---
+++ include/linux/kabi.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * kabi.h - Euler kABI abstraction header
+ *
+ * TODO: modify the comments and add more information.
+ */
+
+#ifndef _LINUX_KABI_H
+#define _LINUX_KABI_H
+
+/*
+ * TODO: add a note for this
+ */
+
+#define KABI_RESERVE(n)		unsigned long kabi_reserved##n;
+
+#endif /* _LINUX_KABI_H */
