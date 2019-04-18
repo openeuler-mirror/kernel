e8cb8c21cc35fb408033a4ef28e4d3fc73a72854
Xiongfeng Wang <wangxiongfeng2@huawei.com>
2019-04-18 16:01:43 +0800

    sdei_watchdog: set secure timer period base on 'watchdog_thresh'
    euler inclusion
    category: feature
    Bugzilla: 5515
    CVE: N/A
    
    ----------------------------------------
    
    The period of the secure timer is set to 3s by BIOS. That means the
    secure timer interrupt will trigger every 3 seconds. To further decrease
    the NMI watchdog's effect on performance, this patch set the period of
    the secure timer base on 'watchdog_thresh'. This variable is initiallized
    to 10s. We can also set the period at runtime by modifying
    '/proc/sys/kernel/watchdog_thresh'
    
    Signed-off-by: Xiongfeng Wang <wangxiongfeng2@huawei.com>
    Reviewed-by: Hanjun Guo <guohanjun@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- include/linux/arm_sdei.h
+++ include/linux/arm_sdei.h
@@ -42,6 +42,7 @@ int sdei_api_event_interrupt_bind(int hwirq);
 int sdei_api_event_disable(u32 event_num);
 int sdei_api_event_enable(u32 event_num);
 int sdei_api_clear_eoi(int hwirq);
+int sdei_api_set_secure_timer_period(int sec);
 
 #ifdef CONFIG_ARM_SDE_INTERFACE
 /* For use by arch code when CPU hotplug notifiers are not appropriate. */
