006c73b9572273cb6ef9b8b5112548c1d38569d7
Lang Cheng <chenglang@huawei.com>
2019-05-06 17:32:19 +0800

    RDMA/hns: Fixup qp release bug
    driver inclusion
    category: bugfix
    bugzilla: NA
    CVE: NA
    
    Hip06 reserve 12 qps, Hip08 reserve 8 qps.
    When the QP is released, the chip model is not judged,
    and the Hip08 cannot release the qpn 8~12.
    
    Feature or Bugfix:Bugfix
    
    Signed-off-by: Lang Cheng <chenglang@huawei.com>
    Reviewed-by: liuyixian <liuyixian@huawei.com>
    Reviewed-by: Yang Yingliang <yangyingliang@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- drivers/infiniband/hw/hns/hns_roce_qp.c
+++ drivers/infiniband/hw/hns/hns_roce_qp.c
@@ -328,7 +328,7 @@ void hns_roce_release_range_qp(struct hns_roce_dev *hr_dev, int base_qpn,
 {
 	struct hns_roce_qp_table *qp_table = &hr_dev->qp_table;
 
-	if (base_qpn < SQP_NUM)
+	if (base_qpn < hr_dev->caps.reserved_qps)
 		return;
 
 	hns_roce_bitmap_free_range(&qp_table->bitmap, base_qpn, cnt, BITMAP_RR);
@@ -1405,11 +1405,7 @@ int hns_roce_init_qp_table(struct hns_roce_dev *hr_dev)
 	spin_lock_init(&qp_table->lock);
 	INIT_RADIX_TREE(&hr_dev->qp_table_tree, GFP_ATOMIC);
 
-	/* In hw v1, a port include two SQP, six ports total 12 */
-	if (hr_dev->caps.max_sq_sg <= HNS_ROCE_MAX_SGE_NUM)
-		reserved_from_bot = SQP_NUM;
-	else
-		reserved_from_bot = hr_dev->caps.reserved_qps;
+	reserved_from_bot = hr_dev->caps.reserved_qps;
 
 	ret = hns_roce_bitmap_init(&qp_table->bitmap, hr_dev->caps.num_qps,
 				   hr_dev->caps.num_qps - 1, reserved_from_bot,
