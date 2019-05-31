1fd450647987bf78a8ccc8492ae56dcb35c5081f
Yupeng Zhou <zhouyupeng1@huawei.com>
2019-05-31 18:33:08 +0800

    hisi_sas: default enable the debugfs feature.
    driver inclusion
    category: feature
    bugzilla: NA
    CVE: NA
    
    default enable the debugfs feature.
    
    Signed-off-by: Yupeng Zhou <zhouyupeng1@huawei.com>
    Reviewed-by: luojian <luojian5@huawei.com>
    Reviewed-by: chenxiang <chenxiang66@hisilicon.com>
    Reviewed-by: Yang Yingliang <yangyingliang@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- drivers/scsi/hisi_sas/hisi_sas_main.c
+++ drivers/scsi/hisi_sas/hisi_sas_main.c
@@ -3304,7 +3304,7 @@ int hisi_sas_remove(struct platform_device *pdev)
 }
 EXPORT_SYMBOL_GPL(hisi_sas_remove);
 
-bool hisi_sas_debugfs_enable;
+bool hisi_sas_debugfs_enable = true;
 EXPORT_SYMBOL_GPL(hisi_sas_debugfs_enable);
 module_param_named(debugfs_enable, hisi_sas_debugfs_enable, bool, 0444);
 MODULE_PARM_DESC(hisi_sas_debugfs_enable, "Enable driver debugfs (default disabled)");
