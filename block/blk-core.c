4b21f1466243d4c64ebd49a630d4811fd97cbd46
Ming Lei <ming.lei@redhat.com>
2019-06-28 15:07:50 +0800

    blk-mq: move cancel of hctx->run_work into blk_mq_hw_sysfs_release
    mainline inclusion
    from mainline-5.2-rc1
    commit 1b97871b501f1bac0fd39a073c4c8473ee457a55
    category: bugfix
    bugzilla: 14864
    CVE: NA
    ---------------------------
    
    hctx is always released after requeue is freed.
    
    With holding queue's kobject refcount, it is safe for driver to run queue,
    so one run queue might be scheduled after blk_sync_queue() is done.
    
    So moving the cancel of hctx->run_work into blk_mq_hw_sysfs_release()
    for avoiding run released queue.
    
    Cc: Dongli Zhang <dongli.zhang@oracle.com>
    Cc: James Smart <james.smart@broadcom.com>
    Cc: Bart Van Assche <bart.vanassche@wdc.com>
    Cc: linux-scsi@vger.kernel.org,
    Cc: Martin K . Petersen <martin.petersen@oracle.com>,
    Cc: Christoph Hellwig <hch@lst.de>,
    Cc: James E . J . Bottomley <jejb@linux.vnet.ibm.com>,
    Reviewed-by: Bart Van Assche <bvanassche@acm.org>
    Reviewed-by: Hannes Reinecke <hare@suse.com>
    Tested-by: James Smart <james.smart@broadcom.com>
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Conflict:
    	block/blk-core.c
    
    Signed-off-by: yangerkun <yangerkun@huawei.com>
    Reviewed-by: zhangyi (F) <yi.zhang@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- block/blk-core.c
+++ block/blk-core.c
@@ -407,15 +407,8 @@ void blk_sync_queue(struct request_queue *q)
 	del_timer_sync(&q->timeout);
 	cancel_work_sync(&q->timeout_work);
 
-	if (q->mq_ops) {
-		struct blk_mq_hw_ctx *hctx;
-		int i;
-
-		queue_for_each_hw_ctx(q, hctx, i)
-			cancel_delayed_work_sync(&hctx->run_work);
-	} else {
+	if (!q->mq_ops)
 		cancel_delayed_work_sync(&q->delay_work);
-	}
 }
 EXPORT_SYMBOL(blk_sync_queue);
 
