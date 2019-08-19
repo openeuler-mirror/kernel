729c71bc1d34c188d8612c6ddd0356125718b81f
Hongbo Yao <yaohongbo@huawei.com>
2019-08-19 17:29:11 +0800

    Revert "perf: arm_spe: Enable ACPI/Platform automatic module loading"
    hulk inclusion
    category: feature
    bugzilla: 16072
    CVE: NA
    ---------------------------
    
    This reverts commit 1e15594e6cd587350e006db646be55f747d2654c.
    In Linux 5.3.0, SPE ACPI enablement has been upstreamed. SPE patches
    in  hulk-4.19 are the old version, and they need to be reverted
    to the mainline version.
    
    Signed-off-by: Hongbo Yao <yaohongbo@huawei.com>
    Reviewed-by: Hanjun Guo <guohanjun@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- drivers/perf/arm_spe_pmu.c
+++ drivers/perf/arm_spe_pmu.c
@@ -1176,13 +1176,7 @@ static const struct of_device_id arm_spe_pmu_of_match[] = {
 };
 MODULE_DEVICE_TABLE(of, arm_spe_pmu_of_match);
 
-static const struct platform_device_id arm_spe_match[] = {
-	{ "arm,spe-v1", 0},
-	{ }
-};
-MODULE_DEVICE_TABLE(platform, arm_spe_match);
-
-static int arm_spe_pmu_device_probe(struct platform_device *pdev)
+static int arm_spe_pmu_device_dt_probe(struct platform_device *pdev)
 {
 	int ret;
 	struct arm_spe_pmu *spe_pmu;
@@ -1242,12 +1236,11 @@ static int arm_spe_pmu_device_remove(struct platform_device *pdev)
 }
 
 static struct platform_driver arm_spe_pmu_driver = {
-	.id_table = arm_spe_match,
 	.driver	= {
 		.name		= DRVNAME,
 		.of_match_table	= of_match_ptr(arm_spe_pmu_of_match),
 	},
-	.probe	= arm_spe_pmu_device_probe,
+	.probe	= arm_spe_pmu_device_dt_probe,
 	.remove	= arm_spe_pmu_device_remove,
 };
 
