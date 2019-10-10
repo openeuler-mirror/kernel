d05db05ebb148b5f2eca2e2a972336408d211db8
Guojia Liao <liaoguojia@huawei.com>
2019-10-10 22:14:16 +0800

    net: hns3: fixes some incorrect type in assignment reported by sparse.
    driver inclusion
    category: bugfix
    bugzilla: NA
    CVE: NA
    
    This patch fixes some incorrect type in assignment as below:
    -warning: cast to restricted __le32
    
    Fixes: 791fa4d77505 ("Fixes byte order issues when printed")
    
    Feature or Bugfix:Bugfix
    
    Signed-off-by: Guojia Liao <liaoguojia@huawei.com>
    Reviewed-by: lipeng <lipeng321@huawei.com>
    Reviewed-by: Yunsheng Lin <linyunsheng@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_main.c
+++ drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_main.c
@@ -5811,8 +5811,7 @@ static int hclge_del_fd_entry(struct hnae3_handle *handle,
 
 	if (!hclge_fd_rule_exist(hdev, fs->location)) {
 		dev_err(&hdev->pdev->dev,
-			"Delete fail, rule %u is inexistent\n",
-			le32_to_cpu(fs->location));
+			"Delete fail, rule %u is inexistent\n", fs->location);
 		return -ENOENT;
 	}
 
