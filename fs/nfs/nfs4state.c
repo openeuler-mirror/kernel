8133ffd09f5928859750681dc1166656c2ccb59d
Wenwen Wang <wenwen@cs.uga.edu>
2019-10-29 16:18:22 +0800

    NFSv4: Fix a memory leak bug
    mainline inclusion
    from mainline-v5.4
    commit 1e672e3644940d83bd94e7cb46bac6bb3627de02
    category: bugfix
    bugzilla: 23341
    CVE: NA
    
    -------------------------------------------------
    
    In nfs4_try_migration(), if nfs4_begin_drain_session() fails, the
    previously allocated 'page' and 'locations' are not deallocated, leading to
    memory leaks. To fix this issue, go to the 'out' label to free 'page' and
    'locations' before returning the error.
    
    Signed-off-by: Wenwen Wang <wenwen@cs.uga.edu>
    Signed-off-by: Anna Schumaker <Anna.Schumaker@Netapp.com>
    
    v1->v2: add bugzilla id.
    Signed-off-by: Zhang Xiaoxu <zhangxiaoxu5@huawei.com>
    Reviewed-by: zhangyi (F) <yi.zhang@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- fs/nfs/nfs4state.c
+++ fs/nfs/nfs4state.c
@@ -2073,8 +2073,10 @@ static int nfs4_try_migration(struct nfs_server *server, struct rpc_cred *cred)
 	}
 
 	status = nfs4_begin_drain_session(clp);
-	if (status != 0)
-		return status;
+	if (status != 0) {
+		result = status;
+		goto out;
+	}
 
 	status = nfs4_replace_transport(server, locations);
 	if (status != 0) {
