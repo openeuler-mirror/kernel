073a55a031aa82bd3c0cde452ba6cc2b59074464
Jan Kara <jack@suse.cz>
2019-10-30 21:50:43 +0800

    fs: Export generic_fadvise()
    mainline inclusion
    from mainline-5.4-rc1
    commit cf1ea0592dbf109e7e7935b7d5b1a47a1ba04174
    category: bugfix
    bugzilla: 22583
    CVE: NA
    
    ---------------------------
    
    Filesystems will need to call this function from their fadvise handlers.
    
    CC: stable@vger.kernel.org
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: yu kuai <yukuai3@huawei.com>
    Reviewed-by: Hou Tao <houtao1@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- mm/fadvise.c
+++ mm/fadvise.c
@@ -27,7 +27,7 @@
  * deactivate the pages and clear PG_Referenced.
  */
 
-static int generic_fadvise(struct file *file, loff_t offset, loff_t len,
+int generic_fadvise(struct file *file, loff_t offset, loff_t len,
 			   int advice)
 {
 	struct inode *inode;
@@ -178,6 +178,7 @@ static int generic_fadvise(struct file *file, loff_t offset, loff_t len,
 	}
 	return 0;
 }
+EXPORT_SYMBOL(generic_fadvise);
 
 int vfs_fadvise(struct file *file, loff_t offset, loff_t len, int advice)
 {
