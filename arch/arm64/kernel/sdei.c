1a0094f7c95a8e30fd8ebdf5beb494504e0f048c
Xu Qiang <xuqiang36@huawei.com>
2019-11-07 16:29:55 +0800

    drivers: support nmi by sdei
    ascend inclusion
    category: feature
    bugzilla: NA
    CVE: NA
    
    ------------
    
    SDEI is a set of interfaces used for communication between the OS and ATF.
    The Ascend uses the SDEI interface to implement the NMI function.
    NMI depends on the soft lockup detector. The OS of the Ascend runs on EL1.
    
    Signed-off-by: Xu Qiang <xuqiang36@huawei.com>
    Signed-off-by: Lijun Fang <fanglijun3@huawei.com>
    Reviewed-by: Hanjun Guo <guohanjun@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- arch/arm64/kernel/sdei.c
+++ arch/arm64/kernel/sdei.c
@@ -145,6 +145,7 @@ bool _on_sdei_stack(unsigned long sp, struct stack_info *info)
 
 unsigned long sdei_arch_get_entry_point(int conduit)
 {
+#ifndef CONFIG_ARCH_ASCEND
 	/*
 	 * SDEI works between adjacent exception levels. If we booted at EL1 we
 	 * assume a hypervisor is marshalling events. If we booted at EL2 and
@@ -156,6 +157,7 @@ unsigned long sdei_arch_get_entry_point(int conduit)
 		return 0;
 	}
 
+#endif
 	if (IS_ENABLED(CONFIG_VMAP_STACK)) {
 		if (init_sdei_stacks())
 			return 0;
