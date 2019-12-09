fb55aa477dfa7f488f112a8b87116962d623ff63
gaoxun3 <gaoxun3@huawei.com>
2019-12-09 10:18:13 +0800

    net: hns3: place cae regconfig-related implementations in an independent file
    driver inclusion
    category: feature
    bugzilla: NA
    CVE: NA
    
    --------------------------------
    
    this is the part of cae decoupling effort regards to reg config commands
    
    Signed-off-by: gaoxun3 <gaoxun3@huawei.com>
    Reviewed-by: dengweiwei <dengweiwei@huawei.com>
    Reviewed-by: chenjunxin <chenjunxin1@huawei.com>
    Reviewed-by: liyongxin1 <liyongxin1@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- drivers/net/ethernet/hisilicon/hns3/hns3_cae/hns3_cae_common.h
+++ drivers/net/ethernet/hisilicon/hns3/hns3_cae/hns3_cae_common.h
@@ -20,24 +20,6 @@ struct reg_ret_param {
 	u32 value[REG_RDATA_NUM];
 };
 
-struct cmd_desc {
-	u16 opcode;
-	u16 flag;
-	u16 retval;
-	u16 rsv;
-	u32 data[6];
-};
-
-struct com_reg_param {
-	struct cmd_desc reg_desc;
-	u32 fw_dw_opcode;
-	u32 is_read;
-};
-
 int hns3_cae_reg_cfg(struct hns3_nic_priv *net_priv, void *buf_in, u32 in_size,
 		     void *buf_out, u32 out_size);
-
-int hns3_reg_cfg(struct hns3_nic_priv *net_priv, void *buf_in, u32 in_size,
-		 void *buf_out, u32 out_size);
-
 #endif
