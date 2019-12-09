09b9f5b92613d9c1553e1d8c2ad8839940ebe94b
gaoxun3 <gaoxun3@huawei.com>
2019-12-09 10:18:14 +0800

    net: hns3: place cae m7_cmd-related implementations in an independent file
    driver inclusion
    category: feature
    bugzilla: NA
    CVE: NA
    
    -----------------------------
    
    this is the part of cae decoupling effort regards to m7 commands
    
    Signed-off-by: gaoxun3 <gaoxun3@huawei.com>
    Reviewed-by: dengweiwei <dengweiwei@huawei.com>
    Reviewed-by: chenjunxin <chenjunxin1@huawei.com>
    Reviewed-by: liyongxin1 <liyongxin1@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- drivers/net/ethernet/hisilicon/hns3/hns3_cae/hns3_cae_lib.c
+++ drivers/net/ethernet/hisilicon/hns3/hns3_cae/hns3_cae_lib.c
@@ -26,7 +26,6 @@
 #include "hns3_cae_fd.h"
 #include "hns3_cae_rss.h"
 #include "hns3_cae_common.h"
-#include "hns3_cae_m7_cmd.h"
 #include "hns3_cae_qres.h"
 #include "hns3_cae_stat.h"
 #include "hns3_cae_irq.h"
@@ -596,6 +595,58 @@ int hns3_cae_common_cmd_send(struct hns3_nic_priv *net_priv, void *buf_in,
 		for (i = 0; i < MAX_DESC_DATA_LEN; i++)
 			param_out->reg_desc.data[i] = desc.data[i];
 	}
+
+	return 0;
+}
+
+int hns3_m7_cmd_handle(struct hns3_nic_priv *nic_dev, void *buf_in, u32 in_size,
+		       void *buf_out, u32 out_size)
+{
+	struct hclge_vport *vport = hns3_cae_get_vport(nic_dev->ae_handle);
+	struct m7_cmd_para *cmd_para = (struct m7_cmd_para *)buf_in;
+	struct hclge_dev *hdev = vport->back;
+	struct hclge_desc *desc;
+	u32 bd_size;
+	bool check;
+	int ret;
+
+	check = !buf_in || in_size < sizeof(struct m7_cmd_para);
+	if (check) {
+		pr_err("input param buf_in error in %s function\n", __func__);
+		return -EFAULT;
+	}
+
+	bd_size = sizeof(struct hclge_desc) * cmd_para->bd_count;
+	desc = kzalloc(bd_size, GFP_KERNEL);
+	if (ZERO_OR_NULL_PTR(desc)) {
+		pr_err("desc kzalloc failed in m7_cmd_handle function\n");
+		return -ENOMEM;
+	}
+	if (copy_from_user((void *)desc, cmd_para->bd_data, bd_size)) {
+		pr_err("copy from user failed in m7_cmd_handle function\n");
+		kfree(desc);
+		return -EFAULT;
+	}
+
+	ret = hns3_cae_cmd_send(hdev, desc, cmd_para->bd_count);
+	if (ret) {
+		dev_err(&hdev->pdev->dev,
+			"generic cmd send fail, ret is %d.\n", ret);
+		kfree(desc);
+		return ret;
+	}
+
+	if (desc->flag & HCLGE_CMD_FLAG_WR) {
+		if (!buf_out || out_size < bd_size) {
+			pr_err("input param buf_out error in %s function\n",
+			       __func__);
+			kfree(desc);
+			return -EFAULT;
+		}
+		memcpy(buf_out, desc, bd_size);
+	}
+
+	kfree(desc);
 
 	return 0;
 }
