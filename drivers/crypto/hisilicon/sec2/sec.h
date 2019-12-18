19c13ecdf7d9bf50cff1841aff356a61352400cd
yekai13 <yekai13@huawei.com>
2019-12-18 20:50:16 +0800

    acc:Revert "hisi_sec2:refactor the sec2 module code."
    driver inclusion
    category: bugfix
    bugzilla: NA
    CVE: NA
    
    This reverts commit f6a275ced40b7689f01ed5e72a11c7be466bda21.
    There is a problem with current code.need to revert.
    
    Signed-off-by: yekai13 <yekai13@huawei.com>
    Reviewed-by: Zhang Wei <zhangwei375@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- drivers/crypto/hisilicon/sec2/sec.h
+++ drivers/crypto/hisilicon/sec2/sec.h
@@ -1,145 +1,28 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-/* Copyright (c) 2018-2019 HiSilicon Limited. */
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2018-2019 HiSilicon Limited.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ */
 
-#ifndef __HISI_SEC_V2_H
-#define __HISI_SEC_V2_H
+#ifndef HISI_SEC_H
+#define HISI_SEC_H
 
 #include <linux/list.h>
-
 #include "../qm.h"
-#include "sec_crypto.h"
+#include "sec_usr_if.h"
 
-/* Cipher resource per hardware SEC queue */
-struct sec_cipher_res {
-	u8 *c_ivin;
-	dma_addr_t c_ivin_dma;
-#ifdef CONFIG_CRYPTO_DEV_HISI_SEC2_FUSION
-	struct skcipher_request **sk_reqs;
-	struct scatterlist *src;
-	struct scatterlist *dst;
-#endif
-};
+#undef pr_fmt
+#define pr_fmt(fmt)	"hisi_sec: " fmt
 
-/* Cipher request of SEC private */
-struct sec_cipher_req {
-	struct hisi_acc_hw_sgl *c_in;
-	dma_addr_t c_in_dma;
-	struct hisi_acc_hw_sgl *c_out;
-	dma_addr_t c_out_dma;
-	u8 *c_ivin;
-	dma_addr_t c_ivin_dma;
-	struct skcipher_request *sk_req;
-	u32 c_len;
-#ifdef CONFIG_CRYPTO_DEV_HISI_SEC2_FUSION
-	u32 gran_num;
-	u64 lba;
-	struct scatterlist *src;
-	struct scatterlist *dst;
-#endif
-	bool encrypt;
-};
-
-/* SEC request of Crypto */
-struct sec_req {
-	struct sec_sqe sec_sqe;
-	struct sec_ctx *ctx;
-	struct sec_qp_ctx *qp_ctx;
-
-	/* Cipher supported only at present */
-	struct sec_cipher_req c_req;
-	int err_type;
-	int req_id;
-
-	/* Status of the SEC request */
-	int fake_busy;
-#ifdef CONFIG_CRYPTO_DEV_HISI_SEC2_FUSION
-	struct skcipher_request **sk_reqs;
-	struct scatterlist *src;
-	struct scatterlist *dst;
-	int fusion_num;
-	ktime_t st_time;
-#endif
-};
-
-/**
- * struct sec_req_op - Operations for SEC request
- * @get_res: Get resources for TFM on the SEC device
- * @resource_alloc: Allocate resources for queue context on the SEC device
- * @resource_free: Free resources for queue context on the SEC device
- * @buf_map: DMA map the SGL buffers of the request
- * @buf_unmap: DMA unmap the SGL buffers of the request
- * @bd_fill: Fill the SEC queue BD
- * @bd_send: Send the SEC BD into the hardware queue
- * @callback: Call back for the request
- * @process: Main processing logic of Skcipher
- */
-struct sec_req_op {
-	int (*get_res)(struct sec_ctx *ctx, struct sec_req *req);
-	int (*resource_alloc)(struct sec_ctx *ctx, struct sec_qp_ctx *qp_ctx);
-	void (*resource_free)(struct sec_ctx *ctx, struct sec_qp_ctx *qp_ctx);
-	int (*buf_map)(struct sec_ctx *ctx, struct sec_req *req);
-	void (*buf_unmap)(struct sec_ctx *ctx, struct sec_req *req);
-	void (*do_transfer)(struct sec_ctx *ctx, struct sec_req *req);
-	int (*bd_fill)(struct sec_ctx *ctx, struct sec_req *req);
-	int (*bd_send)(struct sec_ctx *ctx, struct sec_req *req);
-	void (*callback)(struct sec_ctx *ctx, struct sec_req *req);
-	int (*process)(struct sec_ctx *ctx, struct sec_req *req);
-};
-
-/* SEC cipher context which cipher's relatives */
-struct sec_cipher_ctx {
-	u8 *c_key;
-	dma_addr_t c_key_dma;
-	sector_t iv_offset;
-	u32 c_gran_size;
-	u32 ivsize;
-	u8 c_mode;
-	u8 c_alg;
-	u8 c_key_len;
-};
-
-/* SEC queue context which defines queue's relatives */
-struct sec_qp_ctx {
-	struct hisi_qp *qp;
-	struct sec_req **req_list;
-	struct idr req_idr;
-	void *alg_meta_data;
-	struct sec_ctx *ctx;
-	struct mutex req_lock;
-	struct hisi_acc_sgl_pool *c_in_pool;
-	struct hisi_acc_sgl_pool *c_out_pool;
-	atomic_t pending_reqs;
-#ifdef CONFIG_CRYPTO_DEV_HISI_SEC2_FUSION
-	struct sec_req *fusion_req;
-	int fusion_num;
-#endif
-};
-
-/* SEC Crypto TFM context which defines queue and cipher .etc relatives */
-struct sec_ctx {
-	struct sec_qp_ctx *qp_ctx;
-	struct sec_dev *sec;
-	const struct sec_req_op *req_op;
-
-	/* Half queues for encipher, and half for decipher */
-	u32 hlf_q_num;
-
-	/* Threshold for fake busy, trigger to return -EBUSY to user */
-	u32 fake_req_limit;
-
-	/* Currrent cyclic index to select a queue for encipher */
-	atomic_t enc_qcyclic;
-
-	 /* Currrent cyclic index to select a queue for decipher */
-	atomic_t dec_qcyclic;
-	struct sec_cipher_ctx c_ctx;
-#ifdef CONFIG_CRYPTO_DEV_HISI_SEC2_FUSION
-	atomic_t thread_cnt;
-	bool is_fusion;
-	struct hrtimer timer;
-	struct work_struct work;
-#endif
-};
+#define CTX_Q_NUM_DEF		24
+#define FUSION_LIMIT_DEF	1
+#define FUSION_LIMIT_MAX	64
+#define FUSION_TMOUT_NSEC_DEF	(400 * 1000)
 
 enum sec_endian {
 	SEC_LE = 0,
@@ -147,24 +30,17 @@ enum sec_endian {
 	SEC_64BE
 };
 
-enum sec_debug_file_index {
-	SEC_CURRENT_QM,
-	SEC_CLEAR_ENABLE,
-	SEC_DEBUG_FILE_NUM,
+struct hisi_sec_ctrl;
+
+enum hisi_sec_status {
+	HISI_SEC_RESET,
 };
 
-struct sec_debug_file {
-	enum sec_debug_file_index index;
-	spinlock_t lock;
-	struct hisi_qm *qm;
-};
-
-struct sec_dfx {
+struct hisi_sec_dfx {
 	u64 send_cnt;
-	u64 recv_cnt;
-#ifdef CONFIG_CRYPTO_DEV_HISI_SEC2_FUSION
 	u64 send_by_tmout;
 	u64 send_by_full;
+	u64 recv_cnt;
 	u64 get_task_cnt;
 	u64 put_task_cnt;
 	u64 gran_task_cnt;
@@ -172,29 +48,20 @@ struct sec_dfx {
 	u64 fake_busy_cnt;
 	u64 busy_comp_cnt;
 	u64 sec_ctrl;
-#endif
 };
 
-struct sec_debug {
-	struct sec_dfx dfx;
-	struct sec_debug_file files[SEC_DEBUG_FILE_NUM];
-};
-
-struct sec_dev {
+struct hisi_sec {
 	struct hisi_qm qm;
 	struct list_head list;
-	struct sec_debug debug;
-	u32 ctx_q_num;
-	u32 num_vfs;
+	struct hisi_sec_dfx sec_dfx;
+	struct hisi_sec_ctrl *ctrl;
+	struct mutex *hisi_sec_list_lock;
+	int q_ref;
+	int ctx_q_num;
+	int fusion_limit;
+	int fusion_tmout_nsec;
 	unsigned long status;
-#ifdef CONFIG_CRYPTO_DEV_HISI_SEC2_FUSION
-	struct workqueue_struct *wq;
-	u32 fusion_limit;
-	u32 fusion_time;
-#endif
 };
 
-struct sec_dev *sec_find_device(int node);
-int sec_register_to_crypto(void);
-void sec_unregister_from_crypto(void);
+struct hisi_sec *find_sec_device(int node);
 #endif
