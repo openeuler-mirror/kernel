86f49b53d842f7a51fdcb3537960fe7ed71032f3
Zenghui Yu <yuzenghui@huawei.com>
2020-01-13 22:13:13 +0800

    perf, kvm/arm64: perf-kvm-stat to report VM TRAP
    euleros inclusion
    category: feature
    feature: perf kvm stat on arm64
    
    picked from https://patchwork.kernel.org/patch/10989121/.
    
    -------------------------------------------------
    
    When guest exits due to "TRAP", we can analyze the guest exit reasons
    deeplier. Enhance perf-kvm-stat to record and analyze VM TRAP events.
    
    There is a mapping between guest's "trap_code" (ESR_ELx's bits[31:26])
    and "trap_reason" - kvm_arm_exception_class. Copy it from kernel to
    aarch64_guest_exits.h, export it to userspace.
    
    This patch records two new KVM tracepoints: "kvm:kvm_trap_enter" and
    "kvm:kvm_trap_exit", and reports statistical data between these two
    tracepoints.
    
    A simple test go below:
    
     # ./tools/perf/perf kvm stat record -p 20763
    [ perf record: Woken up 92 times to write data ]
    [ perf record: Captured and wrote 203.727 MB perf.data.guest (2601786 samples) ]
    
     # ./tools/perf/perf kvm stat report --event=vmexit
    
    Analyze events for all VMs, all VCPUs:
    
                 VM-EXIT    Samples  Samples%     Time%    Min Time    Max Time         Avg time
    
                    TRAP     640931    97.12%   100.00%      2.44us  14683.86us   3446.49us ( +-   0.05% )
                     IRQ      19019     2.88%     0.00%      0.90us    461.94us      2.12us ( +-   2.09% )
    
    Total Samples:659950, Total events handled time:2209005391.30us.
    
     # ./tools/perf/perf kvm stat report --event=trap
    
    Analyze events for all VMs, all VCPUs:
    
              TRAP-EVENT    Samples  Samples%     Time%    Min Time    Max Time         Avg time
    
                     WFx     601194    93.80%    99.98%      0.90us   4294.04us   3671.01us ( +-   0.03% )
                   SYS64      33714     5.26%     0.01%      1.10us     41.34us      5.68us ( +-   0.18% )
                DABT_LOW       6014     0.94%     0.00%      1.12us     18.04us      2.57us ( +-   0.91% )
                IABT_LOW         12     0.00%     0.01%  12597.76us  14679.96us  12893.61us ( +-   1.34% )
    
    Total Samples:640934, Total events handled time:2207353434.56us.
    
    Signed-off-by: Zenghui Yu <yuzenghui@huawei.com>
    Reviewed-by: Hailiang Zhang <zhang.zhanghailiang@huawei.com>
    Signed-off-by: Zenghui Yu <yuzenghui@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- tools/perf/arch/arm64/util/kvm-stat.c
+++ tools/perf/arch/arm64/util/kvm-stat.c
@@ -9,6 +9,7 @@
 #include "aarch64_guest_exits.h"
 
 define_exit_reasons_table(arm64_exit_reasons, kvm_arm_exception_type);
+define_exit_reasons_table(arm64_trap_reasons, kvm_arm_exception_class);
 
 static struct kvm_events_ops exit_events = {
 	.is_begin_event = exit_event_begin,
@@ -23,14 +24,78 @@ const char *kvm_exit_reason = "ret";
 const char *kvm_entry_trace = "kvm:kvm_entry";
 const char *kvm_exit_trace = "kvm:kvm_exit";
 
+const char *kvm_trap_reason = "esr_ec";
+const char *kvm_trap_enter_trace = "kvm:kvm_trap_enter";
+const char *kvm_trap_exit_trace = "kvm:kvm_trap_exit";
+
+static void trap_event_get_key(struct perf_evsel *evsel,
+			       struct perf_sample *sample,
+			       struct event_key *key)
+{
+	key->info = 0;
+	key->key = perf_evsel__intval(evsel, sample, kvm_trap_reason);
+}
+
+static const char *get_trap_reason(u64 exit_code)
+{
+	struct exit_reasons_table *tbl = arm64_trap_reasons;
+
+	while (tbl->reason != NULL) {
+		if (tbl->exit_code == exit_code)
+			return tbl->reason;
+		tbl++;
+	}
+
+	pr_err("Unknown kvm trap exit code: %lld on aarch64\n",
+	       (unsigned long long)exit_code);
+	return "UNKNOWN";
+}
+
+static bool trap_event_end(struct perf_evsel *evsel,
+			   struct perf_sample *sample __maybe_unused,
+			   struct event_key *key __maybe_unused)
+{
+	return (!strcmp(evsel->name, kvm_trap_exit_trace));
+}
+
+static bool trap_event_begin(struct perf_evsel *evsel,
+			     struct perf_sample *sample, struct event_key *key)
+{
+	if (!strcmp(evsel->name, kvm_trap_enter_trace)) {
+		trap_event_get_key(evsel, sample, key);
+		return true;
+	}
+
+	return false;
+}
+
+static void trap_event_decode_key(struct perf_kvm_stat *kvm __maybe_unused,
+				  struct event_key *key,
+				  char *decode)
+{
+	const char *trap_reason = get_trap_reason(key->key);
+
+	scnprintf(decode, decode_str_len, "%s", trap_reason);
+}
+
+static struct kvm_events_ops trap_events = {
+	.is_begin_event = trap_event_begin,
+	.is_end_event = trap_event_end,
+	.decode_key = trap_event_decode_key,
+	.name = "TRAP-EVENT",
+};
+
 const char *kvm_events_tp[] = {
 	"kvm:kvm_entry",
 	"kvm:kvm_exit",
+	"kvm:kvm_trap_enter",
+	"kvm:kvm_trap_exit",
 	NULL,
 };
 
 struct kvm_reg_events_ops kvm_reg_events_ops[] = {
 	{ .name = "vmexit", .ops = &exit_events },
+	{ .name = "trap", .ops = &trap_events },
 	{ NULL, NULL },
 };
 
