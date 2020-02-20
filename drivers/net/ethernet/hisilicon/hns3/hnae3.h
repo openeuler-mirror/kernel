62ee8e94895c252963e2f263a9029c7978df2712
Huazhong Tan <tanhuazhong@huawei.com>
2020-02-20 16:54:12 +0800

    net: hns3: sync some code from net-next part1
    driver inclusion
    category: bugfix
    bugzilla: NA
    CVE: NA
    
    This patch merges back some code from net-next about below patches:
    net: hns3: add support for configuring VF MAC from the host
    net: hns3: add support for spoof check setting
    net: hns3: add support for setting VF trust
    net: hns3: add support for configuring bandwidth of VF on the host
    net: hns3: add support for setting VF link status on the host
    net: hns3: support tx-scatter-gather-fraglist feature
    net: hns3: fix spelling mistake "undeflow" -> "underflow"
    
    Signed-off-by: Huazhong Tan <tanhuazhong@huawei.com>
    Reviewed-by: Peng Li <lipeng321@huawei.com>
    Reviewed-by: Zhong Zhaohui <zhongzhaohui@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- drivers/net/ethernet/hisilicon/hns3/hnae3.h
+++ drivers/net/ethernet/hisilicon/hns3/hnae3.h
@@ -364,6 +364,10 @@ struct hnae3_ae_dev {
  *   Enable/disable HW GRO
  * add_arfs_entry
  *   Check the 5-tuples of flow, and create flow director rule
+ * get_vf_config
+ *   Get the VF configuration setting by the host
+ * set_vf_link_state
+ *   Set VF link status
  * set_vf_spoofchk
  *   Enable/disable spoof check for specified vf
  * set_vf_trust
@@ -371,6 +375,8 @@ struct hnae3_ae_dev {
  *   it can enable promisc mode
  * set_vf_rate
  *   Set the max tx rate of specified vf.
+ * set_vf_mac
+ *   Configure the default MAC for specified VF
  */
 struct hnae3_ae_ops {
 	int (*init_ae_dev)(struct hnae3_ae_dev *ae_dev);
@@ -551,10 +557,10 @@ struct hnae3_ae_ops {
 				 int link_state);
 	int (*set_vf_spoofchk)(struct hnae3_handle *handle, int vf,
 			       bool enable);
-	int (*set_vf_mac)(struct hnae3_handle *handle, int vf, u8 *p);
 	int (*set_vf_trust)(struct hnae3_handle *handle, int vf, bool enable);
 	int (*set_vf_rate)(struct hnae3_handle *handle, int vf,
 			   int min_tx_rate, int max_tx_rate, bool force);
+	int (*set_vf_mac)(struct hnae3_handle *handle, int vf, u8 *p);
 };
 
 struct hnae3_dcb_ops {
