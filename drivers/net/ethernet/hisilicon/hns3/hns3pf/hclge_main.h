8ddbd6dc822e9ff115f9e95c567ee757bfa4bedc
Shengzui You <youshengzui@huawei.com>
2020-03-12 11:36:22 +0800

    net: hns3: update hns3 version to 1.9.37.1
    driver inclusion
    category: feature
    bugzilla: NA
    CVE: NA
    
    -------------------------
    
    This patch is used to modify hns3 version to 1.9.37.1
    
    Signed-off-by: Shengzui You <youshengzui@huawei.com>
    Reviewed-by: Weiwei Deng <dengweiwei@huawei.com>
    Reviewed-by: Zhaohui Zhong <zhongzhaohui@huawei.com>
    Reviewed-by: Junxing Chen <chenjunxin1@huawei.com>
    Reviewed-by: Zhong Zhaohui <zhongzhaohui@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_main.h
+++ drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_main.h
@@ -12,7 +12,7 @@
 #include "hclge_cmd.h"
 #include "hnae3.h"
 
-#define HCLGE_MOD_VERSION "1.9.37.0"
+#define HCLGE_MOD_VERSION "1.9.37.1"
 #define HCLGE_DRIVER_NAME "hclge"
 
 #define HCLGE_MAX_PF_NUM		8
