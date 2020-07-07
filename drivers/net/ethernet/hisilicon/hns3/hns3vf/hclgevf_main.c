1660dad43888b6944db97b3911895268aed33f3c
Yunsheng Lin <linyunsheng@huawei.com>
2020-07-07 11:05:30 +0000

    net: hns3: drop the WQ_MEM_RECLAIM flag when allocating wq
    driver inclusion
    category: bugfix
    bugzilla: NA
    CVE: NA
    
    ------------------------------
    
    The wq in hns3 driver is allocated with WQ_MEM_RECLAIM flag
    in order to guarantee forward progress, which may cause hns3'
    WQ_MEM_RECLAIM wq flushing infiniband' !WQ_MEM_RECLAIM wq
    
    Signed-off-by: Yunsheng Lin <linyunsheng@huawei.com>
    Reviewed-by: Peng Li <lipeng321@huawei.com>
    Reviewed-by: Weiwei Deng <dengweiwei@huawei.com>
    Reviewed-by: Junxin Chen <chenjunxin1@huawei.com>
    Signed-off-by: Shengzui You <youshengzui@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- drivers/net/ethernet/hisilicon/hns3/hns3vf/hclgevf_main.c
+++ drivers/net/ethernet/hisilicon/hns3/hns3vf/hclgevf_main.c
@@ -2291,8 +2291,10 @@ static enum hclgevf_evt_cause hclgevf_check_evt_cause(struct hclgevf_dev *hdev,
 		return HCLGEVF_VECTOR0_EVENT_MBX;
 	}
 
-	dev_dbg(&hdev->pdev->dev, "vector 0 interrupt from unknown source\n");
-
+	/* print other vector0 event source */
+	dev_info(&hdev->pdev->dev,
+		 "vector 0 interrupt from unknown source, cmdq_drivers/net/ethernet/hisilicon = %#x\n",
+		 cmdq_stat_reg);
 	return HCLGEVF_VECTOR0_EVENT_OTHER;
 }
 
@@ -3552,7 +3554,7 @@ static int hclgevf_init(void)
 {
 	pr_info("%s is initializing\n", HCLGEVF_NAME);
 
-	hclgevf_wq = alloc_workqueue("%s", WQ_MEM_RECLAIM, 0, HCLGEVF_NAME);
+	hclgevf_wq = alloc_workqueue("%s", 0, 0, HCLGEVF_NAME);
 	if (!hclgevf_wq) {
 		pr_err("%s: failed to create workqueue\n", HCLGEVF_NAME);
 		return -ENOMEM;
