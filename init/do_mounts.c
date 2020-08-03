0fff101152e79995eb267b367abca6d6d2f55984
Roberto Sassu <roberto.sassu@huawei.com>
2020-08-03 20:37:50 +0000

    init: Add kernel option to force usage of tmpfs for rootfs
    hulk inclusion
    category: feature
    feature: initramfs-xattrs
    
    ---------------------------
    
    This patch adds the new option initramtmpfs for the kernel command line, to
    force usage of tmpfs instead of ramfs as filesystem for rootfs.
    
    This option should be used when the initial ram disk contains xattrs, as
    only tmpfs supports them.
    
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>
    Acked-by: Hanjun Guo <guohanjun@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- init/do_mounts.c
+++ init/do_mounts.c
@@ -31,6 +31,7 @@ int root_mountflags = MS_RDONLY | MS_SILENT;
 static char * __initdata root_device_name;
 static char __initdata saved_root_name[64];
 static int root_wait;
+static int initramtmpfs;
 
 dev_t ROOT_DEV;
 
@@ -319,9 +320,16 @@ static int __init root_delay_setup(char *str)
 	return 1;
 }
 
+static int __init initramtmpfs_setup(char *str)
+{
+	initramtmpfs = 1;
+	return 1;
+}
+
 __setup("rootflags=", root_data_setup);
 __setup("rootfstype=", fs_names_setup);
 __setup("rootdelay=", root_delay_setup);
+__setup("initramtmpfs", initramtmpfs_setup);
 
 static void __init get_fs_names(char *page)
 {
@@ -622,7 +630,8 @@ int __init init_rootfs(void)
 	if (err)
 		return err;
 
-	if (IS_ENABLED(CONFIG_TMPFS) && !saved_root_name[0] &&
+	if (IS_ENABLED(CONFIG_TMPFS) &&
+		(!saved_root_name[0] || initramtmpfs) &&
 		(!root_fs_names || strstr(root_fs_names, "tmpfs"))) {
 		err = shmem_init();
 		is_tmpfs = true;
