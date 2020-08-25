c1237e4bcd8354d139d0920ede3ff8ff082dfe91
Yury Norov <ynorov@caviumnetworks.com>
2020-08-25 11:19:28 +0800

    arm64: signal32: move ilp32 and aarch32 common code to separated file
    hulk inclusion
    category: feature
    bugzilla: NA
    CVE: NA
    ---------------------------
    
    ILP32 needs to mix 32bit struct siginfo and 64bit sigframe for its signal
    handlers. Move the existing compat code for copying siginfo to user space
    and manipulating signal masks into signal32_common.c so it can be used to
    deliver aarch32 and ilp32 signals.
    
    Signed-off-by: Yury Norov <ynorov@caviumnetworks.com>
    Signed-off-by: Xiongfeng Wang <wangxiongfeng2@huawei.com>
    Reviewed-by: Hanjun Guo <guohanjun@huawei.com <mailto:guohanjun@huawei.com>>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- arch/arm64/kernel/signal32.c
+++ arch/arm64/kernel/signal32.c
@@ -27,6 +27,7 @@
 #include <asm/fpsimd.h>
 #include <asm/signal32.h>
 #include <asm/traps.h>
+#include <asm/signal32_common.h>
 #include <linux/uaccess.h>
 #include <asm/unistd.h>
 
@@ -103,28 +104,6 @@ struct a32_rt_sigframe {
 };
 
 #define _BLOCKABLE (~(sigmask(SIGKILL) | sigmask(SIGSTOP)))
-
-static inline int put_sigset_t(compat_sigset_t __user *uset, sigset_t *set)
-{
-	compat_sigset_t	cset;
-
-	cset.sig[0] = set->sig[0] & 0xffffffffull;
-	cset.sig[1] = set->sig[0] >> 32;
-
-	return copy_to_user(uset, &cset, sizeof(*uset));
-}
-
-static inline int get_sigset_t(sigset_t *set,
-			       const compat_sigset_t __user *uset)
-{
-	compat_sigset_t s32;
-
-	if (copy_from_user(&s32, uset, sizeof(*uset)))
-		return -EFAULT;
-
-	set->sig[0] = s32.sig[0] | (((long)s32.sig[1]) << 32);
-	return 0;
-}
 
 /*
  * VFP save/restore code.
