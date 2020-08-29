66b2cd21d078e290e1f5f7e0220a34e8f510f61e
Will Deacon <will.deacon@arm.com>
2020-08-29 15:48:08 +0800

    arm64: ptr auth: Move per-thread keys from thread_info to thread_struct
    mainline inclusion
    from v4.20-rc3
    commit 84931327a807
    category: feature
    bugzilla: 27615
    CVE: NA
    
    -------------------------------------------------
    
    We don't need to get at the per-thread keys from assembly at all, so
    they can live alongside the rest of the per-thread register state in
    thread_struct instead of thread_info.
    
    This will also allow straighforward whitelisting of the keys for
    hardened usercopy should we expose them via a ptrace request later on.
    
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    
    Conflicts:
    	arch/arm64/include/asm/thread_info.h
    [Zheng Zengkai: fix conflicts caused by skipping the following commit.
    396244 arm64: preempt: Provide our own implementation of asm/preempt.h]
    
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>
    Reviewed-by: Hanjun Guo <guohanjun@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- arch/arm64/include/asm/thread_info.h
+++ arch/arm64/include/asm/thread_info.h
@@ -28,7 +28,6 @@
 struct task_struct;
 
 #include <asm/memory.h>
-#include <asm/pointer_auth.h>
 #include <asm/stack_pointer.h>
 #include <asm/types.h>
 
@@ -44,9 +43,6 @@ struct thread_info {
 	u64			ttbr0;		/* saved TTBR0_EL1 */
 #endif
 	int			preempt_count;	/* 0 => preemptable, <0 => bug */
-#ifdef CONFIG_ARM64_PTR_AUTH
-	struct ptrauth_keys	keys_user;
-#endif
 };
 
 #define thread_saved_pc(tsk)	\
