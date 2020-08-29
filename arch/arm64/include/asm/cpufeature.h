79d4fe4a3e2268ae8fa616f451c0e70bc753304e
Will Deacon <will.deacon@arm.com>
2020-08-29 15:48:13 +0800

    arm64: cpufeature: Reduce number of pointer auth CPU caps from 6 to 4
    mainline inclusion
    from v5.0-rc1
    commit a56005d32105
    category: feature
    bugzilla: 27615
    CVE: NA
    
    -------------------------------------------------
    
    We can easily avoid defining the two meta-capabilities for the address
    and generic keys, so remove them and instead just check both of the
    architected and impdef capabilities when determining the level of system
    support.
    
    Reviewed-by: Suzuki Poulose <suzuki.poulose@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    
    Conflicts:
    	arch/arm64/include/asm/cpucaps.h
    [Zheng Zengkai: adjust context and fix conflicts caused by skipping
    the following commit.
    95b861a4a arm64: arch_timer: Add workaround for ARM erratum 1188873
    8b2cca9ad arm64: KVM: Force VHE for systems affected by erratum 1165522]
    
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>
    Reviewed-by: Hanjun Guo <guohanjun@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- arch/arm64/include/asm/cpufeature.h
+++ arch/arm64/include/asm/cpufeature.h
@@ -530,13 +530,15 @@ static inline bool system_has_prio_mask_debugging(void)
 static inline bool system_supports_address_auth(void)
 {
 	return IS_ENABLED(CONFIG_ARM64_PTR_AUTH) &&
-		cpus_have_const_cap(ARM64_HAS_ADDRESS_AUTH);
+		(cpus_have_const_cap(ARM64_HAS_ADDRESS_AUTH_ARCH) ||
+		 cpus_have_const_cap(ARM64_HAS_ADDRESS_AUTH_IMP_DEF));
 }
 
 static inline bool system_supports_generic_auth(void)
 {
 	return IS_ENABLED(CONFIG_ARM64_PTR_AUTH) &&
-		cpus_have_const_cap(ARM64_HAS_GENERIC_AUTH);
+		(cpus_have_const_cap(ARM64_HAS_GENERIC_AUTH_ARCH) ||
+		 cpus_have_const_cap(ARM64_HAS_GENERIC_AUTH_IMP_DEF));
 }
 
 #define ARM64_SSBD_UNKNOWN		-1
