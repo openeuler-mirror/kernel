c2d5a08f51de4f1a87f6000bab9a9fd3996165a1
Yang Yingliang <yaingyingliang@huawei.com>
2020-08-31 20:41:26 +0800

    irq-gic-v3: Add support to init ts core GICR
    ascend inclusion
    category: feature
    bugzilla: NA
    CVE: NA
    
    ------------
    
    For Ascend platform, other NON-OS managed GICRs need be initialized
    in OS.
    
    Signed-off-by: Yang Yingliang <yaingyingliang@huawei.com>
    Signed-off-by: Xu Qiang <xuqiang36@huawei.com>
    Signed-off-by: Lijun Fang <fanglijun3@huawei.com>
    Reviewed-by: Hanjun Guo <guohanjun@huawei.com>
    Signed-off-by: Xu Qiang <xuqiang36@huawei.com>
    Reviewed-by: Ding Tianhong <dingtianhong@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- include/linux/irqchip/arm-gic-v3.h
+++ include/linux/irqchip/arm-gic-v3.h
@@ -608,6 +608,13 @@ struct rdists {
 struct irq_domain;
 struct fwnode_handle;
 int its_cpu_init(void);
+#ifdef CONFIG_ASCEND_INIT_ALL_GICR
+void its_set_gicr_nr(int nr);
+int its_gicr_nr(void);
+void its_enable_init_all_gicr(void);
+bool its_init_all_gicr(void);
+int its_cpu_init_others(void __iomem *base, phys_addr_t phys_base, int idx);
+#endif
 int its_init(struct fwnode_handle *handle, struct rdists *rdists,
 	     struct irq_domain *domain);
 int mbi_init(struct fwnode_handle *fwnode, struct irq_domain *parent);
