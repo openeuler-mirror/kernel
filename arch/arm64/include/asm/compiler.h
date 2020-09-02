18832b357b968f50b7411a6212cb4938e833780e
Amit Daniel Kachhap <amit.kachhap@arm.com>
2020-09-02 22:16:08 +0800

    arm64: mask PAC bits of __builtin_return_address
    mainline inclusion
    from v5.7-rc1
    commit 689eae42afd7
    category: feature
    bugzilla: 27615
    CVE: NA
    
    -------------------------------------------------
    
    Functions like vmap() record how much memory has been allocated by their
    callers, and callers are identified using __builtin_return_address(). Once
    the kernel is using pointer-auth the return address will be signed. This
    means it will not match any kernel symbol, and will vary between threads
    even for the same caller.
    
    The output of /proc/vmallocinfo in this case may look like,
    0x(____ptrval____)-0x(____ptrval____)   20480 0x86e28000100e7c60 pages=4 vmalloc N0=4
    0x(____ptrval____)-0x(____ptrval____)   20480 0x86e28000100e7c60 pages=4 vmalloc N0=4
    0x(____ptrval____)-0x(____ptrval____)   20480 0xc5c78000100e7c60 pages=4 vmalloc N0=4
    
    The above three 64bit values should be the same symbol name and not
    different LR values.
    
    Use the pre-processor to add logic to clear the PAC to
    __builtin_return_address() callers. This patch adds a new file
    asm/compiler.h and is transitively included via include/compiler_types.h on
    the compiler command line so it is guaranteed to be loaded and the users of
    this macro will not find a wrong version.
    
    Helper macros ptrauth_kernel_pac_mask/ptrauth_clear_pac are created for
    this purpose and added in this file. Existing macro ptrauth_user_pac_mask
    moved from asm/pointer_auth.h.
    
    Signed-off-by: Amit Daniel Kachhap <amit.kachhap@arm.com>
    Reviewed-by: James Morse <james.morse@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    
    Conflicts:
    	arch/arm64/include/asm/compiler.h
    	arch/arm64/include/asm/pointer_auth.h
    [Zheng Zengkai: use VA_BITS instead of vabits_actual to fix conflicts
    in pointer_auth.h and fix conflicts in compiler.h caused by skipping
    the following commit.
    9376b1e7b6 arm64: remove unused asm/compiler.h header file]
    
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>
    Reviewed-by: Hanjun Guo <guohanjun@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- arch/arm64/include/asm/compiler.h
+++ arch/arm64/include/asm/compiler.h
@@ -27,4 +27,23 @@
  */
 #define __asmeq(x, y)  ".ifnc " x "," y " ; .err ; .endif\n\t"
 
+#if defined(CONFIG_ARM64_PTR_AUTH)
+
+/*
+ * The EL0/EL1 pointer bits used by a pointer authentication code.
+ * This is dependent on TBI0/TBI1 being enabled, or bits 63:56 would also apply.
+ */
+#define ptrauth_user_pac_mask()		GENMASK_ULL(54, VA_BITS)
+#define ptrauth_kernel_pac_mask()	GENMASK_ULL(63, VA_BITS)
+
+/* Valid for EL0 TTBR0 and EL1 TTBR1 instruction pointers */
+#define ptrauth_clear_pac(ptr)						\
+	((ptr & BIT_ULL(55)) ? (ptr | ptrauth_kernel_pac_mask()) :	\
+			       (ptr & ~ptrauth_user_pac_mask()))
+
+#define __builtin_return_address(val)					\
+	(void *)(ptrauth_clear_pac((unsigned long)__builtin_return_address(val)))
+
+#endif /* CONFIG_ARM64_PTR_AUTH */
+
 #endif	/* __ASM_COMPILER_H */
