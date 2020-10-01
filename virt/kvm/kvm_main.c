4c01c4d2fa38a49be53b7bb492b9ecefbe37d034
Peter Xu <peterx@redhat.com>
2020-09-30 21:22:24 -0400

    KVM: Make dirty ring exclusive to dirty bitmap log
    mainline inclusion
    from mainline-v5.11-rc1
    commit b2cc64c4f3829c25b618f23f472a493668d9cb80
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I9BPVF
    CVE: NA
    
    Reference:https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?h=linux-5.11.y&id=b2cc64c4f3829c25b618f23f472a493668d9cb80
    
    --------------------------------
    
    There's no good reason to use both the dirty bitmap logging and the
    new dirty ring buffer to track dirty bits.  We should be able to even
    support both of them at the same time, but it could complicate things
    which could actually help little.  Let's simply make it the rule
    before we enable dirty ring on any arch, that we don't allow these two
    interfaces to be used together.
    
    The big world switch would be KVM_CAP_DIRTY_LOG_RING capability
    enablement.  That's where we'll switch from the default dirty logging
    way to the dirty ring way.  As long as kvm->dirty_ring_size is setup
    correctly, we'll once and for all switch to the dirty ring buffer mode
    for the current virtual machine.
    
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Message-Id: <20201001012224.5818-1-peterx@redhat.com>
    [Change errno from EINVAL to ENXIO. - Paolo]
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -1810,6 +1810,10 @@ int kvm_get_dirty_log(struct kvm *kvm, struct kvm_dirty_log *log,
 	unsigned long n;
 	unsigned long any = 0;
 
+	/* Dirty ring tracking is exclusive to dirty log tracking */
+	if (kvm->dirty_ring_size)
+		return -ENXIO;
+
 	*memslot = NULL;
 	*is_dirty = 0;
 
@@ -1870,6 +1874,10 @@ static int kvm_get_dirty_log_protect(struct kvm *kvm, struct kvm_dirty_log *log)
 	unsigned long *dirty_bitmap;
 	unsigned long *dirty_bitmap_buffer;
 	bool flush;
+
+	/* Dirty ring tracking is exclusive to dirty log tracking */
+	if (kvm->dirty_ring_size)
+		return -ENXIO;
 
 	as_id = log->slot >> 16;
 	id = (u16)log->slot;
@@ -1978,6 +1986,10 @@ static int kvm_clear_dirty_log_protect(struct kvm *kvm,
 	unsigned long *dirty_bitmap;
 	unsigned long *dirty_bitmap_buffer;
 	bool flush;
+
+	/* Dirty ring tracking is exclusive to dirty log tracking */
+	if (kvm->dirty_ring_size)
+		return -ENXIO;
 
 	as_id = log->slot >> 16;
 	id = (u16)log->slot;
