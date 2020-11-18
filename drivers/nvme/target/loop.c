c4b7246a2d7a423cac50ff8d72b1b1209bc945e8
Israel Rukshin <israelr@mellanox.com>
2020-11-18 10:07:04 +0800

    nvme: Make nvme_uninit_ctrl symmetric to nvme_init_ctrl
    mainline inclusion
    from mainline-v5.7-rc1
    commit 726612b6b8259afa41d265a2722991c87f059223
    category: bugfix
    bugzilla: NA
    CVE: NA
    Link: https://gitee.com/openeuler/kernel/issues/I1WGZE
    
    --------------------------------
    
    Put the ctrl reference count at nvme_uninit_ctrl as opposed to
    nvme_init_ctrl which takes it. This decrease the reference count at the
    core layer instead of decreasing it on each transport separately.
    Also move the call of nvme_uninit_ctrl at PCI driver after calling to
    nvme_release_prp_pools and nvme_dev_unmap, in order to put the reference
    count after using the dev. This is safe because those functions use
    nvme_dev which is freed only later at nvme_pci_free_ctrl.
    
    Signed-off-by: Israel Rukshin <israelr@mellanox.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Keith Busch <kbusch@kernel.org>
    Conflicts:
        drivers/nvme/host/tcp.c
        drivers/nvme/host/core.c
    [No code about TCP in current version and adjust context.]
    Reviewed-by: Chao Leng <lengchao@huawei.com>
    Reviewed-by: Jike Cheng <chengjike.cheng@huawei.com>
    Signed-off-by: Lijie <lijie34@huawei.com>
    Reviewed-by: Hou Tao <houtao1@huawei.com>
    Acked-by: Hanjun Guo <guohanjun@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- drivers/nvme/target/loop.c
+++ drivers/nvme/target/loop.c
@@ -506,7 +506,6 @@ out_destroy_admin:
 out_disable:
 	dev_warn(ctrl->ctrl.device, "Removing after reset failure\n");
 	nvme_uninit_ctrl(&ctrl->ctrl);
-	nvme_put_ctrl(&ctrl->ctrl);
 }
 
 static const struct nvme_ctrl_ops nvme_loop_ctrl_ops = {
@@ -656,7 +655,6 @@ out_free_queues:
 	kfree(ctrl->queues);
 out_uninit_ctrl:
 	nvme_uninit_ctrl(&ctrl->ctrl);
-	nvme_put_ctrl(&ctrl->ctrl);
 out_put_ctrl:
 	nvme_put_ctrl(&ctrl->ctrl);
 	if (ret > 0)
