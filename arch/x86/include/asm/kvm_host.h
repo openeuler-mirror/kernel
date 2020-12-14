ef2e4f7a4929e77f387f35ed9129fdc88430608b
Paolo Bonzini <pbonzini@redhat.com>
2020-12-14 10:26:51 -0500

    KVM: x86: introduce complete_emulated_msr callback
    mainline inclusion
    from mainline-v5.11
    commit f9a4d621761a2c7db686cc47772a0688d389f2d7
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/ICYLY9
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=f9a4d621761a2c7db686cc47772a0688d389f2d7
    
    ---------------------------
    
    This will be used by SEV-ES to inject MSR failure via the GHCB.
    
    Reviewed-by: Tom Lendacky <thomas.lendacky@amd.com>
    Change-Id: I4485935ab2baf1af5a103e165c2418d156e8988f
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Zhiguang Ni <nizhiguang@hygon.cn>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -1431,6 +1431,7 @@ struct kvm_x86_ops {
 
 	void (*migrate_timers)(struct kvm_vcpu *vcpu);
 	void (*msr_filter_changed)(struct kvm_vcpu *vcpu);
+	int (*complete_emulated_msr)(struct kvm_vcpu *vcpu, int err);
 };
 
 struct kvm_x86_nested_ops {
