729f96a4e0c32f1c1bfa60e4ff485aa91890963d
Yufeng Mo <moyufeng@huawei.com>
2020-12-23 15:18:05 +0800

    net: hns3: optimize the process of queue reset
    driver inclusion
    category: bugfix
    bugzilla: NA
    CVE: NA
    
    ----------------------------
    
    Currently, the queue reset process needs to be performed one by
    one, which is inefficient. However, the queue reset of the same
    function is always performed at the same time. Therefore, according
    to the UM, we use HCLGE_OPC_CFG_RST_TRIGGER to reset all queues of
    the same function at a time, in order to optimiz the queue reset
    process.
    
    Signed-off-by: Yufeng Mo <moyufeng@huawei.com>
    Reviewed-by: Jian Shen <shenjian15@huawei.com>
    Reviewed-by: Peng Li <lipeng321@huawei.com>
    Signed-off-by: Yonglong Liu <liuyonglong@huawei.com>
    Reviewed-by: Yongxin Li <liyongxin1@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_cmd.h
+++ drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_cmd.h
@@ -927,10 +927,15 @@ struct hclge_reset_tqp_queue_cmd {
 
 #define HCLGE_CFG_RESET_MAC_B		3
 #define HCLGE_CFG_RESET_FUNC_B		7
+#define HCLGE_CFG_RESET_RCB_B		1
 struct hclge_reset_cmd {
 	u8 mac_func_reset;
 	u8 fun_reset_vfid;
-	u8 rsv[22];
+	u8 fun_reset_rcb;
+	__le16 fun_reset_rcb_vqid_start;
+	__le16 fun_reset_rcb_vqid_num;
+	u8 fun_reset_rcb_return_status;
+	u8 rsv[16];
 };
 
 #define HCLGE_PF_RESET_DONE_BIT		BIT(0)
