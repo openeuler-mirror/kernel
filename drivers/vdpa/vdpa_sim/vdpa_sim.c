769f5f3e221e2bd786e3d88910fb2cbead96e43e
Parav Pandit <parav@nvidia.com>
2021-01-05 12:32:03 +0200

    vdpa_sim_net: Add support for user supported devices
    stable inclusion
    from stable-v5.12
    commit a3c06ae158dd6fa8336157c31d9234689d068d02
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I5WXCZ
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=a3c06ae158dd6fa8336157c31d9234689d068d02
    
    ----------------------------------------------------------------------
    
    Enable user to create vdpasim net simulate devices.
    
    Show vdpa management device that supports creating, deleting vdpa devices.
    
    $ vdpa mgmtdev show
    vdpasim_net:
      supported_classes
        net
    
    $ vdpa mgmtdev show -jp
    {
        "show": {
            "vdpasim_net": {
                "supported_classes": {
                  "net"
            }
        }
    }
    
    Create a vdpa device of type networking named as "foo2" from
    the management device vdpasim:
    
    $ vdpa dev add mgmtdev vdpasim_net name foo2
    
    Show the newly created vdpa device by its name:
    $ vdpa dev show foo2
    foo2: type network mgmtdev vdpasim_net vendor_id 0 max_vqs 2 max_vq_size 256
    
    $ vdpa dev show foo2 -jp
    {
        "dev": {
            "foo2": {
                "type": "network",
                "mgmtdev": "vdpasim_net",
                "vendor_id": 0,
                "max_vqs": 2,
                "max_vq_size": 256
            }
        }
    }
    
    Delete the vdpa device after its use:
    $ vdpa dev del foo2
    
    Signed-off-by: Parav Pandit <parav@nvidia.com>
    Reviewed-by: Eli Cohen <elic@nvidia.com>
    Acked-by: Jason Wang <jasowang@redhat.com>
    Link: https://lore.kernel.org/r/20210105103203.82508-7-parav@nvidia.com
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Pengyuan Zhao <zhaopengyuan@hisilicon.com>

--- drivers/vdpa/vdpa_sim/vdpa_sim.c
+++ drivers/vdpa/vdpa_sim/vdpa_sim.c
@@ -222,7 +222,7 @@ struct vdpasim *vdpasim_create(struct vdpasim_dev_attr *dev_attr)
 		ops = &vdpasim_net_config_ops;
 
 	vdpasim = vdpa_alloc_device(struct vdpasim, vdpa, NULL, ops,
-				    NULL);
+				    dev_attr->name);
 	if (!vdpasim)
 		goto err_alloc;
 
@@ -236,6 +236,7 @@ struct vdpasim *vdpasim_create(struct vdpasim_dev_attr *dev_attr)
 	if (dma_set_mask_and_coherent(dev, DMA_BIT_MASK(64)))
 		goto err_iommu;
 	set_dma_ops(dev, &vdpasim_dma_ops);
+	vdpasim->vdpa.mdev = dev_attr->mgmt_dev;
 
 	vdpasim->config = kzalloc(dev_attr->config_size, GFP_KERNEL);
 	if (!vdpasim->config)
