c9fe2e513dc05380355bcc4e91d9297ed8fb1772
Sean Christopherson <seanjc@google.com>
2021-02-25 12:47:35 -0800

    KVM: x86/mmu: Stop using software available bits to denote MMIO SPTEs
    mainline inclusion
    from mainline-v5.13-rc1
    commit 8120337a4c5502118e255b170799040eefe2f280
    category: feature
    bugzilla: https://gitee.com/openeuler/intel-kernel/issues/I7S3VQ
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=8120337a4c5502118e255b170799040eefe2f280
    
    ----------------------------------------------------------------------
    
    Stop tagging MMIO SPTEs with specific available bits and instead detect
    MMIO SPTEs by checking for their unique SPTE value.  The value is
    guaranteed to be unique on shadow paging and NPT as setting reserved
    physical address bits on any other type of SPTE would consistute a KVM
    bug.  Ditto for EPT, as creating a WX non-MMIO would also be a bug.
    
    Note, this approach is also future-compatibile with TDX, which will need
    to reflect MMIO EPT violations as #VEs into the guest.  To create an EPT
    violation instead of a misconfig, TDX EPTs will need to have RWX=0,  But,
    MMIO SPTEs will also be the only case where KVM clears SUPPRESS_VE, so
    MMIO SPTEs will still be guaranteed to have a unique value within a given
    MMU context.
    
    The main motivation is to make it easier to reason about which types of
    SPTEs use which available bits.  As a happy side effect, this frees up
    two more bits for storing the MMIO generation.
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Message-Id: <20210225204749.1512652-11-seanjc@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Yu Zhang <yu.c.zhang@linux.intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -4585,7 +4585,8 @@ static void ept_set_mmio_spte_mask(void)
 	 * EPT Misconfigurations can be generated if the value of bits 2:0
 	 * of an EPT paging-structure entry is 110b (write/execute).
 	 */
-	kvm_mmu_set_mmio_spte_mask(VMX_EPT_MISCONFIG_WX_VALUE, 0);
+	kvm_mmu_set_mmio_spte_mask(VMX_EPT_MISCONFIG_WX_VALUE,
+				   VMX_EPT_RWX_MASK, 0);
 }
 
 static inline int vmx_get_pid_table_order(struct kvm *kvm)
