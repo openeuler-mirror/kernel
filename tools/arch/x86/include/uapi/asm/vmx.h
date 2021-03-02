1418957638a3331b7b509b3db1935b323b9f32b5
Arnaldo Carvalho de Melo <acme@redhat.com>
2021-03-02 17:20:08 -0300

    tools headers UAPI: Sync KVM's kvm.h and vmx.h headers with the kernel sources
    mainline inclusion
    from mainline-v5.12-rc3
    commit 33dc525f93216bc83935ce98518644def04d6c54
    category: feature
    feature: KVM Bus Lock VM Exit
    bugzilla: https://gitee.com/openeuler/intel-kernel/issues/I5RJCB
    CVE: N/A
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/
    commit/?id=33dc525f93216bc83935ce98518644def04d6c54
    
    Intel-SIG: commit 33dc525f9321 ("tools headers UAPI: Sync KVM's kvm.h and vmx.h headers with the kernel sources")
    
    -------------------------------------
    
    tools headers UAPI: Sync KVM's kvm.h and vmx.h headers with the kernel sources
    
    To pick the changes in:
    
      fe6b6bc802b40081 ("KVM: VMX: Enable bus lock VM exit")
    
    That makes 'perf kvm-stat' aware of this new BUS_LOCK exit reason, thus
    addressing the following perf build warning:
    
      Warning: Kernel ABI header at 'tools/arch/x86/include/uapi/asm/vmx.h' differs from latest version at 'arch/x86/include/uapi/asm/vmx.h'
      diff -u tools/arch/x86/include/uapi/asm/vmx.h arch/x86/include/uapi/asm/vmx.h
    
    Cc: Chenyi Qiang <chenyi.qiang@intel.com>
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Signed-off-by: Aichun Shi <aichun.shi@intel.com>

--- tools/arch/x86/include/uapi/asm/vmx.h
+++ tools/arch/x86/include/uapi/asm/vmx.h
@@ -88,6 +88,7 @@
 #define EXIT_REASON_XRSTORS             64
 #define EXIT_REASON_UMWAIT              67
 #define EXIT_REASON_TPAUSE              68
+#define EXIT_REASON_BUS_LOCK            74
 
 #define VMX_EXIT_REASONS \
 	{ EXIT_REASON_EXCEPTION_NMI,         "EXCEPTION_NMI" }, \
@@ -148,7 +149,8 @@
 	{ EXIT_REASON_XSAVES,                "XSAVES" }, \
 	{ EXIT_REASON_XRSTORS,               "XRSTORS" }, \
 	{ EXIT_REASON_UMWAIT,                "UMWAIT" }, \
-	{ EXIT_REASON_TPAUSE,                "TPAUSE" }
+	{ EXIT_REASON_TPAUSE,                "TPAUSE" }, \
+	{ EXIT_REASON_BUS_LOCK,              "BUS_LOCK" }
 
 #define VMX_EXIT_REASON_FLAGS \
 	{ VMX_EXIT_REASONS_FAILED_VMENTRY,	"FAILED_VMENTRY" }
