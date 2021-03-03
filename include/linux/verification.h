59cc26afc5a95e48342b24d95b388e463b58e779
Roberto Sassu <roberto.sassu@huawei.com>
2021-03-03 08:44:20 +0100

    certs: Introduce search_trusted_key()
    hulk inclusion
    category: feature
    feature: IMA Digest Lists extension
    bugzilla: 46797
    
    -------------------------------------------------
    
    Introduce search_trusted_key() to extend the key search to the primary or
    secondary built-in keyrings.
    
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>
    Signed-off-by: Tianxing Zhang <zhangtianxing3@huawei.com>
    Reviewed-by: Jason Yan <yanaijie@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- include/linux/verification.h
+++ include/linux/verification.h
@@ -8,6 +8,8 @@
 #ifndef _LINUX_VERIFICATION_H
 #define _LINUX_VERIFICATION_H
 
+#include <linux/key.h>
+
 /*
  * Indicate that both builtin trusted keys and secondary trusted keys
  * should be used.
@@ -57,6 +59,9 @@ extern int verify_pefile_signature(const void *pebuf, unsigned pelen,
 				   struct key *trusted_keys,
 				   enum key_being_used_for usage);
 #endif
+
+struct key *search_trusted_key(struct key *trusted_keys, struct key_type *type,
+			       char *name);
 
 #endif /* CONFIG_SYSTEM_DATA_VERIFICATION */
 #endif /* _LINUX_VERIFY_PEFILE_H */
