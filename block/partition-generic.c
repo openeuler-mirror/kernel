4a34928be80331132e8d15794ad3df2f0606ec84
Ye Bin <yebin10@huawei.com>
2021-03-11 17:25:13 +0800

    Revert "scsi: sd: block: Fix read-only flag residuals when partition table change"
    hulk inclusion
    category: bugfix
    bugzilla: 46833
    CVE: NA
    
    -----------------------------------------------
    
    c4f20b042b70 patch may lead to clear read-only flag when re-read
    partition table.
    
    This reverts commit c4f20b042b7060f505134dcb1fc283893ec41e2a
    
    Fixes:c4f20b042b70("scsi: sd: block: Fix read-only flag residuals when partition table change")
    Signed-off-by: Ye Bin <yebin10@huawei.com>
    Reviewed-by: Jason Yan <yanaijie@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
    Signed-off-by: Cheng Jian <cj.chengjian@huawei.com>

--- block/partition-generic.c
+++ block/partition-generic.c
@@ -286,7 +286,6 @@ void delete_partition(struct gendisk *disk, int partno)
 	if (!part)
 		return;
 
-	clear_bit(partno, disk->user_ro_bitmap);
 	get_device(disk_to_dev(disk));
 	rcu_assign_pointer(ptbl->part[partno], NULL);
 
