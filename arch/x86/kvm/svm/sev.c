51e7dfd2f449afc576301b3ad5ab9a6e64310cbb
hanliyang <hanliyang@hygon.cn>
2021-04-08 08:39:49 -0400

    KVM: SVM: Add KVM_SEV_RECEIVE_UPDATE_VMSA command
    hygon inclusion
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/9043
    CVE: NA
    
    ---------------------------
    
    The command is used for copying the incoming buffer into the VMSA
    memory regions of CSV2 guest.
    
    Signed-off-by: hanliyang <hanliyang@hygon.cn>
    Signed-off-by: nileilei <nileilei@hygon.cn>

--- arch/x86/kvm/svm/sev.c
+++ arch/x86/kvm/svm/sev.c
@@ -1725,6 +1725,81 @@ e_free_hdr:
 	return ret;
 }
 
+static int sev_receive_update_vmsa(struct kvm *kvm, struct kvm_sev_cmd *argp)
+{
+	struct kvm_sev_info *sev = &to_kvm_svm(kvm)->sev_info;
+	struct kvm_sev_receive_update_vmsa params;
+	struct sev_data_receive_update_vmsa *vmsa;
+	struct kvm_vcpu *vcpu;
+	void *hdr = NULL, *trans = NULL;
+	int ret;
+
+	if (!sev_es_guest(kvm))
+		return -ENOTTY;
+
+	if (copy_from_user(&params, (void __user *)(uintptr_t)argp->data,
+			   sizeof(struct kvm_sev_receive_update_vmsa)))
+		return -EFAULT;
+
+	if (!params.hdr_uaddr || !params.hdr_len ||
+	    !params.trans_uaddr || !params.trans_len)
+		return -EINVAL;
+
+	/* Get the target vcpu */
+	vcpu = kvm_get_vcpu_by_id(kvm, params.vcpu_id);
+	if (!vcpu) {
+		pr_err("%s: invalid vcpu\n", __func__);
+		return -EINVAL;
+	}
+
+	pr_debug("%s: vcpu (%d)\n", __func__, vcpu->vcpu_id);
+
+	hdr = psp_copy_user_blob(params.hdr_uaddr, params.hdr_len);
+	if (IS_ERR(hdr))
+		return PTR_ERR(hdr);
+
+	trans = psp_copy_user_blob(params.trans_uaddr, params.trans_len);
+	if (IS_ERR(trans)) {
+		ret = PTR_ERR(trans);
+		goto e_free_hdr;
+	}
+
+	ret = -ENOMEM;
+	vmsa = kzalloc(sizeof(*vmsa), GFP_KERNEL);
+	if (!vmsa)
+		goto e_free_trans;
+
+	vmsa->hdr_address = __psp_pa(hdr);
+	vmsa->hdr_len = params.hdr_len;
+	vmsa->trans_address = __psp_pa(trans);
+	vmsa->trans_len = params.trans_len;
+
+	/*
+	 * Flush before RECEIVE_UPDATE_VMSA, the PSP encrypts the
+	 * written VMSA memory content with the guest's key), and
+	 * the cache may contain dirty, unencrypted data.
+	 */
+	clflush_cache_range(to_svm(vcpu)->vmsa, PAGE_SIZE);
+
+	/* The RECEIVE_UPDATE_VMSA command requires C-bit to be always set. */
+	vmsa->guest_address = __pa(to_svm(vcpu)->vmsa) | sev_me_mask;
+	vmsa->guest_len = PAGE_SIZE;
+	vmsa->handle = sev->handle;
+
+	ret = sev_issue_cmd(kvm, SEV_CMD_RECEIVE_UPDATE_VMSA, vmsa, &argp->error);
+
+	if (!ret)
+		vcpu->arch.guest_state_protected = true;
+
+	kfree(vmsa);
+e_free_trans:
+	kfree(trans);
+e_free_hdr:
+	kfree(hdr);
+
+	return ret;
+}
+
 int svm_mem_enc_op(struct kvm *kvm, void __user *argp)
 {
 	struct kvm_sev_cmd sev_cmd;
@@ -1801,6 +1876,12 @@ int svm_mem_enc_op(struct kvm *kvm, void __user *argp)
 		break;
 	case KVM_SEV_RECEIVE_UPDATE_DATA:
 		r = sev_receive_update_data(kvm, &sev_cmd);
+		break;
+	case KVM_SEV_RECEIVE_UPDATE_VMSA:
+		if (boot_cpu_data.x86_vendor == X86_VENDOR_HYGON)
+			r = sev_receive_update_vmsa(kvm, &sev_cmd);
+		else
+			r = -EINVAL;
 		break;
 	case KVM_SEV_RECEIVE_FINISH:
 		r = sev_receive_finish(kvm, &sev_cmd);
