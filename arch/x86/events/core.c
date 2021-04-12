4b8f72cf9eb1d8db3ff1d77fc717b7186e9127c0
Kan Liang <kan.liang@linux.intel.com>
2021-04-12 07:30:44 -0700

    perf/x86/intel: Hybrid PMU support for perf capabilities
    mainline inclusion
    from mainline-v5.13-rc1
    commit d0946a882e6220229a29f9031641e54379be5a1e
    category: feature
    feature: SRF core PMU support
    bugzilla: https://gitee.com/openeuler/intel-kernel/issues/I8RWG5
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=d0946a882e6220229a29f9031641e54379be5a1e
    
    Intel-SIG: commit d0946a882e62 perf/x86/intel: Hybrid PMU support for perf capabilities
    Backport as a dependency for Sierra Forrest core PMU support.
    
    -------------------------------------
    
    Some platforms, e.g. Alder Lake, have hybrid architecture. Although most
    PMU capabilities are the same, there are still some unique PMU
    capabilities for different hybrid PMUs. Perf should register a dedicated
    pmu for each hybrid PMU.
    
    Add a new struct x86_hybrid_pmu, which saves the dedicated pmu and
    capabilities for each hybrid PMU.
    
    The architecture MSR, MSR_IA32_PERF_CAPABILITIES, only indicates the
    architecture features which are available on all hybrid PMUs. The
    architecture features are stored in the global x86_pmu.intel_cap.
    
    For Alder Lake, the model-specific features are perf metrics and
    PEBS-via-PT. The corresponding bits of the global x86_pmu.intel_cap
    should be 0 for these two features. Perf should not use the global
    intel_cap to check the features on a hybrid system.
    Add a dedicated intel_cap in the x86_hybrid_pmu to store the
    model-specific capabilities. Use the dedicated intel_cap to replace
    the global intel_cap for thse two features. The dedicated intel_cap
    will be set in the following "Add Alder Lake Hybrid support" patch.
    
    Add is_hybrid() to distinguish a hybrid system. ADL may have an
    alternative configuration. With that configuration, the
    X86_FEATURE_HYBRID_CPU is not set. Perf cannot rely on the feature bit.
    Add a new static_key_false, perf_is_hybrid, to indicate a hybrid system.
    It will be assigned in the following "Add Alder Lake Hybrid support"
    patch as well.
    
    Suggested-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/1618237865-33448-5-git-send-email-kan.liang@linux.intel.com
    Signed-off-by: Yunying Sun <yunying.sun@intel.com>

--- arch/x86/events/core.c
+++ arch/x86/events/core.c
@@ -54,6 +54,7 @@ DEFINE_PER_CPU(struct cpu_hw_events, cpu_hw_events) = {
 
 DEFINE_STATIC_KEY_FALSE(rdpmc_never_available_key);
 DEFINE_STATIC_KEY_FALSE(rdpmc_always_available_key);
+DEFINE_STATIC_KEY_FALSE(perf_is_hybrid);
 
 /*
  * This here uses DEFINE_STATIC_CALL_NULL() to get a static_call defined
@@ -1095,8 +1096,9 @@ static void del_nr_metric_event(struct cpu_hw_events *cpuc,
 static int collect_event(struct cpu_hw_events *cpuc, struct perf_event *event,
 			 int max_count, int n)
 {
+	union perf_capabilities intel_cap = hybrid(cpuc->pmu, intel_cap);
 
-	if (x86_pmu.intel_cap.perf_metrics && add_nr_metric_event(cpuc, event))
+	if (intel_cap.perf_metrics && add_nr_metric_event(cpuc, event))
 		return -EINVAL;
 
 	if (n >= max_count + cpuc->n_metric)
@@ -1583,6 +1585,7 @@ void x86_pmu_stop(struct perf_event *event, int flags)
 static void x86_pmu_del(struct perf_event *event, int flags)
 {
 	struct cpu_hw_events *cpuc = this_cpu_ptr(&cpu_hw_events);
+	union perf_capabilities intel_cap = hybrid(cpuc->pmu, intel_cap);
 	int i;
 
 	/*
@@ -1622,7 +1625,7 @@ static void x86_pmu_del(struct perf_event *event, int flags)
 	}
 	cpuc->event_constraint[i-1] = NULL;
 	--cpuc->n_events;
-	if (x86_pmu.intel_cap.perf_metrics)
+	if (intel_cap.perf_metrics)
 		del_nr_metric_event(cpuc, event);
 
 	perf_event_update_userpage(event);
