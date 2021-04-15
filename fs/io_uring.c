ca175df5a8a6edfca74b897984d1a95387eb82b2
Bijan Mottahedeh <bijan.mottahedeh@oracle.com>
2021-04-15 17:35:09 +0800

    io_uring: clear req->result always before issuing a read/write request
    mainline inclusion
    from mainline-5.5-rc7
    commit 797f3f535d59f05ad12c629338beef6cb801d19e
    category: feature
    bugzilla: https://bugzilla.openeuler.org/show_bug.cgi?id=27
    CVE: NA
    ---------------------------
    
    req->result is cleared when io_issue_sqe() calls io_read/write_pre()
    routines.  Those routines however are not called when the sqe
    argument is NULL, which is the case when io_issue_sqe() is called from
    io_wq_submit_work().  io_issue_sqe() may then examine a stale result if
    a polled request had previously failed with -EAGAIN:
    
            if (ctx->flags & IORING_SETUP_IOPOLL) {
                    if (req->result == -EAGAIN)
                            return -EAGAIN;
    
                    io_iopoll_req_issued(req);
            }
    
    and in turn cause a subsequently completed request to be re-issued in
    io_wq_submit_work().
    
    Signed-off-by: Bijan Mottahedeh <bijan.mottahedeh@oracle.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: yangerkun <yangerkun@huawei.com>
    Reviewed-by: zhangyi (F) <yi.zhang@huawei.com>
    Signed-off-by: Cheng Jian <cj.chengjian@huawei.com>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -1842,6 +1842,7 @@ static int io_read(struct io_kiocb *req, struct io_kiocb **nxt,
 	if (!force_nonblock)
 		req->rw.kiocb.ki_flags &= ~IOCB_NOWAIT;
 
+	req->result = 0;
 	io_size = ret;
 	if (req->flags & REQ_F_LINK)
 		req->result = io_size;
@@ -1929,6 +1930,7 @@ static int io_write(struct io_kiocb *req, struct io_kiocb **nxt,
 	if (!force_nonblock)
 		req->rw.kiocb.ki_flags &= ~IOCB_NOWAIT;
 
+	req->result = 0;
 	io_size = ret;
 	if (req->flags & REQ_F_LINK)
 		req->result = io_size;
