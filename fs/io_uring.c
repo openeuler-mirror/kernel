822318e4ef548d90795b878f053bc31982df5fce
Pavel Begunkov <asml.silence@gmail.com>
2021-04-15 17:41:51 +0800

    io_uring: remove REQ_F_MUST_PUNT
    mainline inclusion
    from mainline-5.9-rc1
    commit 24c74678634b3cbdb325b3b7706366c83811b311
    category: feature
    bugzilla: https://bugzilla.openeuler.org/show_bug.cgi?id=27
    CVE: NA
    ---------------------------
    
    REQ_F_MUST_PUNT may seem looking good and clear, but it's the same
    as not having REQ_F_NOWAIT set. That rather creates more confusion.
    Moreover, it doesn't even affect any behaviour (e.g. see the patch
    removing it from io_{read,write}).
    
    Kill theg flag and update already outdated comments.
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Conflicts:
    	fs/io_uring.c
    [8eb7e2d00763 ("io_uring: kill REQ_F_TIMEOUT_NOSEQ") and a1a4661691c5
    ("io_uring: kill REQ_F_TIMEOUT") and 6795c5aba247 ("io_uring: clean up
    req->result setting by rw") and 607ec89ed18f ("io_uring: fix SQPOLL
    IORING_OP_CLOSE cancelation state") include first]
    
    Signed-off-by: yangerkun <yangerkun@huawei.com>
    Reviewed-by: zhangyi (F) <yi.zhang@huawei.com>
    Signed-off-by: Cheng Jian <cj.chengjian@huawei.com>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -542,7 +542,6 @@ enum {
 	REQ_F_NOWAIT_BIT,
 	REQ_F_LINK_TIMEOUT_BIT,
 	REQ_F_ISREG_BIT,
-	REQ_F_MUST_PUNT_BIT,
 	REQ_F_COMP_LOCKED_BIT,
 	REQ_F_NEED_CLEANUP_BIT,
 	REQ_F_OVERFLOW_BIT,
@@ -585,8 +584,6 @@ enum {
 	REQ_F_LINK_TIMEOUT	= BIT(REQ_F_LINK_TIMEOUT_BIT),
 	/* regular file */
 	REQ_F_ISREG		= BIT(REQ_F_ISREG_BIT),
-	/* must be punted even for NONBLOCK */
-	REQ_F_MUST_PUNT		= BIT(REQ_F_MUST_PUNT_BIT),
 	/* completion under lock */
 	REQ_F_COMP_LOCKED	= BIT(REQ_F_COMP_LOCKED_BIT),
 	/* needs cleanup */
@@ -2877,10 +2874,7 @@ static int io_read(struct io_kiocb *req, bool force_nonblock,
 	io_size = ret;
 	req->result = io_size;
 
-	/*
-	 * If the file doesn't support async, mark it as REQ_F_MUST_PUNT so
-	 * we know to async punt it even if it was opened O_NONBLOCK
-	 */
+	/* If the file doesn't support async, just async punt */
 	if (force_nonblock && !io_file_supports_async(req->file, READ))
 		goto copy_iov;
 
@@ -2958,10 +2952,7 @@ static int io_write(struct io_kiocb *req, bool force_nonblock,
 	io_size = ret;
 	req->result = io_size;
 
-	/*
-	 * If the file doesn't support async, mark it as REQ_F_MUST_PUNT so
-	 * we know to async punt it even if it was opened O_NONBLOCK
-	 */
+	/* If the file doesn't support async, just async punt */
 	if (force_nonblock && !io_file_supports_async(req->file, WRITE))
 		goto copy_iov;
 
@@ -3645,8 +3636,10 @@ static int io_close(struct io_kiocb *req, bool force_nonblock,
 	if (close->put_file->f_op->flush && force_nonblock) {
 		/* not safe to cancel at this point */
 		req->work.flags |= IO_WQ_WORK_NO_CANCEL;
+		/* was never set, but play safe */
+		req->flags &= ~REQ_F_NOWAIT;
 		/* avoid grabbing files - we don't need the files */
-		req->flags |= REQ_F_NO_FILE_TABLE | REQ_F_MUST_PUNT;
+		req->flags |= REQ_F_NO_FILE_TABLE;
 		return -EAGAIN;
 	}
 
@@ -4614,7 +4607,7 @@ static bool io_arm_poll_handler(struct io_kiocb *req)
 
 	if (!req->file || !file_can_poll(req->file))
 		return false;
-	if (req->flags & (REQ_F_MUST_PUNT | REQ_F_POLLED))
+	if (req->flags & REQ_F_POLLED)
 		return false;
 	if (!def->pollin && !def->pollout)
 		return false;
@@ -5809,8 +5802,7 @@ again:
 	 * We async punt it if the file wasn't marked NOWAIT, or if the file
 	 * doesn't support non-blocking read/write attempts
 	 */
-	if (ret == -EAGAIN && (!(req->flags & REQ_F_NOWAIT) ||
-	    (req->flags & REQ_F_MUST_PUNT))) {
+	if (ret == -EAGAIN && !(req->flags & REQ_F_NOWAIT)) {
 		if (io_arm_poll_handler(req)) {
 			if (linked_timeout)
 				io_queue_linked_timeout(linked_timeout);
