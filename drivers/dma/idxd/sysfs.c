a42c47f9df1f0f377a803d08d5942e0b5755c60b
Dave Jiang <dave.jiang@intel.com>
2021-04-20 11:46:34 -0700

    dmaengine: idxd: add interrupt handle request and release support
    mainline inclusion
    from mainline-v5.13
    commit eb15e7154fbfa3e61c777704b2ff28eb3a0d4796
    category: feature
    bugzilla: https://gitee.com/openeuler/intel-kernel/issues/I596WO
    CVE: NA
    
    Intel-SIG: commit eb15e7154fbf dmaengine: idxd: add interrupt handle request and release support.
    Incremental backporting patches for DSA/IAA on Intel Xeon platform.
    
    --------------------------------
    
    DSA spec states that when Request Interrupt Handle and Release Interrupt
    Handle command bits are set in the CMDCAP register, these device commands
    must be supported by the driver.
    
    The interrupt handle is programmed in a descriptor. When Request Interrupt
    Handle is not supported, the interrupt handle is the index of the desired
    entry in the MSI-X table. When the command is supported, driver must use
    the command to obtain a handle to be programmed in the submitted
    descriptor.
    
    A requested handle may be revoked. After the handle is revoked, any use of
    the handle will result in Invalid Interrupt Handle error.
    
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>
    Link: https://lore.kernel.org/r/161894439422.3202472.17579543737810265471.stgit@djiang5-desk3.ch.intel.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>
    Signed-off-by: Xiaochen Shen <xiaochen.shen@intel.com>

--- drivers/dma/idxd/sysfs.c
+++ drivers/dma/idxd/sysfs.c
@@ -1606,6 +1606,7 @@ static void idxd_conf_device_release(struct device *dev)
 	kfree(idxd->wqs);
 	kfree(idxd->engines);
 	kfree(idxd->irq_entries);
+	kfree(idxd->int_handles);
 	ida_free(&idxd_ida, idxd->id);
 	kfree(idxd);
 }
