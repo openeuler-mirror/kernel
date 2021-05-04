d6e4e3d41ceb45fa3f754bb779b2c736f9023fb9
Michael S. Tsirkin <mst@redhat.com>
2021-05-04 04:17:20 -0400

    virtio_pci_modern: correct sparse tags for notify
    stable inclusion
    from stable-v5.13
    commit d7bce85aa7b92b5de8f69b3bcedfe51d7b1aabe1
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I5WXCZ
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=d7bce85aa7b92b5de8f69b3bcedfe51d7b1aabe1
    ----------------------------------------------------------------------
    
    When switching virtio_pci_modern to use a helper for mappings we lost an
    __iomem tag. Restore it.
    
    Reported-by: kernel test robot <lkp@intel.com>
    Fixes: 9e3bb9b79a71 ("virtio_pci_modern: introduce helper to map vq notify area")
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Pengyuan Zhao <zhaopengyuan@hisilicon.com>

--- include/linux/virtio_pci_modern.h
+++ include/linux/virtio_pci_modern.h
@@ -109,8 +109,8 @@ void __iomem *vp_modern_map_capability(struct virtio_pci_modern_device *mdev, in
 				       u32 align,
 				       u32 start, u32 size,
 				       size_t *len, resource_size_t *pa);
-void *vp_modern_map_vq_notify(struct virtio_pci_modern_device *mdev,
-                              u16 index, resource_size_t *pa);
+void __iomem *vp_modern_map_vq_notify(struct virtio_pci_modern_device *mdev,
+				      u16 index, resource_size_t *pa);
 int vp_modern_probe(struct virtio_pci_modern_device *mdev);
 void vp_modern_remove(struct virtio_pci_modern_device *mdev);
 #endif
