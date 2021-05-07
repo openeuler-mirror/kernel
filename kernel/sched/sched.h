ea08b58eb670ddaaef3e49f4bf4ae7e7830e8413
Cheng Jian <cj.chengjian@huawei.com>
2021-05-07 19:37:22 +0800

    Revert "sched: Introduce qos scheduler for co-location"
    This reverts commit 709d65618750e239e76c7ceac081673f36b265d0.

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -400,11 +400,7 @@ struct task_group {
 
 	struct cfs_bandwidth	cfs_bandwidth;
 
-#if defined(CONFIG_QOS_SCHED) && !defined(__GENKSYMS__)
-	long qos_level;
-#else
 	KABI_RESERVE(1)
-#endif
 	KABI_RESERVE(2)
 };
 
