51260392bdf00ccd10f5d11bb81c78b39e888ae2
Peng Li <lipeng321@huawei.com>
2021-06-08 21:22:43 +0800

    net: hns3: add match_id to check mailbox response from PF to VF
    driver inclusion
    category: bugfix
    bugzilla: NA
    CVE: NA
    
    ----------------------------
    
    When VF need response from PF, VF will wait (1us - 1s) to receive
    the response, or it will wait timeout and the VF action fails.
    If VF do not receive response in 1st action because timeout,
    the 2nd action may receive response for the 1st action, and get
    incorrect response data.VF must reciveve the right response from
    PF,or it will cause unexpected error.
    
    This patch adds match_id to check mailbox response from PF to VF,
    to make sure VF get the right response:
    1. The message sent from VF was labelled with match_id which was a
    unique 16-bit non-zero value.
    2. The response sent from PF will label with match_id which got from
    the request.
    3. The VF uses the match_id to match request and response message.
    
    This scheme depends on the PF driver, if the PF driver don't support
    then VF will uses the original scheme.
    
    PF driver adds match_id by the patch
    430acf6 ("net: hns3: fix possible mismatches resp of mailbox").
    
    Signed-off-by: Peng Li <lipeng321@huawei.com>
    Signed-off-by: Yonglong Liu <liuyonglong@huawei.com>
    Reviewed-by: li yongxin <liyongxin1@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- drivers/net/ethernet/hisilicon/hns3/hns3vf/hclgevf_mbx.c
+++ drivers/net/ethernet/hisilicon/hns3/hns3vf/hclgevf_mbx.c
@@ -35,6 +35,7 @@ static int hclgevf_resp_to_errno(u16 resp_code)
 	return -EIO;
 }
 
+#define HCLGEVF_MBX_MATCH_ID_START	1
 static void hclgevf_reset_mbx_resp_status(struct hclgevf_dev *hdev)
 {
 	/* this function should be called with mbx_resp.mbx_mutex held
@@ -43,6 +44,10 @@ static void hclgevf_reset_mbx_resp_status(struct hclgevf_dev *hdev)
 	hdev->mbx_resp.received_resp  = false;
 	hdev->mbx_resp.origin_mbx_msg = 0;
 	hdev->mbx_resp.resp_status    = 0;
+	hdev->mbx_resp.match_id++;
+	/* Update match_id and ensure the value of match_id is not zero */
+	if (hdev->mbx_resp.match_id == 0)
+		hdev->mbx_resp.match_id = HCLGEVF_MBX_MATCH_ID_START;
 	memset(hdev->mbx_resp.additional_info, 0, HCLGE_MBX_MAX_RESP_DATA_SIZE);
 }
 
@@ -137,6 +142,7 @@ int hclgevf_send_mbx_msg(struct hclgevf_dev *hdev,
 	if (need_resp) {
 		mutex_lock(&hdev->mbx_resp.mbx_mutex);
 		hclgevf_reset_mbx_resp_status(hdev);
+		req->match_id = hdev->mbx_resp.match_id;
 		status = hclgevf_cmd_send(&hdev->hw, &desc, 1);
 		if (status) {
 			dev_err(&hdev->pdev->dev,
@@ -220,7 +226,6 @@ void hclgevf_mbx_handler(struct hclgevf_dev *hdev)
 				dev_warn(&hdev->pdev->dev,
 					 "VF mbx resp flag not clear(%u)\n",
 					 req->msg.vf_mbx_msg_code);
-			resp->received_resp = true;
 
 			resp->origin_mbx_msg =
 					(req->msg.vf_mbx_msg_code << 16);
@@ -232,6 +237,19 @@ void hclgevf_mbx_handler(struct hclgevf_dev *hdev)
 			for (i = 0; i < HCLGE_MBX_MAX_RESP_DATA_SIZE; i++) {
 				resp->additional_info[i] = *temp;
 				temp++;
+			}
+
+			if (req->match_id) {
+				/* If match_id is not zero, it means PF support
+				 * match_id. If the match_id is right, VF get
+				 * the right response, orignore the response,
+				 * and driver will clear hdev->mbx_resp when
+				 * send next message which need response.
+				 */
+				if (req->match_id == resp->match_id)
+					resp->received_resp = true;
+			} else {
+				resp->received_resp = true;
 			}
 			break;
 		case HCLGE_MBX_LINK_STAT_CHANGE:
