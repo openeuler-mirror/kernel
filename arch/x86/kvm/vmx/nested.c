11ee1c7857f06172362f0b3f4a7491cc3835d547
Sean Christopherson <seanjc@google.com>
2021-06-22 10:57:34 -0700

    KVM: x86: Enhance comments for MMU roles and nested transition trickiness
    mainline inclusion
    from mainline-v5.14-rc1
    commit 616007c866a250143e95ea7a696bd924df251f8a
    category: feature
    bugzilla: https://gitee.com/openeuler/intel-kernel/issues/I9HH9U
    CVE: N/A
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=616007c866a250143e95ea7a696bd924df251f8a
    
    -------------------------------------
    
    Expand the comments for the MMU roles.  The interactions with gfn_track
    PGD reuse in particular are hairy.
    
    Regarding PGD reuse, add comments in the nested virtualization flows to
    call out why kvm_init_mmu() is unconditionally called even when nested
    TDP is used.
    
    Cc: Vitaly Kuznetsov <vkuznets@redhat.com>
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Message-Id: <20210622175739.3610207-50-seanjc@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Yu Zhang <yu.c.zhang@linux.intel.com>

--- arch/x86/kvm/vmx/nested.c
+++ arch/x86/kvm/vmx/nested.c
@@ -1100,6 +1100,7 @@ static int nested_vmx_load_cr3(struct kvm_vcpu *vcpu, unsigned long cr3, bool ne
 	vcpu->arch.cr3 = cr3;
 	kvm_register_mark_available(vcpu, VCPU_EXREG_CR3);
 
+	/* Re-initialize the MMU, e.g. to pick up CR4 MMU role changes. */
 	kvm_init_mmu(vcpu);
 
 	return 0;
