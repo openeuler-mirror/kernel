abe331ea915db3756cd86117eca06e2a8de2115e
Naveen Krishna Chatradhi <nchatrad@amd.com>
2021-06-30 20:58:24 +0530

    EDAC/mc: Add new HBM2 memory type
    mainline inclusion
    from mainline-v5.13
    commit e1ca90b7cc5cb5d3a38321cbb65ad36a59fcb574
    category: feature
    bugzilla: https://gitee.com/openeuler/intel-kernel/issues/I5HAC1
    CVE: NA
    
    Intel-SIG: commit e1ca90b7cc5c EDAC/mc: Add new HBM2 memory type.
    Backport to add EDAC HBM support.
    
    --------------------------------
    
    Add a new entry to 'enum mem_type' and a new string to 'edac_mem_types[]'
    for HBM2 (High Bandwidth Memory Gen 2) new memory type.
    
    Reviewed-by: Yazen Ghannam <yazen.ghannam@amd.com>
    Signed-off-by: Muralidhara M K <muralimk@amd.com>
    Signed-off-by: Naveen Krishna Chatradhi <nchatrad@amd.com>
    Signed-off-by: Tony Luck <tony.luck@intel.com>
    Link: https://lore.kernel.org/r/20210630152828.162659-4-nchatrad@amd.com
    Signed-off-by: Youquan Song <youquan.song@intel.com>
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>

--- drivers/edac/edac_mc.c
+++ drivers/edac/edac_mc.c
@@ -163,6 +163,7 @@ const char * const edac_mem_types[] = {
 	[MEM_LRDDR4]	= "Load-Reduced-DDR4-RAM",
 	[MEM_DDR5]	= "Unbuffered-DDR5",
 	[MEM_NVDIMM]	= "Non-volatile-RAM",
+	[MEM_HBM2]	= "High-bandwidth-memory-Gen2",
 };
 EXPORT_SYMBOL_GPL(edac_mem_types);
 
