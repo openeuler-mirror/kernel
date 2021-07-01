c493573c3d35f966a625c318a0ec495f9a27a1a0
Arnaldo Carvalho de Melo <acme@redhat.com>
2021-07-01 13:39:15 -0300

    tools headers cpufeatures: Sync with the kernel sources
    mainline inclusion
    from mainline-v5.18-rc1
    commit 5ced81243593dd84f46ee3e7bb3cb8a69ede0276
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/IDC603
    CVE: NA
    
    Reference: https://github.com/torvalds/linux/commit/5ced81243593dd84f46ee3e7bb3cb8a69ede0276
    
    --------------------------------
    
    commit 5ced81243593dd84f46ee3e7bb3cb8a69ede0276 upstream
    
    To pick the changes from:
    
      991625f3dd2cbc4b ("x86/ibt: Add IBT feature, MSR and #CP handling")
    
    This only causes these perf files to be rebuilt:
    
      CC       /tmp/build/perf/bench/mem-memcpy-x86-64-asm.o
      CC       /tmp/build/perf/bench/mem-memset-x86-64-asm.o
    
    And addresses this perf build warning:
    
      Warning: Kernel ABI header at 'tools/arch/x86/include/asm/cpufeatures.h' differs from latest version at 'arch/x86/include/asm/cpufeatures.h'
      diff -u tools/arch/x86/include/asm/cpufeatures.h arch/x86/include/asm/cpufeatures.h
    
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: https://lore.kernel.org/lkml/YkSCx2kr4ambH+Qe@kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Signed-off-by: Hemanth Selam <Hemanth.Selam@amd.com>
    Signed-off-by: PvsNarasimha <PVS.NarasimhaRao@amd.com>

--- tools/arch/x86/include/asm/cpufeatures.h
+++ tools/arch/x86/include/asm/cpufeatures.h
@@ -411,6 +411,7 @@
 #define X86_FEATURE_TSXLDTRK		(18*32+16) /* TSX Suspend Load Address Tracking */
 #define X86_FEATURE_PCONFIG		(18*32+18) /* Intel PCONFIG */
 #define X86_FEATURE_ARCH_LBR		(18*32+19) /* Intel ARCH LBR */
+#define X86_FEATURE_IBT			(18*32+20) /* Indirect Branch Tracking */
 #define X86_FEATURE_SPEC_CTRL		(18*32+26) /* "" Speculation Control (IBRS + IBPB) */
 #define X86_FEATURE_INTEL_STIBP		(18*32+27) /* "" Single Thread Indirect Branch Predictors */
 #define X86_FEATURE_FLUSH_L1D		(18*32+28) /* Flush L1D cache */
