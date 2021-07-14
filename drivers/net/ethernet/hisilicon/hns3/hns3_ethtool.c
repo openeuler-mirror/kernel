15bdd20991482827cece16d646f25cd2446a4cdf
Huazhong Tan <tanhuazhong@huawei.com>
2021-07-14 20:55:27 +0800

    net: hns3: rename gl_adapt_enable in struct hns3_enet_coalesce
    mainline inclusion
    from mainline-v5.11-rc1
    commit de25bcc47fba49a848764fdfab76741b7e17ca2f
    category: feature
    bugzilla: 173966
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=de25bcc47fba49a848764fdfab76741b7e17ca2f
    
    ----------------------------------------------------------------------
    
    Besides GL(Gap Limiting), QL(Quantity Limiting) can be modified
    dynamically when DIM is supported. So rename gl_adapt_enable as
    adapt_enable in struct hns3_enet_coalesce.
    
    Signed-off-by: Huazhong Tan <tanhuazhong@huawei.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Reviewed-by: li yongxin <liyongxin1@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- drivers/net/ethernet/hisilicon/hns3/hns3_ethtool.c
+++ drivers/net/ethernet/hisilicon/hns3/hns3_ethtool.c
@@ -1107,9 +1107,9 @@ static int hns3_get_coalesce_per_queue(struct net_device *netdev, u32 queue,
 	rx_vector = priv->ring[queue_num + queue].tqp_vector;
 
 	cmd->use_adaptive_tx_coalesce =
-			tx_vector->tx_group.coal.gl_adapt_enable;
+			tx_vector->tx_group.coal.adapt_enable;
 	cmd->use_adaptive_rx_coalesce =
-			rx_vector->rx_group.coal.gl_adapt_enable;
+			rx_vector->rx_group.coal.adapt_enable;
 
 	cmd->tx_coalesce_usecs = tx_vector->tx_group.coal.int_gl;
 	cmd->rx_coalesce_usecs = rx_vector->rx_group.coal.int_gl;
@@ -1270,9 +1270,9 @@ static void hns3_set_coalesce_per_queue(struct net_device *netdev,
 	tx_vector = priv->ring[queue].tqp_vector;
 	rx_vector = priv->ring[queue_num + queue].tqp_vector;
 
-	tx_vector->tx_group.coal.gl_adapt_enable =
+	tx_vector->tx_group.coal.adapt_enable =
 				cmd->use_adaptive_tx_coalesce;
-	rx_vector->rx_group.coal.gl_adapt_enable =
+	rx_vector->rx_group.coal.adapt_enable =
 				cmd->use_adaptive_rx_coalesce;
 
 	tx_vector->tx_group.coal.int_gl = cmd->tx_coalesce_usecs;
