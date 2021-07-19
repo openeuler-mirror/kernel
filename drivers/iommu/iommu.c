85412c048741ef1c9d3ae4f1f6218ed15ceac587
Eric Auger <eric.auger@redhat.com>
2021-07-19 11:20:40 +0800

    iommu/smmuv3: Enforce incompatibility between nested mode and HW MSI regions
    virt inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I401IF
    CVE: NA
    
    ------------------------------
    
    Nested mode currently is not compatible with HW MSI reserved regions.
    Indeed MSI transactions targeting this MSI doorbells bypass the SMMU.
    
    Let's check nested mode is not attempted in such configuration.
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    Signed-off-by: Kunkun Jiang<jiangkunkun@huawei.com>
    Reviewed-by: Keqian Zhu <zhukeqian1@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -3186,6 +3186,7 @@ void iommu_get_resv_regions(struct device *dev, struct list_head *list)
 	if (ops && ops->get_resv_regions)
 		ops->get_resv_regions(dev, list);
 }
+EXPORT_SYMBOL_GPL(iommu_get_resv_regions);
 
 void iommu_put_resv_regions(struct device *dev, struct list_head *list)
 {
@@ -3194,6 +3195,7 @@ void iommu_put_resv_regions(struct device *dev, struct list_head *list)
 	if (ops && ops->put_resv_regions)
 		ops->put_resv_regions(dev, list);
 }
+EXPORT_SYMBOL_GPL(iommu_put_resv_regions);
 
 /**
  * generic_iommu_put_resv_regions - Reserved region driver helper
