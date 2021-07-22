536473d5f9e8e43f86e42ab9dde169384a8fafa5
Kai Ye <yekai13@huawei.com>
2021-07-22 15:01:13 +0800

    uacce: modify the module author information.
    mainline inclusion
    from mainline-v5.11-rc1
    commit 385997dc17ab4927bad332c1283dc461a973ab1d
    category: bugfix
    bugzilla: 173981
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=385997dc17ab4927bad332c1283dc461a973ab1d
    
    ----------------------------------------------------------------------
    
    The spelling of "Hisilicon" is modified.
    
    Reviewed-by: Zhou Wang <wangzhou1@hisilicon.com>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@Huawei.com>
    Acked-by: Zhangfei Gao <zhangfei.gao@linaro.org>
    Signed-off-by: Kai Ye <yekai13@huawei.com>
    Link: https://lore.kernel.org/r/1604309965-21752-3-git-send-email-yekai13@huawei.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Mingqiang Ling <lingmingqiang@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- drivers/misc/uacce/uacce.c
+++ drivers/misc/uacce/uacce.c
@@ -543,5 +543,5 @@ subsys_initcall(uacce_init);
 module_exit(uacce_exit);
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Hisilicon Tech. Co., Ltd.");
+MODULE_AUTHOR("HiSilicon Tech. Co., Ltd.");
 MODULE_DESCRIPTION("Accelerator interface for Userland applications");
