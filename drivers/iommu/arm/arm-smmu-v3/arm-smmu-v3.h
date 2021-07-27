ba3b3b128306281113fbda77d33629df09e450d6
Zhen Lei <thunder.leizhen@huawei.com>
2021-07-27 15:10:30 +0800

    iommu/arm-smmu-v3: Add support for ECMDQ register mode
    hulk inclusion
    category: feature
    bugzilla: 174251
    CVE: NA
    
    -------------------------------------------------------------------------
    
    Ensure that each core exclusively occupies an ECMDQ and all of them are
    enabled during initialization. During this initialization process, any
    errors will result in a fallback to using normal CMDQ.
    
    When GERROR is triggered by ECMDQ, all ECMDQs need to be traversed: the
    ECMDQs with errors will be processed and the ECMDQs without errors will
    be skipped directly.
    
    Compared with register SMMU_CMDQ_PROD, register SMMU_ECMDQ_PROD has one
    more 'EN' bit and one more 'ERRACK' bit. Therefore, an extra member
    'ecmdq_prod' is added to record the values of these two bits. Each time
    register SMMU_ECMDQ_PROD is updated, the value of 'ecmdq_prod' is ORed.
    After the error indicated by SMMU_GERROR.CMDQP_ERR is fixed, the 'ERRACK'
    bit needs to be toggled to resume the corresponding ECMDQ. Therefore, a
    rwlock is used to protect the write operation to bit 'ERRACK' during error
    handling and the read operation to bit 'ERRACK' during command insertion.
    
    Signed-off-by: Zhen Lei <thunder.leizhen@huawei.com>
    Reviewed-by: Hanjun Guo <guohanjun@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.h
+++ drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.h
@@ -45,6 +45,7 @@
 #define IDR0_S2P			(1 << 0)
 
 #define ARM_SMMU_IDR1			0x4
+#define IDR1_ECMDQ			(1 << 31)
 #define IDR1_TABLES_PRESET		(1 << 30)
 #define IDR1_QUEUES_PRESET		(1 << 29)
 #define IDR1_REL			(1 << 28)
@@ -115,6 +116,7 @@
 #define ARM_SMMU_IRQ_CTRLACK		0x54
 
 #define ARM_SMMU_GERROR			0x60
+#define GERROR_CMDQP_ERR		(1 << 9)
 #define GERROR_SFM_ERR			(1 << 8)
 #define GERROR_MSI_GERROR_ABT_ERR	(1 << 7)
 #define GERROR_MSI_PRIQ_ABT_ERR		(1 << 6)
@@ -159,6 +161,26 @@
 #define ARM_SMMU_PRIQ_IRQ_CFG0		0xd0
 #define ARM_SMMU_PRIQ_IRQ_CFG1		0xd8
 #define ARM_SMMU_PRIQ_IRQ_CFG2		0xdc
+
+#define ARM_SMMU_IDR6			0x190
+#define IDR6_LOG2NUMP			GENMASK(27, 24)
+#define IDR6_LOG2NUMQ			GENMASK(19, 16)
+#define IDR6_BA_DOORBELLS		GENMASK(9, 0)
+
+#define ARM_SMMU_ECMDQ_BASE		0x00
+#define ARM_SMMU_ECMDQ_PROD		0x08
+#define ARM_SMMU_ECMDQ_CONS		0x0c
+#define ECMDQ_MAX_SZ_SHIFT		8
+#define ECMDQ_PROD_EN			(1 << 31)
+#define ECMDQ_CONS_ENACK		(1 << 31)
+#define ECMDQ_CONS_ERR			(1 << 23)
+#define ECMDQ_PROD_ERRACK		(1 << 23)
+
+#define ARM_SMMU_ECMDQ_CP_BASE		0x4000
+#define ECMDQ_CP_ADDR			GENMASK_ULL(51, 12)
+#define ECMDQ_CP_CMDQGS			GENMASK_ULL(2, 1)
+#define ECMDQ_CP_PRESET			(1UL << 0)
+#define ECMDQ_CP_RRESET_SIZE		0x10000
 
 #define ARM_SMMU_REG_SZ			0xe00
 
@@ -540,6 +562,8 @@ struct arm_smmu_ll_queue {
 struct arm_smmu_queue {
 	struct arm_smmu_ll_queue	llq;
 	int				irq; /* Wired interrupt */
+	u32				ecmdq_prod;
+	rwlock_t			ecmdq_lock;
 
 	__le64				*base;
 	dma_addr_t			base_dma;
@@ -563,6 +587,11 @@ struct arm_smmu_cmdq {
 	atomic_long_t			*valid_map;
 	atomic_t			owner_prod;
 	atomic_t			lock;
+};
+
+struct arm_smmu_ecmdq {
+	struct arm_smmu_cmdq		cmdq;
+	void __iomem			*base;
 };
 
 struct arm_smmu_cmdq_batch {
@@ -667,12 +696,19 @@ struct arm_smmu_device {
 #define ARM_SMMU_FEAT_HD		(1 << 20)
 #define ARM_SMMU_FEAT_BBML1		(1 << 21)
 #define ARM_SMMU_FEAT_BBML2		(1 << 22)
+#define ARM_SMMU_FEAT_ECMDQ		(1 << 23)
 	u32				features;
 
 #define ARM_SMMU_OPT_SKIP_PREFETCH	(1 << 0)
 #define ARM_SMMU_OPT_PAGE0_REGS_ONLY	(1 << 1)
 #define ARM_SMMU_OPT_MSIPOLL		(1 << 2)
 	u32				options;
+
+	union {
+		u32			nr_ecmdq;
+		u32			ecmdq_enabled;
+	};
+	struct arm_smmu_ecmdq *__percpu	*ecmdq;
 
 	struct arm_smmu_cmdq		cmdq;
 	struct arm_smmu_evtq		evtq;
