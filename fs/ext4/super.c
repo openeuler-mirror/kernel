3bac47e1a52a796aaef0343e907d0f513f6d5582
Theodore Ts'o <tytso@mit.edu>
2021-07-29 20:05:36 +0800

    ext4: inline jbd2_journal_[un]register_shrinker()
    mainline inclusion
    from mainline-5.14-rc1
    commit 0705e8d1e2207ceeb83dc6e1751b6b82718b353a
    category: bugfix
    bugzilla: 50788
    CVE: NA
    ---------------------------
    
    The function jbd2_journal_unregister_shrinker() was getting called
    twice when the file system was getting unmounted.  On Power and ARM
    platforms this was causing kernel crash when unmounting the file
    system, when a percpu_counter was destroyed twice.
    
    Fix this by removing jbd2_journal_[un]register_shrinker() functions,
    and inlining the shrinker setup and teardown into
    journal_init_common() and jbd2_journal_destroy().  This means that
    ext4 and ocfs2 now no longer need to know about registering and
    unregistering jbd2's shrinker.
    
    Also, while we're at it, rename the percpu counter from
    j_jh_shrink_count to j_checkpoint_jh_count, since this makes it
    clearer what this counter is intended to track.
    
    Link: https://lore.kernel.org/r/20210705145025.3363130-1-tytso@mit.edu
    Fixes: 4ba3fcdde7e3 ("jbd2,ext4: add a shrinker to release checkpointed buffers")
    Reported-by: Jon Hunter <jonathanh@nvidia.com>
    Reported-by: Sachin Sant <sachinp@linux.vnet.ibm.com>
    Tested-by: Sachin Sant <sachinp@linux.vnet.ibm.com>
    Tested-by: Jon Hunter <jonathanh@nvidia.com>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    
    Conflict:
      fs/jbd2/journal.c
    
    Signed-off-by: Zhang Yi <yi.zhang@huawei.com>
    Reviewed-by: Yang Erkun <yangerkun@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -1161,7 +1161,6 @@ static void ext4_put_super(struct super_block *sb)
 	destroy_workqueue(sbi->rsv_conversion_wq);
 
 	if (sbi->s_journal) {
-		jbd2_journal_unregister_shrinker(sbi->s_journal);
 		aborted = is_journal_aborted(sbi->s_journal);
 		err = jbd2_journal_destroy(sbi->s_journal);
 		sbi->s_journal = NULL;
@@ -4832,7 +4831,6 @@ failed_mount_wq:
 		sbi->s_ea_block_cache = NULL;
 	}
 	if (sbi->s_journal) {
-		jbd2_journal_unregister_shrinker(sbi->s_journal);
 		jbd2_journal_destroy(sbi->s_journal);
 		sbi->s_journal = NULL;
 	}
@@ -5149,12 +5147,6 @@ static int ext4_load_journal(struct super_block *sb,
 
 		/* Make sure we flush the recovery flag to disk. */
 		ext4_commit_super(sb);
-	}
-
-	err = jbd2_journal_register_shrinker(journal);
-	if (err) {
-		EXT4_SB(sb)->s_journal = NULL;
-		goto err_out;
 	}
 
 	return 0;
