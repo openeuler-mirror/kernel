d647e8508e29c83a8e25d25568e1051ea56c8b84
Shunfeng Yang <yangshunfeng2@huawei.com>
2021-09-02 21:27:15 +0800

    RDMA/hns: Avoid enabling RQ inline on UD
    mainline inclusion
    from mainline-v5.13
    commit 24f3f1cd5154
    category: cleanup
    bugzilla: NA
    CVE: NA
    
    Avoid enabling RQ inline on UD
    
    Signed-off-by: Shunfeng Yang <yangshunfeng2@huawei.com>
    Signed-off-by: Yangyang Li <liyangyang20@huawei.com>
    Reviewed-by: chunzhi hu <huchunzhi@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- drivers/infiniband/hw/hns/hns_roce_hw_v2.c
+++ drivers/infiniband/hw/hns/hns_roce_hw_v2.c
@@ -4036,8 +4036,9 @@ static void modify_qp_reset_to_init(struct ib_qp *ibqp,
 	context->rq_db_record_addr = cpu_to_le32(hr_qp->rdb.dma >> 32);
 	qpc_mask->rq_db_record_addr = 0;
 
-	roce_set_bit(context->byte_76_srqn_op_en, V2_QPC_BYTE_76_RQIE_S,
-		    (hr_dev->caps.flags & HNS_ROCE_CAP_FLAG_RQ_INLINE) ? 1 : 0);
+	if (ibqp->qp_type != IB_QPT_UD && ibqp->qp_type != IB_QPT_GSI)
+		roce_set_bit(context->byte_76_srqn_op_en, V2_QPC_BYTE_76_RQIE_S,
+			!!(hr_dev->caps.flags & HNS_ROCE_CAP_FLAG_RQ_INLINE));
 	roce_set_bit(qpc_mask->byte_76_srqn_op_en, V2_QPC_BYTE_76_RQIE_S, 0);
 
 	roce_set_field(context->byte_80_rnr_rx_cqn, V2_QPC_BYTE_80_RX_CQN_M,
