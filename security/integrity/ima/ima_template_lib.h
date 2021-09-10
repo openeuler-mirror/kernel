0cb2b7009e1ce56dd4c5d22195797901ddc422b7
Krzysztof Struczynski <krzysztof.struczynski@huawei.com>
2021-09-10 19:59:39 +0800

    ima: Add a new ima template that includes namespace ID
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I49KW1
    CVE: NA
    
    --------------------------------
    
    Add a new ima-ns template:
    "d-ng|n-ng|ns"
    
    Signed-off-by: Krzysztof Struczynski <krzysztof.struczynski@huawei.com>
    Reviewed-by: Zhang Tianxing <zhangtianxing3@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- security/integrity/ima/ima_template_lib.h
+++ security/integrity/ima/ima_template_lib.h
@@ -48,4 +48,6 @@ int ima_eventmodsig_init(struct ima_event_data *event_data,
 			 struct ima_field_data *field_data);
 int ima_eventevmsig_init(struct ima_event_data *event_data,
 			 struct ima_field_data *field_data);
+int ima_eventns_init(struct ima_event_data *event_data,
+		     struct ima_field_data *field_data);
 #endif /* __LINUX_IMA_TEMPLATE_LIB_H */
