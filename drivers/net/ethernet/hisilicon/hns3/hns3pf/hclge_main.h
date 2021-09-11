fd29ac0e9a46731cadcd341702caef1cebd40d7c
Yufeng Mo <moyufeng@huawei.com>
2021-09-11 15:33:56 +0800

    net: hns3: change hclge/hclgevf workqueue to WQ_UNBOUND mode
    driver inclusion
    category: bugfix
    bugzilla: NA
    CVE: NA
    
    ----------------------------
    
    Currently, the workqueue of hclge/hclgevf is executed on
    the CPU that initiates scheduling requests by default. In
    stress scenarios, the CPU may be busy and workqueue scheduling
    is completed after a long period of time. To avoid this
    situation and implement proper scheduling, use the WQ_UNBOUND
    mode instead. In this way, the workqueue can be performed on
    a relatively idle CPU.
    
    Signed-off-by: Yufeng Mo <moyufeng@huawei.com>
    Signed-off-by: Yonglong Liu <liuyonglong@huawei.com>
    Reviewed-by: Junxin Chen <chenjunxin1@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_main.h
+++ drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_main.h
@@ -864,7 +864,6 @@ struct hclge_dev {
 
 	/* affinity mask and notify for misc interrupt */
 	cpumask_t affinity_mask;
-	struct irq_affinity_notify affinity_notify;
 };
 
 /* VPort level vlan tag configuration for TX direction */
