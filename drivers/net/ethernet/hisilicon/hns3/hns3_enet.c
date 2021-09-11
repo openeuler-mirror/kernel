e5aa751f091b3884d3fbb0e729d52fdde8151cc3
Hao Chen <chenhao288@hisilicon.com>
2021-09-11 15:34:17 +0800

    net: hns3: fix for vxlan gpe tx checksum bug
    mainline inclusion
    from mainline-v5.13-rc1
    commit 905416f18fe74bdd4de91bf94ef5a790a36e4b99
    category: bugfix
    bugzilla: NA
    CVE: NA
    
    ----------------------------
    
    When skb->ip_summed is CHECKSUM_PARTIAL, for non-tunnel udp packet,
    which has a dest port as the IANA assigned, the hardware is expected
    to do the checksum offload, but the hardware whose version is below
    V3 will not do the checksum offload when udp dest port is 4790.
    
    So fixes it by doing the checksum in software for this case.
    
    Fixes: 76ad4f0ee747 ("net: hns3: Add support of HNS3 Ethernet Driver for hip08 SoC")
    Signed-off-by: Hao Chen <chenhao288@hisilicon.com>
    Signed-off-by: Huazhong Tan <tanhuazhong@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Yonglong Liu <liuyonglong@huawei.com>
    Reviewed-by: Junxin Chen <chenjunxin1@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- drivers/net/ethernet/hisilicon/hns3/hns3_enet.c
+++ drivers/net/ethernet/hisilicon/hns3/hns3_enet.c
@@ -775,7 +775,7 @@ static int hns3_get_l4_protocol(struct sk_buff *skb, u8 *ol4_proto,
  * and it is udp packet, which has a dest port as the IANA assigned.
  * the hardware is expected to do the checksum offload, but the
  * hardware will not do the checksum offload when udp dest port is
- * 4789 or 6081.
+ * 4789, 4790 or 6081.
  */
 static bool hns3_tunnel_csum_bug(struct sk_buff *skb)
 {
@@ -787,13 +787,18 @@ static bool hns3_tunnel_csum_bug(struct sk_buff *skb)
 #define GENEVE_UDP_PORT 6081
 #endif
 
+#ifndef IANA_VXLAN_GPE_UDP_PORT
+#define IANA_VXLAN_GPE_UDP_PORT 4790
+#endif
+
 	union l4_hdr_info l4;
 
 	l4.hdr = skb_transport_header(skb);
 
 	if (!(!skb->encapsulation &&
 	      (l4.udp->dest == htons(IANA_VXLAN_UDP_PORT) ||
-	      l4.udp->dest == htons(GENEVE_UDP_PORT))))
+	      l4.udp->dest == htons(GENEVE_UDP_PORT) ||
+	      l4.udp->dest == htons(IANA_VXLAN_GPE_UDP_PORT))))
 		return false;
 
 	skb_checksum_help(skb);
