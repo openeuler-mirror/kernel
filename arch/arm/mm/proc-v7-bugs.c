e90765b065f29e6f4d8c9404107b70d69f07df43
GONG, Ruiqi <gongruiqi1@huawei.com>
2021-10-19 18:04:44 +0800

    ARM: spectre-v2: turn off the mitigation via boot cmdline param
    hulk inclusion
    category: feature
    feature: switch of spectre mitigation
    bugzilla: 180851 https://gitee.com/openeuler/kernel/issues/I4EF1O
    
    -------------------------------------------------
    
    We enable spectre mitigation by default for ARM32, which may
    cause performance regression. To offer an option to turn off
    this feature, implement a cmdline parameter 'nospectre_v2' compatible
    with mainline, which sets up a switch to skip invalidating BTB/icache
    for A9/A15 in context switching and user abort.
    
    Signed-off-by: GONG, Ruiqi <gongruiqi1@huawei.com>
    Cc: Hanjun Guo <guohanjun@huawei.com>
    Reviewed-by: Hanjun Guo <guohanjun@huawei.com>
    Reviewed-by: Xiu Jianfeng <xiujianfeng@huawei.com>
    Signed-off-by: Chen Jun <chenjun102@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- arch/arm/mm/proc-v7-bugs.c
+++ arch/arm/mm/proc-v7-bugs.c
@@ -8,6 +8,19 @@
 #include <asm/proc-fns.h>
 #include <asm/system_misc.h>
 
+/*
+ * 32-bit ARM spectre hardening, enabled by default, can be disabled via boot
+ * cmdline param 'nospectre_v2' to avoid performance regression.
+ */
+int nospectre_v2 __read_mostly;
+
+static int __init nospectre_v2_setup(char *str)
+{
+	nospectre_v2 = 1;
+	return 0;
+}
+early_param("nospectre_v2", nospectre_v2_setup);
+
 #ifdef CONFIG_HARDEN_BRANCH_PREDICTOR
 DEFINE_PER_CPU(harden_branch_predictor_fn_t, harden_branch_predictor_fn);
 
@@ -40,6 +53,11 @@ static void cpu_v7_spectre_init(void)
 {
 	const char *spectre_v2_method = NULL;
 	int cpu = smp_processor_id();
+
+	if (nospectre_v2) {
+		pr_info_once("Spectre v2: hardening is disabled\n");
+		return;
+	}
 
 	if (per_cpu(harden_branch_predictor_fn, cpu))
 		return;
