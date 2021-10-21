77bd601afc0aae9f2e9a02af3762a6259c6771b0
Johannes Weiner <hannes@cmpxchg.org>
2021-10-21 21:05:55 +0800

    mm: vmscan: fix missing psi annotation for node_reclaim()
    mainline inclusion
    from mainline-5.14-rc7
    commit 57f29762cdd4687a02f245d1b1e78de046388eac
    category: bugfix
    bugzilla: 176927 https://gitee.com/openeuler/kernel/issues/I4DDEL
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=57f29762cdd4687a02f245d1b1e78de046388eac
    
    ------------------------------------------------
    
    In a debugging session the other day, Rik noticed that node_reclaim()
    was missing memstall annotations.  This means we'll miss pressure and
    lost productivity resulting from reclaim on an overloaded local NUMA
    node when vm.zone_reclaim_mode is enabled.
    
    There haven't been any reports, but that's likely because
    vm.zone_reclaim_mode hasn't been a commonly used feature recently, and
    the intersection between such setups and psi users is probably nil.
    
    But secondary memory such as CXL-connected DIMMS, persistent memory etc,
    and the page demotion patches that handle them
    (https://lore.kernel.org/lkml/20210401183216.443C4443@viggo.jf.intel.com/)
    could soon make this a more common codepath again.
    
    Link: https://lkml.kernel.org/r/20210818152457.35846-1-hannes@cmpxchg.org
    Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>
    Reported-by: Rik van Riel <riel@surriel.com>
    Reviewed-by: Shakeel Butt <shakeelb@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Jing Xiangfeng <jingxiangfeng@huawei.com>
    Reviewed-by: Chen Wandun <chenwandun@huawei.com>
    
    Signed-off-by: Chen Jun <chenjun102@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -4393,11 +4393,13 @@ static int __node_reclaim(struct pglist_data *pgdat, gfp_t gfp_mask, unsigned in
 		.may_swap = 1,
 		.reclaim_idx = gfp_zone(gfp_mask),
 	};
+	unsigned long pflags;
 
 	trace_mm_vmscan_node_reclaim_begin(pgdat->node_id, order,
 					   sc.gfp_mask);
 
 	cond_resched();
+	psi_memstall_enter(&pflags);
 	fs_reclaim_acquire(sc.gfp_mask);
 	/*
 	 * We need to be able to allocate from the reserves for RECLAIM_UNMAP
@@ -4422,6 +4424,7 @@ static int __node_reclaim(struct pglist_data *pgdat, gfp_t gfp_mask, unsigned in
 	current->flags &= ~PF_SWAPWRITE;
 	memalloc_noreclaim_restore(noreclaim_flag);
 	fs_reclaim_release(sc.gfp_mask);
+	psi_memstall_leave(&pflags);
 
 	trace_mm_vmscan_node_reclaim_end(sc.nr_reclaimed);
 
