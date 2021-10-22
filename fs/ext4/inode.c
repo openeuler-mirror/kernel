56220a995c661f0429e54ae8771ab0a69711366d
Zhang Yi <yi.zhang@huawei.com>
2021-10-22 10:41:14 +0800

    ext4: correct the error path of ext4_write_inline_data_end()
    mainline inclusion
    from mainline-5.15-rc4
    commit 55ce2f649b9e88111270333a8127e23f4f8f42d7
    category: perf
    bugzilla: NA
    CVE: NA
    ---------------------------
    
    Current error path of ext4_write_inline_data_end() is not correct.
    
    Firstly, it should pass out the error value if ext4_get_inode_loc()
    return fail, or else it could trigger infinite loop if we inject error
    here. And then it's better to add inode to orphan list if it return fail
    in ext4_journal_stop(), otherwise we could not restore inline xattr
    entry after power failure. Finally, we need to reset the 'ret' value if
    ext4_write_inline_data_end() return success in ext4_write_end() and
    ext4_journalled_write_end(), otherwise we could not get the error return
    value of ext4_journal_stop().
    
    Signed-off-by: Zhang Yi <yi.zhang@huawei.com>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Link: https://lore.kernel.org/r/20210716122024.1105856-3-yi.zhang@huawei.com
    
    Conflicts:
    	fs/ext4/inode.c
    Reviewed-by: Yang Erkun <yangerkun@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -1416,6 +1416,7 @@ static int ext4_write_end(struct file *file,
 			goto errout;
 		}
 		copied = ret;
+		ret = 0;
 	} else
 		copied = block_write_end(file, mapping, pos,
 					 len, copied, page, fsdata);
@@ -1438,13 +1439,14 @@ static int ext4_write_end(struct file *file,
 	if (i_size_changed || inline_data)
 		ext4_mark_inode_dirty(handle, inode);
 
+errout:
 	if (pos + len > inode->i_size && ext4_can_truncate(inode))
 		/* if we have allocated more blocks and copied
 		 * less. We will have blocks allocated outside
 		 * inode->i_size. So truncate them
 		 */
 		ext4_orphan_add(handle, inode);
-errout:
+
 	ret2 = ext4_journal_stop(handle);
 	if (!ret)
 		ret = ret2;
@@ -1526,6 +1528,7 @@ static int ext4_journalled_write_end(struct file *file,
 			goto errout;
 		}
 		copied = ret;
+		ret = 0;
 	} else if (unlikely(copied < len) && !PageUptodate(page)) {
 		copied = 0;
 		ext4_journalled_zero_new_buffers(handle, page, from, to);
@@ -1554,6 +1557,7 @@ static int ext4_journalled_write_end(struct file *file,
 			ret = ret2;
 	}
 
+errout:
 	if (pos + len > inode->i_size && ext4_can_truncate(inode))
 		/* if we have allocated more blocks and copied
 		 * less. We will have blocks allocated outside
@@ -1561,7 +1565,6 @@ static int ext4_journalled_write_end(struct file *file,
 		 */
 		ext4_orphan_add(handle, inode);
 
-errout:
 	ret2 = ext4_journal_stop(handle);
 	if (!ret)
 		ret = ret2;
