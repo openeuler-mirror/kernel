661b972e802c8e252911361538651db906c084bb
Parav Pandit <parav@nvidia.com>
2021-10-26 20:55:13 +0300

    vdpa: Introduce query of device config layout
    mainline inclusion
    from mainline-v5.16-rc1
    commit ad69dd0bf26b88ec6ab26f8bbe5cd74fbed7672a
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I5WXCZ
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=ad69dd0bf26b88ec6ab26f8bbe5cd74fbed7672a
    
    ----------------------------------------------------------------------
    
    Introduce a command to query a device config layout.
    
    An example query of network vdpa device:
    
    $ vdpa dev add name bar mgmtdev vdpasim_net
    
    $ vdpa dev config show
    bar: mac 00:35:09:19:48:05 link up link_announce false mtu 1500
    
    $ vdpa dev config show -jp
    {
        "config": {
            "bar": {
                "mac": "00:35:09:19:48:05",
                "link ": "up",
                "link_announce ": false,
                "mtu": 1500,
            }
        }
    }
    
    Signed-off-by: Parav Pandit <parav@nvidia.com>
    Signed-off-by: Eli Cohen <elic@nvidia.com>
    Acked-by: Jason Wang <jasowang@redhat.com>
    Link: https://lore.kernel.org/r/20211026175519.87795-3-parav@nvidia.com
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Pengyuan Zhao <zhaopengyuan@hisilicon.com>

--- include/linux/vdpa.h
+++ include/linux/vdpa.h
@@ -64,6 +64,7 @@ struct vdpa_mgmt_dev;
  * @dma_dev: the actual device that is performing DMA
  * @driver_override: driver name to force a match
  * @config: the configuration ops for this device.
+ * @cf_mutex: Protects get and set access to configuration layout.
  * @index: device index
  * @features_valid: were features initialized? for legacy guests
  * @use_va: indicate whether virtual address must be used by this device
@@ -76,6 +77,7 @@ struct vdpa_device {
 	struct device *dma_dev;
 	const char *driver_override;
 	const struct vdpa_config_ops *config;
+	struct mutex cf_mutex; /* Protects get/set config */
 	unsigned int index;
 	bool features_valid;
 	bool use_va;
