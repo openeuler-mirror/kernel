571693cd3033282898f96df8fb6c8c44ead3a8a0
Hou Tao <houtao1@huawei.com>
2021-11-05 17:33:05 +0800

    Partially revert "xfs: let writable tracepoint enable to clear flag of f_mode"
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I4H3JT
    CVE: NA
    
    ---------------------------
    
    There are two issues with the current solution:
    1) tracepoint xfs_read_file is visible in tracefs
    It forms an ABI for userspace. It is bad because new field may be added
    into xfs_writable_file to export more information to userspace.
    
    2) tracepoint xfs_read_file is specific to xfs
    HDFS can be stacked on ext4.
    
    This reverts commit b1e9dddb580ac9d589d8dca9787235583f2baa21, but keep
    the definitions of FMODE_WILLNEED & FMODE_SPC_READAHEAD.
    
    Signed-off-by: Hou Tao <houtao1@huawei.com>
    Reviewed-by: Kuohai Xu <xukuohai@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- include/uapi/linux/xfs.h
+++ include/uapi/linux/xfs.h
@@ -7,11 +7,9 @@
 
 struct xfs_writable_file {
 	const unsigned char *name;
-	unsigned int clear_f_mode; /* can be cleared from file->f_mode */
 	unsigned int f_mode; /* can be set into file->f_mode */
 	long long i_size; /* file size */
-	long long prev_pos;  /* ra->prev_pos page index */
-	long long pos;  /* iocb->ki_pos page index */
+	long long prev_pos;  /* ra->prev_pos */
 };
 
 #endif /* _UAPI_LINUX_XFS_H */
