ec6b79f65f22f4cf73fb8488b300b39bce63ae0b
Xu Qiang <xuqiang36@huawei.com>
2021-11-26 16:27:12 +0800

    watchdog/corelockup: Optimized core lockup detection judgment rules
    ascend inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I4F3V1
    CVE: NA
    
    --------------------------------
    
    Optimized core lockup detection judgment rules to
    make it easier to understand.
    
    Core suspension detection is performed in the hrtimer
    interrupt processing function. The detection condition
    is that the hrtimer interrupt and NMI interrupt are not
    updated for multiple consecutive times.
    
    Signed-off-by: Xu Qiang <xuqiang36@huawei.com>
    Reviewed-by: Ding Tianhong <dingtianhong@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
    Reviewed-by: Ding Tianhong <dingtianhong@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- arch/arm64/include/asm/barrier.h
+++ arch/arm64/include/asm/barrier.h
@@ -15,23 +15,8 @@
 #define nops(n)		asm volatile(__nops(n))
 
 #define sev()		asm volatile("sev" : : : "memory")
-#ifdef CONFIG_CORELOCKUP_DETECTOR
-extern unsigned int close_wfi_wfe;
-#define wfe()							\
-	do {							\
-		if (likely(close_wfi_wfe == 0))		\
-			asm volatile("wfe" : : : "memory");	\
-	} while (0)
-#define wfi()							\
-	do {							\
-		if (likely(close_wfi_wfe == 0))		\
-			asm volatile("wfi" : : : "memory");	\
-	} while (0)
-
-#else
 #define wfe()		asm volatile("wfe" : : : "memory")
 #define wfi()		asm volatile("wfi" : : : "memory")
-#endif
 
 #define isb()		asm volatile("isb" : : : "memory")
 #define dmb(opt)	asm volatile("dmb " #opt : : : "memory")
