20894dfeb3aa3a7a4b06b91e68d95146b135e236
Miaohe Lin <linmiaohe@huawei.com>
2021-11-30 16:33:41 +0800

    mm, memcg: remove unused functions
    mainline inclusion
    from mainline-v5.15-rc1
    commit bec49c067c679e9b7ca7c1aac50b56618c12d879
    category: feature
    bugzilla: 185803 https://gitee.com/openeuler/kernel/issues/I4JOG9?from=project-issue
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=bec49c067c67
    
    -----------------------------------------------------------------------
    
    Since commit 2d146aa3aa84 ("mm: memcontrol: switch to rstat"), last user
    of memcg_stat_item_in_bytes() is gone.  And since commit fa40d1ee9f15
    ("mm: vmscan: memcontrol: remove mem_cgroup_select_victim_node()"), only
    the declaration of mem_cgroup_select_victim_node() is remained here.
    Remove them.
    
    Link: https://lkml.kernel.org/r/20210807082835.61281-2-linmiaohe@huawei.com
    Signed-off-by: Miaohe Lin <linmiaohe@huawei.com>
    Reviewed-by: Shakeel Butt <shakeelb@google.com>
    Reviewed-by: Muchun Song <songmuchun@bytedance.com>
    Acked-by: Roman Gushchin <guro@fb.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Cc: Alex Shi <alexs@kernel.org>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Vladimir Davydov <vdavydov.dev@gmail.com>
    Cc: Wei Yang <richard.weiyang@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Lu Jialin <lujialin4@huawei.com>
    Reviewed-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -638,13 +638,6 @@ static inline bool set_page_objcgs(struct page *page,
 }
 #endif
 
-static __always_inline bool memcg_stat_item_in_bytes(int idx)
-{
-	if (idx == MEMCG_PERCPU_B)
-		return true;
-	return vmstat_item_in_bytes(idx);
-}
-
 static inline bool mem_cgroup_is_root(struct mem_cgroup *memcg)
 {
 	return (memcg == root_mem_cgroup);
@@ -929,11 +922,6 @@ static inline bool mem_cgroup_online(struct mem_cgroup *memcg)
 		return true;
 	return !!(memcg->css.flags & CSS_ONLINE);
 }
-
-/*
- * For memory reclaim.
- */
-int mem_cgroup_select_victim_node(struct mem_cgroup *memcg);
 
 void mem_cgroup_update_lru_size(struct lruvec *lruvec, enum lru_list lru,
 		int zid, int nr_pages);
