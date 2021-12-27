38b4f0ee2c7220ccf1d3e587fa4a8b59b9be3110
Darrick J. Wong <djwong@kernel.org>
2021-12-27 19:42:31 +0800

    xfs: fix scrub and remount-ro protection when running scrub
    mainline-inclusion
    from mainline-v5.12-rc4
    commit 71bddbccab436a261a22afe5d90de269941d0fe7
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I4KIAO
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=71bddbccab436a261a22afe5d90de269941d0fe7
    
    -------------------------------------------------
    
    While running a new fstest that races a readonly remount with scrub
    running in repair mode, I observed the kernel tripping over debugging
    assertions in the log quiesce code that were checking that the CIL was
    empty.  When the sysadmin runs scrub in repair mode, the scrub code
    allocates real transactions (with reservations) to change things, but
    doesn't increment the superblock writers count to block a readonly
    remount attempt while it is running.
    
    We don't require the userspace caller to have a writable file descriptor
    to run repairs, so we have to call mnt_want_write_file to obtain freeze
    protection and increment the writers count.  It's ok to remove the call
    to sb_start_write for the dry-run case because commit 8321ddb2fa29
    removed the behavior where scrub and fsfreeze fight over the buffer LRU.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Chandan Babu R <chandanrlinux@gmail.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Guo Xuenan <guoxuenan@huawei.com>
    Reviewed-by: Lihong Kou <koulihong@huawei.com>
    Reviewed-by: Zhang Yi <yi.zhang@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- fs/xfs/xfs_ioctl.c
+++ fs/xfs/xfs_ioctl.c
@@ -1848,7 +1848,7 @@ out_free:
 
 STATIC int
 xfs_ioc_scrub_metadata(
-	struct xfs_inode		*ip,
+	struct file			*file,
 	void				__user *arg)
 {
 	struct xfs_scrub_metadata	scrub;
@@ -1860,7 +1860,7 @@ xfs_ioc_scrub_metadata(
 	if (copy_from_user(&scrub, arg, sizeof(scrub)))
 		return -EFAULT;
 
-	error = xfs_scrub_metadata(ip, &scrub);
+	error = xfs_scrub_metadata(file, &scrub);
 	if (error)
 		return error;
 
@@ -2159,7 +2159,7 @@ xfs_file_ioctl(
 		return xfs_ioc_getfsmap(ip, arg);
 
 	case XFS_IOC_SCRUB_METADATA:
-		return xfs_ioc_scrub_metadata(ip, arg);
+		return xfs_ioc_scrub_metadata(filp, arg);
 
 	case XFS_IOC_FD_TO_HANDLE:
 	case XFS_IOC_PATH_TO_HANDLE:
