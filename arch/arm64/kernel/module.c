01ae8a2c9482981c81096028399e08991a8fbb4c
Kefeng Wang <wangkefeng.wang@huawei.com>
2021-12-30 16:26:05 +0800

    arm64: Support huge vmalloc mappings
    maillist inclusion
    category: Feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I4NDAW
    CVE: NA
    
    Reference: https://lore.kernel.org/lkml/20211226083912.166512-4-wangkefeng.wang@huawei.com/t/
    
    -------------------
    
    This patch select HAVE_ARCH_HUGE_VMALLOC to let arm64 support huge
    vmalloc mappings, it is disabled by default, use hugevmalloc=on to
    enable it in some scenarios.
    
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Signed-off-by: Wang Wensheng <wangwensheng4@huawei.com>
    Reviewed-by: Weilong Chen <chenweilong@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- arch/arm64/kernel/module.c
+++ arch/arm64/kernel/module.c
@@ -35,7 +35,7 @@ void *module_alloc(unsigned long size)
 		module_alloc_end = MODULES_END;
 
 	p = __vmalloc_node_range(size, MODULE_ALIGN, module_alloc_base,
-				module_alloc_end, gfp_mask, PAGE_KERNEL, 0,
+				module_alloc_end, gfp_mask, PAGE_KERNEL, VM_NO_HUGE_VMAP,
 				NUMA_NO_NODE, __builtin_return_address(0));
 
 	if (!p && IS_ENABLED(CONFIG_ARM64_MODULE_PLTS) &&
@@ -51,7 +51,7 @@ void *module_alloc(unsigned long size)
 		 */
 		p = __vmalloc_node_range(size, MODULE_ALIGN, module_alloc_base,
 				module_alloc_base + SZ_2G, GFP_KERNEL,
-				PAGE_KERNEL, 0, NUMA_NO_NODE,
+				PAGE_KERNEL, VM_NO_HUGE_VMAP, NUMA_NO_NODE,
 				__builtin_return_address(0));
 
 	if (p && (kasan_module_alloc(p, size) < 0)) {
