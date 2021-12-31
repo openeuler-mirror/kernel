bdae998b460e62beac0bf0c801dfcf5e8224dc5c
Wang ShaoBo <bobo.shaobowang@huawei.com>
2021-12-31 15:59:45 +0800

    kabi: reserve space for struct module
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I4KFY7?from=project-issue
    CVE: NA
    
    -------------------------------
    
    Reserve space for struct module.
    
    Signed-off-by: Wang ShaoBo <bobo.shaobowang@huawei.com>
    Reviewed-by: Cheng Jian <cj.chengjian@huawei.com>
    Reviewed-by: Cheng Jian <cj.chengjian@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- include/linux/module.h
+++ include/linux/module.h
@@ -553,6 +553,10 @@ struct module {
 	struct error_injection_entry *ei_funcs;
 	unsigned int num_ei_funcs;
 #endif
+	KABI_RESERVE(1)
+	KABI_RESERVE(2)
+	KABI_RESERVE(3)
+	KABI_RESERVE(4)
 } ____cacheline_aligned __randomize_layout;
 #ifndef MODULE_ARCH_INIT
 #define MODULE_ARCH_INIT {}
