4b380022f7e2593ac1915102b68af3a66e4ac9bd
Thomas Gleixner <tglx@linutronix.de>
2022-01-05 04:35:12 -0800

    x86/fpu: Extend fpu_xstate_prctl() with guest permissions
    mainline inclusion
    from mainline-v5.17-rc1
    commit 980fe2fddcff21937c93532b4597c8ea450346c1
    category: feature
    bugzilla: https://gitee.com/openeuler/intel-kernel/issues/I5RQLJ
    CVE: NA
    
    Intel-SIG: commit 980fe2fddcff x86/fpu: Extend fpu_xstate_prctl() with guest permissions.
    
    --------------------------------
    
    KVM requires a clear separation of host user space and guest permissions
    for dynamic XSTATE components.
    
    Add a guest permissions member to struct fpu and a separate set of prctl()
    arguments: ARCH_GET_XCOMP_GUEST_PERM and ARCH_REQ_XCOMP_GUEST_PERM.
    
    The semantics are equivalent to the host user space permission control
    except for the following constraints:
    
      1) Permissions have to be requested before the first vCPU is created
    
      2) Permissions are frozen when the first vCPU is created to ensure
         consistency. Any attempt to expand permissions via the prctl() after
         that point is rejected.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Jing Liu <jing2.liu@intel.com>
    Signed-off-by: Yang Zhong <yang.zhong@intel.com>
    Message-Id: <20220105123532.12586-2-yang.zhong@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Lin Wang <lin.x.wang@intel.com>

--- arch/x86/include/asm/fpu/types.h
+++ arch/x86/include/asm/fpu/types.h
@@ -389,6 +389,8 @@ struct fpstate {
 	/* @regs is dynamically sized! Don't add anything after @regs! */
 } __aligned(64);
 
+#define FPU_GUEST_PERM_LOCKED		BIT_ULL(63)
+
 struct fpu_state_perm {
 	/*
 	 * @__state_perm:
@@ -477,6 +479,13 @@ struct fpu {
 	 * Permission related information
 	 */
 	KABI_EXTEND(struct fpu_state_perm perm)
+
+	/*
+	 * @guest_perm:
+	 *
+	 * Permission related information for guest pseudo FPUs
+	 */
+	struct fpu_state_perm           guest_perm;
 
 	/*
 	 * @__fpstate:
