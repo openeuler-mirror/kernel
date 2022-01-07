dae7bed961c55d9837eada7f98f34f1adb0e9d21
Wei Yongjun <weiyongjun1@huawei.com>
2022-01-07 23:07:17 +0800

    tcp_comp: add sysctl for enable/disable compression
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I4PNEK
    CVE: NA
    
    -------------------------------------------------
    
    Add sysctl interface for enable/disable tcp compression by ports.
    
    Example:
    
    $ echo 4000 > /proc/sys/net/ipv4/tcp_compression_ports
    will enable port 4000 for tcp compression
    
    $ echo 4000,5000 > /proc/sys/net/ipv4/tcp_compression_ports
    will enable both port 4000 and 5000 for tcp compression
    
    $ echo > /proc/sys/net/ipv4/tcp_compression_ports
    will disable tcp compression.
    
    Signed-off-by: Wei Yongjun <weiyongjun1@huawei.com>
    Signed-off-by: Wang Yufen <wangyufen@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
    Signed-off-by: Lu Wei <luwei32@huawei.com>
    Reviewed-by: Wei Yongjun <weiyongjun1@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- net/ipv4/tcp_comp.c
+++ net/ipv4/tcp_comp.c
@@ -7,6 +7,10 @@
 
 #include <net/tcp.h>
 
+static unsigned long tcp_compression_ports[65536 / 8];
+
+unsigned long *sysctl_tcp_compression_ports = tcp_compression_ports;
+
 bool tcp_syn_comp_enabled(const struct tcp_sock *tp)
 {
 	return true;
