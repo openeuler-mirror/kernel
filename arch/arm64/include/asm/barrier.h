58eed3234c5c6e79d9cffaa6831bea383d7c2997
Xiongfeng Wang <wangxiongfeng2@huawei.com>
2022-01-17 21:07:57 +0800

    asm-generic: introduce io_stop_wc() and add implementation for ARM64
    mainline inclusion
    from mainline-v5.16-rc3
    commit d5624bb29f49b849ac8d1e9783dbf9c65cf33457
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I4QKQ3
    CVE: NA
    
    ----------------------
    
    For memory accesses with write-combining attributes (e.g. those returned
    by ioremap_wc()), the CPU may wait for prior accesses to be merged with
    subsequent ones. But in some situation, such wait is bad for the
    performance.
    
    We introduce io_stop_wc() to prevent the merging of write-combining
    memory accesses before this macro with those after it.
    
    We add implementation for ARM64 using DGH instruction and provide NOP
    implementation for other architectures.
    
    Signed-off-by: Xiongfeng Wang <wangxiongfeng2@huawei.com>
    Suggested-by: Will Deacon <will@kernel.org>
    Suggested-by: Catalin Marinas <catalin.marinas@arm.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Link: https://lore.kernel.org/r/20211221035556.60346-1-wangxiongfeng2@huawei.com
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    
     Conflicts:
    	arch/arm64/include/asm/barrier.h
    Signed-off-by: Xiongfeng Wang <wangxiongfeng2@huawei.com>
    Reviewed-by: Hanjun Guo <guohanjun@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- arch/arm64/include/asm/barrier.h
+++ arch/arm64/include/asm/barrier.h
@@ -25,6 +25,14 @@
 #define psb_csync()	asm volatile("hint #17" : : : "memory")
 #define csdb()		asm volatile("hint #20" : : : "memory")
 
+/*
+ * Data Gathering Hint:
+ * This instruction prevents merging memory accesses with Normal-NC or
+ * Device-GRE attributes before the hint instruction with any memory accesses
+ * appearing after the hint instruction.
+ */
+#define dgh()		asm volatile("hint #6" : : : "memory")
+
 #define spec_bar()	asm volatile(ALTERNATIVE("dsb nsh\nisb\n",		\
 						 SB_BARRIER_INSN"nop\n",	\
 						 ARM64_HAS_SB))
@@ -48,6 +56,8 @@
 #define dma_mb()	dmb(osh)
 #define dma_rmb()	dmb(oshld)
 #define dma_wmb()	dmb(oshst)
+
+#define io_stop_wc()	dgh()
 
 /*
  * Generate a mask for array_index__nospec() that is ~0UL when 0 <= idx < sz
