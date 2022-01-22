openeuler-mirror/kernel

commit 6ab918569ad40a0853765b649514b31e480b054b

watchdog: Fix check_preemption_disabled() error
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/I4JUZZ

-------------------------------------------------

When enabling CONFIG_DEBUG_PREEMPT and CONFIG_PREEMPT, it triggers a 'BUG'
in the pmu based nmi_watchdog initializaion:

[    3.341853] BUG: using smp_processor_id() in preemptible [00000000] code: swapper/0/1
[    3.344392] caller is debug_smp_processor_id+0x17/0x20
[    3.344395] CPU: 1 PID: 1 Comm: swapper/0 Not tainted 5.10.0+ #398
[    3.344397] Hardware name: QEMU Standard PC (i440FX + PIIX, 1996), BIOS rel-1.10.2-0-g5f4c7b1-prebuilt.qemu-project.org 04/01/2014
[    3.344399] Call Trace:
[    3.344410]  dump_stack+0x60/0x76
[    3.344412]  check_preemption_disabled+0xba/0xc0
[    3.344415]  debug_smp_processor_id+0x17/0x20
[    3.344422]  hardlockup_detector_event_create+0xf/0x60
[    3.344427]  hardlockup_detector_perf_init+0xf/0x41
[    3.344430]  watchdog_nmi_probe+0xe/0x10
[    3.344432]  lockup_detector_init+0x22/0x5b
[    3.344437]  kernel_init_freeable+0x20c/0x245
[    3.344439]  ? rest_init+0xd0/0xd0
[    3.344441]  kernel_init+0xe/0x110
[    3.344446]  ret_from_fork+0x22/0x30

This issue was introduced by commit a79050434b45, which move down
lockup_detector_init() after do_basic_setup(), after sched_init_smp() too.

  hardlockup_detector_event_create
    |- hardlockup_detector_perf_init	(unsafe)
      |- watchdog_nmi_probe
        |- lockup_detector_init
    |- hardlockup_detector_perf_enable
      |- watchdog_nmi_enable
        |- watchdog_enable
          |- lockup_detector_online_cpu
          |- softlockup_start_fn
            |- softlockup_start_all
              |- lockup_detector_reconfigure
                |- lockup_detector_setup
                  |- lockup_detector_init

After analysing the calling context, it's only unsafe to use
smp_processor_id() in hardlockup_detector_perf_init() as the thread
'kernel_init' is preemptible after sched_init_smp().

kernel/watchdog_hld.c
@@ -504,14 +504,17 @@ void __init hardlockup_detector_perf_restart(void)
  */
 int __init hardlockup_detector_perf_init(void)
 {
-	int ret = hardlockup_detector_event_create();
+	int ret;
 
+	preempt_disable();
+	ret = hardlockup_detector_event_create();
 	if (ret) {
 		pr_info("Perf NMI watchdog permanently disabled\n");
 	} else {
 		perf_event_release_kernel(this_cpu_read(watchdog_ev));
 		this_cpu_write(watchdog_ev, NULL);
 	}
+	preempt_enable();
 	return ret;
 }
 #endif /* CONFIG_HARDLOCKUP_DETECTOR_PERF */

commit 364de98eacea8dbfe4f926f62085aebf8534f403

watchdog: Fix sleeping function called from atomic context
hulk inclusion
category: bugfix
bugzilla: 173968, https://gitee.com/openeuler/kernel/issues/I3J87Y
CVE: NA

-------------------------------------------------

In '6ab918569ad4 ("watchdog: Fix check_preemption_disabled() error")', we
tried to fix check_preemption_disabled() error by disabling preemption in
hardlockup_detector_perf_init(), but missed that function
perf_event_create_kernel_counter() may sleep.

To fix the issue fully, reimplement hardlockup_detector_perf_init()
through smp_call_on_cpu() instead of disabling preemption.

Fixes: 6ab918569ad4 ("watchdog: Fix check_preemption_disabled() error")
Signed-off-by: Wei Li <liwei391@huawei.com>
Reviewed-by: Cheng Jian <cj.chengjian@huawei.com>
Reviewed-by: Xiongfeng Wang <wangxiongfeng2@huawei.com>
Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

kernel/watchdog_hld.c
@@ -499,22 +499,25 @@ void __init hardlockup_detector_perf_restart(void)
 	}
 }
 
-/**
- * hardlockup_detector_perf_init - Probe whether NMI event is available at all
- */
-int __init hardlockup_detector_perf_init(void)
+int __init __hardlockup_detector_perf_init(void *not_used)
 {
-	int ret;
+	int ret = hardlockup_detector_event_create();
 
-	preempt_disable();
-	ret = hardlockup_detector_event_create();
 	if (ret) {
 		pr_info("Perf NMI watchdog permanently disabled\n");
 	} else {
 		perf_event_release_kernel(this_cpu_read(watchdog_ev));
 		this_cpu_write(watchdog_ev, NULL);
 	}
-	preempt_enable();
 	return ret;
+}
+
+/**
+ * hardlockup_detector_perf_init - Probe whether NMI event is available at all
+ */
+int __init hardlockup_detector_perf_init(void)
+{
+	return smp_call_on_cpu(get_boot_cpu_id(),
+				__hardlockup_detector_perf_init, NULL, false);
 }
 #endif /* CONFIG_HARDLOCKUP_DETECTOR_PERF */