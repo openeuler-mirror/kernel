259a29968d432f39a74fbf092831bc41e76b37ed
Paolo Bonzini <pbonzini@redhat.com>
2022-02-04 04:12:31 -0500

    KVM: x86/mmu: load new PGD after the shadow MMU is initialized
    mainline inclusion
    from mainline-v5.18-rc1
    commit 3cffc89d9d0795a74ee1b1027bba2f6099b51c33
    category: feature
    bugzilla: https://gitee.com/openeuler/intel-kernel/issues/I9HH9U
    CVE: N/A
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=3cffc89d9d0795a74ee1b1027bba2f6099b51c33
    
    -------------------------------------
    
    Now that __kvm_mmu_new_pgd does not look at the MMU's root_level and
    shadow_root_level anymore, pull the PGD load after the initialization of
    the shadow MMUs.
    
    Besides being more intuitive, this enables future simplifications
    and optimizations because it's not necessary anymore to compute the
    role outside kvm_init_mmu.  In particular, kvm_mmu_reset_context was not
    attempting to use a cached PGD to avoid having to figure out the new role.
    With this change, it could follow what nested_{vmx,svm}_load_cr3 are doing,
    and avoid unloading all the cached roots.
    
    Reviewed-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    
    Conflicts:
    	arch/x86/kvm/mmu/mmu.c
    Signed-off-by: Yu Zhang <yu.c.zhang@linux.intel.com>

--- arch/x86/kvm/vmx/nested.c
+++ arch/x86/kvm/vmx/nested.c
@@ -1096,14 +1096,14 @@ static int nested_vmx_load_cr3(struct kvm_vcpu *vcpu, unsigned long cr3, bool ne
 		}
 	}
 
-	if (!nested_ept)
-		kvm_mmu_new_pgd(vcpu, cr3);
-
 	vcpu->arch.cr3 = cr3;
 	kvm_register_mark_available(vcpu, VCPU_EXREG_CR3);
 
 	/* Re-initialize the MMU, e.g. to pick up CR4 MMU role changes. */
 	kvm_init_mmu(vcpu);
+
+	if (!nested_ept)
+		kvm_mmu_new_pgd(vcpu, cr3);
 
 	return 0;
 }
