bae092b409da9c9bd1645cc9e7d997f407f5ac16
Gu Zitao <guzitao@wxiat.com>
2022-02-22 22:12:47 +0800

    tools uapi: fix sw64 support
    Sunway inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I4SPZD
    CVE: NA
    
    -------------------------------
    
    Signed-off-by: Gu Zitao <guzitao@wxiat.com> #openEuler_contributor
    Signed-off-by: Laibin Qiu <qiulaibin@huawei.com>
    Reviewed-by: Hanjun Guo <guohanjun@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- tools/include/uapi/asm/bitsperlong.h
+++ tools/include/uapi/asm/bitsperlong.h
@@ -17,6 +17,8 @@
 #include "../../../arch/riscv/include/uapi/asm/bitsperlong.h"
 #elif defined(__alpha__)
 #include "../../../arch/alpha/include/uapi/asm/bitsperlong.h"
+#elif defined(__sw_64__)
+#include "../../../arch/sw_64/include/uapi/asm/bitsperlong.h"
 #else
 #include <asm-generic/bitsperlong.h>
 #endif
