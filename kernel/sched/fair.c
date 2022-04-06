85d48ae6821f56e583fc5bc654be4620977723b8
zgpeng <zgpeng.linux@gmail.com>
2022-04-06 17:57:05 +0800

    sched/fair: Move calculate of avg_load to a better location
    stable inclusion
    from stable-v5.10.178
    commit 68387ae3b63e7bd1723767ce1bc2186e82490208
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8ALH3
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=68387ae3b63e7bd1723767ce1bc2186e82490208
    
    --------------------------------
    
    [ Upstream commit 06354900787f25bf5be3c07a68e3cdbc5bf0fa69 ]
    
    In calculate_imbalance function, when the value of local->avg_load is
    greater than or equal to busiest->avg_load, the calculated sds->avg_load is
    not used. So this calculation can be placed in a more appropriate position.
    
    Signed-off-by: zgpeng <zgpeng@tencent.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Samuel Liao <samuelliao@tencent.com>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lore.kernel.org/r/1649239025-10010-1-git-send-email-zgpeng@tencent.com
    Stable-dep-of: 91dcf1e8068e ("sched/fair: Fix imbalance overflow")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: sanglipeng <sanglipeng1@jd.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -10759,8 +10759,6 @@ static inline void calculate_imbalance(struct lb_env *env, struct sd_lb_stats *s
 		local->avg_load = (local->group_load * SCHED_CAPACITY_SCALE) /
 				  local->group_capacity;
 
-		sds->avg_load = (sds->total_load * SCHED_CAPACITY_SCALE) /
-				sds->total_capacity;
 		/*
 		 * If the local group is more loaded than the selected
 		 * busiest group don't try to pull any tasks.
@@ -10769,6 +10767,9 @@ static inline void calculate_imbalance(struct lb_env *env, struct sd_lb_stats *s
 			env->imbalance = 0;
 			return;
 		}
+
+		sds->avg_load = (sds->total_load * SCHED_CAPACITY_SCALE) /
+				sds->total_capacity;
 	}
 
 	/*
