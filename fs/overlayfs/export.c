c1662fc5abe162b5d73e11cc524af8fa26e42680
Miklos Szeredi <mszeredi@redhat.com>
2022-04-14 09:33:38 +0000

    ovl: fix uninitialized pointer read in ovl_lookup_real_one()
    mainline inclusion
    from mainline-v5.14-rc6
    commit 580c610429b3994e8db24418927747cf28443cde
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I51PNB
    CVE: NA
    
    --------------------------------
    
    One error path can result in release_dentry_name_snapshot() being called
    before "name" was initialized by take_dentry_name_snapshot().
    
    Fix by moving the release_dentry_name_snapshot() to immediately after the
    only use.
    
    Reported-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    
    conflicts:
    	fs/overlayfs/export.c
    Signed-off-by: Zheng Liang <zhengliang6@huawei.com>
    Reviewed-by: Zhang Yi <yi.zhang@huawei.com>
    Signed-off-by: Yongqiang Liu <liuyongqiang13@huawei.com>

--- fs/overlayfs/export.c
+++ fs/overlayfs/export.c
@@ -398,6 +398,7 @@ static struct dentry *ovl_lookup_real_one(struct dentry *connected,
 	 */
 	take_dentry_name_snapshot(&name, real);
 	this = lookup_one_len(name.name, connected, strlen(name.name));
+	release_dentry_name_snapshot(&name);
 	err = PTR_ERR(this);
 	if (IS_ERR(this)) {
 		goto fail;
@@ -412,7 +413,6 @@ static struct dentry *ovl_lookup_real_one(struct dentry *connected,
 	}
 
 out:
-	release_dentry_name_snapshot(&name);
 	dput(parent);
 	inode_unlock(dir);
 	return this;
