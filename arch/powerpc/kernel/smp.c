4bf5123d17eb83901b80e02bf2638ba3dcaaeeac
Michael Ellerman <mpe@ellerman.id.au>
2022-04-27 11:15:37 +0800

    powerpc/smp: Move setup_profiling_timer() under CONFIG_PROFILING
    stable inclusion
    from stable-v5.10.94
    commit 4b25aad655c92d4d99aaf733f7e128852e05069c
    bugzilla: https://gitee.com/openeuler/kernel/issues/I531X9
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=4b25aad655c92d4d99aaf733f7e128852e05069c
    
    --------------------------------
    
    [ Upstream commit a4ac0d249a5db80e79d573db9e4ad29354b643a8 ]
    
    setup_profiling_timer() is only needed when CONFIG_PROFILING is enabled.
    
    Fixes the following W=1 warning when CONFIG_PROFILING=n:
      linux/arch/powerpc/kernel/smp.c:1638:5: error: no previous prototype for ‘setup_profiling_timer’
    
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20211124093254.1054750-5-mpe@ellerman.id.au
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>
    Acked-by: Xie XiuQi <xiexiuqi@huawei.com>

--- arch/powerpc/kernel/smp.c
+++ arch/powerpc/kernel/smp.c
@@ -1491,10 +1491,12 @@ void start_secondary(void *unused)
 	BUG();
 }
 
+#ifdef CONFIG_PROFILING
 int setup_profiling_timer(unsigned int multiplier)
 {
 	return 0;
 }
+#endif
 
 static void fixup_topology(void)
 {
