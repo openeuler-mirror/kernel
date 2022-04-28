bce9feeb7eedb4d373c5cef5cb066d1d2110f40b
Yajun Deng <yajun.deng@linux.dev>
2022-04-28 15:52:41 +0800

    netdevice: add the case if dev is NULL
    stable inclusion
    from stable-v5.10.110
    commit ff919a7ad90c361634f0bc5c58120252d87b887a
    bugzilla: https://gitee.com/src-openeuler/kernel/issues/I51YBN
    CVE: CVE-2022-28356
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?ff919a7ad90c361634f0bc5c58120252d87b887a
    
    --------------------------------
    
    commit b37a466837393af72fe8bcb8f1436410f3f173f3 upstream.
    
    Add the case if dev is NULL in dev_{put, hold}, so the caller doesn't
    need to care whether dev is NULL or not.
    
    Signed-off-by: Yajun Deng <yajun.deng@linux.dev>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Cc: Pavel Machek <pavel@denx.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Huang Guobin <huangguobin4@huawei.com>
    Reviewed-by: Wei Yongjun <weiyongjun1@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -4033,7 +4033,8 @@ void netdev_run_todo(void);
  */
 static inline void dev_put(struct net_device *dev)
 {
-	this_cpu_dec(*dev->pcpu_refcnt);
+	if (dev)
+		this_cpu_dec(*dev->pcpu_refcnt);
 }
 
 /**
@@ -4044,7 +4045,8 @@ static inline void dev_put(struct net_device *dev)
  */
 static inline void dev_hold(struct net_device *dev)
 {
-	this_cpu_inc(*dev->pcpu_refcnt);
+	if (dev)
+		this_cpu_inc(*dev->pcpu_refcnt);
 }
 
 /* Carrier loss detection, dial on demand. The functions netif_carrier_on
