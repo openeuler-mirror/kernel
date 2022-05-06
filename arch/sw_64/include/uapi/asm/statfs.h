65d722507cd65bbf42f9c00d92c7016bc92968bf
He Sheng <hesheng@wxiat.com>
2022-05-06 09:28:36 +0800

    sw64: uapi: generate some uapi headers from generic ones
    Sunway inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I56OLG
    
    --------------------------------
    
    These uapi headers are proved to be the same with generic version.
    
    Herein we remove the arch-specific statfs.h and types.h to end
    support for old system.
    
    Signed-off-by: He Sheng <hesheng@wxiat.com>
    
    Signed-off-by: Gu Zitao <guzitao@wxiat.com>

--- arch/sw_64/include/uapi/asm/statfs.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
-#ifndef _UAPI_ASM_SW64_STATFS_H
-#define _UAPI_ASM_SW64_STATFS_H
-
-#include <linux/types.h>
-
-#include <asm-generic/statfs.h>
-
-#endif
