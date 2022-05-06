211af39aab2e322ad14fd7727977946b29fff3ba
He Sheng <hesheng@wxiat.com>
2022-05-06 11:09:36 +0800

    sw64: kapi: include generic modules.h
    Sunway inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I56OLG
    
    --------------------------------
    
    This patch removes replicated macros which have been defined in
    generic version.
    
    Signed-off-by: He Sheng <hesheng@wxiat.com>
    
    Signed-off-by: Gu Zitao <guzitao@wxiat.com>

--- arch/sw_64/include/asm/module.h
+++ arch/sw_64/include/asm/module.h
@@ -2,17 +2,11 @@
 #ifndef _ASM_SW64_MODULE_H
 #define _ASM_SW64_MODULE_H
 
+#include <asm-generic/module.h>
+
 struct mod_arch_specific {
 	unsigned int gotsecindex;
 };
-
-#define Elf_Sym		Elf64_Sym
-#define Elf_Shdr	Elf64_Shdr
-#define Elf_Ehdr	Elf64_Ehdr
-#define Elf_Phdr	Elf64_Phdr
-#define Elf_Dyn		Elf64_Dyn
-#define Elf_Rel		Elf64_Rel
-#define Elf_Rela	Elf64_Rela
 
 #define ARCH_SHF_SMALL	SHF_SW64_GPREL
 
