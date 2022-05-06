927e8e16d604582a095d7c0ae9714f8050f0205d
He Sheng <hesheng@wxiat.com>
2022-05-06 10:00:14 +0800

    sw64: kapi: generate some kapi headers from generic ones
    Sunway inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I56OLG
    
    --------------------------------
    
    These kapi headers are proved to be the same with generic version.
    
    Signed-off-by: He Sheng <hesheng@wxiat.com>
    
    Signed-off-by: Gu Zitao <guzitao@wxiat.com>

--- arch/sw_64/include/asm/irq_regs.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef _ASM_SW64_IRQ_REGS_H
-#define _ASM_SW64_IRQ_REGS_H
-
-#include <asm-generic/irq_regs.h>
-
-#endif
--- arch/sw_64/include/asm/shmparam.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef _ASM_SW64_SHMPARAM_H
-#define _ASM_SW64_SHMPARAM_H
-
-#define	SHMLBA PAGE_SIZE		 /* attach addr a multiple of this */
-
-#endif /* _ASM_SW64_SHMPARAM_H */
