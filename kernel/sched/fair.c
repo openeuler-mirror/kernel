42f42feeaae6ba1017954f8fcb63b9feed56aa7e
Guan Jing <guanjing6@huawei.com>
2022-05-10 23:04:57 +0800

    sched: Add statistics for qos smt expeller
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I52611
    CVE: NA
    
    --------------------------------
    
    We have added two statistics for qos smt expeller:
    a) nr_qos_smt_send_ipi:the times of ipi which online task expel offline tasks;
    b) nr_qos_smt_expelled:the statistics that offline task will not be picked times.
    
    Signed-off-by: Guan Jing <guanjing6@huawei.com>
    Reviewed-by: Chen Hui <judy.chenhui@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7455,6 +7455,7 @@ static void qos_smt_send_ipi(int this_cpu)
 		    rq->cfs.h_nr_running == 0)
 			continue;
 
+		schedstat_inc(current->se.statistics.nr_qos_smt_send_ipi);
 		smp_send_reschedule(cpu);
 	}
 }
@@ -7526,6 +7527,7 @@ again:
 #ifdef CONFIG_QOS_SCHED_SMT_EXPELLER
 	if (qos_smt_expelled(this_cpu)) {
 		__this_cpu_write(qos_smt_status, QOS_LEVEL_OFFLINE);
+		schedstat_inc(rq->curr->se.statistics.nr_qos_smt_expelled);
 		return NULL;
 	}
 #endif
