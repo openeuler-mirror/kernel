9f66c5ccc4b727e295469e0ee105aae644bd0dd6
James Clark <james.clark@arm.com>
2022-05-18 18:14:06 +0800

    perf: Always wake the parent event
    stable inclusion
    from stable-v5.10.101
    commit d0774cf730564a4306c0ef47f8966dfd4443435c
    bugzilla: https://gitee.com/openeuler/kernel/issues/I5669Z
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=d0774cf730564a4306c0ef47f8966dfd4443435c
    
    --------------------------------
    
    [ Upstream commit 961c39121759ad09a89598ec4ccdd34ae0468a19 ]
    
    When using per-process mode and event inheritance is set to true,
    forked processes will create a new perf events via inherit_event() ->
    perf_event_alloc(). But these events will not have ring buffers
    assigned to them. Any call to wakeup will be dropped if it's called on
    an event with no ring buffer assigned because that's the object that
    holds the wakeup list.
    
    If the child event is disabled due to a call to
    perf_aux_output_begin() or perf_aux_output_end(), the wakeup is
    dropped leaving userspace hanging forever on the poll.
    
    Normally the event is explicitly re-enabled by userspace after it
    wakes up to read the aux data, but in this case it does not get woken
    up so the event remains disabled.
    
    This can be reproduced when using Arm SPE and 'stress' which forks once
    before running the workload. By looking at the list of aux buffers read,
    it's apparent that they stop after the fork:
    
      perf record -e arm_spe// -vvv -- stress -c 1
    
    With this patch applied they continue to be printed. This behaviour
    doesn't happen when using systemwide or per-cpu mode.
    
    Reported-by: Ruben Ayrapetyan <Ruben.Ayrapetyan@arm.com>
    Signed-off-by: James Clark <james.clark@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20211206113840.130802-2-james.clark@arm.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Yu Liao <liaoyu15@huawei.com>
    Reviewed-by: Wei Li <liwei391@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -5830,6 +5830,8 @@ static void ring_buffer_attach(struct perf_event *event,
 	struct perf_buffer *old_rb = NULL;
 	unsigned long flags;
 
+	WARN_ON_ONCE(event->parent);
+
 	if (event->rb) {
 		/*
 		 * Should be impossible, we set this when removing
@@ -5887,6 +5889,9 @@ static void ring_buffer_wakeup(struct perf_event *event)
 {
 	struct perf_buffer *rb;
 
+	if (event->parent)
+		event = event->parent;
+
 	rcu_read_lock();
 	rb = rcu_dereference(event->rb);
 	if (rb) {
@@ -5899,6 +5904,9 @@ static void ring_buffer_wakeup(struct perf_event *event)
 struct perf_buffer *ring_buffer_get(struct perf_event *event)
 {
 	struct perf_buffer *rb;
+
+	if (event->parent)
+		event = event->parent;
 
 	rcu_read_lock();
 	rb = rcu_dereference(event->rb);
@@ -6572,7 +6580,7 @@ static unsigned long perf_prepare_sample_aux(struct perf_event *event,
 	if (WARN_ON_ONCE(READ_ONCE(sampler->oncpu) != smp_processor_id()))
 		goto out;
 
-	rb = ring_buffer_get(sampler->parent ? sampler->parent : sampler);
+	rb = ring_buffer_get(sampler);
 	if (!rb)
 		goto out;
 
@@ -6638,7 +6646,7 @@ static void perf_aux_sample_output(struct perf_event *event,
 	if (WARN_ON_ONCE(!sampler || !data->aux_size))
 		return;
 
-	rb = ring_buffer_get(sampler->parent ? sampler->parent : sampler);
+	rb = ring_buffer_get(sampler);
 	if (!rb)
 		return;
 
