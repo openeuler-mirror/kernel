f5a968e7e242a9e930782953bcba0ac9eab5696e
Borislav Petkov <bp@suse.de>
2022-05-25 18:12:30 +0200

    x86/microcode: Default-disable late loading
    mainline inclusion
    from mainline-v5.19-rc1
    commit a77a94f86273ce42a39cb479217dd8d68acfe0ff
    category: feature
    feature: Backport Intel In Field Scan(IFS) multi-blob images support
    bugzilla: https://gitee.com/openeuler/intel-kernel/issues/I6L337
    CVE: N/A
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/
    commit/?id=a77a94f86273ce42a39cb479217dd8d68acfe0ff
    
    Intel-SIG: commit a77a94f86273 ("x86/microcode: Default-disable late loading")
    
    -------------------------------------
    
    x86/microcode: Default-disable late loading
    
    It is dangerous and it should not be used anyway - there's a nice early
    loading already.
    
    Requested-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/r/20220525161232.14924-3-bp@alien8.de
    Signed-off-by: Aichun Shi <aichun.shi@intel.com>

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -2140,6 +2140,7 @@ void cpu_init(void)
 	load_fixmap_gdt(cpu);
 }
 
+#ifdef CONFIG_MICROCODE_LATE_LOADING
 /*
  * The microcode loader calls this upon late microcode load to recheck features,
  * only when microcode has been updated. Caller holds microcode_mutex and CPU
@@ -2169,6 +2170,7 @@ void microcode_check(void)
 	pr_warn("x86/CPU: CPU features have changed after loading microcode, but might not take effect.\n");
 	pr_warn("x86/CPU: Please consider either early loading through initrd/built-in or a potential BIOS update.\n");
 }
+#endif
 
 /*
  * Invoked from core CPU hotplug code after hotplug operations
