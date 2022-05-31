3fd6dd280197c51220c8d24bddeb3acb6f302b85
John Garry <john.garry@huawei.com>
2022-05-31 23:03:37 +0800

    blk-mq: Use request queue-wide tags for tagset-wide sbitmap
    mainline inclusion
    from mainline-v5.14-rc1
    commit d97e594c51660bea510a387731637b894651e4b5
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I597XM
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=d97e594c51660bea510a387731637b894651e4b5
    
    --------------------------------
    
    The tags used for an IO scheduler are currently per hctx.
    
    As such, when q->nr_hw_queues grows, so does the request queue total IO
    scheduler tag depth.
    
    This may cause problems for SCSI MQ HBAs whose total driver depth is
    fixed.
    
    Ming and Yanhui report higher CPU usage and lower throughput in scenarios
    where the fixed total driver tag depth is appreciably lower than the total
    scheduler tag depth:
    https://lore.kernel.org/linux-block/440dfcfc-1a2c-bd98-1161-cec4d78c6dfc@huawei.com/T/#mc0d6d4f95275a2743d1c8c3e4dc9ff6c9aa3a76b
    
    In that scenario, since the scheduler tag is got first, much contention
    is introduced since a driver tag may not be available after we have got
    the sched tag.
    
    Improve this scenario by introducing request queue-wide tags for when
    a tagset-wide sbitmap is used. The static sched requests are still
    allocated per hctx, as requests are initialised per hctx, as in
    blk_mq_init_request(..., hctx_idx, ...) ->
    set->ops->init_request(.., hctx_idx, ...).
    
    For simplicity of resizing the request queue sbitmap when updating the
    request queue depth, just init at the max possible size, so we don't need
    to deal with the possibly with swapping out a new sbitmap for old if
    we need to grow.
    
    Signed-off-by: John Garry <john.garry@huawei.com>
    Reviewed-by: Ming Lei <ming.lei@redhat.com>
    Link: https://lore.kernel.org/r/1620907258-30910-3-git-send-email-john.garry@huawei.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    conflict:
    	block/blk-mq-sched.c
    	block/blk-mq-sched.h
    	block/blk-mq-tag.c
    Signed-off-by: Yufen Yu <yuyufen@huawei.com>
    Reviewed-by: Jason Yan <yanaijie@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- block/blk-mq-sched.h
+++ block/blk-mq-sched.h
@@ -5,6 +5,8 @@
 #include "blk-mq.h"
 #include "blk-mq-tag.h"
 
+#define MAX_SCHED_RQ (16 * BLKDEV_MAX_RQ)
+
 void blk_mq_sched_assign_ioc(struct request *rq);
 
 void blk_mq_sched_request_inserted(struct request *rq);
