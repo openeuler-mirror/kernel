8092378eae3b2ce46416a4dc8f0136441fa938d0
Lu Feifei <lufeifei@wxiat.com>
2022-06-07 17:42:10 +0800

    sw64: unify access to LONGTIME for guest and emulator
    Sunway inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I56WV8
    
    --------------------------------
    
    Signed-off-by: Lu Feifei <lufeifei@wxiat.com>
    
    Signed-off-by: Gu Zitao <guzitao@wxiat.com>
    Acked-by: Xie XiuQi <xiexiuqi@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- arch/sw_64/chip/chip3/chip.c
+++ arch/sw_64/chip/chip3/chip.c
@@ -58,14 +58,9 @@ static struct clocksource clocksource_longtime = {
 static u64 read_vtime(struct clocksource *cs)
 {
 	u64 result;
-	unsigned long node;
 	unsigned long vtime_addr = PAGE_OFFSET | IO_BASE | LONG_TIME;
 
-	if (is_in_guest())
-		result = rdio64(vtime_addr);
-	else
-		result = sw64_io_read(node, LONG_TIME);
-
+	result = rdio64(vtime_addr);
 	return result;
 }
 
