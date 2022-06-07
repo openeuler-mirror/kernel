77669ea9028a9964219ab40c7cf5deed4455445a
Mao Minkai <maominkai@wxiat.com>
2022-06-07 17:41:56 +0800

    sw64: fix coding style problems
    Sunway inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I56OLG
    
    --------------------------------
    
    Signed-off-by: Mao Minkai <maominkai@wxiat.com>
    
    Signed-off-by: Gu Zitao <guzitao@wxiat.com>
    Acked-by: Xie XiuQi <xiexiuqi@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- arch/sw_64/include/asm/vga.h
+++ arch/sw_64/include/asm/vga.h
@@ -55,29 +55,29 @@ extern void scr_memcpyw(u16 *d, const u16 *s, unsigned int count);
 
 extern struct pci_controller *pci_vga_hose;
 
-# define __is_port_vga(a) \
+#define __is_port_vga(a) \
 	(((a) >= 0x3b0) && ((a) < 0x3e0) && \
 	((a) != 0x3b3) && ((a) != 0x3d3))
 
-# define __is_mem_vga(a) \
+#define __is_mem_vga(a) \
 	(((a) >= 0xa0000) && ((a) <= 0xc0000))
 
-# define FIXUP_IOADDR_VGA(a) do { \
+#define FIXUP_IOADDR_VGA(a) do { \
 	if (pci_vga_hose && __is_port_vga(a)) \
 		(a) += pci_vga_hose->io_space->start; \
 } while (0)
 
-# define FIXUP_MEMADDR_VGA(a) do { \
+#define FIXUP_MEMADDR_VGA(a) do { \
 	if (pci_vga_hose && __is_mem_vga(a)) \
 		(a) += pci_vga_hose->mem_space->start; \
 } while (0)
 
 #else /* CONFIG_VGA_HOSE */
-# define pci_vga_hose 0
-# define __is_port_vga(a) 0
-# define __is_mem_vga(a) 0
-# define FIXUP_IOADDR_VGA(a)
-# define FIXUP_MEMADDR_VGA(a)
+#define pci_vga_hose 0
+#define __is_port_vga(a) 0
+#define __is_mem_vga(a) 0
+#define FIXUP_IOADDR_VGA(a)
+#define FIXUP_MEMADDR_VGA(a)
 #endif /* CONFIG_VGA_HOSE */
 
 #define VGA_MAP_MEM(x, s)	((unsigned long)ioremap(x, s))
