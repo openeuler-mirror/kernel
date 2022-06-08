de13548b9a24e73d988b43fbad57f32cfb4adfd1
Zhu Donghong <zhudonghong@wxiat.com>
2022-06-08 09:49:30 +0800

    drivers/irqchip: add sw64 interrupt controller support
    Sunway inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I5GDHC
    
    --------------------------------
    
    Add a driver for the SW64 implementation of the CPU interrupt
    controller(INTC). In fact, the INTC is just a software simulated
    module to connect global interrupt sources to the local interrupt
    controller on each core.
    
    In addition, we rename irq-lpc.c and SW64_CHIP3_LPC config.
    
    Signed-off-by: Zhu Donghong <zhudonghong@wxiat.com>
    
    Signed-off-by: Gu Zitao <guzitao@wxiat.com>

--- drivers/irqchip/irq-sw64-intc-v2.c
+++ drivers/irqchip/irq-sw64-intc-v2.c
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <linux/irqdomain.h>
 #include <linux/irqchip.h>
-
-#include <asm/irq_impl.h>
+#include <linux/acpi.h>
+#include <linux/acpi_iort.h>
+#include <linux/of_irq.h>
+#include <asm/sw64io.h>
 
 static void fake_irq_mask(struct irq_data *data)
 {
@@ -31,6 +33,64 @@ static const struct irq_domain_ops sw64_intc_domain_ops = {
 	.xlate = irq_domain_xlate_onecell,
 	.map = sw64_intc_domain_map,
 };
+
+static int __init
+intc_parse_madt(union acpi_subtable_headers *header,
+		       const unsigned long end)
+{
+	struct acpi_madt_io_sapic *its_entry;
+	static struct irq_domain *root_domain;
+	int intc_irqs = 8, irq_base = NR_IRQS_LEGACY;
+	irq_hw_number_t hwirq_base = 0;
+	int irq_start = -1;
+
+	its_entry = (struct acpi_madt_io_sapic *)header;
+
+	intc_irqs -= hwirq_base; /* calculate # of irqs to allocate */
+
+	irq_base = irq_alloc_descs(irq_start, 16, intc_irqs,
+			numa_node_id());
+	if (irq_base < 0) {
+		WARN(1, "Cannot allocate irq_descs @ IRQ%d, assuming pre-allocated\n",
+				irq_start);
+		irq_base = irq_start;
+	}
+
+	root_domain = irq_domain_add_legacy(NULL, intc_irqs, irq_base,
+			hwirq_base, &sw64_intc_domain_ops, NULL);
+
+	if (!root_domain)
+		pr_err("Failed to create irqdomain");
+
+	irq_set_default_host(root_domain);
+
+	sw64_io_write(0, MCU_DVC_INT_EN, 0xff);
+
+	return 0;
+}
+
+static int __init acpi_intc_init(void)
+{
+	int count = 0;
+
+	count = acpi_table_parse_madt(ACPI_MADT_TYPE_IO_SAPIC,
+			      intc_parse_madt, 0);
+
+	if (count <= 0) {
+		pr_err("No valid intc entries exist\n");
+		return -EINVAL;
+	}
+	return 0;
+}
+
+static int __init intc_init(void)
+{
+	acpi_intc_init();
+
+	return 0;
+}
+
+subsys_initcall(intc_init);
 
 static struct irq_domain *root_domain;
 
