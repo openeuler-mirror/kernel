a465423531ccaec39c8586c14901ae345fecb3b7
Wangming Shao <shaowangming@h-partners.com>
2022-07-06 18:04:55 +0800

    scsi: hisi_sas: Change the debugfs parameters dump count default value
    driver inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I5DKH2
    
    -----------------------------------------------------------------------------
    
    If the default hisi_sas_debugfs_dump_count is 50, a large amount of memory
    is occupied.
    Therefore, to avoid high memory usage, reduce the
    hisi_sas_debugfs_dump_count from 50 to 1.
    
    Signed-off-by: Wangming Shao <shaowangming@h-partners.com>
    Reviewed-by: Jason Yan <yanaijie@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- drivers/scsi/hisi_sas/hisi_sas_main.c
+++ drivers/scsi/hisi_sas/hisi_sas_main.c
@@ -2852,12 +2852,12 @@ EXPORT_SYMBOL_GPL(hisi_sas_remove);
 #if IS_ENABLED(CONFIG_SCSI_HISI_SAS_DEBUGFS_DEFAULT_ENABLE)
 #define DEBUGFS_ENABLE_DEFAULT  "enabled"
 bool hisi_sas_debugfs_enable = true;
-u32 hisi_sas_debugfs_dump_count = 50;
 #else
 #define DEBUGFS_ENABLE_DEFAULT "disabled"
 bool hisi_sas_debugfs_enable;
-u32 hisi_sas_debugfs_dump_count = 1;
 #endif
+
+u32 hisi_sas_debugfs_dump_count = 1;
 
 EXPORT_SYMBOL_GPL(hisi_sas_debugfs_enable);
 module_param_named(debugfs_enable, hisi_sas_debugfs_enable, bool, 0444);
