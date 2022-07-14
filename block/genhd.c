faf2662e328cdc731ed0a180c4f9db065766f595
Yu Kuai <yukuai3@huawei.com>
2022-07-14 02:49:56 +0000

    block: fix that part scan is disabled in device_add_disk()
    hulk inclusion
    category: bugfix
    bugzilla: 187190, https://gitee.com/src-openeuler/kernel/issues/I5GWOV
    CVE: NA
    
    --------------------------------
    
    Commit f20a726bedfc ("block: Fix warning in bd_link_disk_holder()")
    moves the setting of flag 'GENHD_FL_UP' behind blkdev_get, which will
    disabled part scan:
    
    devcie_add_disk
     register_disk
      blkdev_get
       __blkdev_get
        bdev_get_gendisk
         get_gendisk -> failed because 'GENHD_FL_UP' is not set
    
    And this will cause tests block/017, block/018 and scsi/004 to fail.
    
    Fix the problem by moving part scan as well.
    
    Fixes: f20a726bedfc ("block: Fix warning in bd_link_disk_holder()")
    Signed-off-by: Yu Kuai <yukuai3@huawei.com>
    Reviewed-by: Jason Yan <yanaijie@huawei.com>
    Signed-off-by: Yongqiang Liu <liuyongqiang13@huawei.com>

--- block/genhd.c
+++ block/genhd.c
@@ -598,9 +598,6 @@ static int exact_lock(dev_t devt, void *data)
 static void register_disk(struct device *parent, struct gendisk *disk)
 {
 	struct device *ddev = disk_to_dev(disk);
-	struct block_device *bdev;
-	struct disk_part_iter piter;
-	struct hd_struct *part;
 	int err;
 
 	ddev->parent = parent;
@@ -633,35 +630,6 @@ static void register_disk(struct device *parent, struct gendisk *disk)
 
 	if (disk->flags & GENHD_FL_HIDDEN)
 		return;
-
-	/* No minors to use for partitions */
-	if (!disk_part_scan_enabled(disk))
-		goto exit;
-
-	/* No such device (e.g., media were just removed) */
-	if (!get_capacity(disk))
-		goto exit;
-
-	bdev = bdget_disk(disk, 0);
-	if (!bdev)
-		goto exit;
-
-	bdev->bd_invalidated = 1;
-	err = blkdev_get(bdev, FMODE_READ, NULL);
-	if (err < 0)
-		goto exit;
-	blkdev_put(bdev, FMODE_READ);
-
-exit:
-	/* announce disk after possible partitions are created */
-	dev_set_uevent_suppress(ddev, 0);
-	kobject_uevent(&ddev->kobj, KOBJ_ADD);
-
-	/* announce possible partitions */
-	disk_part_iter_init(&piter, disk, 0);
-	while ((part = disk_part_iter_next(&piter)))
-		kobject_uevent(&part_to_dev(part)->kobj, KOBJ_ADD);
-	disk_part_iter_exit(&piter);
 
 	if (disk->queue->backing_dev_info->dev) {
 		err = sysfs_create_link(&ddev->kobj,
