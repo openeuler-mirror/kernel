fb9d8132efb7097ac7fcf5ba20da2ea4dda5d74b
Jianmin Lv <lvjianmin@loongson.cn>
2022-07-20 18:51:32 +0800

    irqchip / ACPI: Introduce ACPI_IRQ_MODEL_LPIC for LoongArch
    mainline inclusion
    from mainline-v6.0-rc1
    commit e8bba72b396cef7c919c73710f3c5884521adb4e
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I5OHOB
    CVE: NA
    
    --------------------------------
    
    For LoongArch, ACPI_IRQ_MODEL_LPIC is introduced, and then the
    callback acpi_get_gsi_domain_id and acpi_gsi_to_irq_fallback are
    implemented.
    
    The acpi_get_gsi_domain_id callback returns related fwnode handle
    of irqdomain for different GSI range.
    
    The acpi_gsi_to_irq_fallback will create new mapping for gsi when
    the mapping of it is not found.
    
    Signed-off-by: Jianmin Lv <lvjianmin@loongson.cn>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/1658314292-35346-14-git-send-email-lvjianmin@loongson.cn

--- drivers/acpi/bus.c
+++ drivers/acpi/bus.c
@@ -994,6 +994,9 @@ static int __init acpi_bus_init_irq(void)
 	case ACPI_IRQ_MODEL_PLATFORM:
 		message = "platform specific model";
 		break;
+	case ACPI_IRQ_MODEL_LPIC:
+		message = "LPIC";
+		break;
 	default:
 		printk(KERN_WARNING PREFIX "Unknown interrupt routing model\n");
 		return -ENODEV;
