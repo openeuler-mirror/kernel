915c4dfcd52d26a893c3febd0bdfcdfe9ea8fb6d
Roman Gushchin <guro@fb.com>
2022-07-27 08:43:05 +0000

    bpf: sched: introduce bpf_sched_enable()
    maillist inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I5F6X6
    CVE: NA
    
    Reference: https://lore.kernel.org/all/20210916162451.709260-1-guro@fb.com/
    
    -------------------
    
    Introduce a dedicated static key and the bpf_sched_enabled() wrapper
    to guard all invocations of bpf programs in the scheduler code.
    
    It will help to avoid any potential performance regression in a case
    when no scheduler bpf programs are attached.
    
    Signed-off-by: Roman Gushchin <guro@fb.com>
    Signed-off-by: Chen Hui <judy.chenhui@huawei.com>
    Signed-off-by: Ren Zhijie <renzhijie2@huawei.com>

--- include/linux/bpf_sched.h
+++ include/linux/bpf_sched.h
@@ -6,6 +6,8 @@
 
 #ifdef CONFIG_BPF_SCHED
 
+#include <linux/jump_label.h>
+
 #define BPF_SCHED_HOOK(RET, DEFAULT, NAME, ...) \
 	RET bpf_sched_##NAME(__VA_ARGS__);
 #include <linux/sched_hook_defs.h>
@@ -14,12 +16,34 @@
 int bpf_sched_verify_prog(struct bpf_verifier_log *vlog,
 			  const struct bpf_prog *prog);
 
+DECLARE_STATIC_KEY_FALSE(bpf_sched_enabled_key);
+
+static inline bool bpf_sched_enabled(void)
+{
+	return static_branch_unlikely(&bpf_sched_enabled_key);
+}
+
+static inline void bpf_sched_inc(void)
+{
+	static_branch_inc(&bpf_sched_enabled_key);
+}
+
+static inline void bpf_sched_dec(void)
+{
+	static_branch_dec(&bpf_sched_enabled_key);
+}
+
 #else /* !CONFIG_BPF_SCHED */
 
 static inline int bpf_sched_verify_prog(struct bpf_verifier_log *vlog,
 			  const struct bpf_prog *prog)
 {
 	return -EOPNOTSUPP;
+}
+
+static inline bool bpf_sched_enabled(void)
+{
+	return false;
 }
 
 #endif /* CONFIG_BPF_SCHED */
