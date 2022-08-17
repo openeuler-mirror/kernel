a058295cd9f581544bab09dbebc54d5726a52702
Binfeng Wu <wubinfeng@huawei.com>
2022-08-17 17:58:45 +0800

    ascend agent smmu: an implementation of ARM SMMUv3 ATOS feature
    ascend inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I5JSWJ
    CVE: NA
    
    -------------------------------------------------
    
    To support HCCS bus using in Ascend series accelerators, the SMMU ATOS
    (a software-accessible Address Translation Operations facility) feature
    is enabled for a special SMMU aka Agent SMMU in the Ascend accelerator.
    
    In the VM scenario, the hypervisor creates Stage1 page table for the
    Agent SMMU. The Agent SMMU provides an interface for components in
    accelerator to translate addresses from IPA to PA. This allows the
    components to DMA on the HCCS bus using PA.
    
    The origin SMMU ATOS feature only support translation of only a single
    group of addresses at a time. Ascend Agent SMMUs use the IMPLEMENTATION
    DEFINED region to implement translation of max 32 groups of addresses at
    the same time which can greatly improve the efficiency.
    
    Reviewed-by: Yingtai Xie <xieyingtai@huawei.com>
    Reviewed-by: Xiaoyang Xu <xuxiaoyang2@huawei.com>
    Signed-off-by: Binfeng Wu <wubinfeng@huawei.com>
    Reviewed-by: Weilong Chen <chenweilong@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

---
+++ include/linux/ascend_smmu.h
@@ -0,0 +1,52 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __LINUX_ASCEND_SMMU_H
+#define __LINUX_ASCEND_SMMU_H
+
+#define INV_REQ			0xff
+#define INV_STAGE		0xfe
+#define INTERNAL_ERR		0xfd
+#define C_BAD_STREAMID		0x02
+#define F_STE_FETCH		0x03
+#define C_BAD_STE		0x04
+#define F_STREAM_DISABLED	0x06
+#define C_BAD_SUBSTREAMID	0x08
+#define F_CD_FETCH		0x09
+#define C_BAD_CD		0x0a
+#define F_WALK_EABT		0x0b
+#define F_TRANSLATION		0x10
+#define F_ADDR_SIZE		0x11
+#define F_ACCESS		0x12
+#define F_PERMISSION		0x13
+#define F_TLB_CONFLICT		0x20
+#define F_CFG_CONFLICT		0x21
+#define F_VMS_FETCH		0x25
+
+/**
+ * struct agent_smmu_atos_data - information required for address translation
+ * @sid: stream id
+ * @ssid: substream id
+ * @PnU: 0 for Unprivileged, 1 for Privileged
+ * @RnW: 0 for Write, 1 for Read
+ * @InD: 0 for Data, 1 for Instruction
+ * @HTTUI: 0 for HTTU might occur, 1 for HTTU inhibited
+ * @nr: number of addresses
+ * @iova: iova addresses to be translated
+ * @pa: translated physical addresses
+ * @device_id: agent smmu uid
+ */
+struct agent_smmu_atos_data {
+	u32 sid;
+	u32 ssid;
+	u32 pnu;
+	u32 rnw;
+	u32 ind;
+	u32 httui;
+	int nr;
+	dma_addr_t *iova;
+	phys_addr_t *pa;
+	u64 device_id;
+};
+
+extern int agent_smmu_iova_to_phys(struct agent_smmu_atos_data *data, int *succeed);
+
+#endif
