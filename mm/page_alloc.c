fbf7639ad0b76b4e10e5aba19ab29b34c4c81732
Mike Rapoport <rppt@linux.ibm.com>
2022-08-17 17:58:32 +0800

    mm/page_alloc: always initialize memory map for the holes
    mainline inclusion
    from mainline-v5.15-rc1
    commit c3ab6baf6a004eab7344a1d8880a971f2414e1b6
    category: bugfix
    bugzilla: 186414, https://gitee.com/openeuler/kernel/issues/I5K7IY
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=c3ab6baf6a004eab7344a1d8880a971f2414e1b6
    
    --------------------------------
    
    Patch series "mm: ensure consistency of memory map poisoning".
    
    Currently memory map allocation for FLATMEM case does not poison the
    struct pages regardless of CONFIG_PAGE_POISON setting.
    
    This happens because allocation of the memory map for FLATMEM and SPARSMEM
    use different memblock functions and those that are used for SPARSMEM case
    (namely memblock_alloc_try_nid_raw() and memblock_alloc_exact_nid_raw())
    implicitly poison the allocated memory.
    
    Another side effect of this implicit poisoning is that early setup code
    that uses the same functions to allocate memory burns cycles for the
    memory poisoning even if it was not intended.
    
    These patches introduce memmap_alloc() wrapper that ensure that the memory
    map allocation is consistent for different memory models.
    
    This patch (of 4):
    
    Currently memory map for the holes is initialized only when SPARSEMEM
    memory model is used.  Yet, even with FLATMEM there could be holes in the
    physical memory layout that have memory map entries.
    
    For instance, the memory reserved using e820 API on i386 or
    "reserved-memory" nodes in device tree would not appear in memblock.memory
    and hence the struct pages for such holes will be skipped during memory
    map initialization.
    
    These struct pages will be zeroed because the memory map for FLATMEM
    systems is allocated with memblock_alloc_node() that clears the allocated
    memory.  While zeroed struct pages do not cause immediate problems, the
    correct behaviour is to initialize every page using __init_single_page().
    Besides, enabling page poison for FLATMEM case will trigger
    PF_POISONED_CHECK() unless the memory map is properly initialized.
    
    Make sure init_unavailable_range() is called for both SPARSEMEM and
    FLATMEM so that struct pages representing memory holes would appear as
    PG_Reserved with any memory layout.
    
    [rppt@kernel.org: fix microblaze]
      Link: https://lkml.kernel.org/r/YQWW3RCE4eWBuMu/@kernel.org
    
    Link: https://lkml.kernel.org/r/20210714123739.16493-1-rppt@kernel.org
    Link: https://lkml.kernel.org/r/20210714123739.16493-2-rppt@kernel.org
    Signed-off-by: Mike Rapoport <rppt@linux.ibm.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Tested-by: Guenter Roeck <linux@roeck-us.net>
    Cc: Michal Simek <monstr@monstr.eu>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Conflicts:
    	arch/microblaze/include/asm/page.h
    	mm/page_alloc.c
    Signed-off-by: Liu Shixin <liushixin2@huawei.com>
    Reviewed-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -6451,7 +6451,6 @@ static void __meminit zone_init_free_lists(struct zone *zone)
 	}
 }
 
-#if !defined(CONFIG_FLAT_NODE_MEM_MAP)
 /*
  * Only struct pages that correspond to ranges defined by memblock.memory
  * are zeroed and initialized by going through __init_single_page() during
@@ -6496,13 +6495,6 @@ static void __init init_unavailable_range(unsigned long spfn,
 		pr_info("On node %d, zone %s: %lld pages in unavailable ranges",
 			node, zone_names[zone], pgcnt);
 }
-#else
-static inline void init_unavailable_range(unsigned long spfn,
-					  unsigned long epfn,
-					  int zone, int node)
-{
-}
-#endif
 
 static void __init memmap_init_zone_range(struct zone *zone,
 					  unsigned long start_pfn,
