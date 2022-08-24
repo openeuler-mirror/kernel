dfb80162dba89cc5b2b4b6ccafb60edac0320818
Tony Luck <tony.luck@intel.com>
2022-08-24 10:57:18 -0700

    x86/cpu: Add CPU model numbers for Meteor Lake
    mainline inclusion
    from mainline-v6.0
    commit 5515d21c6817bc27b0b13c61edf22b09b58bc647
    category: feature
    feature: SRF core PMU support
    bugzilla: https://gitee.com/openeuler/intel-kernel/issues/I8RWG5
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=5515d21c6817bc27b0b13c61edf22b09b58bc647
    
    Intel-SIG: commit 5515d21c6817 x86/cpu: Add CPU model numbers for Meteor Lake
    Backport as a dependency for Sierra Forrest core PMU support.
    
    -------------------------------------
    
    Add model numbers for client and mobile parts.
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Link: https://lkml.kernel.org/r/20220824175718.232384-1-tony.luck@intel.com
    Signed-off-by: Yunying Sun <yunying.sun@intel.com>

--- arch/x86/include/asm/intel-family.h
+++ arch/x86/include/asm/intel-family.h
@@ -116,6 +116,9 @@
 #define INTEL_FAM6_RAPTORLAKE		0xB7
 #define INTEL_FAM6_RAPTORLAKE_P		0xBA
 
+#define INTEL_FAM6_METEORLAKE		0xAC
+#define INTEL_FAM6_METEORLAKE_L		0xAA
+
 /* "Small Core" Processors (Atom/E-Core) */
 
 #define INTEL_FAM6_ATOM_BONNELL		0x1C /* Diamondville, Pineview */
