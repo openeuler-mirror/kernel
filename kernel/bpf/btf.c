3f5caa2f0818f317f4bb16d644435e8253a93859
Andrii Nakryiko <andrii@kernel.org>
2022-08-24 11:19:35 +0800

    bpf: Compile out btf_parse_module() if module BTF is not enabled
    mainline inclusion
    from mainline-5.11-rc1
    commit 7112d127984bd7b0c8ded7973b358829f16735f5
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I5EUVD
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=7112d127984bd7b0c8ded7973b358829f16735f5
    
    -------------------------------------------------
    
    Make sure btf_parse_module() is compiled out if module BTFs are not enabled.
    
    Fixes: 36e68442d1af ("bpf: Load and verify kernel module BTFs")
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20201111040645.903494-1-andrii@kernel.org
    (cherry picked from commit 7112d127984bd7b0c8ded7973b358829f16735f5)
    Signed-off-by: Wang Yufen <wangyufen@huawei.com>

--- kernel/bpf/btf.c
+++ kernel/bpf/btf.c
@@ -4477,6 +4477,8 @@ errout:
 	return ERR_PTR(err);
 }
 
+#ifdef CONFIG_DEBUG_INFO_BTF_MODULES
+
 static struct btf *btf_parse_module(const char *module_name, const void *data, unsigned int data_size)
 {
 	struct btf_verifier_env *env = NULL;
@@ -4545,6 +4547,8 @@ errout:
 	}
 	return ERR_PTR(err);
 }
+
+#endif /* CONFIG_DEBUG_INFO_BTF_MODULES */
 
 struct btf *bpf_prog_get_target_btf(const struct bpf_prog *prog)
 {
