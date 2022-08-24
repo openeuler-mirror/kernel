50f4967473d7f4dc37a1d38c53b7b1db99f72616
He Chuyue <hechuyue@wxiat.com>
2022-08-24 09:56:54 +0800

    sw64: perf: fix perf_get_regs_user
    Sunway inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I56X48
    
    --------------------------------
    
    The past implementation of perf_get_regs_user could not get regs by
    default, but it was actually available via task_pt_regs. Fix it now.
    
    Signed-off-by: He Chuyue <hechuyue@wxiat.com>
    Signed-off-by: Gu Zitao <guzitao@wxiat.com>

--- arch/sw_64/kernel/perf_regs.c
+++ arch/sw_64/kernel/perf_regs.c
@@ -28,6 +28,6 @@ u64 perf_reg_abi(struct task_struct *task)
 void perf_get_regs_user(struct perf_regs *regs_user,
 			struct pt_regs *regs)
 {
-	regs_user->regs = NULL;
-	regs_user->abi = PERF_SAMPLE_REGS_ABI_NONE;
+	regs_user->regs = task_pt_regs(current);
+	regs_user->abi = perf_reg_abi(current);
 }
