269d4b0686bb448b07f9df26b2231cd1d981a234
Jianmin Lv <lvjianmin@loongson.cn>
2022-09-06 16:41:32 +0800

    PCI: PM: Fix pcie mrrs restoring
    LoongArch inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I6BWFP
    
    --------------------------------
    
    Don't limit mmrs during resume, so that saved value can be
    restored.
    
    Fix patch "PCI: loongson: Improve the MRRS quirk for LS7A"
    
    Signed-off-by: Jianmin Lv <lvjianmin@loongson.cn>
    Change-Id: I63c49e5c1d7e2a0a6eb3de9faa13cef1f94a4462

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -31,6 +31,7 @@
 #include <linux/vmalloc.h>
 #include <asm/dma.h>
 #include <linux/aer.h>
+#include <linux/suspend.h>
 #include "pci.h"
 
 DEFINE_MUTEX(pci_slot_mutex);
@@ -146,6 +147,11 @@ EXPORT_SYMBOL_GPL(pci_ats_disabled);
 static bool pci_bridge_d3_disable;
 /* Force bridge_d3 for all PCIe ports */
 static bool pci_bridge_d3_force;
+
+#ifndef CONFIG_PM_SLEEP
+suspend_state_t pm_suspend_target_state;
+#define pm_suspend_target_state (PM_SUSPEND_ON)
+#endif
 
 static int __init pcie_port_pm_setup(char *str)
 {
@@ -5769,7 +5775,8 @@ int pcie_set_readrq(struct pci_dev *dev, int rq)
 
 	v = (ffs(rq) - 8) << 12;
 
-	if (bridge->no_inc_mrrs) {
+	if (pm_suspend_target_state == PM_SUSPEND_ON &&
+		bridge->no_inc_mrrs) {
 		if (rq > pcie_get_readrq(dev))
 			return -EINVAL;
 	}
