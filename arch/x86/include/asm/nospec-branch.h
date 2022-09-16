0348b60e9e41859f3894b7f21a18f7ecdead8ccf
Peter Zijlstra <peterz@infradead.org>
2022-09-16 18:48:06 +0800

    x86/retpoline: Create a retpoline thunk array
    stable inclusion
    from stable-v5.10.133
    commit 6eb95718f3ea92affae94401f273b580f6b97372
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I5PTAS
    CVE: CVE-2022-29900,CVE-2022-23816,CVE-2022-29901
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=6eb95718f3ea92affae94401f273b580f6b97372
    
    --------------------------------
    
    commit 1a6f74429c42a3854980359a758e222005712aee upstream.
    
    Stick all the retpolines in a single symbol and have the individual
    thunks as inner labels, this should guarantee thunk order and layout.
    
    Previously there were 16 (or rather 15 without rsp) separate symbols and
    a toolchain might reasonably expect it could displace them however it
    liked, with disregard for their relative position.
    
    However, now they're part of a larger symbol. Any change to their
    relative position would disrupt this larger _array symbol and thus not
    be sound.
    
    This is the same reasoning used for data symbols. On their own there
    is no guarantee about their relative position wrt to one aonther, but
    we're still able to do arrays because an array as a whole is a single
    larger symbol.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Borislav Petkov <bp@suse.de>
    Acked-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Tested-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/r/20211026120310.169659320@infradead.org
    Signed-off-by: Thadeu Lima de Souza Cascardo <cascardo@canonical.com>
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Lin Yujun <linyujun809@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- arch/x86/include/asm/nospec-branch.h
+++ arch/x86/include/asm/nospec-branch.h
@@ -12,6 +12,8 @@
 #include <asm/msr-index.h>
 #include <asm/unwind_hints.h>
 
+#define RETPOLINE_THUNK_SIZE	32
+
 /*
  * Fill the CPU return stack buffer.
  *
@@ -120,10 +122,21 @@
 
 #ifdef CONFIG_RETPOLINE
 
+typedef u8 retpoline_thunk_t[RETPOLINE_THUNK_SIZE];
+
+#ifdef __GENKSYMS__
 #define GEN(reg) \
 	extern asmlinkage void __x86_indirect_thunk_ ## reg (void);
 #include <asm/GEN-for-each-reg.h>
 #undef GEN
+#else
+#define GEN(reg) \
+        extern retpoline_thunk_t __x86_indirect_thunk_ ## reg;
+#include <asm/GEN-for-each-reg.h>
+#undef GEN
+#endif
+
+extern retpoline_thunk_t __x86_indirect_thunk_array[];
 
 #ifdef CONFIG_X86_64
 
