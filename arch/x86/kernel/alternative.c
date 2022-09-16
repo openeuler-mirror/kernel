c3b0438d5595350acd340420e4b1b50a373704d5
Peter Zijlstra <peterz@infradead.org>
2022-09-16 18:49:11 +0800

    x86/retbleed: Add fine grained Kconfig knobs
    stable inclusion
    from stable-v5.10.133
    commit b24fdd0f1c3328cf8ee0c518b93a7187f8cee097
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I5PTAS
    CVE: CVE-2022-29900,CVE-2022-23816,CVE-2022-29901
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=b24fdd0f1c3328cf8ee0c518b93a7187f8cee097
    
    --------------------------------
    
    commit f43b9876e857c739d407bc56df288b0ebe1a9164 upstream.
    
    Do fine-grained Kconfig for all the various retbleed parts.
    
    NOTE: if your compiler doesn't support return thunks this will
    silently 'upgrade' your mitigation to IBPB, you might not like this.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    [cascardo: there is no CONFIG_OBJTOOL]
    [cascardo: objtool calling and option parsing has changed]
    Signed-off-by: Thadeu Lima de Souza Cascardo <cascardo@canonical.com>
    [bwh: Backported to 5.10:
     - In scripts/Makefile.build, add the objtool option with an ifdef
       block, same as for other options
     - Adjust filename, context]
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    conflict:
    	arch/x86/include/asm/disabled-features.h
    
    Signed-off-by: Lin Yujun <linyujun809@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- arch/x86/kernel/alternative.c
+++ arch/x86/kernel/alternative.c
@@ -612,6 +612,7 @@ void __init_or_module noinline apply_retpolines(s32 *start, s32 *end)
 	}
 }
 
+#ifdef CONFIG_RETHUNK
 /*
  * Rewrite the compiler generated return thunk tail-calls.
  *
@@ -673,6 +674,10 @@ void __init_or_module noinline apply_returns(s32 *start, s32 *end)
 		}
 	}
 }
+#else
+void __init_or_module noinline apply_returns(s32 *start, s32 *end) { }
+#endif /* CONFIG_RETHUNK */
+
 #else /* !RETPOLINES || !CONFIG_STACK_VALIDATION */
 
 void __init_or_module noinline apply_retpolines(s32 *start, s32 *end) { }
