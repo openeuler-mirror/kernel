24f890297774090e7efc35987bb2eef1bc09d8db
Phil Auld <pauld@redhat.com>
2022-09-30 18:08:41 +0800

    arch/arm64: Fix topology initialization for core scheduling
    mainline inclusion
    from mainline-v5.18-rc2
    commit 5524cbb1bfcdff0cad0aaa9f94e6092002a07259
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I5OOWG
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=5524cbb1bfcdff0cad0aaa9f94e6092002a07259
    
    --------------------------------------------------------------------------
    
    Arm64 systems rely on store_cpu_topology() to call update_siblings_masks()
    to transfer the toplogy to the various cpu masks. This needs to be done
    before the call to notify_cpu_starting() which tells the scheduler about
    each cpu found, otherwise the core scheduling data structures are setup
    in a way that does not match the actual topology.
    
    With smt_mask not setup correctly we bail on `cpumask_weight(smt_mask) == 1`
    for !leaders in:
    
     notify_cpu_starting()
       cpuhp_invoke_callback_range()
         sched_cpu_starting()
           sched_core_cpu_starting()
    
    which leads to rq->core not being correctly set for !leader-rq's.
    
    Without this change stress-ng (which enables core scheduling in its prctl
    tests in newer versions -- i.e. with PR_SCHED_CORE support) causes a warning
    and then a crash (trimmed for legibility):
    
    [ 1853.805168] ------------[ cut here ]------------
    [ 1853.809784] task_rq(b)->core != rq->core
    [ 1853.809792] WARNING: CPU: 117 PID: 0 at kernel/sched/fair.c:11102 cfs_prio_less+0x1b4/0x1c4
    ...
    [ 1854.015210] Unable to handle kernel NULL pointer dereference at virtual address 0000000000000010
    ...
    [ 1854.231256] Call trace:
    [ 1854.233689]  pick_next_task+0x3dc/0x81c
    [ 1854.237512]  __schedule+0x10c/0x4cc
    [ 1854.240988]  schedule_idle+0x34/0x54
    
    Fixes: 9edeaea1bc45 ("sched: Core-wide rq->lock")
    Signed-off-by: Phil Auld <pauld@redhat.com>
    Reviewed-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Tested-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Link: https://lore.kernel.org/r/20220331153926.25742-1-pauld@redhat.com
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Lin Shengwang <linshengwang1@huawei.com>
    Reviewed-by: lihua <hucool.lihua@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- arch/arm64/kernel/smp.c
+++ arch/arm64/kernel/smp.c
@@ -250,6 +250,7 @@ asmlinkage notrace void secondary_start_kernel(void)
 	 * Log the CPU info before it is marked online and might get read.
 	 */
 	cpuinfo_store_cpu();
+	store_cpu_topology(cpu);
 
 	/*
 	 * Enable GIC and timers.
@@ -258,7 +259,6 @@ asmlinkage notrace void secondary_start_kernel(void)
 
 	ipi_setup(cpu);
 
-	store_cpu_topology(cpu);
 	numa_add_cpu(cpu);
 
 	/*
