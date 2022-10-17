53ad6bf76d9c646e3c8494ed82d90f304c50de1f
Barry Song <song.bao.hua@hisilicon.com>
2022-10-17 15:01:55 +0800

    sched: Add per_cpu cluster domain info and cpus_share_lowest_cache API
    kunpeng inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I5W44S
    CVE: NA
    
    Reference: https://lore.kernel.org/lkml/20220915073423.25535-1-yangyicong@huawei.com/
    
    ----------------------------------------------------------------------
    
    Add per-cpu cluster domain info and cpus_share_lowest_cache() API.
    This is the preparation for the optimization of select_idle_cpu()
    on platforms with cluster scheduler level.
    
    Tested-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Signed-off-by: Barry Song <song.bao.hua@hisilicon.com>
    Signed-off-by: Yicong Yang <yangyicong@hisilicon.com>
    Reviewed-by: Gautham R. Shenoy <gautham.shenoy@amd.com>
    Reviewed-by: Tim Chen <tim.c.chen@linux.intel.com>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Jie Liu <liujie375@h-partners.com>

--- include/linux/sched/topology.h
+++ include/linux/sched/topology.h
@@ -46,7 +46,7 @@ static inline int cpu_smt_flags(void)
 #ifdef CONFIG_SCHED_CLUSTER
 static inline int cpu_cluster_flags(void)
 {
-	return SD_SHARE_PKG_RESOURCES;
+	return SD_CLUSTER | SD_SHARE_PKG_RESOURCES;
 }
 #endif
 
@@ -185,6 +185,7 @@ cpumask_var_t *alloc_sched_domains(unsigned int ndoms);
 void free_sched_domains(cpumask_var_t doms[], unsigned int ndoms);
 
 bool cpus_share_cache(int this_cpu, int that_cpu);
+bool cpus_share_lowest_cache(int this_cpu, int that_cpu);
 
 typedef const struct cpumask *(*sched_domain_mask_f)(int cpu);
 typedef int (*sched_domain_flags_f)(void);
@@ -234,6 +235,11 @@ partition_sched_domains(int ndoms_new, cpumask_var_t doms_new[],
 }
 
 static inline bool cpus_share_cache(int this_cpu, int that_cpu)
+{
+	return true;
+}
+
+static inline bool cpus_share_lowest_cache(int this_cpu, int that_cpu)
 {
 	return true;
 }
--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3014,6 +3014,18 @@ bool cpus_share_cache(int this_cpu, int that_cpu)
 	return per_cpu(sd_llc_id, this_cpu) == per_cpu(sd_llc_id, that_cpu);
 }
 
+/*
+ * Whether CPUs are share lowest cache, which means LLC on non-cluster
+ * machines and LLC tag or L2 on machines with clusters.
+ */
+bool cpus_share_lowest_cache(int this_cpu, int that_cpu)
+{
+	if (this_cpu == that_cpu)
+		return true;
+
+	return per_cpu(sd_lowest_cache_id, this_cpu) == per_cpu(sd_lowest_cache_id, that_cpu);
+}
+
 static inline bool ttwu_queue_cond(int cpu, int wake_flags)
 {
 	/*
