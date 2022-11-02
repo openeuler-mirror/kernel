0eb46fc1d1ad0678348707332f56c2a368f75444
Mark Rutland <mark.rutland@arm.com>
2022-11-02 15:36:45 +0800

    arm64: fix rodata=full
    mainline inclusion
    from mainline-v6.0-rc3
    commit 2e8cff0a0eee87b27f0cf87ad8310eb41b5886ab
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I5PPS3
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=2e8cff0a0eee87b27f0cf87ad8310eb41b5886ab
    
    --------------------------------
    
    On arm64, "rodata=full" has been suppored (but not documented) since
    commit:
    
      c55191e96caa9d78 ("arm64: mm: apply r/o permissions of VM areas to its linear alias as well")
    
    As it's necessary to determine the rodata configuration early during
    boot, arm64 has an early_param() handler for this, whereas init/main.c
    has a __setup() handler which is run later.
    
    Unfortunately, this split meant that since commit:
    
      f9a40b0890658330 ("init/main.c: return 1 from handled __setup() functions")
    
    ... passing "rodata=full" would result in a spurious warning from the
    __setup() handler (though RO permissions would be configured
    appropriately).
    
    Further, "rodata=full" has been broken since commit:
    
      0d6ea3ac94ca77c5 ("lib/kstrtox.c: add "false"/"true" support to kstrtobool()")
    
    ... which caused strtobool() to parse "full" as false (in addition to
    many other values not documented for the "rodata=" kernel parameter.
    
    This patch fixes this breakage by:
    
    * Moving the core parameter parser to an __early_param(), such that it
      is available early.
    
    * Adding an (optional) arch hook which arm64 can use to parse "full".
    
    * Updating the documentation to mention that "full" is valid for arm64.
    
    * Having the core parameter parser handle "on" and "off" explicitly,
      such that any undocumented values (e.g. typos such as "ful") are
      reported as errors rather than being silently accepted.
    
    Note that __setup() and early_param() have opposite conventions for
    their return values, where __setup() uses 1 to indicate a parameter was
    handled and early_param() uses 0 to indicate a parameter was handled.
    
    Fixes: f9a40b089065 ("init/main.c: return 1 from handled __setup() functions")
    Fixes: 0d6ea3ac94ca ("lib/kstrtox.c: add "false"/"true" support to kstrtobool()")
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Andy Shevchenko <andy.shevchenko@gmail.com>
    Cc: Ard Biesheuvel <ardb@kernel.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Jagdish Gediya <jvgediya@linux.ibm.com>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Randy Dunlap <rdunlap@infradead.org>
    Cc: Will Deacon <will@kernel.org>
    Reviewed-by: Ard Biesheuvel <ardb@kernel.org>
    Link: https://lore.kernel.org/r/20220817154022.3974645-1-mark.rutland@arm.com
    Signed-off-by: Will Deacon <will@kernel.org>
    
    conflicts:
    	Documentation/admin-guide/kernel-parameters.txt
    	arch/arm64/include/asm/setup.h
    	arch/arm64/mm/mmu.c
    	init/main.c
    
    Signed-off-by: Xia Longlong <xialonglong1@huawei.com>
    Reviewed-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Reviewed-by: sunnanyong <sunnanyong@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

---
+++ arch/arm64/include/asm/setup.h
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#ifndef __ARM64_ASM_SETUP_H
+#define __ARM64_ASM_SETUP_H
+
+#include <linux/string.h>
+#include <uapi/asm/setup.h>
+
+static inline bool arch_parse_debug_rodata(char *arg)
+{
+	extern bool rodata_enabled;
+	extern bool rodata_full;
+
+	if (arg && !strcmp(arg, "full")) {
+		rodata_enabled = true;
+		rodata_full = true;
+		return true;
+	}
+
+	return false;
+}
+#define arch_parse_debug_rodata arch_parse_debug_rodata
+
+#endif
