1da34210ba63a428da3cc4876ff0d2d59468c8a8
Ye Bin <yebin10@huawei.com>
2022-11-07 20:52:02 +0800

    blktrace: remove unnessary stop block trace in 'blk_trace_shutdown'
    mainline inclusion
    from mainline-v6.1-rc2
    commit 2db96217e7e515071726ca4ec791742c4202a1b2
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I5ZI04
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?h=v6.1-rc3&id=2db96217e7e515071726ca4ec791742c4202a1b2
    
    --------------------------------
    
    As previous commit, 'blk_trace_cleanup' will stop block trace if
    block trace's state is 'Blktrace_running'.
    So remove unnessary stop block trace in 'blk_trace_shutdown'.
    
    Signed-off-by: Ye Bin <yebin10@huawei.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Link: https://lore.kernel.org/r/20221019033602.752383-4-yebin@huaweicloud.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    conflicts:
    kernel/trace/blktrace.c
    
    Signed-off-by: Ye Bin <yebin10@huawei.com>
    Reviewed-by: Jason Yan <yanaijie@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

--- kernel/trace/blktrace.c
+++ kernel/trace/blktrace.c
@@ -775,10 +775,8 @@ void blk_trace_shutdown(struct request_queue *q)
 {
 	mutex_lock(&q->debugfs_mutex);
 	if (rcu_dereference_protected(q->blk_trace,
-				      lockdep_is_held(&q->debugfs_mutex))) {
-		__blk_trace_startstop(q, 0);
+				      lockdep_is_held(&q->debugfs_mutex)))
 		__blk_trace_remove(q);
-	}
 
 	mutex_unlock(&q->debugfs_mutex);
 }
