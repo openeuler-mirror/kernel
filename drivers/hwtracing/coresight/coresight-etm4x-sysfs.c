b749b9c096b403ad5b82079fe2f05082108623d2
Suzuki K Poulose <suzuki.poulose@arm.com>
2022-11-08 14:38:27 +0800

    coresight: etm4x: Convert all register accesses
    mainline inclusion
    from mainline-v5.11-rc5
    commit f5bd523690d2ff7bc4bd3f535888eaf9349be176
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I5YCYK
    CVE: NA
    
    Reference: https://lore.kernel.org/r/20210201181351.1475223-11-mathieu.poirier@linaro.org
    
    --------------------------------------------------------------------------
    
    Convert all register accesses from etm4x driver to use a wrapper
    to allow switching the access at runtime with little overhead.
    
    co-developed by sed tool ;-), mostly equivalent to :
    
    s/readl\(_relaxed\)\?(drvdata->base + \(.*\))/etm4x_\1_read32(csdev, \2)
    s/writel\(_relaxed\)\?(\(.*\), drvdata->base + \(.*\))/etm4x_\1_write32(csdev, \2, \3)
    
    We don't want to replace them with the csdev_access_* to
    avoid a function call for every register access for system
    register access. This is a prepartory step to add system
    register access later where the support is available.
    
    Link: https://lore.kernel.org/r/20210110224850.1880240-9-suzuki.poulose@arm.com
    Cc: Mike Leach <mike.leach@linaro.org>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Link: https://lore.kernel.org/r/20210201181351.1475223-11-mathieu.poirier@linaro.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/hwtracing/coresight/coresight-etm4x-sysfs.c
+++ drivers/hwtracing/coresight/coresight-etm4x-sysfs.c
@@ -2323,7 +2323,8 @@ static struct attribute *coresight_etmv4_attrs[] = {
 };
 
 struct etmv4_reg {
-	void __iomem *addr;
+	struct coresight_device *csdev;
+	u32 offset;
 	u32 data;
 };
 
@@ -2331,14 +2332,16 @@ static void do_smp_cross_read(void *data)
 {
 	struct etmv4_reg *reg = data;
 
-	reg->data = readl_relaxed(reg->addr);
+	reg->data = etm4x_relaxed_read32(&reg->csdev->access, reg->offset);
 }
 
 static u32 etmv4_cross_read(const struct etmv4_drvdata *drvdata, u32 offset)
 {
 	struct etmv4_reg reg;
 
-	reg.addr = drvdata->base + offset;
+	reg.offset = offset;
+	reg.csdev = drvdata->csdev;
+
 	/*
 	 * smp cross call ensures the CPU will be powered up before
 	 * accessing the ETMv4 trace core registers
