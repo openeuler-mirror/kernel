a203d07d780fad39f3efeff0d848683dfce5dfb9
Namjae Jeon <linkinjeon@kernel.org>
2022-11-14 20:53:35 +0800

    ksmbd: limits exceeding the maximum allowable outstanding requests
    mainline inclusion
    from mainline-5.17-rc1
    commit b589f5db6d4af8f14d70e31e1276b4c017668a26
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I60T7G
    CVE: NA
    
    Reference: https://git.kernel.org/torvalds/linux/c/b589f5db6d4a
    
    -------------------------------
    
    If the client ignores the CreditResponse received from the server and
    continues to send the request, ksmbd limits the requests if it exceeds
    smb2 max credits.
    
    Signed-off-by: Namjae Jeon <linkinjeon@kernel.org>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Signed-off-by: Jason Yan <yanaijie@huawei.com>
    Signed-off-by: Zhong Jinghua <zhongjinghua@huawei.com>

--- fs/ksmbd/connection.c
+++ fs/ksmbd/connection.c
@@ -63,6 +63,7 @@ struct ksmbd_conn *ksmbd_conn_alloc(void)
 	atomic_set(&conn->req_running, 0);
 	atomic_set(&conn->r_count, 0);
 	conn->total_credits = 1;
+	conn->outstanding_credits = 1;
 
 	init_waitqueue_head(&conn->req_running_q);
 	init_waitqueue_head(&conn->r_count_q);
