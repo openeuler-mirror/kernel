30bc83a735dd77246984c0071abad170d16eb922
DuanqiangWen <duanqiangwen@net-swift.com>
2022-11-15 02:34:00 -0500

    openeuler: net: txgbe: fix arm32 compile error because 64bit div and warnning
    driver inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I5Y6RT
    CVE: NA
    
    1.fix arm32 compile error because 64bit div operate in txgbe_get_thermal_sensor_data function
    2.fix txgbe_sysfs compile warning.
    
    Signed-off-by: DuanqiangWen <duanqiangwen@net-swift.com>

--- drivers/net/ethernet/netswift/txgbe/txgbe_sysfs.c
+++ drivers/net/ethernet/netswift/txgbe/txgbe_sysfs.c
@@ -1,14 +1,3 @@
-/*
- * WangXun 10 Gigabit PCI Express Linux driver
- * Copyright (c) 2015 - 2017 Beijing WangXun Technology Co., Ltd.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
- *
- * This program is distributed in the hope it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
 // SPDX-License-Identifier: GPL-2.0
 /* Copyright (c) 2019 - 2022 Beijing WangXun Technology Co., Ltd. */
 
