8f6f9b84eef9abe01dbcf2cebce029dad536e125
Jiaxi Chen <jiaxi.chen@linux.intel.com>
2022-11-25 20:58:43 +0800

    KVM: x86: Advertise AVX-VNNI-INT8 CPUID to user space
    mainline inclusion
    from mainline-v6.2-rc1
    commit 24d74b9f5f2a972ac9228372adeac62b2dc10ea2
    category: feature
    bugzilla: https://gitee.com/openeuler/intel-kernel/issues/I8GYV5
    CVE: N/A
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=24d74b9f5f2a972ac9228372adeac62b2dc10ea2
    
    -------------------------------------
    
    Intel-SIG: commit 24d74b9f5f2a ("KVM: x86: Advertise AVX-VNNI-INT8 CPUID to user space")
    
    -------------------------------------
    
    KVM: x86: Advertise AVX-VNNI-INT8 CPUID to user space
    
    AVX-VNNI-INT8 is a new set of instructions in the latest Intel platform
    Sierra Forest, aims for the platform to have superior AI capabilities.
    This instruction multiplies the individual bytes of two unsigned or
    unsigned source operands, then adds and accumulates the results into the
    destination dword element size operand.
    
    The bit definition:
    CPUID.(EAX=7,ECX=1):EDX[bit 4]
    
    AVX-VNNI-INT8 is on a new and sparse CPUID leaf and all bits on this
    leaf have no truly kernel use case for now. Given that and to save space
    for kernel feature bits, move this new leaf to KVM-only subleaf and plus
    an x86_FEATURE definition for AVX-VNNI-INT8 to direct it to the KVM
    entry.
    
    Advertise AVX-VNNI-INT8 to KVM userspace. This is safe because there are
    no new VMX controls or additional host enabling required for guests to
    use this feature.
    
    Signed-off-by: Jiaxi Chen <jiaxi.chen@linux.intel.com>
    Message-Id: <20221125125845.1182922-7-jiaxi.chen@linux.intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Quanxian Wang <quanxian.wang@intel.com>

--- arch/x86/kvm/cpuid.h
+++ arch/x86/kvm/cpuid.h
@@ -14,6 +14,7 @@
  */
 enum kvm_only_cpuid_leafs {
 	CPUID_12_EAX	 = NCAPINTS,
+	CPUID_7_1_EDX,
 	CPUID_8000_0021_EAX,
 	NR_KVM_CPU_CAPS,
 
@@ -80,6 +81,9 @@ static inline bool kvm_vcpu_is_illegal_gpa(struct kvm_vcpu *vcpu, gpa_t gpa)
 	return (gpa >= BIT_ULL(cpuid_maxphyaddr(vcpu)));
 }
 
+/* Intel-defined sub-features, CPUID level 0x00000007:1 (EDX) */
+#define X86_FEATURE_AVX_VNNI_INT8       KVM_X86_FEATURE(CPUID_7_1_EDX, 4)
+
 struct cpuid_reg {
 	u32 function;
 	u32 index;
@@ -103,6 +107,7 @@ static const struct cpuid_reg reverse_cpuid[] = {
 	[CPUID_7_EDX]         = {         7, 0, CPUID_EDX},
 	[CPUID_7_1_EAX]       = {         7, 1, CPUID_EAX},
 	[CPUID_12_EAX]        = {0x00000012, 0, CPUID_EAX},
+	[CPUID_7_1_EDX]       = {         7, 1, CPUID_EDX},
 	[CPUID_8000_0021_EAX] = {0x80000021, 0, CPUID_EAX},
 };
 
