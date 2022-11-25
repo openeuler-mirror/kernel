d2d3b8e656fe494a16cd4b7f869e89b88862a3c4
Roman Gushchin <guro@fb.com>
2022-11-25 11:56:10 +0800

    bpf: sched: introduce bpf_sched_enable()
    maillist inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I5F6X6
    CVE: NA
    
    Reference: https://lore.kernel.org/all/20210916162451.709260-1-guro@fb.com/
    
    -------------------
    
    Introduce a dedicated static key and the bpf_sched_enabled() wrapper
    to guard all invocations of bpf programs in the scheduler code.
    
    It will help to avoid any potential performance regression in a case
    when no scheduler bpf programs are attached.
    
    Signed-off-by: Roman Gushchin <guro@fb.com>
    Signed-off-by: Chen Hui <judy.chenhui@huawei.com>
    Signed-off-by: Ren Zhijie <renzhijie2@huawei.com>
    Signed-off-by: Hui Tang <tanghui20@huawei.com>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -31,6 +31,7 @@
 #include <linux/poll.h>
 #include <linux/bpf-netns.h>
 #include <linux/rcupdate_trace.h>
+#include <linux/bpf_sched.h>
 
 #define IS_FD_ARRAY(map) ((map)->map_type == BPF_MAP_TYPE_PERF_EVENT_ARRAY || \
 			  (map)->map_type == BPF_MAP_TYPE_CGROUP_ARRAY || \
@@ -2531,6 +2532,11 @@ static void bpf_tracing_link_release(struct bpf_link *link)
 	struct bpf_tracing_link *tr_link =
 		container_of(link, struct bpf_tracing_link, link);
 
+#ifdef CONFIG_BPF_SCHED
+	if (link->prog->type == BPF_PROG_TYPE_SCHED)
+		bpf_sched_dec();
+#endif
+
 	WARN_ON_ONCE(bpf_trampoline_unlink_prog(link->prog,
 						tr_link->trampoline));
 
@@ -2717,6 +2723,11 @@ static int bpf_tracing_prog_attach(struct bpf_prog *prog,
 		link = NULL;
 		goto out_unlock;
 	}
+
+#ifdef CONFIG_BPF_SCHED
+	if (prog->type == BPF_PROG_TYPE_SCHED)
+		bpf_sched_inc();
+#endif
 
 	link->tgt_prog = tgt_prog;
 	link->trampoline = tr;
