2bc344acbb2b930cac3e6b9c555b586b6d140d58
Chen Hui <judy.chenhui@huawei.com>
2022-11-25 11:56:23 +0800

    bpf:programmable: Add cpumask ops collection
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I5KUFB
    CVE: NA
    
    --------------------------------
    
    Add cpumask ops collection, such as cpumask_empty,
    cpumask_and, cpumask_andnot, cpumask_subset,
    cpumask_equal, cpumask_copy.
    
    Signed-off-by: Chen Hui <judy.chenhui@huawei.com>
    Signed-off-by: Hui Tang <tanghui20@huawei.com>

--- scripts/bpf_helpers_doc.py
+++ scripts/bpf_helpers_doc.py
@@ -440,6 +440,8 @@ class PrinterHelpers(Printer):
             'struct bpf_cpu_topology',
             'struct bpf_cpumask_info',
             'struct sched_entity',
+            'struct cpumask',
+            'struct cpumask_op_args',
     ]
     known_types = {
             '...',
@@ -488,6 +490,8 @@ class PrinterHelpers(Printer):
             'struct bpf_cpu_topology',
             'struct bpf_cpumask_info',
             'struct sched_entity',
+            'struct cpumask',
+            'struct cpumask_op_args',
     }
     mapped_types = {
             'u8': '__u8',
