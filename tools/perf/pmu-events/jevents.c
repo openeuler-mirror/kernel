a04b3c6f456f839505c4c9cf68fe2d16caa9d309
Qi Liu <liuqi115@huawei.com>
2022-11-30 19:01:39 +0800

    perf jevents: Add support for HiSilicon SLLC PMU aliasing
    driver inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I63VF5
    
    --------------------------------------------------------------------------
    
    Add support for HiSilicon SLLC PMU aliasing on Hip09 platform.
    
    Signed-off-by: Qi Liu <liuqi115@huawei.com>
    Signed-off-by: Junhao He <hejunhao3@huawei.com>

--- tools/perf/pmu-events/jevents.c
+++ tools/perf/pmu-events/jevents.c
@@ -281,6 +281,7 @@ static struct map {
 	{ "hisi_sccl,ddrc", "hisi_sccl,ddrc" },
 	{ "hisi_sccl,hha", "hisi_sccl,hha" },
 	{ "hisi_sccl,l3c", "hisi_sccl,l3c" },
+	{ "hisi_sccl,sllc", "hisi_sccl,sllc" },
 	{ "L3PMC", "amd_l3" },
 	{ "DFPMC", "amd_df" },
 	{}
