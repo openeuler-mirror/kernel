3c7d1aac94eb53581dc580b5cdaa9d1e112eb458
Youling Tang <tangyouling@loongson.cn>
2022-12-10 22:39:48 +0800

    LoongArch: Switch to relative exception tables
    mainline inclusion
    from mainline-v6.2-rc1
    commit 3d36f4298ba91fbdec6bc56aa7bb0663cba6ab0c
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I6BWFP
    CVE: NA
    
    --------------------------------
    
    Similar to other architectures such as arm64, x86, riscv and so on, use
    offsets relative to the exception table entry values rather than their
    absolute addresses for both the exception location and the fixup.
    
    However, LoongArch label difference because it will actually produce two
    relocations, a pair of R_LARCH_ADD32 and R_LARCH_SUB32. Take simple code
    below for example:
    
    $ cat test_ex_table.S
    .section .text
    1:
            nop
    .section __ex_table,"a"
            .balign 4
            .long (1b - .)
    .previous
    
    $ loongarch64-unknown-linux-gnu-gcc -c test_ex_table.S
    $ loongarch64-unknown-linux-gnu-readelf -Wr test_ex_table.o
    
    Relocation section '.rela__ex_table' at offset 0x100 contains 2 entries:
        Offset            Info             Type         Symbol's Value   Symbol's Name + Addend
    0000000000000000 0000000600000032 R_LARCH_ADD32    0000000000000000  .L1^B1 + 0
    0000000000000000 0000000500000037 R_LARCH_SUB32    0000000000000000  L0^A + 0
    
    The modpost will complain the R_LARCH_SUB32 relocation, so we need to
    patch modpost.c to skip this relocation for .rela__ex_table section.
    
    Signed-off-by: Youling Tang <tangyouling@loongson.cn>
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>
    Change-Id: If42e54904f36a0d7cafc40a75fd89b582fbaa09e
    (cherry picked from commit a2603778e57ece2a4f0ff1594361ff37a0d4bd22)

--- scripts/sorttable.c
+++ scripts/sorttable.c
@@ -347,11 +347,11 @@ static int do_file(char const *const fname, void *addr)
 	case EM_PPC:
 	case EM_ARM:
 	case EM_PPC64:
+	case EM_LOONGARCH:
 		custom_sort = sort_relative_table;
 		break;
 	case EM_ARCOMPACT:
 	case EM_ARCV2:
-	case EM_LOONGARCH:
 	case EM_MICROBLAZE:
 	case EM_MIPS:
 	case EM_XTENSA:
