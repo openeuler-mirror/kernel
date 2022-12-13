77ba50381cbc1b29533f2c15aa60413761bc367e
Dai Xin <daixin@wxiat.com>
2022-12-13 09:24:49 +0000

    sw64: split out the idle loop in idle.c
    Sunway inclusion
    category: performance
    bugzilla: https://gitee.com/openeuler/kernel/issues/I5XTK7
    
    --------------------------------
    
    Signed-off-by: Dai Xin <daixin@wxiat.com>
    Reviewed-by: He Sheng <hesheng@wxiat.com>
    Signed-off-by: Gu Zitao <guzitao@wxiat.com>

--- arch/sw_64/kernel/process.c
+++ arch/sw_64/kernel/process.c
@@ -26,29 +26,6 @@ struct halt_info {
 	char *restart_cmd;
 };
 
-#ifdef CONFIG_HOTPLUG_CPU
-void arch_cpu_idle_dead(void)
-{
-	play_dead();
-}
-#endif
-
-void arch_cpu_idle(void)
-{
-	int i;
-
-	local_irq_enable();
-	cpu_relax();
-
-	if (is_in_guest())
-		hcall(HCALL_HALT, 0, 0, 0);
-	else {
-		for (i = 0; i < 16; i++)
-			asm("nop");
-		asm("halt");
-	}
-}
-
 static void common_shutdown_1(void *generic_ptr)
 {
 	struct halt_info *how = (struct halt_info *)generic_ptr;
