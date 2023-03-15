771e157b8c50cc59fb256294be14a27bce39d64a
Yu Kuai <yukuai3@huawei.com>
2023-03-15 17:37:41 +0800

    raid1, raid10: switch to precise io accounting
    hulk inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I6L586
    CVE: NA
    
    --------------------------------
    
    'ios' and 'sectors' is counted in bio_start_io_acct() while io is
    started insted of io is done. Hence switch to precise io accounting to
    count them when io is done.
    
    Signed-off-by: Yu Kuai <yukuai3@huawei.com>
    Reviewed-by: Hou Tao <houtao1@huawei.com>
    Signed-off-by: Jialin Zhang <zhangjialin11@huawei.com>

--- drivers/md/raid1.c
+++ drivers/md/raid1.c
@@ -301,7 +301,7 @@ static void call_bio_endio(struct r1bio *r1_bio)
 		bio->bi_status = BLK_STS_IOERR;
 
 	if (blk_queue_io_stat(bio->bi_disk->queue))
-		bio_end_io_acct(bio, r1_bio->start_time);
+		bio_end_precise_io_acct(bio, r1_bio->start_time);
 	bio_endio(bio);
 }
 
@@ -1295,7 +1295,7 @@ static void raid1_read_request(struct mddev *mddev, struct bio *bio,
 	r1_bio->read_disk = rdisk;
 
 	if (!r1bio_existed && blk_queue_io_stat(bio->bi_disk->queue))
-		r1_bio->start_time = bio_start_io_acct(bio);
+		r1_bio->start_time = bio_start_precise_io_acct(bio);
 
 	read_bio = bio_clone_fast(bio, gfp, &mddev->bio_set);
 
@@ -1487,7 +1487,7 @@ static void raid1_write_request(struct mddev *mddev, struct bio *bio,
 	}
 
 	if (blk_queue_io_stat(bio->bi_disk->queue))
-		r1_bio->start_time = bio_start_io_acct(bio);
+		r1_bio->start_time = bio_start_precise_io_acct(bio);
 	atomic_set(&r1_bio->remaining, 1);
 	atomic_set(&r1_bio->behind_remaining, 0);
 
