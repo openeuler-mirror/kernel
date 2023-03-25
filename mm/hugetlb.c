a19acce44557d100f54ab69240deb49f32b36f3b
Peng Liu <liupeng256@huawei.com>
2023-03-25 11:19:16 +0800

    hugetlb: fix wrong use of nr_online_nodes
    mainline inclusion
    from mainline-v5.19-rc1
    commit 0a7a0f6f7f3679c906fc55e3805c1d5e2c566f55
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I6PMWS
    CVE: NA
    
    --------------------------------
    
    Patch series "hugetlb: Fix some incorrect behavior", v3.
    
    This series fix three bugs of hugetlb:
    1) Invalid use of nr_online_nodes;
    2) Inconsistency between 1G hugepage and 2M hugepage;
    3) Useless information in dmesg.
    
    This patch (of 4):
    
    Certain systems are designed to have sparse/discontiguous nodes.  In this
    case, nr_online_nodes can not be used to walk through numa node.  Also, a
    valid node may be greater than nr_online_nodes.
    
    However, in hugetlb, it is assumed that nodes are contiguous.
    
    For sparse/discontiguous nodes, the current code may treat a valid node
    as invalid, and will fail to allocate all hugepages on a valid node that
    "nid >= nr_online_nodes".
    
    As David suggested:
    
    	if (tmp >= nr_online_nodes)
    		goto invalid;
    
    Just imagine node 0 and node 2 are online, and node 1 is offline.
    Assuming that "node < 2" is valid is wrong.
    
    Recheck all the places that use nr_online_nodes, and repair them one by
    one.
    
    [liupeng256@huawei.com: v4]
      Link: https://lkml.kernel.org/r/20220416103526.3287348-1-liupeng256@huawei.com
    Link: https://lkml.kernel.org/r/20220413032915.251254-1-liupeng256@huawei.com
    Link: https://lkml.kernel.org/r/20220413032915.251254-2-liupeng256@huawei.com
    Fixes: 4178158ef8ca ("hugetlbfs: fix issue of preallocation of gigantic pages can't work")
    Fixes: b5389086ad7b ("hugetlbfs: extend the definition of hugepages parameter to support node allocation")
    Fixes: e79ce9832316 ("hugetlbfs: fix a truncation issue in hugepages parameter")
    Fixes: f9317f77a6e0 ("hugetlb: clean up potential spectre issue warnings")
    Signed-off-by: Peng Liu <liupeng256@huawei.com>
    Suggested-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Reviewed-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Reviewed-by: Davidlohr Bueso <dave@stgolabs.net>
    Reviewed-by: Mike Kravetz <mike.kravetz@oracle.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Cc: Zhenguo Yao <yaozhenguo1@gmail.com>
    Cc: Muchun Song <songmuchun@bytedance.com>
    Cc: Liu Yuntao <liuyuntao10@huawei.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Conflicts:
    	mm/hugetlb.c
    Signed-off-by: Liu Shixin <liushixin2@huawei.com>
    Reviewed-by: tong tiangen <tongtiangen@huawei.com>
    Signed-off-by: Yongqiang Liu <liuyongqiang13@huawei.com>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -2514,9 +2514,6 @@ int __alloc_bootmem_huge_page(struct hstate *h, int nid)
 	struct huge_bootmem_page *m = NULL; /* initialize for clang */
 	int nr_nodes, node;
 
-	if (nid != NUMA_NO_NODE && nid >= nr_online_nodes)
-		return 0;
-
 	if (!huge_page_limit_check(HUGE_PAGE_BOOTMEM_ALLOC, huge_page_size(h), nid))
 		return 0;
 
@@ -2624,7 +2621,7 @@ static void __init hugetlb_hstate_alloc_pages(struct hstate *h)
 	bool node_specific_alloc = false;
 
 	/* do node specific alloc */
-	for (i = 0; i < nr_online_nodes; i++) {
+	for_each_online_node(i) {
 		if (h->max_huge_pages_node[i] > 0) {
 			hugetlb_hstate_alloc_pages_onenode(h, i);
 			node_specific_alloc = true;
@@ -4243,7 +4240,7 @@ static int __init hugetlb_init(void)
 		if (!default_hstate.max_huge_pages) {
 			default_hstate.max_huge_pages = default_hstate_max_huge_pages;
 
-			for (i = 0; i < nr_online_nodes; i++)
+			for_each_online_node(i)
 				default_hstate.max_huge_pages_node[i] =
 					default_hugepages_in_node[i];
 		}
@@ -4367,7 +4364,7 @@ static int __init hugetlb_nrpages_setup(char *s)
 				pr_warn("HugeTLB: architecture can't support node specific alloc, ignoring!\n");
 				return 0;
 			}
-			if (tmp >= nr_online_nodes)
+			if (tmp >= MAX_NUMNODES || !node_online(tmp))
 				goto invalid;
 			node = tmp;
 			p += count + 1;
