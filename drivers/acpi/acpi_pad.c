fc2503c8ed1bf31a946efbf0de5a9d58c758bf1c
leoliuoc <leoliu-oc@zhaoxin.com>
2023-04-03 15:06:43 +0800

    ACPI, x86: Improve Zhaoxin processors support for NONSTOP TSC
    zhaoxin inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I6SJQ8
    CVE: NA
    
    ----------------------------------------
    
    Zhaoxin CPUs  which use CENTAUR as vendor id, also have NONSTOP TSC
    feature. So enable the ACPI driver support for it too.
    
    Signed-off-by: leoliuoc <leoliu-oc@zhaoxin.com>

--- drivers/acpi/acpi_pad.c
+++ drivers/acpi/acpi_pad.c
@@ -65,6 +65,7 @@ static void power_saving_mwait_init(void)
 	case X86_VENDOR_HYGON:
 	case X86_VENDOR_AMD:
 	case X86_VENDOR_INTEL:
+	case X86_VENDOR_CENTAUR:
 	case X86_VENDOR_ZHAOXIN:
 		/*
 		 * AMD Fam10h TSC will tick in all
