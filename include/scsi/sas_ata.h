28bc42bfefb7c55c540648dab90eff6980e73e9f
Jie Zhan <zhanjie9@hisilicon.com>
2023-04-14 16:20:39 +0800

    scsi: libsas: Do not export sas_ata_wait_after_reset()
    mainline inclusion
    from mainline-v6.2-rc1
    commit 4d450cf2b00d
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I6V6I1
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=4d450cf2b00d
    
    ----------------------------------------------------------------------
    
    sas_ata_wait_after_reset() does not need to be exported since it is no
    longer referenced outside libsas.
    
    Signed-off-by: Jie Zhan <zhanjie9@hisilicon.com>
    Link: https://lore.kernel.org/r/20221118083714.4034612-6-zhanjie9@hisilicon.com
    Reviewed-by: John Garry <john.garry@huawei.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: xiabing <xiabing12@h-partners.com>

--- include/scsi/sas_ata.h
+++ include/scsi/sas_ata.h
@@ -34,7 +34,6 @@ void sas_suspend_sata(struct asd_sas_port *port);
 void sas_resume_sata(struct asd_sas_port *port);
 void sas_ata_end_eh(struct ata_port *ap);
 void sas_ata_device_link_abort(struct domain_device *dev, bool force_reset);
-int sas_ata_wait_after_reset(struct domain_device *dev, unsigned long deadline);
 int smp_ata_check_ready_type(struct ata_link *link);
 #else
 
@@ -92,12 +91,6 @@ static inline void sas_ata_end_eh(struct ata_port *ap)
 static inline void sas_ata_device_link_abort(struct domain_device *dev,
 					     bool force_reset)
 {
-}
-
-static inline int sas_ata_wait_after_reset(struct domain_device *dev,
-					   unsigned long deadline)
-{
-	return -ETIMEDOUT;
 }
 
 static inline int smp_ata_check_ready_type(struct ata_link *link)
