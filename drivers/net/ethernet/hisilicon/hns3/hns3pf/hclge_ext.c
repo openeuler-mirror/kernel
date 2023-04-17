0f4d888eac4191c5ff81af35cf9934e0f7c9458f
shaojijie <shaojijie@huawei.com>
2023-04-17 19:40:57 +0800

    net: hns3: add support disable nic clock
    driver inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I97T47
    CVE: NA
    
    ----------------------------------------------------------------------
    
    The patch disables the clocks of the PPP, PPU, IGU_EGU, ROCE, and
    TM modules by configuring the network subctrl register.
    
    Signed-off-by: shaojijie <shaojijie@huawei.com>
    Signed-off-by: Jiantao Xiao <xiaojiantao1@h-partners.com>

--- drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_ext.c
+++ drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_ext.c
@@ -534,6 +534,22 @@ static int hclge_get_net_lane_status(struct hclge_dev *hdev, void *data,
 	return 0;
 }
 
+static int hclge_disable_nic_clock(struct hclge_dev *hdev, void *data,
+				   size_t length)
+{
+	struct hclge_desc desc;
+	int ret;
+
+	hclge_cmd_setup_basic_desc(&desc, HCLGE_OPC_CONFIG_NIC_CLOCK, false);
+	desc.data[0] = 0;
+
+	ret = hclge_cmd_send(&hdev->hw, &desc, 1);
+	if (ret)
+		dev_err(&hdev->pdev->dev,
+			"failed to disable nic clock, ret = %d\n", ret);
+	return ret;
+}
+
 static void hclge_ext_resotre_config(struct hclge_dev *hdev)
 {
 	if (hdev->reset_type != HNAE3_IMP_RESET &&
@@ -700,6 +716,7 @@ static const hclge_priv_ops_fn hclge_ext_func_arr[] = {
 	[HNAE3_EXT_OPC_SET_SFP_STATE] = hclge_set_sfp_state,
 	[HNAE3_EXT_OPC_DISABLE_LANE] = hclge_disable_net_lane,
 	[HNAE3_EXT_OPC_GET_LANE_STATUS] = hclge_get_net_lane_status,
+	[HNAE3_EXT_OPC_DISABLE_CLOCK] = hclge_disable_nic_clock,
 };
 
 int hclge_ext_ops_handle(struct hnae3_handle *handle, int opcode,
