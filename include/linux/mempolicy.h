94e5cb7eda65f27b7373994b353fef191d6afcf5
Feng Tang <feng.tang@intel.com>
2023-04-27 19:13:50 +0800

    mm/hugetlb: add dedicated func to get 'allowed' nodemask for current process
    mainline inclusion
    from mainline-v6.1-rc1
    commit d2226ebd5484afcf9f9b71b394ec1567a7730eb1
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I6I1Z2
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=d2226ebd5484afcf9f9b71b394ec1567a7730eb1
    
    --------------------------------
    
    Muchun Song found that after MPOL_PREFERRED_MANY policy was introduced in
    commit b27abaccf8e8 ("mm/mempolicy: add MPOL_PREFERRED_MANY for multiple
    preferred nodes"), the policy_nodemask_current()'s semantics for this new
    policy has been changed, which returns 'preferred' nodes instead of
    'allowed' nodes.
    
    With the changed semantic of policy_nodemask_current, a task with
    MPOL_PREFERRED_MANY policy could fail to get its reservation even though
    it can fall back to other nodes (either defined by cpusets or all online
    nodes) for that reservation failing mmap calles unnecessarily early.
    
    The fix is to not consider MPOL_PREFERRED_MANY for reservations at all
    because they, unlike MPOL_MBIND, do not pose any actual hard constrain.
    
    Michal suggested the policy_nodemask_current() is only used by hugetlb,
    and could be moved to hugetlb code with more explicit name to enforce the
    'allowed' semantics for which only MPOL_BIND policy matters.
    
    apply_policy_zone() is made extern to be called in hugetlb code and its
    return value is changed to bool.
    
    [1]. https://lore.kernel.org/lkml/20220801084207.39086-1-songmuchun@bytedance.com/t/
    
    Link: https://lkml.kernel.org/r/20220805005903.95563-1-feng.tang@intel.com
    Fixes: b27abaccf8e8 ("mm/mempolicy: add MPOL_PREFERRED_MANY for multiple preferred nodes")
    Signed-off-by: Feng Tang <feng.tang@intel.com>
    Reported-by: Muchun Song <songmuchun@bytedance.com>
    Suggested-by: Michal Hocko <mhocko@suse.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Reviewed-by: Muchun Song <songmuchun@bytedance.com>
    Cc: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: Dave Hansen <dave.hansen@intel.com>
    Cc: Ben Widawsky <bwidawsk@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Conflicts:
    	include/linux/mempolicy.h
    	mm/hugetlb.c
    Signed-off-by: Ma Wupeng <mawupeng1@huawei.com>

--- include/linux/mempolicy.h
+++ include/linux/mempolicy.h
@@ -155,13 +155,6 @@ extern bool mempolicy_nodemask_intersects(struct task_struct *tsk,
 				const nodemask_t *mask);
 extern nodemask_t *policy_nodemask(gfp_t gfp, struct mempolicy *policy);
 
-static inline nodemask_t *policy_nodemask_current(gfp_t gfp)
-{
-	struct mempolicy *mpol = get_task_policy(current);
-
-	return policy_nodemask(gfp, mpol);
-}
-
 extern unsigned int mempolicy_slab_node(void);
 
 extern enum zone_type policy_zone;
@@ -210,6 +203,9 @@ static inline bool mpol_is_preferred_many(struct mempolicy *pol)
 {
 	return  (pol->mode == MPOL_PREFERRED_MANY);
 }
+
+extern bool apply_policy_zone(struct mempolicy *policy, enum zone_type zone);
+
 #else
 
 struct mempolicy {};
@@ -319,11 +315,6 @@ static inline long __do_mbind(unsigned long start, unsigned long len,
 
 static inline void mpol_put_task_policy(struct task_struct *task)
 {
-}
-
-static inline nodemask_t *policy_nodemask_current(gfp_t gfp)
-{
-	return NULL;
 }
 
 static inline bool mpol_is_preferred_many(struct mempolicy *pol)
