9b0317abfd0122b81f4cf72b2b364f1b4a8ba300
Christoph Hellwig <hch@lst.de>
2023-05-31 14:58:17 +0800

    block: merge disk_scan_partitions and blkdev_reread_part
    mainline inclusion
    from mainline-v5.17-rc1
    commit e16e506ccd673a3a888a34f8f694698305840044
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I6MQLP
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=e16e506ccd673a3a888a34f8f694698305840044
    
    --------------------------------
    
    Unify the functionality that implements a partition rescan for a
    gendisk.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Link: https://lore.kernel.org/r/20211122130625.1136848-6-hch@lst.de
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Conflicts:
    	block/blk.h
    	block/genhd.c
    	block/ioctl.c
    Signed-off-by: Yu Kuai <yukuai3@huawei.com>
    Reviewed-by: Hou Tao <houtao1@huawei.com>
    Signed-off-by: Jialin Zhang <zhangjialin11@huawei.com>
    (cherry picked from commit 1c0b1b48e3cee71e05820ab9cd53ec3a33fc953e)

--- block/ioctl.c
+++ block/ioctl.c
@@ -90,31 +90,6 @@ static int compat_blkpg_ioctl(struct block_device *bdev,
 }
 #endif
 
-static int blkdev_reread_part(struct block_device *bdev, fmode_t mode)
-{
-	struct block_device *tmp;
-
-	if (!disk_part_scan_enabled(bdev->bd_disk) || bdev_is_partition(bdev))
-		return -EINVAL;
-	if (!capable(CAP_SYS_ADMIN))
-		return -EACCES;
-	if (bdev->bd_part_count)
-		return -EBUSY;
-
-	/*
-	 * Reopen the device to revalidate the driver state and force a
-	 * partition rescan.
-	 */
-	mode &= ~FMODE_EXCL;
-	set_bit(GD_NEED_PART_SCAN, &bdev->bd_disk->state);
-
-	tmp = blkdev_get_by_dev(bdev->bd_dev, mode, NULL);
-	if (IS_ERR(tmp))
-		return PTR_ERR(tmp);
-	blkdev_put(tmp, mode);
-	return 0;
-}
-
 static int blk_ioctl_discard(struct block_device *bdev, fmode_t mode,
 		unsigned long arg, unsigned long flags)
 {
@@ -562,7 +537,13 @@ static int blkdev_common_ioctl(struct block_device *bdev, fmode_t mode,
 		bdev->bd_bdi->ra_pages = (arg * 512) / PAGE_SIZE;
 		return 0;
 	case BLKRRPART:
-		return blkdev_reread_part(bdev, mode);
+		if (!capable(CAP_SYS_ADMIN))
+			return -EACCES;
+		if (bdev_is_partition(bdev))
+			return -EINVAL;
+		if (bdev->bd_part_count)
+			return -EBUSY;
+		return disk_scan_partitions(bdev->bd_disk, mode & ~FMODE_EXCL);
 	case BLKTRACESTART:
 	case BLKTRACESTOP:
 	case BLKTRACETEARDOWN:
