f29efbf31be2eefdbc2c22c38c139da788d7ea3f
Zhangfei Gao <zhangfei.gao@linaro.org>
2023-06-02 14:09:01 +0800

    uacce: use filep->f_mapping to replace inode->i_mapping
    driver inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I79JRM
    CVE: NA
    
    ----------------------------------------------------------------------
    
    The inode can be different in a container, for example, a docker and host
    both open the same uacce parent device, which uses the same uacce struct
    but different inode, so uacce->inode is not enough.
    
    What's worse, when docker stopped, the inode will be destroyed as well,
    causing use-after-free in uacce_remove.
    
    So use q->filep->f_mapping to replace uacce->inode->i_mapping.
    
    Signed-off-by: Zhangfei Gao <zhangfei.gao@linaro.org>
    Signed-off-by: JiangShui Yang <yangjiangshui@h-partners.com>

--- drivers/misc/uacce/uacce.c
+++ drivers/misc/uacce/uacce.c
@@ -355,8 +355,8 @@ static int uacce_fops_open(struct inode *inode, struct file *filep)
 	atomic_inc(&uacce->ref);
 	init_waitqueue_head(&q->wait);
 	filep->private_data = q;
-	uacce->inode = inode;
 	q->state = UACCE_Q_INIT;
+	q->private_data = filep;
 	mutex_init(&q->mutex);
 	list_add(&q->list, &uacce->queues);
 	mutex_unlock(&uacce->mutex);
@@ -1054,12 +1054,6 @@ void uacce_remove(struct uacce_device *uacce)
 
 	if (!uacce)
 		return;
-	/*
-	 * unmap remaining mapping from user space, preventing user still
-	 * access the mmaped area while parent device is already removed
-	 */
-	if (uacce->inode)
-		unmap_mapping_range(uacce->inode->i_mapping, 0, 0, 1);
 
 	/*
 	 * uacce_fops_open() may be running concurrently, even after we remove
@@ -1069,6 +1063,8 @@ void uacce_remove(struct uacce_device *uacce)
 	mutex_lock(&uacce->mutex);
 	/* ensure no open queue remains */
 	list_for_each_entry_safe(q, next_q, &uacce->queues, list) {
+		struct file *filep = q->private_data;
+
 		/*
 		 * Taking q->mutex ensures that fops do not use the defunct
 		 * uacce->ops after the queue is disabled.
@@ -1077,6 +1073,12 @@ void uacce_remove(struct uacce_device *uacce)
 		uacce_put_queue(q);
 		mutex_unlock(&q->mutex);
 		uacce_unbind_queue(q);
+
+		/*
+		 * unmap remaining mapping from user space, preventing user still
+		 * access the mmaped area while parent device is already removed
+		 */
+		unmap_mapping_range(filep->f_mapping, 0, 0, 1);
 	}
 
 	/* disable sva now since no opened queues */
