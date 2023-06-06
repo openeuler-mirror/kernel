3b34acf0f4705ce76c18edb435f1e16db5329361
Dave Chinner <dchinner@redhat.com>
2023-06-06 11:40:17 +0800

    xfs: rework attr2 feature and mount options
    mainline inclusion
    from mainline-v5.14-rc4
    commit e23b55d537c9be60ae918fa6c3be0d699986f346
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I4KIAO
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=e23b55d537c9be60ae918fa6c3be0d699986f346
    
    --------------------------------
    
    The attr2 feature is somewhat unique in that it has both a superblock
    feature bit to enable it and mount options to enable and disable it.
    
    Back when it was first introduced in 2005, attr2 was disabled unless
    either the attr2 superblock feature bit was set, or the attr2 mount
    option was set. If the superblock feature bit was not set but the
    mount option was set, then when the first attr2 format inode fork
    was created, it would set the superblock feature bit. This is as it
    should be - the superblock feature bit indicated the presence of the
    attr2 on disk format.
    
    The noattr2 mount option, however, did not affect the superblock
    feature bit. If noattr2 was specified, the on-disk superblock
    feature bit was ignored and the code always just created attr1
    format inode forks.  If neither of the attr2 or noattr2 mounts
    option were specified, then the behaviour was determined by the
    superblock feature bit.
    
    This was all pretty sane.
    
    Fast foward 3 years, and we are dealing with fallout from the
    botched sb_features2 addition and having to deal with feature
    mismatches between the sb_features2 and sb_bad_features2 fields. The
    attr2 feature bit was one of these flags. The reconciliation was
    done well after mount option parsing and, unfortunately, the feature
    reconciliation had a bug where it ignored the noattr2 mount option.
    
    For reasons lost to the mists of time, it was decided that resolving
    this issue in commit 7c12f296500e ("[XFS] Fix up noattr2 so that it
    will properly update the versionnum and features2 fields.") required
    noattr2 to clear the superblock attr2 feature bit.  This greatly
    complicated the attr2 behaviour and broke rules about feature bits
    needing to be set when those specific features are present in the
    filesystem.
    
    By complicated, I mean that it introduced problems due to feature
    bit interactions with log recovery. All of the superblock feature
    bit checks are done prior to log recovery, but if we crash after
    removing a feature bit, then on the next mount we see the feature
    bit in the unrecovered superblock, only to have it go away after the
    log has been replayed.  This means our mount time feature processing
    could be all wrong.
    
    Hence you can mount with noattr2, crash shortly afterwards, and
    mount again without attr2 or noattr2 and still have attr2 enabled
    because the second mount sees attr2 still enabled in the superblock
    before recovery runs and removes the feature bit. It's just a mess.
    
    Further, this is all legacy code as the v5 format requires attr2 to
    be enabled at all times and it cannot be disabled.  i.e. the noattr2
    mount option returns an error when used on v5 format filesystems.
    
    To straighten this all out, this patch reverts the attr2/noattr2
    mount option behaviour back to the original behaviour. There is no
    reason for disabling attr2 these days, so we will only do this when
    the noattr2 mount option is set. This will not remove the superblock
    feature bit. The superblock bit will provide the default behaviour
    and only track whether attr2 is present on disk or not. The attr2
    mount option will enable the creation of attr2 format inode forks,
    and if the superblock feature bit is not set it will be added when
    the first attr2 inode fork is created.
    
    Signed-off-by: Dave Chinner <dchinner@redhat.com>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Long Li <leo.lilong@huawei.com>
    (cherry picked from commit e552c98151aee15e5e5e2a8e58c9a2f47140398e)

--- fs/xfs/xfs_super.c
+++ fs/xfs/xfs_super.c
@@ -1008,14 +1008,6 @@ xfs_finish_flags(
 	}
 
 	/*
-	 * mkfs'ed attr2 will turn on attr2 mount unless explicitly
-	 * told by noattr2 to turn it off
-	 */
-	if (xfs_sb_version_hasattr2(&mp->m_sb) &&
-	    !(mp->m_flags & XFS_MOUNT_NOATTR2))
-		mp->m_flags |= XFS_MOUNT_ATTR2;
-
-	/*
 	 * prohibit r/w mounts of read-only filesystems
 	 */
 	if ((mp->m_sb.sb_flags & XFS_SBF_READONLY) && !ronly) {
@@ -1377,7 +1369,6 @@ xfs_fc_parse_param(
 		return 0;
 	case Opt_noattr2:
 		xfs_fs_warn_deprecated(fc, param, XFS_MOUNT_NOATTR2, true);
-		parsing_mp->m_flags &= ~XFS_MOUNT_ATTR2;
 		parsing_mp->m_flags |= XFS_MOUNT_NOATTR2;
 		return 0;
 	default:
@@ -1400,6 +1391,13 @@ xfs_fc_validate_params(
 		xfs_warn(mp, "no-recovery mounts must be read-only.");
 		return -EINVAL;
 	}
+
+	if ((mp->m_flags & (XFS_MOUNT_ATTR2|XFS_MOUNT_NOATTR2)) ==
+			  (XFS_MOUNT_ATTR2|XFS_MOUNT_NOATTR2)) {
+		xfs_warn(mp, "attr2 and noattr2 cannot both be specified.");
+		return -EINVAL;
+	}
+
 
 	if ((mp->m_flags & XFS_MOUNT_NOALIGN) &&
 	    (mp->m_dalign || mp->m_swidth)) {
