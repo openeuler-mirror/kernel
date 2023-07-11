3a461cca3350df3d27a99bd7758ef45264384e58
Wei Li <liwei391@huawei.com>
2023-07-11 16:17:50 +0800

    checkpatch: Update check of link tags
    hulk inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I7KBJI
    
    --------------------------------
    
    Custom 'bugzilla' and 'Reference' tags can also be URL, update the check
    of link tags.
    
    Signed-off-by: Wei Li <liwei391@huawei.com>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -620,7 +620,7 @@ our $signature_tags = qr{(?xi:
 	Cc:
 )};
 
-our @link_tags = qw(Link Closes);
+our @link_tags = qw(Link Closes bugzilla Reference);
 
 #Create a search and print patterns for all these strings to be used directly below
 our $link_tags_search = "";
