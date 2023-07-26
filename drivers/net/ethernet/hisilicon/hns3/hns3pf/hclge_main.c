564846ff305c1e030958ed610bf4b5d5ee530360
Hao Chen <chenhao418@huawei.com>
2023-07-26 10:02:12 +0800

    net: hns3: correct the logic of hclge_sync_vf_qb_mode()
    driver inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I7YRUW
    CVE: NA
    
    ----------------------------------------------------------------------
    
    Now, hns3 driver set request_enable to qb_en whether hclge_set_fd_qb()
    returns failure, it's incorrect.
    
    We only set request_enable to qb_en when hclge_set_fd_qb() returns 0.
    so, change it.
    
    Signed-off-by: Hao Chen <chenhao418@huawei.com>

--- drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_main.c
+++ drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_main.c
@@ -4619,7 +4619,8 @@ static int hclge_sync_vf_qb_mode(struct hclge_vport *vport)
 	ret = hclge_set_fd_qb(hdev, vport->vport_id, request_enable);
 	if (ret)
 		set_bit(HCLGE_VPORT_STATE_QB_CHANGE, &vport->state);
-	vport->vf_info.qb_en = request_enable ? 1 : 0;
+	else
+		vport->vf_info.qb_en = request_enable ? 1 : 0;
 
 	return ret;
 }
