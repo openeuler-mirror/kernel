2c7b35364b9a23c5198bc5af97cee0cf8abcc66f
Jiantao Xiao <xiaojiantao1@h-partners.com>
2023-08-09 11:46:44 +0800

    Revert "net: hns3: disbable pfc en before the reset"
    driver inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I7S5M4
    CVE: NA
    
    ----------------------------------------------------------------------
    
    This reverts commit 29e4bb1adeaa5636e054392e7223bdf7fcd8ad03.
    
    Signed-off-by: Jiantao Xiao <xiaojiantao1@h-partners.com>

--- drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_ext.c
+++ drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_ext.c
@@ -6,7 +6,6 @@
 #include "hnae3_ext.h"
 #include "hclge_cmd.h"
 #include "hclge_ext.h"
-#include "hclge_tm.h"
 
 static nic_event_fn_t nic_event_call;
 
@@ -1449,7 +1448,6 @@ static const hclge_priv_ops_fn hclge_ext_func_arr[] = {
 	[HNAE3_EXT_OPC_DISABLE_LANE] = hclge_disable_net_lane,
 	[HNAE3_EXT_OPC_GET_LANE_STATUS] = hclge_get_net_lane_status,
 	[HNAE3_EXT_OPC_DISABLE_CLOCK] = hclge_disable_nic_clock,
-	[HNAE3_EXT_OPC_SET_PFC_TIME] = hclge_set_pause_trans_time,
 	[HNAE3_EXT_OPC_GET_HILINK_REF_LOS] = hclge_get_hilink_ref_los,
 	[HNAE3_EXT_OPC_GET_PORT_FAULT_STATUS] = hclge_get_port_fault_status,
 	[HNAE3_EXT_OPC_GET_PORT_TYPE] = hclge_get_port_wire_type,
