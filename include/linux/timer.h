ba38e6132725b5fda294f00b3140a1d71f39a284
Yu Liao <liaoyu15@huawei.com>
2023-08-11 16:48:20 +0800

    timers: Keep del_timer[_sync]() exported
    hulk inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I7R8WG
    
    --------------------------------
    
    A previous commit made del_timer[_sync]() obsolete and unexported, which
    caused kabi to break. So making del_timer[_sync]() exported, the same as
    before.
    
    Signed-off-by: Yu Liao <liaoyu15@huawei.com>
    (cherry picked from commit ec2d781ac58c0079a3a026c07864327f24a1b689)

--- include/linux/timer.h
+++ include/linux/timer.h
@@ -190,34 +190,10 @@ extern void add_timer(struct timer_list *timer);
 extern int try_to_del_timer_sync(struct timer_list *timer);
 extern int timer_delete_sync(struct timer_list *timer);
 extern int timer_delete(struct timer_list *timer);
+extern int del_timer_sync(struct timer_list *timer);
+extern int del_timer(struct timer_list *timer);
 extern int timer_shutdown_sync(struct timer_list *timer);
 extern int timer_shutdown(struct timer_list *timer);
-
-/**
- * del_timer_sync - Delete a pending timer and wait for a running callback
- * @timer:	The timer to be deleted
- *
- * See timer_delete_sync() for detailed explanation.
- *
- * Do not use in new code. Use timer_delete_sync() instead.
- */
-static inline int del_timer_sync(struct timer_list *timer)
-{
-	return timer_delete_sync(timer);
-}
-
-/**
- * del_timer - Delete a pending timer
- * @timer:	The timer to be deleted
- *
- * See timer_delete() for detailed explanation.
- *
- * Do not use in new code. Use timer_delete() instead.
- */
-static inline int del_timer(struct timer_list *timer)
-{
-	return timer_delete(timer);
-}
 
 extern void init_timers(void);
 extern void run_local_timers(void);
