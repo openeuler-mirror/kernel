a64b9c4a21e67d4f178efb577029636655bf9090
Thomas Gleixner <tglx@linutronix.de>
2023-08-14 10:18:35 +0200

    x86/cpu: Move cu_id into topology info
    mainline inclusion
    from mainline-v6.7-rc1
    commit e3c0c5d52ad34ab2c97f93ca4a0c2e9ca2fdc06b
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8408
    CVE: N/A
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=e3c0c5d52ad34ab2c97f93ca4a0c2e9ca2fdc06b
    
    (cherry-pick from OLK-6.6 commit 6873bd373c3a)
    
    -------------------------------------
    
    Intel-SIG: commit e3c0c5d52ad3 ("x86/cpu: Move cu_id into topology info")
    
    -------------------------------------
    
    x86/cpu: Move cu_id into topology info
    
    No functional change.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Tested-by: Juergen Gross <jgross@suse.com>
    Tested-by: Sohil Mehta <sohil.mehta@intel.com>
    Tested-by: Michael Kelley <mikelley@microsoft.com>
    Tested-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Tested-by: Zhang Rui <rui.zhang@intel.com>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20230814085112.628405546@linutronix.de
    Signed-off-by: Quanxian Wang <quanxian.wang@intel.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- arch/x86/include/asm/processor.h
+++ arch/x86/include/asm/processor.h
@@ -95,6 +95,9 @@ struct cpuinfo_topology {
 	// Physical die ID on AMD, Relative on Intel
 	u32			die_id;
 
+	// Compute unit ID - AMD specific
+	u32			cu_id;
+
 	// Core ID relative to the package
 	u32			core_id;
 };
@@ -115,7 +118,6 @@ struct cpuinfo_x86 {
 	__u8			x86_phys_bits;
 	/* CPUID returned core id bits: */
 	__u8			x86_coreid_bits;
-	__u8			cu_id;
 	/* Max extended CPUID function supported: */
 	__u32			extended_cpuid_level;
 	/* Maximum supported CPUID level, -1=no CPUID: */
