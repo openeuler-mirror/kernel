db5650715e3352a2b90bf76ddfffc14bf4d570d5
Ben Gardon <bgardon@google.com>
2023-08-14 15:10:38 +0800

    KVM: x86/mmu: Clear dirtied pages mask bit before early break
    mainline inclusion
    from mainline-v5.12-rc1
    commit f1b3b06a058bb5c636ffad0afae138fe30775881
    category: feature
    bugzilla: https://gitee.com/openeuler/intel-kernel/issues/I7S3VQ
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=f1b3b06a058bb5c636ffad0afae138fe30775881
    
    ----------------------------------------------------------------------
    
    In clear_dirty_pt_masked, the loop is intended to exit early after
    processing each of the GFNs with corresponding bits set in mask. This
    does not work as intended if another thread has already cleared the
    dirty bit or writable bit on the SPTE. In that case, the loop would
    proceed to the next iteration early and the bit in mask would not be
    cleared. As a result the loop could not exit early and would proceed
    uselessly. Move the unsetting of the mask bit before the check for a
    no-op SPTE change.
    
    Fixes: a6a0b05da9f3 ("kvm: x86/mmu: Support dirty logging for the TDP
    MMU")
    
    Suggested-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Ben Gardon <bgardon@google.com>
    Message-Id: <20210202185734.1680553-17-bgardon@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Yu Zhang <yu.c.zhang@linux.intel.com>

--- arch/x86/kvm/mmu/tdp_mmu.c
+++ arch/x86/kvm/mmu/tdp_mmu.c
@@ -1013,6 +1013,8 @@ static void clear_dirty_pt_masked(struct kvm *kvm, struct kvm_mmu_page *root,
 		    !(mask & (1UL << (iter.gfn - gfn))))
 			continue;
 
+		mask &= ~(1UL << (iter.gfn - gfn));
+
 		if (wrprot || spte_ad_need_write_protect(iter.old_spte)) {
 			if (is_writable_pte(iter.old_spte))
 				new_spte = iter.old_spte & ~PT_WRITABLE_MASK;
@@ -1026,8 +1028,6 @@ static void clear_dirty_pt_masked(struct kvm *kvm, struct kvm_mmu_page *root,
 		}
 
 		tdp_mmu_set_spte_no_dirty_log(kvm, &iter, new_spte);
-
-		mask &= ~(1UL << (iter.gfn - gfn));
 	}
 }
 
