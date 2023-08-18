75cd43470fe8373e5145b1b7ed724b85cb706dad
Sergey Senozhatsky <senozhatsky@chromium.org>
2023-08-18 15:26:12 +0800

    zram: introduce recompress sysfs knob
    mainline inclusion
    from mainline-v6.2-rc1
    commit 84b33bf7888975d28c0e57011b75c445279c60ec
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I7TWVA
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=84b33bf7888975d28c0e57011b75c445279c60ec
    
    -------------------------------------------
    
    Allow zram to recompress (using secondary compression streams)
    pages.
    
    Re-compression algorithms (we support up to 3 at this stage)
    are selected via recomp_algorithm:
    
      echo "algo=zstd priority=1" > /sys/block/zramX/recomp_algorithm
    
    Please read documentation for more details.
    
    We support several recompression modes:
    
    1) IDLE pages recompression is activated by `idle` mode
    
      echo "type=idle" > /sys/block/zram0/recompress
    
    2) Since there may be many idle pages user-space may pass a size
    threshold value (in bytes) and we will recompress pages only
    of equal or greater size:
    
      echo "threshold=888" > /sys/block/zram0/recompress
    
    3) HUGE pages recompression is activated by `huge` mode
    
      echo "type=huge" > /sys/block/zram0/recompress
    
    4) HUGE_IDLE pages recompression is activated by `huge_idle` mode
    
      echo "type=huge_idle" > /sys/block/zram0/recompress
    
    [senozhatsky@chromium.org: we should always zero out err variable in recompress loop[
      Link: https://lkml.kernel.org/r/20221110143423.3250790-1-senozhatsky@chromium.org
    Link: https://lkml.kernel.org/r/20221109115047.2921851-5-senozhatsky@chromium.org
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Cc: Nathan Chancellor <nathan@kernel.org>
    Cc: Alexey Romanov <avromanov@sberdevices.ru>
    Cc: Nhat Pham <nphamcs@gmail.com>
    Cc: Nitin Gupta <ngupta@vflare.org>
    Cc: Suleiman Souhlal <suleiman@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Conflicts:
    	drivers/block/zram/zram_drv.h
    
    Signed-off-by: Jinjiang Tu <tujinjiang@huawei.com>

--- drivers/block/zram/zram_drv.h
+++ drivers/block/zram/zram_drv.h
@@ -41,6 +41,9 @@
  */
 #define ZRAM_FLAG_SHIFT 24
 
+/* Only 2 bits are allowed for comp priority index */
+#define ZRAM_COMP_PRIORITY_MASK	0x3
+
 /* Flags for zram pages (table[page_no].flags) */
 enum zram_pageflags {
 	/* zram slot is locked */
@@ -50,6 +53,10 @@ enum zram_pageflags {
 	ZRAM_UNDER_WB,	/* page is under writeback */
 	ZRAM_HUGE,	/* Incompressible page */
 	ZRAM_IDLE,	/* not accessed page since last idle marking */
+	ZRAM_INCOMPRESSIBLE, /* none of the algorithms could compress it */
+
+	ZRAM_COMP_PRIORITY_BIT1, /* First bit of comp priority index */
+	ZRAM_COMP_PRIORITY_BIT2, /* Second bit of comp priority index */
 
 	__NR_ZRAM_PAGEFLAGS,
 };
