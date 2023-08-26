232752caeae541bad5a96d7a26aa50b4732eaf7b
Liu Chao <liuchao173@huawei.com>
2023-08-26 15:18:16 +0800

    mm: gmem: Introduce vm_object in preparation for gmem
    euleros inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I7WLVX
    
    ---------------------------------------------
    
    Defines a centrailized logical mapping table that reflects the mapping
    information regardless of the underlying arch-specific MMUs.
    
    Co-developed-by: Yang Kunlin <yangkunlin7@huawei.com>
    Signed-off-by: Liu Chao <liuchao173@huawei.com>

---
+++ include/linux/vm_object.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _VM_OBJECT_H
+#define _VM_OBJECT_H
+
+#ifdef CONFIG_GMEM
+
+static inline struct gm_mapping *vm_object_lookup(vm_object_t *obj, gm_va_t va) { return NULL; }
+static inline int vm_object_mapping_create(vm_object_t *obj, gm_va_t start) { return 0; }
+
+#endif
+
+#endif /* _VM_OBJECT_H */
