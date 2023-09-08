7885b4aa233ec4ec33e7788420ba4e270b329714
Borislav Petkov (AMD) <bp@alien8.de>
2023-09-08 11:35:29 +0800

    x86/CPU/AMD: Fix the DIV(0) initial fix attempt
    stable inclusion
    from stable-v5.10.192
    commit 69712baf249570a1419e75dc1a103a44e375b2cd
    category: bugfix
    bugzilla: https://gitee.com/src-openeuler/kernel/issues/I7WY4J
    CVE: CVE-2023-20588
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=69712baf249570a1419e75dc1a103a44e375b2cd
    
    --------------------------------
    
    commit f58d6fbcb7c848b7f2469be339bc571f2e9d245b upstream.
    
    Initially, it was thought that doing an innocuous division in the #DE
    handler would take care to prevent any leaking of old data from the
    divider but by the time the fault is raised, the speculation has already
    advanced too far and such data could already have been used by younger
    operations.
    
    Therefore, do the innocuous division on every exit to userspace so that
    userspace doesn't see any potentially old data from integer divisions in
    kernel space.
    
    Do the same before VMRUN too, to protect host data from leaking into the
    guest too.
    
    Fixes: 77245f1c3c64 ("x86/CPU/AMD: Do not leak quotient data after a division by 0")
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Cc: <stable@kernel.org>
    Link: https://lore.kernel.org/r/20230811213824.10025-1-bp@alien8.de
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
     Conflicts:
    	arch/x86/entry/common.c
    	arch/x86/kernel/cpu/amd.c
    	arch/x86/kvm/svm.c
    
    Signed-off-by: Jialin Zhang <zhangjialin11@huawei.com>

--- arch/x86/kvm/svm.c
+++ arch/x86/kvm/svm.c
@@ -5536,6 +5536,7 @@ static void svm_flush_tlb_gva(struct kvm_vcpu *vcpu, gva_t gva)
 
 static void svm_prepare_guest_switch(struct kvm_vcpu *vcpu)
 {
+	amd_clear_divider();
 }
 
 static inline void sync_cr8_to_lapic(struct kvm_vcpu *vcpu)
