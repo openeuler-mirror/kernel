75dbc8d5a4ecc62a5f6db2ffa1fe23a13495b622
Borislav Petkov (AMD) <bp@alien8.de>
2023-09-08 15:50:14 +0800

    x86/CPU/AMD: Do not leak quotient data after a division by 0
    stable inclusion
    from stable-v5.10.190
    commit b6fc2fbf89089ecfb8eb9a89a7fc91d444f4fec7
    category: bugfix
    bugzilla: https://gitee.com/src-openeuler/kernel/issues/I7WY4J
    CVE: CVE-2023-20588
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=b6fc2fbf89089ecfb8eb9a89a7fc91d444f4fec7
    
    --------------------------------
    
    commit 77245f1c3c6495521f6a3af082696ee2f8ce3921 upstream.
    
    Under certain circumstances, an integer division by 0 which faults, can
    leave stale quotient data from a previous division operation on Zen1
    microarchitectures.
    
    Do a dummy division 0/1 before returning from the #DE exception handler
    in order to avoid any leaks of potentially sensitive data.
    
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Cc: <stable@kernel.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
     Conflicts:
    	arch/x86/include/asm/cpufeatures.h
    	arch/x86/include/asm/processor.h
    
    Signed-off-by: Jialin Zhang <zhangjialin11@huawei.com>
    (cherry picked from commit d3a5e492e72f2c3ce84e46565ef4569efd510221)

--- arch/x86/include/asm/processor.h
+++ arch/x86/include/asm/processor.h
@@ -807,9 +807,11 @@ DECLARE_PER_CPU(u64, msr_misc_features_shadow);
 #ifdef CONFIG_CPU_SUP_AMD
 extern u16 amd_get_nb_id(int cpu);
 extern u32 amd_get_nodes_per_socket(void);
+extern void amd_clear_divider(void);
 #else
 static inline u16 amd_get_nb_id(int cpu)		{ return 0; }
 static inline u32 amd_get_nodes_per_socket(void)	{ return 0; }
+static inline void amd_clear_divider(void)		{ }
 #endif
 
 static inline uint32_t hypervisor_cpuid_base(const char *sig, uint32_t leaves)
