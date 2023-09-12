6fb80957101cc5fd713e379af71e80f2f2cfecb5
Roberto Sassu <roberto.sassu@huawei.com>
2023-09-12 18:43:53 +0800

    KEYS: Introduce load_pgp_public_keyring()
    euleros inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I7QZ2M
    CVE: NA
    
    -------------------------------------------------
    
    Preload PGP keys from 'pubring.gpg', placed in certs/ of the kernel source
    directory.
    
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>
    Signed-off-by: Tianxing Zhang <zhangtianxing3@huawei.com>
    Reviewed-by: Jason Yan <yanaijie@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>
    Signed-off-by: zhoushuiqing <zhoushuiqing2@huawei.com>

--- certs/system_keyring.c
+++ certs/system_keyring.c
@@ -11,6 +11,9 @@
 #include <linux/cred.h>
 #include <linux/err.h>
 #include <linux/slab.h>
+#ifdef CONFIG_PGP_PRELOAD_PUBLIC_KEYS
+#include <linux/pgp.h>
+#endif
 #include <linux/uidgid.h>
 #include <linux/verification.h>
 #include <keys/asymmetric-type.h>
@@ -215,6 +218,26 @@ static __init int load_system_certificate_list(void)
 	return x509_load_certificate_list(p, size, builtin_trusted_keys);
 }
 late_initcall(load_system_certificate_list);
+#ifdef CONFIG_PGP_PRELOAD_PUBLIC_KEYS
+extern __initconst const u8 pgp_public_keys[];
+extern __initconst const unsigned long pgp_public_keys_size;
+
+/*
+ * Load a list of PGP keys.
+ */
+static __init int load_pgp_public_keyring(void)
+{
+	pr_notice("Load PGP public keys\n");
+
+	if (preload_pgp_keys(pgp_public_keys,
+			     pgp_public_keys_size,
+			     builtin_trusted_keys) < 0)
+		pr_err("Can't load PGP public keys\n");
+
+	return 0;
+}
+late_initcall(load_pgp_public_keyring);
+#endif /* CONFIG_PGP_PRELOAD_PUBLIC_KEYS */
 
 #ifdef CONFIG_SYSTEM_DATA_VERIFICATION
 
