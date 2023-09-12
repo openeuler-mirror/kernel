1395d9fc4a17d31e43ecef2c5fcf14bfaa688ceb
Yizhen Fan <fanyizhen@huawei.com>
2023-09-12 19:44:02 +0800

    ub: add hash table basic function in ubcore
    driver inclusion
    category: feature
    bugzilla: NA
    CVE: NA
    
    --------------------------------
    
    Add hash table basic function in ubcore, which will be used to store info
    in ubcore device.
    
    Use hlist_head and hlist_node as the implementation of hash table.
    
    Provided ops:
    - hashtable alloc/free
    - add/remove/lookup node
    
    Signed-off-by: Guoxin Qian <qianguoxin@huawei.com>
    Signed-off-by: Yizhen Fan <fanyizhen@huawei.com>

---
+++ drivers/ub/urma/ubcore/ubcore_hash_table.h
@@ -0,0 +1,48 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2022-2022. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * Description: define hash table ops
+ * Author: Yan Fangfang
+ * Create: 2022-08-03
+ * Note:
+ * History: 2022-08-03  Yan Fangfang  Add base code
+ */
+
+#ifndef UBCORE_HASH_TABLE_H
+#define UBCORE_HASH_TABLE_H
+
+#include <urma/ubcore_types.h>
+
+static inline void *ubcore_ht_obj(const struct ubcore_hash_table *ht,
+				  const struct hlist_node *hnode)
+{
+	return (char *)hnode - ht->p.node_offset;
+}
+
+static inline void *ubcore_ht_key(const struct ubcore_hash_table *ht,
+				  const struct hlist_node *hnode)
+{
+	return ((char *)hnode - ht->p.node_offset) + ht->p.key_offset;
+}
+/* Init ht head, not calloc hash table itself */
+int ubcore_hash_table_alloc(struct ubcore_hash_table *ht, const struct ubcore_ht_param *p);
+/* Free ht head, not release hash table itself */
+void ubcore_hash_table_free(struct ubcore_hash_table *ht);
+void ubcore_hash_table_free_with_cb(struct ubcore_hash_table *ht, void (*free_cb)(void *));
+void ubcore_hash_table_add(struct ubcore_hash_table *ht, struct hlist_node *hnode, uint32_t hash);
+void ubcore_hash_table_add_nolock(struct ubcore_hash_table *ht, struct hlist_node *hnode,
+				  uint32_t hash);
+void ubcore_hash_table_remove(struct ubcore_hash_table *ht, struct hlist_node *hnode);
+void *ubcore_hash_table_lookup(struct ubcore_hash_table *ht, uint32_t hash, const void *key);
+void *ubcore_hash_table_lookup_nolock(struct ubcore_hash_table *ht, uint32_t hash, const void *key);
+#endif
