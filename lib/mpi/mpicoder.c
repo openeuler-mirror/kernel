44209fafd635ed7a1234280e0c0a0d8e1e2f08aa
Roberto Sassu <roberto.sassu@huawei.com>
2023-09-12 18:43:47 +0800

    mpi: introduce mpi_key_length()
    euleros inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I7QZ2M
    CVE: NA
    
    -------------------------------------------------
    
    Introduce the new function to get the number of bits and bytes from an MPI.
    
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>
    Signed-off-by: Tianxing Zhang <zhangtianxing3@huawei.com>
    Reviewed-by: Jason Yan <yanaijie@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>
    Signed-off-by: zhoushuiqing <zhoushuiqing2@huawei.com>

--- lib/mpi/mpicoder.c
+++ lib/mpi/mpicoder.c
@@ -79,6 +79,42 @@ MPI mpi_read_raw_data(const void *xbuffer, size_t nbytes)
 }
 EXPORT_SYMBOL_GPL(mpi_read_raw_data);
 
+#ifdef CONFIG_PGP_LIBRARY
+int mpi_key_length(const void *xbuffer, unsigned int ret_nread,
+		   unsigned int *nbits_arg, unsigned int *nbytes_arg)
+{
+	const uint8_t *buffer = xbuffer;
+	unsigned int nbits;
+
+	if (ret_nread < 2)
+		return -EINVAL;
+	nbits = buffer[0] << 8 | buffer[1];
+
+	if (nbits > MAX_EXTERN_MPI_BITS) {
+		pr_info("MPI: mpi too large (%u bits)\n", nbits);
+		return -EINVAL;
+	}
+
+	if (nbits_arg)
+		*nbits_arg = nbits;
+	if (nbytes_arg)
+		*nbytes_arg = DIV_ROUND_UP(nbits, 8);
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(mpi_key_length);
+
+MPI mpi_read_from_buffer(const void *xbuffer, unsigned *ret_nread)
+{
+	const uint8_t *buffer = xbuffer;
+	unsigned int nbytes;
+	MPI val;
+	int ret;
+
+	ret = mpi_key_length(xbuffer, *ret_nread, NULL, &nbytes);
+	if (ret < 0)
+		return ERR_PTR(ret);
+#else
 MPI mpi_read_from_buffer(const void *xbuffer, unsigned *ret_nread)
 {
 	const uint8_t *buffer = xbuffer;
@@ -95,6 +131,7 @@ MPI mpi_read_from_buffer(const void *xbuffer, unsigned *ret_nread)
 	}
 
 	nbytes = DIV_ROUND_UP(nbits, 8);
+#endif /* CONFIG_PGP_LIBRARY */
 	if (nbytes + 2 > *ret_nread) {
 		pr_info("MPI: mpi larger than buffer nbytes=%u ret_nread=%u\n",
 				nbytes, *ret_nread);
