08c21c2013b5e1d04f94438506be315096dc5967
Michael Shavit <mshavit@google.com>
2023-09-15 21:17:39 +0800

    iommu/arm-smmu-v3: Update comment about STE liveness
    mainline inclusion
    from mainline-v6.7-rc1
    commit 6032f58498b70f80103f467de4213f2ffbc2b062
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I938E2
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=6032f58498b70f80103f467de4213f2ffbc2b062
    
    --------------------------------
    
    Update the comment to reflect the fact that the STE is not always
    installed. arm_smmu_domain_finalise_s1 intentionnaly calls
    arm_smmu_write_ctx_desc while the STE is not installed.
    
    Reviewed-by: Nicolin Chen <nicolinc@nvidia.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Signed-off-by: Michael Shavit <mshavit@google.com>
    Tested-by: Nicolin Chen <nicolinc@nvidia.com>
    Link: https://lore.kernel.org/r/20230915211705.v8.8.I7a8beb615e2520ad395d96df94b9ab9708ee0d9c@changeid
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>

--- drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
+++ drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
@@ -1320,7 +1320,7 @@ int arm_smmu_write_ctx_desc(struct arm_smmu_master *master, int ssid,
 			tcr &= ~CTXDESC_CD_0_TCR_HA;
 
 		/*
-		 * STE is live, and the SMMU might read dwords of this CD in any
+		 * STE may be live, and the SMMU might read dwords of this CD in any
 		 * order. Ensure that it observes valid values before reading
 		 * V=1.
 		 */
