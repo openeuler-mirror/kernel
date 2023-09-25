ef5e62d9f0359a204afe8c7a33639a93c8c9bbba
Valentine Sinitsyn <valesini@yandex-team.ru>
2023-09-25 11:40:12 +0300

    kernfs: sysfs: support custom llseek method for sysfs entries
    mainline inclusion
    from mainline-v6.7-rc1
    commit 0fedefd4c4e33dd24f726b13b5d7c143e2b483be
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/8967
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=0fedefd4c4e33dd24f726b13b5d7c143e2b483be
    
    (cherry-pick from OLK-6.6 commit d1421acae945)
    
    ---------------------------
    
    As of now, seeking in sysfs files is handled by generic_file_llseek().
    There are situations where one may want to customize seeking logic:
    
    - Many sysfs entries are fixed files while generic_file_llseek() accepts
      past-the-end positions. Not only being useless by itself, this
      also means a bug in userspace code will trigger not at lseek(), but at
      some later point making debugging harder.
    - generic_file_llseek() relies on f_mapping->host to get the file size
      which might not be correct for all sysfs entries.
      See commit 636b21b50152 ("PCI: Revoke mappings like devmem") as an example.
    
    Implement llseek method to override this behavior at sysfs attribute
    level. The method is optional, and if it is absent,
    generic_file_llseek() is called to preserve backwards compatibility.
    
    Signed-off-by: Valentine Sinitsyn <valesini@yandex-team.ru>
    Link: https://lore.kernel.org/r/20230925084013.309399-1-valesini@yandex-team.ru
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
     Conflicts:
    	include/linux/sysfs.h
    [jz: resolve context conflict]
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- include/linux/kernfs.h
+++ include/linux/kernfs.h
@@ -323,6 +323,7 @@ struct kernfs_ops {
 			 struct poll_table_struct *pt);
 
 	int (*mmap)(struct kernfs_open_file *of, struct vm_area_struct *vma);
+	loff_t (*llseek)(struct kernfs_open_file *of, loff_t offset, int whence);
 
 	KABI_RESERVE(1)
 	KABI_RESERVE(2)
