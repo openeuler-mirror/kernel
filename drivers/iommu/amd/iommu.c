94cdf91c88c6bbdcee334b937c18f7dc5cec7a24
Vasant Hegde <vasant.hegde@amd.com>
2023-10-06 09:57:05 +0000

    iommu/amd: Remove unused EXPORT_SYMBOLS
    mainline inclusion
    from mainline-v6.7-rc1
    commit 45d08d85e6f64d3f1002ce2eb5c967e2aa4bcd39
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/9054
    CVE: NA
    
    Reference: https://github.com/torvalds/linux/commit/45d08d85e6f64d3f1002ce2eb5c967e2aa4bcd39
    
    --------------------------------
    
    commit 45d08d85e6f64d3f1002ce2eb5c967e2aa4bcd39 upstream
    
    Drop EXPORT_SYMBOLS for the functions that are not used by any modules.
    
    Signed-off-by: Vasant Hegde <vasant.hegde@amd.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Reviewed-by: Jerry Snitselaar <jsnitsel@redhat.com>
    Tested-by: Alex Deucher <alexander.deucher@amd.com>
    Link: https://lore.kernel.org/r/20231006095706.5694-5-vasant.hegde@amd.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: suryasaimadhu <saimadhu.koyyalaharivenkata@amd.com>
    Signed-off-by: mohanasv <mohanasv@amd.com>

--- drivers/iommu/amd/iommu.c
+++ drivers/iommu/amd/iommu.c
@@ -2528,7 +2528,6 @@ bool amd_iommu_is_attach_deferred(struct device *dev)
 
 	return dev_data->defer_attach;
 }
-EXPORT_SYMBOL_GPL(amd_iommu_is_attach_deferred);
 
 static void amd_iommu_flush_iotlb_all(struct iommu_domain *domain)
 {
@@ -2694,7 +2693,6 @@ int amd_iommu_flush_page(struct iommu_domain *dom, u32 pasid,
 
 	return ret;
 }
-EXPORT_SYMBOL(amd_iommu_flush_page);
 
 static int __amd_iommu_flush_tlb(struct protection_domain *domain, u32 pasid)
 {
@@ -2714,7 +2712,6 @@ int amd_iommu_flush_tlb(struct iommu_domain *dom, u32 pasid)
 
 	return ret;
 }
-EXPORT_SYMBOL(amd_iommu_flush_tlb);
 
 static u64 *__get_gcr3_pte(u64 *root, int level, u32 pasid, bool alloc)
 {
@@ -2794,7 +2791,6 @@ int amd_iommu_domain_set_gcr3(struct iommu_domain *dom, u32 pasid,
 
 	return ret;
 }
-EXPORT_SYMBOL(amd_iommu_domain_set_gcr3);
 
 int amd_iommu_domain_clear_gcr3(struct iommu_domain *dom, u32 pasid)
 {
@@ -2808,7 +2804,6 @@ int amd_iommu_domain_clear_gcr3(struct iommu_domain *dom, u32 pasid)
 
 	return ret;
 }
-EXPORT_SYMBOL(amd_iommu_domain_clear_gcr3);
 
 int amd_iommu_complete_ppr(struct pci_dev *pdev, u32 pasid,
 			   int status, int tag)
@@ -2827,7 +2822,6 @@ int amd_iommu_complete_ppr(struct pci_dev *pdev, u32 pasid,
 
 	return iommu_queue_command(iommu, &cmd);
 }
-EXPORT_SYMBOL(amd_iommu_complete_ppr);
 
 #ifdef CONFIG_IRQ_REMAP
 
