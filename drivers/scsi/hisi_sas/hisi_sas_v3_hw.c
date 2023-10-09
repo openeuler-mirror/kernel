7d982bef843708d5b498fc85300c6e8c7065fd85
Xingui Yang <yangxingui@huawei.com>
2023-10-09 06:47:39 +0000

    scsi: hisi_sas: Handle the NCQ error returned by D2H frame
    driver inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I86GWG
    CVE: NA
    
    -----------------------------------------------------------------------
    
    We find that some disks use D2H frame instead of SDB frame to return NCQ
    error. Currently, only the I/O corresponding to the D2H frame is processed
    in this scenario, which does not meet the processing requirements of the
    NCQ error scenario.
    So we set dev_status to HISI_SAS_DEV_NCQ_ERR and abort all I/Os of the disk
    in this scenario.
    
    Signed-off-by: Xingui Yang <yangxingui@huawei.com>
    Reviewed-by: Xiang Chen <chenxiang66@hisilicon.com>

--- drivers/scsi/hisi_sas/hisi_sas_v3_hw.c
+++ drivers/scsi/hisi_sas/hisi_sas_v3_hw.c
@@ -2341,7 +2341,15 @@ slot_err_v3_hw(struct hisi_hba *hisi_hba, struct sas_task *task,
 	case SAS_PROTOCOL_SATA | SAS_PROTOCOL_STP:
 		if ((dw0 & CMPLT_HDR_RSPNS_XFRD_MSK) &&
 			(sipc_rx_err_type & RX_FIS_STATUS_ERR_MSK)) {
-			ts->stat = SAS_PROTO_RESPONSE;
+			if (task->ata_task.use_ncq) {
+				struct domain_device *device = task->dev;
+				struct hisi_sas_device *sas_dev =
+						device->lldd_dev;
+				sas_dev->dev_status = HISI_SAS_DEV_NCQ_ERR;
+				slot->abort = 1;
+			} else {
+				ts->stat = SAS_PROTO_RESPONSE;
+			}
 		} else if ((dw3 & CMPLT_HDR_IO_IN_TARGET_MSK) ||
 			(dw3 & SATA_DISK_IN_ERROR_STATUS)) {
 			ts->stat = SAS_PHY_DOWN;
