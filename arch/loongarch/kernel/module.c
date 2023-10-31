c6a2cd1d297aa22365b1726510534f197ee5989a
Hongchen Zhang <zhanghongchen@loongson.cn>
2023-10-31 14:54:32 +0800

    LoongArch: Add support for 32_PCREL relocation type
    LoongArch inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8C3BV
    
    ------------------------------------------
    
    When build and update kernel with the latest upstream binutils and
    loongson3_defconfig, module loader fails with:
    
      kmod: zsmalloc: Unsupport relocation type 99, please add its support.
      kmod: fuse: Unsupport relocation type 99, please add its support.
      kmod: ipmi_msghandler: Unsupport relocation type 99, please add its support.
      kmod: ipmi_msghandler: Unsupport relocation type 99, please add its support.
      kmod: pstore: Unsupport relocation type 99, please add its support.
      kmod: drm_display_helper: Unsupport relocation type 99, please add its support.
      kmod: drm_display_helper: Unsupport relocation type 99, please add its support.
      kmod: drm_display_helper: Unsupport relocation type 99, please add its support.
      kmod: fuse: Unsupport relocation type 99, please add its support.
      kmod: fat: Unsupport relocation type 99, please add its support.
    
    This is because the latest upstream binutils replaces a pair of ADD32
    and SUB32 with 32_PCREL, so add support for 32_PCREL relocation type.
    
    Link: https://sourceware.org/git/?p=binutils-gdb.git;a=commit;h=ecb802d02eeb
    Cc: <stable@vger.kernel.org>
    Co-developed-by: Youling Tang <tangyouling@loongson.cn>
    Signed-off-by: Youling Tang <tangyouling@loongson.cn>
    Signed-off-by: Tiezhu Yang <yangtiezhu@loongson.cn>
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>
    Signed-off-by: Hongchen Zhang <zhanghongchen@loongson.cn>

--- arch/loongarch/kernel/module.c
+++ arch/loongarch/kernel/module.c
@@ -372,6 +372,15 @@ static int apply_r_larch_got_pc(struct module *mod, u32 *location, Elf_Addr v,
 	return apply_r_larch_pcala(mod, location, got, rela_stack, rela_stack_top, type);
 }
 
+static int apply_r_larch_32_pcrel(struct module *mod, u32 *location, Elf_Addr v,
+				  s64 *rela_stack, size_t *rela_stack_top, unsigned int type)
+{
+	ptrdiff_t offset = (void *)v - (void *)location;
+
+	*(u32 *)location = offset;
+	return 0;
+}
+
 /*
  * reloc_handlers_rela() - Apply a particular relocation to a module
  * @mod: the module to apply the reloc to
@@ -404,6 +413,7 @@ static reloc_rela_handler reloc_rela_handlers[] = {
 	[R_LARCH_B26]					     = apply_r_larch_b26,
 	[R_LARCH_PCALA_HI20...R_LARCH_PCALA64_HI12]	     = apply_r_larch_pcala,
 	[R_LARCH_GOT_PC_HI20...R_LARCH_GOT_PC_LO12]	     = apply_r_larch_got_pc,
+	[R_LARCH_32_PCREL]				     = apply_r_larch_32_pcrel,
 };
 
 int apply_relocate_add(Elf_Shdr *sechdrs, const char *strtab,
