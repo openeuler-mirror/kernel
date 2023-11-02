a09e2581d6063c91f895bb7398900ed2cb9c2e50
Paul Durrant <pdurrant@amazon.com>
2023-11-02 16:21:28 +0000

    KVM x86/xen: add an override for PVCLOCK_TSC_STABLE_BIT
    mainline inclusion
    from mainline-v6.8-rc1
    commit 6d72283526090850274d065cd5d60af732cc5fc8
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/126
    CVE: NA
    
    Reference: https://github.com/torvalds/linux/commit/6d72283526090850274d065cd5d60af732cc5fc8
    
    --------------------------------
    
    commit 6d72283526090850274d065cd5d60af732cc5fc8 upstream
    
    Unless explicitly told to do so (by passing 'clocksource=tsc' and
    'tsc=stable:socket', and then jumping through some hoops concerning
    potential CPU hotplug) Xen will never use TSC as its clocksource.
    Hence, by default, a Xen guest will not see PVCLOCK_TSC_STABLE_BIT set
    in either the primary or secondary pvclock memory areas. This has
    led to bugs in some guest kernels which only become evident if
    PVCLOCK_TSC_STABLE_BIT *is* set in the pvclocks. Hence, to support
    such guests, give the VMM a new Xen HVM config flag to tell KVM to
    forcibly clear the bit in the Xen pvclocks.
    
    [Backport Changes]
    
    Drop the CONFIG_KVM_XEN guard around the local
    xen_pvclock_tsc_unstable declaration in kvm_guest_time_update().
    
    Upstream this commit guards only the declaration with
    #ifdef CONFIG_KVM_XEN, while leaving its use-sites in the same
    function unguarded. That compiled with CONFIG_KVM_XEN=n only because
    commit ee11ab6bb04e ("KVM: X86: Reduce size of kvm_vcpu_arch
    structure when CONFIG_KVM_XEN=n") had wrapped the surrounding pvclock
    use-block in #ifdef CONFIG_KVM_XEN.
    
    In this tree ee11ab6bb04e is dropped to preserve kABI: the Xen
    members of struct kvm_arch and struct kvm_vcpu_arch must remain
    present unconditionally regardless of CONFIG_KVM_XEN. With that
    commit gone the use-sites are unconditional again, so the now-stranded
    #ifdef around the declaration broke the CONFIG_KVM_XEN=n build:
    
      arch/x86/kvm/x86.c: error: 'xen_pvclock_tsc_unstable' undeclared
      (first use in this function)
    
    Drop the guard so the declaration and its uses are consistently
    unconditional. This is safe because both ka->xen_hvm_config (an
    unconditional member of struct kvm_arch) and the UAPI macro
    KVM_XEN_HVM_CONFIG_PVCLOCK_TSC_UNSTABLE are always available.
    
    Signed-off-by: Paul Durrant <pdurrant@amazon.com>
    Reviewed-by: David Woodhouse <dwmw@amazon.co.uk>
    Link: https://lore.kernel.org/r/20231102162128.2353459-1-paul@xen.org
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: PKumarAditya <PappalaKumar.Aditya@amd.com>
    Signed-off-by: PrithivishS <sprithiv@amd.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -3168,7 +3168,8 @@ u64 get_kvmclock_ns(struct kvm *kvm)
 
 static void kvm_setup_guest_pvclock(struct kvm_vcpu *v,
 				    struct gfn_to_pfn_cache *gpc,
-				    unsigned int offset)
+				    unsigned int offset,
+				    bool force_tsc_unstable)
 {
 	struct kvm_vcpu_arch *vcpu = &v->arch;
 	struct pvclock_vcpu_time_info *guest_hv_clock;
@@ -3205,6 +3206,10 @@ static void kvm_setup_guest_pvclock(struct kvm_vcpu *v,
 	}
 
 	memcpy(guest_hv_clock, &vcpu->hv_clock, sizeof(*guest_hv_clock));
+
+	if (force_tsc_unstable)
+		guest_hv_clock->flags &= ~PVCLOCK_TSC_STABLE_BIT;
+
 	smp_wmb();
 
 	guest_hv_clock->version = ++vcpu->hv_clock.version;
@@ -3225,6 +3230,14 @@ static int kvm_guest_time_update(struct kvm_vcpu *v)
 	u64 tsc_timestamp, host_tsc;
 	u8 pvclock_flags;
 	bool use_master_clock;
+	/*
+	 * For Xen guests we may need to override PVCLOCK_TSC_STABLE_BIT as unless
+	 * explicitly told to use TSC as its clocksource Xen will not set this bit.
+	 * This default behaviour led to bugs in some guest kernels which cause
+	 * problems if they observe PVCLOCK_TSC_STABLE_BIT in the pvclock flags.
+	 */
+	bool xen_pvclock_tsc_unstable =
+		ka->xen_hvm_config.flags & KVM_XEN_HVM_CONFIG_PVCLOCK_TSC_UNSTABLE;
 
 	kernel_ns = 0;
 	host_tsc = 0;
@@ -3303,12 +3316,14 @@ static int kvm_guest_time_update(struct kvm_vcpu *v)
 	vcpu->hv_clock.flags = pvclock_flags;
 
 	if (vcpu->pv_time.active)
-		kvm_setup_guest_pvclock(v, &vcpu->pv_time, 0);
+		kvm_setup_guest_pvclock(v, &vcpu->pv_time, 0, false);
 	if (vcpu->xen.vcpu_info_cache.active)
 		kvm_setup_guest_pvclock(v, &vcpu->xen.vcpu_info_cache,
-					offsetof(struct compat_vcpu_info, time));
+					offsetof(struct compat_vcpu_info, time),
+					xen_pvclock_tsc_unstable);
 	if (vcpu->xen.vcpu_time_info_cache.active)
-		kvm_setup_guest_pvclock(v, &vcpu->xen.vcpu_time_info_cache, 0);
+		kvm_setup_guest_pvclock(v, &vcpu->xen.vcpu_time_info_cache, 0,
+					xen_pvclock_tsc_unstable);
 	kvm_hv_setup_tsc_page(v->kvm, &vcpu->hv_clock);
 	return 0;
 }
@@ -4658,7 +4673,8 @@ int kvm_vm_ioctl_check_extension(struct kvm *kvm, long ext)
 		    KVM_XEN_HVM_CONFIG_INTERCEPT_HCALL |
 		    KVM_XEN_HVM_CONFIG_SHARED_INFO |
 		    KVM_XEN_HVM_CONFIG_EVTCHN_2LEVEL |
-		    KVM_XEN_HVM_CONFIG_EVTCHN_SEND;
+		    KVM_XEN_HVM_CONFIG_EVTCHN_SEND |
+		    KVM_XEN_HVM_CONFIG_PVCLOCK_TSC_UNSTABLE;
 		if (sched_info_on())
 			r |= KVM_XEN_HVM_CONFIG_RUNSTATE |
 			     KVM_XEN_HVM_CONFIG_RUNSTATE_UPDATE_FLAG;
