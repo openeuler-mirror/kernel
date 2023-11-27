b16c8a680a2b07bd1ae96fe9c41b316190bf90a2
Yury Norov <ynorov@caviumnetworks.com>
2023-11-27 20:40:17 +0800

    arm64: ilp32: introduce syscall table for ILP32
    maillist inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8JVJ3
    CVE: NA
    
    Reference: https://github.com/norov/linux/commits/ilp32-5.2
    
    --------------------------------
    
    Depending on the personality of the task, syscalls has to be dispatched
    to either aarch64, aarch32 or aarch64/ilp32 syscall handlers. We add
    the support of ILP32 mode in this series, therefore introduce
    corresponding syscall table.
    
    Some system calls are wired to aarch32 syscall handlers, as listed in
    arch/arm64/kernel/sys_ilp32.c.
    
    For aarch64/ilp32, top halves of syscall arguments are meaningless
    anthough not zeroed by hardware. Do that in the delouse_pt_regs()
    routine to avoid passing garbage by userspace.
    
    Signed-off-by: Yury Norov <ynorov@caviumnetworks.com>
    Signed-off-by: Yury Norov <ynorov@marvell.com>
    Signed-off-by: Xiongfeng Wang <wangxiongfeng2@huawei.com>
    Acked-by: Xie XiuQi <xiexiuqi@huawei.com>
    Signed-off-by: Chen Jun <chenjun102@huawei.com>
    Signed-off-by: Chen Jiahao <chenjiahao16@huawei.com>
    
    Conflicts:
    	arch/arm64/kernel/Makefile
    	arch/arm64/kernel/syscall.c
    
    Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>

--- arch/arm64/kernel/syscall.c
+++ arch/arm64/kernel/syscall.c
@@ -150,9 +150,32 @@ trace_exit:
 	syscall_trace_exit(regs);
 }
 
+#ifdef CONFIG_ARM64_ILP32
+static inline void delouse_pt_regs(struct pt_regs *regs)
+{
+	regs->regs[0] &= UINT_MAX;
+	regs->regs[1] &= UINT_MAX;
+	regs->regs[2] &= UINT_MAX;
+	regs->regs[3] &= UINT_MAX;
+	regs->regs[4] &= UINT_MAX;
+	regs->regs[5] &= UINT_MAX;
+	regs->regs[6] &= UINT_MAX;
+	regs->regs[7] &= UINT_MAX;
+}
+#endif
+
 void do_el0_svc(struct pt_regs *regs)
 {
-	el0_svc_common(regs, regs->regs[8], __NR_syscalls, sys_call_table);
+	const syscall_fn_t *t = sys_call_table;
+
+#ifdef CONFIG_ARM64_ILP32
+	if (is_ilp32_compat_task()) {
+		t = ilp32_sys_call_table;
+		delouse_pt_regs(regs);
+	}
+#endif
+
+	el0_svc_common(regs, regs->regs[8], __NR_syscalls, t);
 }
 
 #ifdef CONFIG_AARCH32_EL0
