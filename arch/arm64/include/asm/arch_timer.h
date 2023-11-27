e218c611e32c8d4315a6d64790b9a86f3d4fa4dc
Yury Norov <ynorov@caviumnetworks.com>
2023-11-27 20:40:14 +0800

    arm64: change compat_elf_hwcap and compat_elf_hwcap2 prefix to a32
    maillist inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8JVJ3
    CVE: NA
    
    Reference: https://github.com/norov/linux/commits/ilp32-5.2
    
    --------------------------------
    
    ILP32 patch series introduces new type of binaries which is also compat.
    So rename existung aarch32 compat_elf_hwcap's helps to avoid confusing.
    
    Signed-off-by: Yury Norov <ynorov@caviumnetworks.com>
    Signed-off-by: Yury Norov <ynorov@marvell.com>
    Signed-off-by: Xiongfeng Wang <wangxiongfeng2@huawei.com>
    Acked-by: Xie XiuQi <xiexiuqi@huawei.com>
    Signed-off-by: Chen Jun <chenjun102@huawei.com>
    Signed-off-by: Chen Jiahao <chenjiahao16@huawei.com>
    
    Conflicts:
    	arch/arm64/kernel/cpufeature.c
    
    Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>

--- arch/arm64/include/asm/arch_timer.h
+++ arch/arm64/include/asm/arch_timer.h
@@ -218,7 +218,7 @@ static inline void arch_timer_set_evtstrm_feature(void)
 {
 	cpu_set_named_feature(EVTSTRM);
 #ifdef CONFIG_AARCH32_EL0
-	compat_elf_hwcap |= COMPAT_HWCAP_EVTSTRM;
+	a32_elf_hwcap |= COMPAT_HWCAP_EVTSTRM;
 #endif
 }
 
