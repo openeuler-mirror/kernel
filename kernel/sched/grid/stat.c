5ed9128f9a941685cf9922a09572228ecb85506a
Yipeng Zou <zouyipeng@huawei.com>
2023-11-28 11:35:21 +0800

    smart_grid: introduce /proc/pid/smart_grid_level
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I7ZBSR
    CVE: NA
    
    ----------------------------------------
    
    Now, we can use /pro/pid/smart_grid_level to {read,set} task current qos
    level.
    
    This allows to determine the scope of dynamic partitioning of the task
    in smart_grid.
    
    SCHED_GRID_QOS_TASK_LEVEL was defined different QoS level. The lower number
    has the higher priority. (E.g. 0 was the highest).
    
    Signed-off-by: Yipeng Zou <zouyipeng@huawei.com>

--- kernel/sched/grid/stat.c
+++ kernel/sched/grid/stat.c
@@ -19,8 +19,20 @@
 #include <linux/sched/grid_qos.h>
 #include "internal.h"
 
+static int qos_stat_set_class_level(struct sched_grid_qos_stat *qos_stat, int level)
+{
+	if (qos_stat == NULL || level >= SCHED_GRID_QOS_TASK_LEVEL_MAX)
+		return -EINVAL;
+
+	qos_stat->class_lvl = level;
+	return 0;
+}
+
 void qos_stat_init(struct sched_grid_qos_stat *stat)
 {
+	if (stat == NULL)
+		return;
+
 	stat->sample[SCHED_GRID_QOS_IPS_INDEX].name = "ips";
 	stat->sample[SCHED_GRID_QOS_IPS_INDEX].index = SCHED_GRID_QOS_IPS_INDEX;
 	stat->sample[SCHED_GRID_QOS_MEMBOUND_RATIO_INDEX].name = "membound_ratio";
@@ -29,4 +41,7 @@ void qos_stat_init(struct sched_grid_qos_stat *stat)
 	stat->sample[SCHED_GRID_QOS_MEMBANDWIDTH_INDEX].name = "memband_width";
 	stat->sample[SCHED_GRID_QOS_MEMBANDWIDTH_INDEX].index =
 		SCHED_GRID_QOS_MEMBANDWIDTH_INDEX;
+
+	stat->set_class_lvl = qos_stat_set_class_level;
+	stat->class_lvl = SCHED_GRID_QOS_TASK_LEVEL_DEFAULT;
 }
