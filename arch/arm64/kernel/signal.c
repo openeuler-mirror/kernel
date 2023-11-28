3e3e0f652bed4c9ef32c4c19906b14f9275131c4
Mark Brown <broonie@kernel.org>
2023-11-28 17:53:57 +0800

    arm64/sme: Implement streaming SVE signal handling
    mainline inclusion
    from mainline-v5.19-rc1
    commit 85ed24dad2904f7c141911d91b7807ab02694b5e
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8E73O
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=85ed24dad2904f7c141911d91b7807ab02694b5e
    
    -------------------------------------------------
    
    When in streaming mode we have the same set of SVE registers as we do in
    regular SVE mode with the exception of FFR and the use of the SME vector
    length. Provide signal handling for these registers by taking one of the
    reserved words in the SVE signal context as a flags field and defining a
    flag which is set for streaming mode. When the flag is set the vector
    length is set to the streaming mode vector length and we save and
    restore streaming mode data. We support entering or leaving streaming
    mode based on the value of the flag but do not support changing the
    vector length, this is not currently supported SVE signal handling.
    
    We could instead allocate a separate record in the signal frame for the
    streaming mode SVE context but this inflates the size of the maximal signal
    frame required and adds complication when validating signal frames from
    userspace, especially given the current structure of the code.
    
    Any implementation of support for streaming mode vectors in signals will
    have some potential for causing issues for applications that attempt to
    handle SVE vectors in signals, use streaming mode but do not understand
    streaming mode in their signal handling code, it is hard to identify a
    case that is clearly better than any other - they all have cases where
    they could cause unexpected register corruption or faults.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Link: https://lore.kernel.org/r/20220419112247.711548-19-broonie@kernel.org
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Conflicts:
    	arch/arm64/kernel/signal.c
    	setup_sigframe move to arch/arm64/include/asm/signal_common.h.
    Signed-off-by: Wang ShaoBo <bobo.shaobowang@huawei.com>

--- arch/arm64/kernel/signal.c
+++ arch/arm64/kernel/signal.c
@@ -180,11 +180,17 @@ int preserve_sve_context(struct sve_context __user *ctx)
 {
 	int err = 0;
 	u16 reserved[ARRAY_SIZE(ctx->__reserved)];
+	u16 flags = 0;
 	unsigned int vl = task_get_sve_vl(current);
 	unsigned int vq = 0;
 
-	if (test_thread_flag(TIF_SVE))
+	if (thread_sm_enabled(&current->thread)) {
+		vl = task_get_sme_vl(current);
 		vq = sve_vq_from_vl(vl);
+		flags |= SVE_SIG_FLAG_SM;
+	} else if (test_thread_flag(TIF_SVE)) {
+		vq = sve_vq_from_vl(vl);
+	}
 
 	memset(reserved, 0, sizeof(reserved));
 
@@ -192,6 +198,7 @@ int preserve_sve_context(struct sve_context __user *ctx)
 	__put_user_error(round_up(SVE_SIG_CONTEXT_SIZE(vq), 16),
 			 &ctx->head.size, err);
 	__put_user_error(vl, &ctx->vl, err);
+	__put_user_error(flags, &ctx->flags, err);
 	BUILD_BUG_ON(sizeof(ctx->__reserved) != sizeof(reserved));
 	err |= __copy_to_user(&ctx->__reserved, reserved, sizeof(reserved));
 
@@ -212,18 +219,28 @@ int preserve_sve_context(struct sve_context __user *ctx)
 int restore_sve_fpsimd_context(struct user_ctxs *user)
 {
 	int err;
-	unsigned int vq;
+	unsigned int vl, vq;
 	struct user_fpsimd_state fpsimd;
 	struct sve_context sve;
 
 	if (__copy_from_user(&sve, user->sve, sizeof(sve)))
 		return -EFAULT;
 
-	if (sve.vl != task_get_sve_vl(current))
+	if (sve.flags & SVE_SIG_FLAG_SM) {
+		if (!system_supports_sme())
+			return -EINVAL;
+
+		vl = task_get_sme_vl(current);
+	} else {
+		vl = task_get_sve_vl(current);
+	}
+
+	if (sve.vl != vl)
 		return -EINVAL;
 
 	if (sve.head.size <= sizeof(*user->sve)) {
 		clear_thread_flag(TIF_SVE);
+		current->thread.svcr &= ~SYS_SVCR_EL0_SM_MASK;
 		goto fpsimd_only;
 	}
 
@@ -255,7 +272,10 @@ int restore_sve_fpsimd_context(struct user_ctxs *user)
 	if (err)
 		return -EFAULT;
 
-	set_thread_flag(TIF_SVE);
+	if (sve.flags & SVE_SIG_FLAG_SM)
+		current->thread.svcr |= SYS_SVCR_EL0_SM_MASK;
+	else
+		set_thread_flag(TIF_SVE);
 
 fpsimd_only:
 	/* copy the FP and status/control registers */
@@ -340,7 +360,7 @@ int __parse_user_sigcontext(struct user_ctxs *user,
 			break;
 
 		case SVE_MAGIC:
-			if (!system_supports_sve())
+			if (!system_supports_sve() && !system_supports_sme())
 				goto invalid;
 
 			if (user->sve)
@@ -470,11 +490,12 @@ int setup_sigframe_layout(struct rt_sigframe_user_layout *user, bool add_all)
 	if (system_supports_sve()) {
 		unsigned int vq = 0;
 
-		if (add_all || test_thread_flag(TIF_SVE)) {
-			int vl = sve_max_vl();
+		if (add_all || test_thread_flag(TIF_SVE) ||
+		    thread_sm_enabled(&current->thread)) {
+			int vl = max(sve_max_vl(), sme_max_vl());
 
 			if (!add_all)
-				vl = task_get_sve_vl(current);
+				vl = thread_get_cur_vl(&current->thread);
 
 			vq = sve_vq_from_vl(vl);
 		}
