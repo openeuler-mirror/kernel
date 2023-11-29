d9d9cd37d244f05c5a809430173cebe794146ab1
Zhen Lei <thunder.leizhen@huawei.com>
2023-11-29 15:20:47 +0800

    sched/debug: Try trigger_single_cpu_backtrace(cpu) in dump_cpu_task()
    mainline inclusion
    from mainline-v6.1-rc1
    commit e73dfe30930b75c98746152e7a2f6a8ab6067b51
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I7OIXK
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=e73dfe30930b75c98746152e7a2f6a8ab6067b51
    
    --------------------------------
    
    The trigger_all_cpu_backtrace() function attempts to send an NMI to the
    target CPU, which usually provides much better stack traces than the
    dump_cpu_task() function's approach of dumping that stack from some other
    CPU.  So much so that most calls to dump_cpu_task() only happen after
    a call to trigger_all_cpu_backtrace() has failed.  And the exception to
    this rule really should attempt to use trigger_all_cpu_backtrace() first.
    
    Therefore, move the trigger_all_cpu_backtrace() invocation into
    dump_cpu_task().
    
    Signed-off-by: Zhen Lei <thunder.leizhen@huawei.com>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Ben Segall <bsegall@google.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Daniel Bristot de Oliveira <bristot@redhat.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Conflicts:
    	kernel/smp.c
    
    Signed-off-by: Zhen Lei <thunder.leizhen@huawei.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -10250,6 +10250,9 @@ struct cgroup_subsys cpu_cgrp_subsys = {
 
 void dump_cpu_task(int cpu)
 {
+	if (trigger_single_cpu_backtrace(cpu))
+		return;
+
 	pr_info("Task dump for CPU %d:\n", cpu);
 	sched_show_task(cpu_curr(cpu));
 }
