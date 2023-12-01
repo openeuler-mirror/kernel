a6d5c810878430cd5e41269857b5f796e5f7aab8
Zhiqi Song <songzhiqi1@huawei.com>
2023-12-01 17:13:58 +0800

    crypto: hisilicon/sec2 - save capability registers in probe process
    driver inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8KHDV
    CVE: NA
    
    ----------------------------------------------------------------------
    
    Pre-store the valid value of the sec alg support related capability
    register in sec_qm_init(), which will be called by probe process.
    It can reduce the number of capability register queries and avoid
    obtaining incorrect values in abnormal scenarios, such as reset
    failed and the memory space disabled.
    
    Signed-off-by: Zhiqi Song <songzhiqi1@huawei.com>
    Signed-off-by: JiangShui Yang <yangjiangshui@h-partners.com>

--- drivers/crypto/hisilicon/sec2/sec_crypto.c
+++ drivers/crypto/hisilicon/sec2/sec_crypto.c
@@ -2538,8 +2538,11 @@ err:
 
 int sec_register_to_crypto(struct hisi_qm *qm)
 {
-	u64 alg_mask = sec_get_alg_bitmap(qm, SEC_DRV_ALG_BITMAP_HIGH, SEC_DRV_ALG_BITMAP_LOW);
+	u64 alg_mask;
 	int ret = 0;
+
+	alg_mask = sec_get_alg_bitmap(qm, SEC_DRV_ALG_BITMAP_HIGH_IDX,
+				      SEC_DRV_ALG_BITMAP_LOW_IDX);
 
 	mutex_lock(&sec_algs_lock);
 	if (sec_available_devs) {
@@ -2569,7 +2572,10 @@ unlock:
 
 void sec_unregister_from_crypto(struct hisi_qm *qm)
 {
-	u64 alg_mask = sec_get_alg_bitmap(qm, SEC_DRV_ALG_BITMAP_HIGH, SEC_DRV_ALG_BITMAP_LOW);
+	u64 alg_mask;
+
+	alg_mask = sec_get_alg_bitmap(qm, SEC_DRV_ALG_BITMAP_HIGH_IDX,
+				      SEC_DRV_ALG_BITMAP_LOW_IDX);
 
 	mutex_lock(&sec_algs_lock);
 	if (--sec_available_devs)
