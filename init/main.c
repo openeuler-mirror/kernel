078428464b38d44898d9aa09dd6b66ebc681ae36
Xiongfeng Wang <wangxiongfeng2@huawei.com>
2023-12-05 10:17:23 +0800

    lockup_detector: init lockup detector after all the init_calls
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8LQCC
    CVE: NA
    
    -------------------------------------------------
    
    We call 'sdei_init' as 'subsys_initcall_sync'. lockup detector need to
    be initialised after sdei_init. The influence of this patch is that we
    can not detect the hard lockup in init_calls.
    
    Signed-off-by: Xiongfeng Wang <wangxiongfeng2@huawei.com>
    Reviewed-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Reviewed-by: Hanjun Guo <guohanjun@huawei.com>

--- init/main.c
+++ init/main.c
@@ -1539,7 +1539,6 @@ static noinline void __init kernel_init_freeable(void)
 
 	rcu_init_tasks_generic();
 	do_pre_smp_initcalls();
-	lockup_detector_init();
 
 	smp_init();
 	sched_init_smp();
@@ -1549,6 +1548,8 @@ static noinline void __init kernel_init_freeable(void)
 	page_alloc_init_late();
 
 	do_basic_setup();
+
+	lockup_detector_init();
 
 	kunit_run_all_tests();
 
