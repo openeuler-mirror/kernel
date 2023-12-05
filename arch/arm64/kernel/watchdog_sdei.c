490288e8343f9914f850f13a00ff98f8b878fdcd
Xiongfeng Wang <wangxiongfeng2@huawei.com>
2023-12-05 10:17:25 +0800

    sdei_watchdog: clear EOI of the secure timer before kdump
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8LQCC
    CVE: NA
    
    -------------------------------------------------
    
    When we panic in hardlockup, the secure timer interrupt remains activate
    because firmware clear eoi after dispatch is completed. This will cause
    arm_arch_timer interrupt failed to trigger in the second kernel.
    
    This patch add a new SMC helper to clear eoi of a certain interrupt and
    clear eoi of the secure timer before booting the second kernel.
    
    Signed-off-by: Xiongfeng Wang <wangxiongfeng2@huawei.com>
    Reviewed-by: Hanjun Guo <guohanjun@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>
    
     Conflicts:
    	include/linux/nmi.h
    Signed-off-by: Xiongfeng Wang <wangxiongfeng2@huawei.com>

--- arch/arm64/kernel/watchdog_sdei.c
+++ arch/arm64/kernel/watchdog_sdei.c
@@ -78,6 +78,12 @@ static int __init disable_sdei_nmi_watchdog_setup(char *str)
 }
 __setup("disable_sdei_nmi_watchdog", disable_sdei_nmi_watchdog_setup);
 
+void sdei_watchdog_clear_eoi(void)
+{
+	if (sdei_watchdog_registered)
+		sdei_api_clear_eoi(SDEI_NMI_WATCHDOG_HWIRQ);
+}
+
 int __init watchdog_hardlockup_probe(void)
 {
 	int ret;
