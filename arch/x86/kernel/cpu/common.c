06345b7902112cf87ed1e50f4350ad50b04813f8
Aichun Shi <aichun.shi@intel.com>
2023-12-06 17:51:30 +0800

    kabi: Fix kabi breakage caused by c1e70ec46591
    Intel inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/intel-kernel/issues/I8LVBS
    CVE: N/A
    
    -------------------------------------
    
    Intel-SIG: commit c1e70ec46591 fix kabi breakage
    Backport x86 related patches from 5.10.189 upstream
    
    -------------------------------------
    
    Fix the following kabi change:
    The following symbols have been changed (this will cause an ABI breakage):
    new kabi:
    0x8fbdfba4      current_task    vmlinux EXPORT_SYMBOL
    old kabi:
    0x01a0ed64      current_task    vmlinux EXPORT_SYMBOL
    
    Signed-off-by: Aichun Shi <aichun.shi@intel.com>

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -24,7 +24,9 @@
 #include <linux/io.h>
 #include <linux/syscore_ops.h>
 #include <linux/pgtable.h>
+#ifndef __GENKSYMS__
 #include <linux/utsname.h>
+#endif
 
 #include <asm/alternative.h>
 #include <asm/cmdline.h>
