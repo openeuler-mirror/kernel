b0bc87bffae9c58b0ed171a2c588958aca5cf27d
Christoph Hellwig <hch@lst.de>
2023-12-07 10:00:17 +0800

    loop: fix order of cleaning up the queue and freeing the tagset
    mainline inclusion
    from mainline-v5.14-rc1
    commit 6a03cd9843fa4dcf827dc3ad60fa9b4217f3057c
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I81XCK
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=6a03cd9843fa4dcf827dc3ad60fa9b4217f3057c
    
    ----------------------------------------
    
    We must release the queue before freeing the tagset.
    
    Fixes: 1c99502fae35 ("loop: use blk_mq_alloc_disk and blk_cleanup_disk")
    Reported-by: Bruno Goncalves <bgoncalv@redhat.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    conflict:
      drivers/block/loop.c
    Signed-off-by: Zhong Jinghua <zhongjinghua@huawei.com>
    Signed-off-by: Li Nan <linan122@huawei.com>

--- drivers/block/loop.c
+++ drivers/block/loop.c
@@ -2194,8 +2194,8 @@ out:
 static void loop_remove(struct loop_device *lo)
 {
 	del_gendisk(lo->lo_disk);
-	blk_mq_free_tag_set(&lo->tag_set);
 	blk_cleanup_disk(lo->lo_disk);
+	blk_mq_free_tag_set(&lo->tag_set);
 	kfree(lo);
 }
 
