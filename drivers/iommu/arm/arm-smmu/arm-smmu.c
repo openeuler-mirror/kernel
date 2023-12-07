a66b5aa89b0936dc839429757d9b83968e63a66f
Jason Gunthorpe <jgg@nvidia.com>
2023-12-07 14:03:12 -0400

    iommu: Mark dev_iommu_priv_set() with a lockdep
    mainline inclusion
    from mainline-v6.8-rc1
    commit eda1a94caf6b05482bbf57dc244e7a31a9dba77c
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I938E2
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=eda1a94caf6b05482bbf57dc244e7a31a9dba77c
    
    --------------------------------
    
    A perfect driver would only call dev_iommu_priv_set() from its probe
    callback. We've made it functionally correct to call it from the of_xlate
    by adding a lock around that call.
    
    lockdep assert that iommu_probe_device_lock is held to discourage misuse.
    
    Exclude PPC kernels with CONFIG_FSL_PAMU turned on because FSL_PAMU uses a
    global static for its priv and abuses priv for its domain.
    
    Remove the pointless stores of NULL, all these are on paths where the core
    code will free dev->iommu after the op returns.
    
    Reviewed-by: Lu Baolu <baolu.lu@linux.intel.com>
    Reviewed-by: Jerry Snitselaar <jsnitsel@redhat.com>
    Tested-by: Hector Martin <marcan@marcan.st>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    Link: https://lore.kernel.org/r/5-v2-16e4def25ebb+820-iommu_fwspec_p1_jgg@nvidia.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>

--- drivers/iommu/arm/arm-smmu/arm-smmu.c
+++ drivers/iommu/arm/arm-smmu/arm-smmu.c
@@ -1483,7 +1483,6 @@ static void arm_smmu_release_device(struct device *dev)
 
 	arm_smmu_rpm_put(cfg->smmu);
 
-	dev_iommu_priv_set(dev, NULL);
 	kfree(cfg);
 }
 
--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -4270,7 +4270,6 @@ static struct iommu_device *intel_iommu_probe_device(struct device *dev)
 		ret = intel_pasid_alloc_table(dev);
 		if (ret) {
 			dev_err(dev, "PASID table allocation failed\n");
-			dev_iommu_priv_set(dev, NULL);
 			kfree(info);
 			return ERR_PTR(ret);
 		}
@@ -4288,7 +4287,6 @@ static void intel_iommu_release_device(struct device *dev)
 	dmar_remove_one_dev_info(dev);
 	intel_pasid_free_table(dev);
 	intel_iommu_debugfs_remove_dev(info);
-	dev_iommu_priv_set(dev, NULL);
 	kfree(info);
 	set_dma_ops(dev, NULL);
 }
