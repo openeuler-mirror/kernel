57ee49e17c0ccb96c2cf75cd28a4f2a473bcfaeb
Zhang Zekun <zhangzekun11@huawei.com>
2023-12-10 14:10:34 +0800

    iommu/iova: Make the rcache depot scale better
    mainline inclusion
    from mainline-v6.7-rc1
    commit 911aa1245da83ff5e76d33bb612d8b5a3f2ec4a5
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8KS9A
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=911aa1245da83ff5e76d33bb612d8b5a3f2ec4a5
    
    --------------------------------------
    
    The algorithm in the original paper specifies the storage of full
    magazines in the depot as an unbounded list rather than a fixed-size
    array. It turns out to be pretty straightforward to do this in our
    implementation with no significant loss of efficiency. This allows
    the depot to scale up to the working set sizes of larger systems,
    while also potentially saving some memory on smaller ones too.
    
    Since this involves touching struct iova_magazine with the requisite
    care, we may as well reinforce the comment with a proper assertion too.
    
    Reviewed-by: John Garry <john.g.garry@oracle.com>
    Reviewed-by: Jerry Snitselaar <jsnitsel@redhat.com>
    Signed-off-by: Robin Murphy <robin.murphy@arm.com>
    Link: https://lore.kernel.org/r/f597aa72fc3e1d315bc4574af0ce0ebe5c31cd22.1694535580.git.robin.murphy@arm.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: Zhang Zekun <zhangzekun11@huawei.com>
    (cherry picked from commit 876b598ef137b53bf85122571e20c4b6ab11e931)

--- include/linux/iova.h
+++ include/linux/iova.h
@@ -26,12 +26,10 @@ struct iova_magazine;
 struct iova_cpu_rcache;
 
 #define IOVA_RANGE_CACHE_MAX_SIZE 6	/* log of max cached IOVA range size (in pages) */
-#define MAX_GLOBAL_MAGS 32	/* magazines per bin */
 
 struct iova_rcache {
 	spinlock_t lock;
-	unsigned long depot_size;
-	struct iova_magazine *depot[MAX_GLOBAL_MAGS];
+	struct iova_magazine *depot;
 	struct iova_cpu_rcache __percpu *cpu_rcaches;
 };
 
