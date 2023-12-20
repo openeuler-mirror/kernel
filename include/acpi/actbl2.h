da0c375b1caddf3fdc642477166d23e99e05c13d
Jing Li <jingli@wxiat.com>
2023-12-20 19:45:03 +0800

    sw64: acpi: add ACPI-style structures for SW PINTC, MSIC and LPC-INTC
    Sunway inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBDJNZ
    
    --------------------------------
    
    SW PINTC    : Sunway Platform Interrupt Controller
    SW MSIC     : Sunway MSI Controller
    SW LPC-INTC : Sunway LPC Interrupt Controller
    
    Signed-off-by: Jing Li <jingli@wxiat.com>
    Reviewed-by: He Sheng <hesheng@wxiat.com>
    Signed-off-by: Gu Zitao <guzitao@wxiat.com>

--- include/acpi/actbl2.h
+++ include/acpi/actbl2.h
@@ -539,6 +539,9 @@ enum acpi_madt_type {
 	 * used(start from 0x80).
 	 */
 	ACPI_MADT_TYPE_SW_CINTC = 0x80,
+	ACPI_MADT_TYPE_SW_PINTC = 0x81,
+	ACPI_MADT_TYPE_SW_MSIC = 0x82,
+	ACPI_MADT_TYPE_SW_LPC_INTC = 0x83
 #endif
 };
 
@@ -768,6 +771,90 @@ enum acpi_madt_sw_cintc_version {
 	ACPI_MADT_SW_CINTC_VERSION_V1 = 1,
 	ACPI_MADT_SW_CINTC_VERSION_V2 = 2,
 	ACPI_MADT_SW_CINTC_VERSION_RESERVED = 3 /* 3 and greater are reserved */
+};
+
+/* 0x81: Sunway Platform Interrupt Controller (To be added to ACPI spec) */
+
+struct acpi_madt_sw_sub_pintc {
+	u8 type;
+	u8 reserved[3];
+	u32 hardware_id;
+	u64 address;
+	u32 size;
+	u32 gsi_base;
+	u32 gsi_count;
+	u32 cascade_vector;
+};
+
+struct acpi_madt_sw_pintc {
+	struct acpi_subtable_header header;
+	u8 version;
+	u8 reserved;
+	u32 flags;
+	u32 node;
+	u64 address;
+	u32 size;
+	u32 sub_num;
+	struct acpi_madt_sw_sub_pintc sub[];
+};
+
+/* Values for version field above */
+
+enum acpi_madt_sw_pintc_version {
+	ACPI_MADT_SW_PINTC_VERSION_NONE = 0,
+	ACPI_MADT_SW_PINTC_VERSION_V1 = 1,
+	ACPI_MADT_SW_PINTC_VERSION_V2 = 2,
+	ACPI_MADT_SW_PINTC_VERSION_RESERVED = 3 /* 3 and greater are reserved */
+};
+
+/* 0x82: Sunway MSI Controller (To be added to ACPI spec) */
+
+struct acpi_madt_sw_msic {
+	struct acpi_subtable_header header;
+	u8 version;
+	u8 reserved0;
+	u32 hardware_id;
+	u32 flags;
+	u64 address;
+	u32 size;
+	u32 cascade_vector;
+	u32 node;
+	u32 rc;
+	u32 num;
+	u32 reserved1[4];
+};
+
+/* Values for version field above */
+
+enum acpi_madt_sw_msic_version {
+	ACPI_MADT_SW_MSIC_VERSION_NONE = 0,
+	ACPI_MADT_SW_MSIC_VERSION_V1 = 1,
+	ACPI_MADT_SW_MSIC_VERSION_V2 = 2,
+	ACPI_MADT_SW_MSIC_VERSION_RESERVED = 3 /* 3 and greater are reserved */
+};
+
+/* 0x83: Sunway LPC Interrupt Controller (To be added to ACPI spec) */
+
+struct acpi_madt_sw_lpc_intc {
+	struct acpi_subtable_header header;
+	u8 version;
+	u8 reserved;
+	u32 hardware_id;
+	u32 flags;
+	u64 address;
+	u32 size;
+	u32 node;
+	u32 cascade_vector;
+	u32 gsi_base;
+	u32 gsi_count;
+};
+
+/* Values for version field above */
+
+enum acpi_madt_sw_lpc_intc_version {
+	ACPI_MADT_SW_LPC_INTC_VERSION_NONE = 0,
+	ACPI_MADT_SW_LPC_INTC_VERSION_V1 = 1,
+	ACPI_MADT_SW_LPC_INTC_VERSION_RESERVED = 2 /* 2 and greater are reserved */
 };
 #endif
 
