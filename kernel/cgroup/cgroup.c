53e871a365e495e13fb06b8695e132615410ba40
Zefan Li <lizefan@huawei.com>
2023-12-22 09:24:51 +0000

    cgroup: wait for cgroup destruction to complete when umount
    hulk inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8QLND
    CVE: N/A
    
    -------------------------------------------------
    
    Since commit 3c606d35fe97 ("cgroup: prevent mount hang due to memory
    controller lifetime"), a cgroup root won't be destroyed if there are any
    child cgroups, dead or alive.
    
    This introduced a small regression.
    
        # cat test.sh
        mount -t cgroup -o cpuset xxx /cgroup
        mkdir /cgroup/tmp
        rmdir /cgroup/tmp
        umount /cgroup
    
    After running this script, you'll probably find the cgroup hierarchy
    is still active.
    
        # cat /proc/cgroups | grep cpuset
        #subsys_name    hierarchy       num_cgroups     enabled
        cpuset  1       1       1
        ...
    
    Fix this by waiting for a while when umount. Now run the script again
    and you'll see:
    
        # cat /proc/cgroups | grep cpuset
        #subsys_name    hierarchy       num_cgroups     enabled
        cpuset  0       1       1
        ...
    
    Cc: stable@vger.kernel.org # 3.19+
    Signed-off-by: Zefan Li <lizefan@huawei.com>
    Signed-off-by: chenridong <chenridong@huawei.com>

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -2018,6 +2018,7 @@ void init_cgroup_root(struct cgroup_fs_context *ctx)
 	atomic_set(&root->nr_cgrps, 1);
 	cgrp->root = root;
 	init_cgroup_housekeeping(cgrp);
+	init_waitqueue_head(&root->wait);
 
 	/* DYNMODS must be modified through cgroup_favor_dynmods() */
 	root->flags = ctx->flags & ~CGRP_ROOT_FAVOR_DYNMODS;
@@ -2253,6 +2254,17 @@ static void cgroup_kill_sb(struct super_block *sb)
 {
 	struct kernfs_root *kf_root = kernfs_root_from_sb(sb);
 	struct cgroup_root *root = cgroup_root_from_kf(kf_root);
+
+	/*
+	* Wait if there are cgroups being destroyed, because the destruction
+	* is asynchronous. On the other hand some controllers like memcg
+	* may pin cgroups for a very long time, so don't wait forever.
+	*/
+	if (root != &cgrp_dfl_root) {
+		wait_event_timeout(root->wait,
+				   list_empty(&root->cgrp.self.children),
+				   msecs_to_jiffies(500));
+	}
 
 	/*
 	 * If @root doesn't have any children, start killing it.
@@ -5445,6 +5457,9 @@ static void css_release_work_fn(struct work_struct *work)
 		if (cgrp->kn)
 			RCU_INIT_POINTER(*(void __rcu __force **)&cgrp->kn->priv,
 					 NULL);
+		if (css->parent && !css->parent->parent &&
+		    list_empty(&css->parent->children))
+			wake_up(&cgrp->root->wait);
 	}
 
 	cgroup_unlock();
