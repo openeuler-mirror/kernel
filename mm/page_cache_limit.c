7d1031b36ebd6c273d9aad316fd9e3e2daa01a85
Ze Zuo <zuoze1@huawei.com>
2023-12-26 11:37:31 +0800

    mm: support pagecache limit
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8NIKC
    
    --------------------------------
    
    Add /pros/sys/vm/cache_limit_mbytes to set page cache limit. This
    interface set the upper limit of page cache, if usage of page cache
    is over cache_limit_mbytes, it will trigger memory reclaim, the
    reclaim size and reclaim interval are decided by interfaces
    /proc/sys/vm/cache_reclaim_s and /proc/sys/vm/cache_reclaim_weight,
    these two intefaces are introduced in previous patch.
    
    Signed-off-by: Ze Zuo <zuoze1@huawei.com>

--- mm/page_cache_limit.c
+++ mm/page_cache_limit.c
@@ -15,6 +15,7 @@ static int vm_cache_reclaim_s_max = 43200;
 static int vm_cache_reclaim_weight __read_mostly = 1;
 static int vm_cache_reclaim_weight_max = 100;
 static int vm_cache_reclaim_enable = 1;
+static unsigned long vm_cache_limit_mbytes __read_mostly;
 
 static void shrink_shepherd(struct work_struct *w);
 static DECLARE_DEFERRABLE_WORK(shepherd, shrink_shepherd);
@@ -30,6 +31,31 @@ static unsigned long node_reclaim_num(void)
 	int nid = numa_node_id();
 
 	return SWAP_CLUSTER_MAX * nr_cpus_node(nid) * vm_cache_reclaim_weight;
+}
+
+static bool page_cache_over_limit(void)
+{
+	unsigned long lru_file;
+	unsigned long limit;
+
+	limit = vm_cache_limit_mbytes << (20 - PAGE_SHIFT);
+	lru_file = global_node_page_state(NR_ACTIVE_FILE) +
+			global_node_page_state(NR_INACTIVE_FILE);
+	if (lru_file > limit)
+		return true;
+
+	return false;
+}
+
+static bool should_reclaim_page_cache(void)
+{
+	if (!should_periodical_reclaim())
+		return false;
+
+	if (!vm_cache_limit_mbytes)
+		return false;
+
+	return true;
 }
 
 int cache_reclaim_enable_handler(struct ctl_table *table, int write,
@@ -63,6 +89,37 @@ int cache_reclaim_sysctl_handler(struct ctl_table *table, int write,
 				(unsigned long)vm_cache_reclaim_s * HZ));
 
 	return ret;
+}
+
+int cache_limit_mbytes_sysctl_handler(struct ctl_table *table, int write,
+		void __user *buffer, size_t *length, loff_t *ppos)
+{
+	int ret;
+	unsigned long vm_cache_limit_mbytes_max;
+	unsigned long origin_mbytes = vm_cache_limit_mbytes;
+	int nr_retries = MAX_RECLAIM_RETRIES;
+
+	vm_cache_limit_mbytes_max = totalram_pages() >> (20 - PAGE_SHIFT);
+	ret = proc_doulongvec_minmax(table, write, buffer, length, ppos);
+	if (ret || !write)
+		return ret;
+
+	if (vm_cache_limit_mbytes > vm_cache_limit_mbytes_max) {
+		vm_cache_limit_mbytes = origin_mbytes;
+		return -EINVAL;
+	}
+
+	if (write) {
+		while (should_reclaim_page_cache() && page_cache_over_limit() &&
+				nr_retries--) {
+			if (signal_pending(current))
+				return -EINTR;
+
+			shrink_memory(node_reclaim_num(), false);
+		}
+	}
+
+	return 0;
 }
 
 static void shrink_shepherd(struct work_struct *w)
@@ -121,6 +178,13 @@ static struct ctl_table page_cache_limit_table[] = {
 		.proc_handler	= cache_reclaim_enable_handler,
 		.extra1		= SYSCTL_ZERO,
 		.extra2		= SYSCTL_ONE,
+	},
+	{
+		.procname	= "cache_limit_mbytes",
+		.data		= &vm_cache_limit_mbytes,
+		.maxlen		= sizeof(vm_cache_limit_mbytes),
+		.mode		= 0644,
+		.proc_handler	= cache_limit_mbytes_sysctl_handler,
 	},
 };
 
