Maxppddcsz/openEuler

efi/libstub: add arm64 nokaslr memory regions
hulk inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/I8RJ1I
CVE: NA

--------------------------------

allow users to mark at most 4 regions as not available for kaslr

Signed-off-by: Felix Fu <fuzhen5@huawei.com>

drivers/firmware/efi/libstub/efi-stub-helper.c
@@ -68,7 +68,12 @@ efi_status_t efi_parse_options(char const *cmdline)
 			break;
 
 		if (!strcmp(param, "nokaslr")) {
-			efi_nokaslr = true;
+#if defined(CONFIG_NOKASLR_MEM_RANGE) && defined(CONFIG_ARM64)
+			if (val)
+				mem_avoid_mem_nokaslr(val);
+			else
+#endif
+				efi_nokaslr = true;
 		} else if (!strcmp(param, "quiet")) {
 			efi_loglevel = CONSOLE_LOGLEVEL_QUIET;
 		} else if (!strcmp(param, "noinitrd")) {

openeuler-mirror/kernel

drivers/firmware/efi/libstub/efi-stub-helper.c
@@ -68,7 +68,12 @@ efi_status_t efi_parse_options(char const *cmdline)
 			break;
 
 		if (!strcmp(param, "nokaslr")) {
-			efi_nokaslr = true;
+#if defined (CONFIG_KASLR_SKIP_MEM_RANGE) && defined (CONFIG_ARM64)
+			if (val)
+				mem_avoid_mem_nokaslr(val);
+			else
+#endif
+				efi_nokaslr = true;
 		} else if (!strcmp(param, "quiet")) {
 			efi_loglevel = CONSOLE_LOGLEVEL_QUIET;
 		} else if (!strcmp(param, "noinitrd")) {