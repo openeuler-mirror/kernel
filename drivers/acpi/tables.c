c7b0debf3dfcde283975ebc22b7e30eecd5392b7
Jing Li <jingli@wxiat.com>
2024-01-17 19:16:33 +0800

    sw64: acpi: support MADT entry print for SW PINTC, MSIC and LPC-INTC
    Sunway inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBDJNZ
    
    --------------------------------
    
    After this commit, we can print MADT entries of Sunway via the function
    acpi_table_print_madt_entry().
    
    Signed-off-by: Jing Li <jingli@wxiat.com>
    Reviewed-by: He Sheng <hesheng@wxiat.com>
    Signed-off-by: Gu Zitao <guzitao@wxiat.com>

--- drivers/acpi/tables.c
+++ drivers/acpi/tables.c
@@ -227,6 +227,33 @@ void acpi_table_print_madt_entry(struct acpi_subtable_header *header)
 				p->version, p->flags, p->hardware_id);
 		}
 		break;
+
+	case ACPI_MADT_TYPE_SW_PINTC:
+		{
+			struct acpi_madt_sw_pintc *p =
+				(struct acpi_madt_sw_pintc *)header;
+			pr_info("SW PINTC (version[%u] flags[0x%x] address[0x%llx])\n",
+					p->version, p->flags, p->address);
+		}
+		break;
+
+	case ACPI_MADT_TYPE_SW_MSIC:
+		{
+			struct acpi_madt_sw_msic *p =
+				(struct acpi_madt_sw_msic *)header;
+			pr_info("SW MSIC (version[%u] flags[0x%x] hardware_id[0x%x])\n",
+					p->version, p->flags, p->hardware_id);
+		}
+		break;
+
+	case ACPI_MADT_TYPE_SW_LPC_INTC:
+		{
+			struct acpi_madt_sw_lpc_intc *p =
+				(struct acpi_madt_sw_lpc_intc *)header;
+			pr_info("SW LPC INTC (version[%u] flags[0x%x] hardware_id[0x%x])\n",
+					p->version, p->flags, p->hardware_id);
+		}
+		break;
 #endif
 
 	default:
