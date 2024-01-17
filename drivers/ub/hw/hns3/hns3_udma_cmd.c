openeuler-mirror/kernel

commit c256e44d064225fe961dcf071666d20c01080f9d

hns3 udma: support loading and unloading of udma driver.
driver inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/I85R2F
CVE: NA

-------------------------------------------------------

This patch adds the function of loading and unloading the udma driver.

Signed-off-by: Xincheng Zhu <zhuxincheng2@huawei.com>
Signed-off-by: Weibo Zhao <zhaoweibo3@huawei.com>

drivers/ub/hw/hns3/hns3_udma_cmd.c
@@ -1,0 +1,302 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Huawei UDMA Linux driver
+ * Copyright (c) 2023-2023 Hisilicon Limited.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ */
+
+#include <linux/dmapool.h>
+#include <linux/iopoll.h>
+#include <linux/slab.h>
+#include "hnae3.h"
+#include "hns3_udma_common.h"
+#include "hns3_udma_cmd.h"
+
+int udma_cmd_init(struct udma_dev *udma_dev)
+{
+	sema_init(&udma_dev->cmd.poll_sem, 1);
+	udma_dev->cmd.use_events = 0;
+	udma_dev->cmd.max_cmds = CMD_MAX_NUM;
+	udma_dev->cmd.pool = dma_pool_create("udma_cmd", udma_dev->dev,
+					     UDMA_MAILBOX_SIZE,
+					     UDMA_MAILBOX_SIZE, 0);
+	if (!udma_dev->cmd.pool)
+		return -ENOMEM;
+
+	init_rwsem(&udma_dev->cmd.udma_mb_rwsem);
+
+	return 0;
+}
+
+void udma_cmd_cleanup(struct udma_dev *udma_dev)
+{
+	down_write(&udma_dev->cmd.udma_mb_rwsem);
+	dma_pool_destroy(udma_dev->cmd.pool);
+	up_write(&udma_dev->cmd.udma_mb_rwsem);
+}
+
+static uint32_t udma_cmd_hw_reseted(struct udma_dev *dev,
+				    uint64_t instance_stage,
+				    uint64_t reset_stage)
+{
+	/* When hardware reset has been completed once or more, we should stop
+	 * sending mailbox&cmq&doorbell to hardware. If now in .init_instance()
+	 * function, we should exit with error. If now at HNAE3_INIT_CLIENT
+	 * stage of soft reset process, we should exit with error, and then
+	 * HNAE3_INIT_CLIENT related process can rollback the operation like
+	 * notifing hardware to free resources, HNAE3_INIT_CLIENT related
+	 * process will exit with error to notify NIC driver to reschedule soft
+	 * reset process once again.
+	 */
+	dev->is_reset = true;
+	dev->dis_db = true;
+
+	if (reset_stage == UDMA_STATE_RST_INIT ||
+	    instance_stage == UDMA_STATE_INIT)
+		return CMD_RST_PRC_EBUSY;
+
+	return CMD_RST_PRC_SUCCESS;
+}
+
+static uint32_t udma_cmd_hw_resetting(struct udma_dev *dev,
+				      uint64_t instance_stage,
+				      uint64_t reset_stage)
+{
+#define HW_RESET_TIMEOUT_US 1000000
+#define HW_RESET_DELAY_US 1
+	struct udma_priv *priv = (struct udma_priv *)dev->priv;
+	struct hnae3_handle *handle = priv->handle;
+	const struct hnae3_ae_ops *ops = handle->ae_algo->ops;
+	uint64_t val;
+	int ret;
+
+	/* When hardware reset is detected, we should stop sending mailbox&cmq&
+	 * doorbell to hardware. If now in .init_instance() function, we should
+	 * exit with error. If now at HNAE3_INIT_CLIENT stage of soft reset
+	 * process, we should exit with error, and then HNAE3_INIT_CLIENT
+	 * related process can rollback the operation like notifing hardware to
+	 * free resources, HNAE3_INIT_CLIENT related process will exit with
+	 * error to notify UBNIC driver to reschedule soft reset process once
+	 * again.
+	 */
+	dev->dis_db = true;
+
+	ret = read_poll_timeout_atomic(ops->ae_dev_reset_cnt, val,
+				       val > dev->reset_cnt, HW_RESET_DELAY_US,
+				       HW_RESET_TIMEOUT_US, false, handle);
+	if (!ret)
+		dev->is_reset = true;
+
+	if (!dev->is_reset || reset_stage == UDMA_STATE_RST_INIT ||
+	    instance_stage == UDMA_STATE_INIT)
+		return CMD_RST_PRC_EBUSY;
+
+	return CMD_RST_PRC_SUCCESS;
+}
+
+static uint32_t udma_cmd_sw_resetting(struct udma_dev *dev)
+{
+	struct udma_priv *priv = (struct udma_priv *)dev->priv;
+	struct hnae3_handle *handle = priv->handle;
+	const struct hnae3_ae_ops *ops = handle->ae_algo->ops;
+
+	/* When software reset is detected at .init_instance() function, we
+	 * should stop sending mailbox&cmq&doorbell to hardware, and exit
+	 * with error.
+	 */
+	dev->dis_db = true;
+	if (ops->ae_dev_reset_cnt(handle) != dev->reset_cnt)
+		dev->is_reset = true;
+
+	return CMD_RST_PRC_EBUSY;
+}
+
+static uint32_t check_aedev_reset_status(struct udma_dev *dev,
+					 struct hnae3_handle *handle)
+{
+	const struct hnae3_ae_ops *ops = handle->ae_algo->ops;
+	uint64_t instance_stage;
+	uint64_t reset_stage;
+	uint64_t reset_cnt;
+	bool sw_resetting;
+	bool hw_resetting;
+
+	/* The meaning of the following variables:
+	 * reset_cnt -- The count value of completed hardware reset.
+	 * hw_resetting -- Whether hardware device is resetting now.
+	 * sw_resetting -- Whether UBNIC's software reset process is
+	 * running now.
+	 */
+	instance_stage = handle->udmainfo.instance_state;
+	reset_stage = handle->udmainfo.reset_state;
+	reset_cnt = ops->ae_dev_reset_cnt(handle);
+	if (reset_cnt != dev->reset_cnt)
+		return udma_cmd_hw_reseted(dev, instance_stage, reset_stage);
+
+	hw_resetting = ops->get_cmdq_stat(handle);
+	if (hw_resetting)
+		return udma_cmd_hw_resetting(dev, instance_stage, reset_stage);
+
+	sw_resetting = ops->ae_dev_resetting(handle);
+	if (sw_resetting && instance_stage == UDMA_STATE_INIT)
+		return udma_cmd_sw_resetting(dev);
+
+	return CMD_RST_PRC_OTHERS;
+}
+
+bool udma_chk_mbox_is_avail(struct udma_dev *dev, bool *busy)
+{
+	struct udma_priv *priv = (struct udma_priv *)dev->priv;
+	uint32_t status;
+
+	/*
+	 * CMDQ fatal err or system reset means hw/fw is in an abnormal or
+	 * unresponsive state, CMDQ command needs to return quickly,
+	 * in order to avoid excessive printing, return CMD_RST_PRC_SUCCESS.
+	 */
+	if (dev->cmd.state == UDMA_CMDQ_STATE_FATAL_ERR ||
+	    dev->is_reset)
+		status = CMD_RST_PRC_SUCCESS;
+	else
+		status = check_aedev_reset_status(dev, priv->handle);
+
+	*busy = (status == CMD_RST_PRC_EBUSY);
+
+	return status == CMD_RST_PRC_OTHERS;
+}
+
+static int udma_cmq_csq_done(struct udma_dev *dev)
+{
+	struct udma_priv *priv = (struct udma_priv *)dev->priv;
+	uint32_t tail = ub_read(dev, UDMA_TX_CMQ_CI_REG);
+
+	return tail == priv->cmq.csq.head;
+}
+
+void udma_cmq_setup_basic_desc(struct udma_cmq_desc *desc,
+			       enum udma_opcode_type opcode,
+			       bool is_read)
+{
+	memset((void *)desc, 0, sizeof(struct udma_cmq_desc));
+	desc->opcode = cpu_to_le16(opcode);
+	desc->flag =
+		cpu_to_le16(UDMA_CMD_FLAG_NO_INTR | UDMA_CMD_FLAG_IN);
+	if (is_read)
+		desc->flag |= cpu_to_le16(UDMA_CMD_FLAG_WR);
+	else
+		desc->flag &= cpu_to_le16(~UDMA_CMD_FLAG_WR);
+}
+
+void dump_desc(struct udma_dev *dev,
+	       struct udma_cmq_desc *desc)
+{
+	if (desc->opcode == UDMA_OPC_QUERY_MB_ST)
+		return;
+
+	if (((desc->data[SUB_OPCODE_IDX] & 0xFF) ==
+	     UDMA_CMD_WRITE_QPC_TIMER_BT0) ||
+	    ((desc->data[SUB_OPCODE_IDX] & 0xFF) ==
+	     UDMA_CMD_WRITE_CQC_TIMER_BT0))
+		dev_err_ratelimited(dev->dev,
+			"Send cmd opcode:0x%4x, data: %08x %08x %08x %08x %08x %08x\n",
+			desc->opcode, desc->data[0],
+			desc->data[1], desc->data[2],
+			desc->data[3], desc->data[4], desc->data[5]);
+	else
+		dev_info(dev->dev,
+			"Send cmd opcode:0x%4x, data: %08x %08x %08x %08x %08x %08x\n",
+			desc->opcode, desc->data[0],
+			desc->data[1], desc->data[2],
+			desc->data[3], desc->data[4], desc->data[5]);
+}
+
+static int __udma_cmq_send(struct udma_dev *dev, struct udma_cmq_desc *desc,
+			   int num)
+{
+	struct udma_priv *priv = (struct udma_priv *)dev->priv;
+	struct udma_cmq_ring *csq = &priv->cmq.csq;
+	uint32_t timeout = 0;
+	uint16_t desc_ret;
+	uint32_t tail;
+	int ret = 0;
+	int i;
+
+	tail = csq->head;
+
+	mutex_lock(&csq->lock);
+
+	for (i = 0; i < num; i++) {
+		csq->desc[csq->head++] = desc[i];
+		if (csq->head == csq->desc_num)
+			csq->head = 0;
+	}
+
+	/* barrier */
+	wmb();
+
+	/* Write to hardware */
+	ub_write(dev, UDMA_TX_CMQ_PI_REG, csq->head);
+
+	do {
+		if (udma_cmq_csq_done(dev))
+			break;
+		udelay(1);
+	} while (++timeout < priv->cmq.tx_timeout);
+
+	if (udma_cmq_csq_done(dev)) {
+		for (i = 0; i < num; i++) {
+			/* check the result of hardware write back */
+			desc[i] = csq->desc[tail++];
+			if (tail == csq->desc_num)
+				tail = 0;
+
+			desc_ret = le16_to_cpu(desc[i].retval);
+			if (likely(desc_ret == CMD_EXEC_SUCCESS))
+				continue;
+
+			dev_err_ratelimited(dev->dev,
+					    "CMDQ IO error, opcode = %x, return = %x\n",
+					    desc->opcode, desc_ret);
+			ret = -EIO;
+		}
+	} else {
+		/* FW/HW reset or incorrect number of desc */
+		tail = ub_read(dev, UDMA_TX_CMQ_CI_REG);
+		dev_warn(dev->dev, "CMDQ move tail from %d to %d\n",
+			 csq->head, tail);
+		csq->head = tail;
+
+		dev->cmd.state = UDMA_CMDQ_STATE_HEAD_TAIL_ERR;
+
+		ret = -EAGAIN;
+	}
+
+	mutex_unlock(&csq->lock);
+
+	return ret;
+}
+
+int udma_cmq_send(struct udma_dev *dev, struct udma_cmq_desc *desc, int num)
+{
+	bool busy;
+	int ret;
+
+	if (!udma_chk_mbox_is_avail(dev, &busy))
+		return busy ? -EBUSY : 0;
+
+	ret = __udma_cmq_send(dev, desc, num);
+	if (ret) {
+		if (!udma_chk_mbox_is_avail(dev, &busy))
+			return busy ? -EBUSY : 0;
+	}
+
+	return ret;
+}

arch/arm64/configs/openeuler_defconfig
@@ -6209,6 +6209,7 @@ CONFIG_ROH=m
 CONFIG_ROH_HNS=m
 CONFIG_UB=m
 CONFIG_UB_URMA=m
+CONFIG_UB_UDMA_HNS3=m
 # end of Device Drivers
 
 #

drivers/ub/Kconfig
@@ -23,4 +23,5 @@ config UB_URMA
 	  need liburma from umdk
 	  <https://gitee.com/openeuler/umdk>.
 
+source "drivers/ub/hw/hns3/Kconfig"
 endif # UB

drivers/ub/Makefile
@@ -4,3 +4,4 @@
 #
 
 obj-$(CONFIG_UB)		+= urma/
+obj-$(CONFIG_UB_UDMA_HNS3)	+= hw/hns3/

drivers/ub/hw/hns3/Kconfig
@@ -1,0 +1,17 @@
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# UDMA_HNS configuration
+#
+
+config UB_UDMA_HNS3
+	default n
+        tristate "HNS3 UB UDMA Driver"
+        depends on UB && UB_URMA
+        depends on ARM64
+        depends on UBL && PCI
+        help
+          This is a UB_UDMA_HNS3 driver for the Hisilicon UDMA engine.
+          This driver depands on ubcore and uburma.
+          This driver depands on UBL(UB link). The UDMA_HNS3 is a PCI device.
+          To compile UB_UDMA driver as module, choose M here.
+          module will be called hns3_udma.

drivers/ub/hw/hns3/Makefile
@@ -1,0 +1,12 @@
+#
+## Makefile for the Huawei UDMA Linux driver.
+#
+##
+
+MODULE_NAME := hns3_udma
+ccflags-y += -I$(srctree)/drivers/net/ethernet/hisilicon/hns3/ \
+		-I$(srctree)/drivers/ub/urma/
+
+$(MODULE_NAME)-objs := hns3_udma_hw.o hns3_udma_main.o hns3_udma_cmd.o
+
+obj-$(CONFIG_UB_UDMA_HNS3) := hns3_udma.o

drivers/ub/hw/hns3/hns3_udma_cmd.h
@@ -1,0 +1,45 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Huawei UDMA Linux driver
+ * Copyright (c) 2023-2023 Hisilicon Limited.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ */
+
+#ifndef _UDMA_CMD_H
+#define _UDMA_CMD_H
+
+#include "hns3_udma_device.h"
+
+#define CMD_MAX_NUM		32
+#define UDMA_MAILBOX_SIZE	4096
+#define UDMA_CMD_TIMEOUT_MSECS	10000
+#define CMD_POLL_TOKEN		0xffff
+#define SUB_OPCODE_IDX		4
+
+enum {
+	/* CQC TIMER commands */
+	UDMA_CMD_WRITE_CQC_TIMER_BT0	= 0x23,
+
+	/* QPC TIMER commands */
+	UDMA_CMD_WRITE_QPC_TIMER_BT0 = 0x33,
+};
+
+enum {
+	CMD_RST_PRC_OTHERS,
+	CMD_RST_PRC_SUCCESS,
+	CMD_RST_PRC_EBUSY,
+};
+
+void udma_cmq_setup_basic_desc(struct udma_cmq_desc *desc,
+			       enum udma_opcode_type opcode,
+			       bool is_read);
+int udma_cmq_send(struct udma_dev *dev, struct udma_cmq_desc *desc, int num);
+#endif /* _UDMA_CMD_H */

drivers/ub/hw/hns3/hns3_udma_common.h
@@ -1,0 +1,59 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Huawei UDMA Linux driver
+ * Copyright (c) 2023-2023 Hisilicon Limited.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ */
+
+#ifndef _UDMA_COMMON_H
+#define _UDMA_COMMON_H
+#include <linux/bitfield.h>
+
+#define ub_write(dev, reg, val)	writel((val), (dev)->reg_base + (reg))
+#define ub_read(dev, reg)	readl((dev)->reg_base + (reg))
+
+#define UDMA_UDP_DPORT		4791
+
+#define udma_get_field(origin, mask, shift)                                    \
+	((le32_to_cpu(origin) & (mask)) >> (uint32_t)(shift))
+
+#define udma_set_field(origin, mask, shift, val)                               \
+	do {                                                                   \
+		(origin) &= ~cpu_to_le32(mask);                                \
+		(origin) |= cpu_to_le32(((uint32_t)(val) <<                    \
+			    (uint32_t)(shift)) & (mask));                      \
+	} while (0)
+
+#define udma_set_bit(origin, shift, val)                                       \
+	udma_set_field((origin), (1ul << (shift)), (shift), (val))
+
+#define _udma_reg_clear(ptr, field)                     \
+	({                                                                     \
+		const uint32_t *_ptr = (uint32_t *)(ptr);                                  \
+		BUILD_BUG_ON((((field) >> 32) / 32) != ((((field) << 32) >> 32) / 32));            \
+		*((uint32_t *)_ptr + ((field) >> 32) / 32) &=                        \
+			~cpu_to_le32(GENMASK(((field) >> 32) % 32, (((field) << 32) >> 32) % 32)); \
+	})
+
+#define udma_reg_clear(ptr, field) _udma_reg_clear(ptr, field)
+
+#define _udma_reg_write(ptr, field, val)                \
+	({                                                                     \
+		uint32_t _val = val;                                           \
+		_udma_reg_clear((ptr), field);          \
+		*((uint32_t *)(ptr) + ((field) >> 32) / 32) |=                       \
+			cpu_to_le32(FIELD_PREP(GENMASK(((field) >> 32) % 32,         \
+				(((field) << 32) >> 32) % 32), _val));                       \
+	})
+
+#define udma_reg_write(ptr, field, val) _udma_reg_write(ptr, field, val)
+
+#endif /* _UDMA_COMMON_H */

commit b3d21fd2e1824b2684bb3dc54d0ebbeca760a109

hns3 udma: add print mailbox description
driver inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/I8V1IQ
CVE: NA

---------------------------------

This patch add print of mailbox if debug_switch is on.
The print of mailbox description is key information of
bug locating.

Fixes: c256e44d0642 ("hns3 udma: support loading and unloading of udma driver.")
Signed-off-by: Weibo Zhao <zhaoweibo3@huawei.com>

drivers/ub/hw/hns3/hns3_udma_cmd.c
@@ -20,6 +20,7 @@
 #include "hns3_udma_common.h"
 #include "hns3_udma_cmd.h"
 
+static int debug_switch = 1;
 int udma_cmd_init(struct udma_dev *udma_dev)
 {
 	sema_init(&udma_dev->cmd.poll_sem, 1);
@@ -299,6 +300,9 @@ static int __udma_cmq_send(struct udma_dev *dev, struct udma_cmq_desc *desc,
 	int ret = 0;
 	int i;
 
+	if (debug_switch)
+		for (i = 0; i < num; i++)
+			dump_desc(dev, desc + i);
 
 	mutex_lock(&csq->lock);
 
@@ -341,7 +345,7 @@ static int __udma_cmq_send(struct udma_dev *dev, struct udma_cmq_desc *desc,
 	} else {
 		/* FW/HW reset or incorrect number of desc */
 		tail = ub_read(dev, UDMA_TX_CMQ_CI_REG);
-		dev_warn(dev->dev, "CMDQ move tail from %d to %d\n",
+		dev_warn(dev->dev, "CMDQ move tail from %u to %u.\n",
 			 csq->head, tail);
 		csq->head = tail;
 
@@ -604,3 +608,6 @@ int udma_cmd_mbox(struct udma_dev *dev, struct udma_cmq_desc *desc,
 	else
 		return udma_cmd_mbox_poll(dev, desc, timeout, vfid);
 }
+
+module_param(debug_switch, int, 0444);
+MODULE_PARM_DESC(debug_switch, "set debug print ON, default: 1");