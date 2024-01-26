652f700fc2d2c5ae46a63001096b153c28db5d8c
Brijesh Singh <brijesh.singh@amd.com>
2024-01-25 22:11:18 -0600

    crypto: ccp: Handle legacy SEV commands when SNP is enabled
    mainline inclusion
    from mainline-v6.9-rc1
    commit a867ad6b340f47b7333b80a54b8507fc2cd80aa4
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/126
    CVE: NA
    
    Reference: https://github.com/torvalds/linux/commit/a867ad6b340f47b7333b80a54b8507fc2cd80aa4
    
    --------------------------------
    
    commit a867ad6b340f47b7333b80a54b8507fc2cd80aa4 upstream
    
    The behavior of legacy SEV commands is altered when the firmware is
    initialized for SNP support. In that case, all command buffer memory
    that may get written to by legacy SEV commands must be marked as
    firmware-owned in the RMP table prior to issuing the command.
    
    Additionally, when a command buffer contains a system physical address
    that points to additional buffers that firmware may write to, special
    handling is needed depending on whether:
    
      1) the system physical address points to guest memory
      2) the system physical address points to host memory
    
    To handle case #1, the pages of these buffers are changed to
    firmware-owned in the RMP table before issuing the command, and restored
    to hypervisor-owned after the command completes.
    
    For case #2, a bounce buffer is used instead of the original address.
    
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>
    Co-developed-by: Michael Roth <michael.roth@amd.com>
    Signed-off-by: Michael Roth <michael.roth@amd.com>
    Signed-off-by: Ashish Kalra <ashish.kalra@amd.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Link: https://lore.kernel.org/r/20240126041126.1927228-19-michael.roth@amd.com
    Signed-off-by: PrithivishS <sprithiv@amd.com>

--- drivers/crypto/ccp/sev-dev.h
+++ drivers/crypto/ccp/sev-dev.h
@@ -54,6 +54,9 @@ struct sev_device {
 	u8 build;
 
 	void *cmd_buf;
+	void *cmd_buf_backup;
+	bool cmd_buf_active;
+	bool cmd_buf_backup_active;
 
 	/* Management of the Hygon RING BUFFER mode */
 	struct csv_ringbuffer_queue ring_buffer[CSV_COMMAND_PRIORITY_NUM];
