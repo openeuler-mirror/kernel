d821610f70e9fe80c0f390049d8cdcc06e306d3b
Lin Yujun <linyujun809@huawei.com>
2024-02-04 14:19:09 +0800

    pm: pm_domain.h: Add kabi_reserve
    hulk inclusion
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8PS7G
    CVE: NA
    
    ---------------------------------------------
    
    Add kabi_reserve in pm_domain.h
    
    Signed-off-by: Lin Yujun <linyujun809@huawei.com>

--- include/linux/pm_domain.h
+++ include/linux/pm_domain.h
@@ -18,6 +18,7 @@
 #include <linux/spinlock.h>
 #include <linux/cpumask.h>
 #include <linux/time64.h>
+#include <linux/kabi.h>
 
 /*
  * Flags to control the behaviour of a genpd.
@@ -166,6 +167,11 @@ struct generic_pm_domain {
 			unsigned long lock_flags;
 		};
 	};
+
+	KABI_RESERVE(1)
+	KABI_RESERVE(2)
+	KABI_RESERVE(3)
+	KABI_RESERVE(4)
 
 };
 
