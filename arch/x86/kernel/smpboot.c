ddf07a8199f35a721362a492641f4c759d40b30b
Thomas Gleixner <tglx@linutronix.de>
2024-02-13 22:06:16 +0100

    x86/cpu/topology: Get rid of cpuinfo::x86_max_cores
    mainline inclusion
    from mainline-v6.9-rc1
    commit 89b0f15f408f7c4ee98c1ec4c3224852fcbc3274
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8408
    CVE: N/A
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=89b0f15f408f7c4ee98c1ec4c3224852fcbc3274
    
    (cherry-pick from OLK-6.6 commit 6eccc0ea5ec1)
    
    -------------------------------------
    
    Intel-SIG: commit 89b0f15f408f ("x86/cpu/topology: Get rid of cpuinfo::x86_max_cores")
    
    -------------------------------------
    
    x86/cpu/topology: Get rid of cpuinfo::x86_max_cores
    
    Now that __num_cores_per_package and __num_threads_per_package are
    available, cpuinfo::x86_max_cores and the related math all over the place
    can be replaced with the ready to consume data.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Tested-by: Michael Kelley <mhklinux@outlook.com>
    Tested-by: Sohil Mehta <sohil.mehta@intel.com>
    Link: https://lore.kernel.org/r/20240213210253.176147806@linutronix.de
    Signed-off-by: Quanxian Wang <quanxian.wang@intel.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- arch/x86/kernel/smpboot.c
+++ arch/x86/kernel/smpboot.c
@@ -635,7 +635,7 @@ int arch_sched_node_distance(int from, int to)
 void set_cpu_sibling_map(int cpu)
 {
 	bool has_smt = __max_threads_per_core > 1;
-	bool has_mp = has_smt || boot_cpu_data.x86_max_cores > 1;
+	bool has_mp = has_smt || topology_num_cores_per_package() > 1;
 	struct cpuinfo_x86 *c = &cpu_data(cpu);
 	struct cpuinfo_x86 *o;
 	int i, threads;
