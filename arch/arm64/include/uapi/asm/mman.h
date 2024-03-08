2245096a617c52fbeec06f4168065f782487ffad
Ma Wupeng <mawupeng1@huawei.com>
2024-03-08 10:54:00 +0800

    arm64: mm: Use subsequent bits to improve future compatibility for pbha
    hulk inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I96IZH
    
    --------------------------------
    
    Use subsequent bits to improve future compatibility which avoid conflicts
    during backport.
    
    Fixes: 5c5063ac8584 ("arm64: mm: Introduce VM_PBHA_BIT0 to enable pbha bit0 for single vma")
    Signed-off-by: Ma Wupeng <mawupeng1@huawei.com>

--- arch/arm64/include/uapi/asm/mman.h
+++ arch/arm64/include/uapi/asm/mman.h
@@ -6,6 +6,6 @@
 
 #define PROT_BTI	0x10		/* BTI guarded page */
 #define PROT_MTE	0x20		/* Normal Tagged mapping */
-#define PROT_PBHA_BIT0	0x40		/* PBHA 59 bit */
+#define PROT_PBHA_BIT0	0x1000		/* PBHA 59 bit */
 
 #endif /* ! _UAPI__ASM_MMAN_H */
