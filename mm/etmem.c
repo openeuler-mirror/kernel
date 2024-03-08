ec19aefe5dd9fd72e8a8f855a6b337be35093856
Yuchen Tang <tangyuchen5@huawei.com>
2024-03-08 14:18:54 +0800

    etmem: add swapcache reclaim to etmem
    euleros inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I96X3J?from=project-issue
    CVE: NA
    
    -------------------------------------------------
    
    etmem, the memory vertical expansion technology,
    
    In the current etmem process, memory folio swapping is implemented by
    invoking shrink_folio_list. When this interface is invoked for the first
    time, folios are added to the swap cache and written to disks.The swap
    cache folio is reclaimed only when this interface is invoked for the
    second time and no process accesses the folio.However, in the etmem
    process, the user mode scans folios that have been accessed, and the
    migration is not delivered to folios that are not accessed by processes.
    Therefore, the swap cache may always be occupied.
    To solve the preceding problem, add the logic for actively reclaiming
    the swap cache.When the swap cache occupies a large amount of memory,
    the system proactively scans the LRU linked list and reclaims the
    swap cache to save memory within the specified range.
    
    Reference:https://gitee.com/openeuler/kernel/commit/44983705e56ab22fda801d66e2a6bd0d1be7ca0b
    
    Signed-off-by: liubo <liubo254@huawei.com>
    Signed-off-by: Yuchen Tang <tangyuchen5@huawei.com>
    Signed-off-by: tangyuchen <tangyuchen5@huawei.com>

--- mm/etmem.c
+++ mm/etmem.c
@@ -93,3 +93,176 @@ struct page *get_page_from_vaddr(struct mm_struct *mm, unsigned long vaddr)
 	return page;
 }
 EXPORT_SYMBOL_GPL(get_page_from_vaddr);
+
+#define SWAP_SCAN_NUM_MAX	32
+
+static unsigned long get_swapcache_reclaim_num(unsigned long *swapcache_watermark)
+{
+	return total_swapcache_pages() >
+		swapcache_watermark[ETMEM_SWAPCACHE_WMARK_LOW] ?
+		(total_swapcache_pages() - swapcache_watermark[ETMEM_SWAPCACHE_WMARK_LOW]) : 0;
+}
+
+static int move_lru_folios_to_list(struct lruvec *lruvec,
+	struct folio *folio, struct list_head *foliolist)
+{
+
+	if (!folio_test_large(folio)) {
+		/* If another process is also mapping this folio */
+		if (folio_mapcount(folio) > 1)
+			return -EACCES;
+	} else if (folio_test_hugetlb(folio)) {
+		/* Do not reclaim hugetlb folios */
+		return -EACCES;
+	} else {
+		/* Try to reclaim THP unless it is mapped by another process */
+		if (folio_entire_mapcount(folio) > 1)
+			return -EACCES;
+	}
+
+	/*
+	 * try to a reference to a folio
+	 * may fail if, the folio has been freed/frozen
+	 */
+	if (!(folio_try_get(folio)))
+		return -1;
+
+	/* racing with another isolation */
+	if (!folio_test_clear_lru(folio)) {
+		folio_put(folio);
+		return -1;
+	}
+
+	list_move(&folio->lru, foliolist);
+	update_lru_size(lruvec,
+			LRU_INACTIVE_ANON,
+			folio_zonenum(folio),
+			-folio_nr_pages(folio));
+	return 0;
+}
+
+/*
+ * For each node, scan the inactive anon lru, isolate and move
+ * appropriate candidates to swapcache_list[nid]
+ */
+static void memcg_reclaim_swapcache(struct list_head *swapcache_list,
+			unsigned long swapcache_to_reclaim)
+{
+	struct mem_cgroup *memcg = NULL, *target_memcg = NULL;
+	struct lruvec *lruvec;
+	int nid;
+	pg_data_t *pgdat;
+	unsigned int scan_count = 0;
+	unsigned long swapcache_total_reclaimable = 0;
+	struct list_head *src = NULL;
+	struct folio *folio = NULL, *next = NULL, *pos = NULL;
+
+	for_each_node_state(nid, N_MEMORY) {
+		INIT_LIST_HEAD(&swapcache_list[nid]);
+		cond_resched();
+		pgdat = NODE_DATA(nid);
+
+		memcg = mem_cgroup_iter(target_memcg, NULL, NULL);
+		do {
+			cond_resched();
+			lruvec = mem_cgroup_lruvec(memcg, pgdat);
+			src = &(lruvec->lists[LRU_INACTIVE_ANON]);
+
+			spin_lock_irq(&lruvec->lru_lock);
+			pos = list_last_entry(src, struct folio, lru);
+			spin_unlock_irq(&lruvec->lru_lock);
+reverse_scan_lru:
+			cond_resched();
+			scan_count = 0;
+
+			spin_lock_irq(&lruvec->lru_lock);
+			if (!pos || list_entry_is_head(pos, src, lru)) {
+				spin_unlock_irq(&lruvec->lru_lock);
+				continue;
+			}
+
+			if (!folio_test_lru(pos) || folio_lru_list(pos) != LRU_INACTIVE_ANON) {
+				spin_unlock_irq(&lruvec->lru_lock);
+				continue;
+			}
+
+			folio = pos;
+
+			list_for_each_entry_safe_reverse_from(folio, next, src, lru) {
+				pos = next;
+				scan_count++;
+				if (scan_count >= SWAP_SCAN_NUM_MAX)
+					break;
+
+				if (!folio_test_swapcache(folio) || folio_mapped(folio))
+					continue;
+
+				if (move_lru_folios_to_list(lruvec,
+							folio,
+							&swapcache_list[nid]) != 0)
+					continue;
+
+				swapcache_total_reclaimable += folio_nr_pages(folio);
+			}
+			spin_unlock_irq(&lruvec->lru_lock);
+
+			if (swapcache_total_reclaimable >= swapcache_to_reclaim)
+				break;
+
+			if (scan_count >= SWAP_SCAN_NUM_MAX)
+				goto reverse_scan_lru;
+
+		} while ((memcg = mem_cgroup_iter(target_memcg, memcg, NULL)));
+	}
+}
+
+static int lru_gen_reclaim_swapcache(struct list_head *swapcache_list,
+			unsigned long swapcache_to_reclaim)
+{
+	return 0;
+}
+
+int do_swapcache_reclaim(unsigned long *swapcache_watermark,
+			unsigned int watermark_nr)
+{
+	int nid;
+	unsigned long swapcache_to_reclaim = 0;
+	struct list_head *swapcache_list = NULL, *folio_list = NULL;
+	struct folio *folio = NULL;
+
+	if (swapcache_watermark == NULL ||
+		watermark_nr < ETMEM_SWAPCACHE_NR_WMARK)
+		return -EINVAL;
+
+	if (lru_gen_enabled())
+		return lru_gen_reclaim_swapcache(swapcache_list, swapcache_to_reclaim);
+
+	swapcache_to_reclaim = get_swapcache_reclaim_num(swapcache_watermark);
+
+	swapcache_list = kcalloc(MAX_NUMNODES, sizeof(struct list_head), GFP_KERNEL);
+	if (swapcache_list == NULL)
+		return -ENOMEM;
+
+	memcg_reclaim_swapcache(swapcache_list, swapcache_to_reclaim);
+
+	/* Reclaim all the swapcache we have scanned */
+	for_each_node_state(nid, N_MEMORY) {
+		cond_resched();
+		reclaim_folio_list(&swapcache_list[nid], NODE_DATA(nid));
+	}
+
+	/* Put pack all the pages that are not reclaimed by shrink_folio_list */
+	for_each_node_state(nid, N_MEMORY) {
+		cond_resched();
+		folio_list = &swapcache_list[nid];
+		while (!list_empty(folio_list)) {
+			folio = lru_to_folio(folio_list);
+			list_del(&folio->lru);
+			folio_putback_lru(folio);
+		}
+	}
+
+	kfree(swapcache_list);
+	return 0;
+}
+EXPORT_SYMBOL_GPL(do_swapcache_reclaim);
