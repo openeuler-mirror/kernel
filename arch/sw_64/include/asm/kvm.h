dc4f149d7220bce03b6b8dd0aeae04ec49e7e9f3
Wang Yuanheng <wangyuanheng@wxiat.com>
2024-03-27 14:28:55 +0800

    sw64: redesign struct kvm_regs for C4
    Sunway inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBDJNZ
    
    --------------------------------
    
    Reuse struct user_pt_regs and struct user_fpsimd_state to redesign
    struct kvm_regs in a way that makes the code clearer.
    
    This requires corresponding modification to firmware.
    
    Signed-off-by: Wang Yuanheng <wangyuanheng@wxiat.com>
    Reviewed-by: He Sheng <hesheng@wxiat.com>
    Signed-off-by: Gu Zitao <guzitao@wxiat.com>

--- arch/sw_64/include/asm/kvm.h
+++ arch/sw_64/include/asm/kvm.h
@@ -64,49 +64,15 @@ struct kvm_regs {
 
 #elif CONFIG_SUBARCH_C4
 struct kvm_regs {
-	unsigned long r0;
-	unsigned long r1;
-	unsigned long r2;
-	unsigned long r3;
-
-	unsigned long r4;
-	unsigned long r5;
-	unsigned long r6;
-	unsigned long r7;
-
-	unsigned long r8;
-	unsigned long r9;
-	unsigned long r10;
-	unsigned long r11;
-
-	unsigned long r12;
-	unsigned long r13;
-	unsigned long r14;
-	unsigned long r15;
-
-	unsigned long r16;
-	unsigned long r17;
-	unsigned long r18;
-	unsigned long r19;
-
-	unsigned long r20;
-	unsigned long r21;
-	unsigned long r22;
-	unsigned long r23;
-
-	unsigned long r24;
-	unsigned long r25;
-	unsigned long r26;
-	unsigned long r27;
-
-	unsigned long r28;
-	unsigned long gp;
-	unsigned long sp;
-	unsigned long fpcr;
-
-	unsigned long fp[124];
-	unsigned long ps;
-	unsigned long pc;
+	union {
+		struct user_pt_regs regs;
+		struct {
+			unsigned long r[31];
+			unsigned long pc;
+			unsigned long ps;
+		};
+	};
+	struct user_fpsimd_state fpstate;
 };
 #endif
 
