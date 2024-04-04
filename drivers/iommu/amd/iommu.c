b2608996b604ae009fda84229701fe1f34692c06
Vasant Hegde <vasant.hegde@amd.com>
2024-04-04 10:27:17 +0000

    iommu/amd: Fix possible irq lock inversion dependency issue
    mainline inclusion
    from mainline-v6.9-rc4
    commit 84b1cec4fac5889b6d138d4b5ff6f2d5ef126c5e
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/9054
    CVE: NA
    
    Reference: https://github.com/torvalds/linux/commit/84b1cec4fac5889b6d138d4b5ff6f2d5ef126c5e
    
    (cherry-pick from OLK-6.6 commit 05d1815bd61e)
    
    --------------------------------
    
    commit 84b1cec4fac5889b6d138d4b5ff6f2d5ef126c5e upstream
    
    LOCKDEP detector reported below warning:
    ----------------------------------------
    [   23.796949] ========================================================
    [   23.796950] WARNING: possible irq lock inversion dependency detected
    [   23.796952] 6.8.0fix+ #811 Not tainted
    [   23.796954] --------------------------------------------------------
    [   23.796954] kworker/0:1/8 just changed the state of lock:
    [   23.796956] ff365325e084a9b8 (&domain->lock){..-.}-{3:3}, at: amd_iommu_flush_iotlb_all+0x1f/0x50
    [   23.796969] but this lock took another, SOFTIRQ-unsafe lock in the past:
    [   23.796970]  (pd_bitmap_lock){+.+.}-{3:3}
    [   23.796972]
    
                   and interrupts could create inverse lock ordering between them.
    
    [   23.796973]
                   other info that might help us debug this:
    [   23.796974] Chain exists of:
                     &domain->lock --> &dev_data->lock --> pd_bitmap_lock
    
    [   23.796980]  Possible interrupt unsafe locking scenario:
    
    [   23.796981]        CPU0                    CPU1
    [   23.796982]        ----                    ----
    [   23.796983]   lock(pd_bitmap_lock);
    [   23.796985]                                local_irq_disable();
    [   23.796985]                                lock(&domain->lock);
    [   23.796988]                                lock(&dev_data->lock);
    [   23.796990]   <Interrupt>
    [   23.796991]     lock(&domain->lock);
    
    Fix this issue by disabling interrupt when acquiring pd_bitmap_lock.
    
    Note that this is temporary fix. We have a plan to replace custom bitmap
    allocator with IDA allocator.
    
    Fixes: 87a6f1f22c97 ("iommu/amd: Introduce per-device domain ID to fix potential TLB aliasing issue")
    Reviewed-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
    Signed-off-by: Vasant Hegde <vasant.hegde@amd.com>
    Link: https://lore.kernel.org/r/20240404102717.6705-1-vasant.hegde@amd.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: priyankagunturi <GunturiLatha.Priyanka@amd.com>
    Signed-off-by: mohanasv <mohanasv@amd.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- drivers/iommu/amd/iommu.c
+++ drivers/iommu/amd/iommu.c
@@ -1663,26 +1663,29 @@ static void domain_flush_devices(struct protection_domain *domain)
 
 static u16 domain_id_alloc(void)
 {
+	unsigned long flags;
 	int id;
 
-	spin_lock(&pd_bitmap_lock);
+	spin_lock_irqsave(&pd_bitmap_lock, flags);
 	id = find_first_zero_bit(amd_iommu_pd_alloc_bitmap, MAX_DOMAIN_ID);
 	BUG_ON(id == 0);
 	if (id > 0 && id < MAX_DOMAIN_ID)
 		__set_bit(id, amd_iommu_pd_alloc_bitmap);
 	else
 		id = 0;
-	spin_unlock(&pd_bitmap_lock);
+	spin_unlock_irqrestore(&pd_bitmap_lock, flags);
 
 	return id;
 }
 
 static void domain_id_free(int id)
 {
-	spin_lock(&pd_bitmap_lock);
+	unsigned long flags;
+
+	spin_lock_irqsave(&pd_bitmap_lock, flags);
 	if (id > 0 && id < MAX_DOMAIN_ID)
 		__clear_bit(id, amd_iommu_pd_alloc_bitmap);
-	spin_unlock(&pd_bitmap_lock);
+	spin_unlock_irqrestore(&pd_bitmap_lock, flags);
 }
 
 static void free_gcr3_tbl_level1(u64 *tbl)
