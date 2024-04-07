081ea2ce8078c2f895ec8dfb3aa915083d7d6c25
Liam R. Howlett <Liam.Howlett@oracle.com>
2024-04-07 14:49:14 +0800

    maple_tree: remove unnecessary default labels from switch statements
    mainline inclusion
    from mainline-v6.8-rc1
    commit 37a8ab24d3d4c465b070bd704e2ad2fa277df9d7
    category: performance
    bugzilla: https://gitee.com/openeuler/kernel/issues/I9EHBO
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=37a8ab24d3d4c465b070bd704e2ad2fa277df9d7
    
    --------------------------------
    
    Patch series "maple_tree: iterator state changes".
    
    These patches have some general cleanup and a change to separate the maple
    state status tracking from the maple state node.
    
    The maple state status change allows for walks to continue from previous
    places when the status needs to be recorded to make logical sense for the
    next call to the maple state.  For instance, it allows for prev/next to
    function in a way that better resembles the linked list.  It also allows
    switch statements to be used to detect missed states during compile, and
    the addition of fast-path "active" state is cleaner as an enum.
    
    While making the status change, perf showed some very small (one line)
    functions that were not inlined even with the inline key word.  Making
    these small functions __always_inline is less expensive according to perf.
    As part of that change, some inlines have been dropped from larger
    functions.
    
    Perf also showed that the commonly used mas_for_each() iterator was
    spending a lot of time finding the end of the node.  This series
    introduces caching of the end of the node in the maple state (and updating
    it during writes).  This caching along with the inline changes yielded at
    23.25% improvement on the BENCH_MAS_FOR_EACH maple tree test framework
    benchmark.
    
    I've also included a change to mtree_range_walk and mtree_lookup_walk to
    take advantage of Peng's change [1] to the initial pivot setup.
    
    mmtests did not produce any significant gains.
    
    [1] https://lore.kernel.org/all/20230711035444.526-1-zhangpeng.00@bytedance.com/T/#u
    
    This patch (of 12):
    
    Removing the default types from the switch statements will cause compile
    warnings on missing cases.
    
    Link: https://lkml.kernel.org/r/20231101171629.3612299-2-Liam.Howlett@oracle.com
    Signed-off-by: Liam R. Howlett <Liam.Howlett@oracle.com>
    Suggested-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: ZhangPeng <zhangpeng362@huawei.com>

--- lib/maple_tree.c
+++ lib/maple_tree.c
@@ -771,7 +771,6 @@ static inline void mte_set_pivot(struct maple_enode *mn, unsigned char piv,
 
 	BUG_ON(piv >= mt_pivots[type]);
 	switch (type) {
-	default:
 	case maple_range_64:
 	case maple_leaf_64:
 		node->mr64.pivot[piv] = val;
@@ -795,7 +794,6 @@ static inline void mte_set_pivot(struct maple_enode *mn, unsigned char piv,
 static inline void __rcu **ma_slots(struct maple_node *mn, enum maple_type mt)
 {
 	switch (mt) {
-	default:
 	case maple_arange_64:
 		return mn->ma64.slot;
 	case maple_range_64:
@@ -804,6 +802,8 @@ static inline void __rcu **ma_slots(struct maple_node *mn, enum maple_type mt)
 	case maple_dense:
 		return mn->slot;
 	}
+
+	return NULL;
 }
 
 static inline bool mt_write_locked(const struct maple_tree *mt)
@@ -7013,7 +7013,6 @@ static void mt_dump_range(unsigned long min, unsigned long max,
 		else
 			pr_info("%.*s%lx-%lx: ", depth * 2, spaces, min, max);
 		break;
-	default:
 	case mt_dump_dec:
 		if (min == max)
 			pr_info("%.*s%lu: ", depth * 2, spaces, min);
@@ -7053,7 +7052,6 @@ static void mt_dump_range64(const struct maple_tree *mt, void *entry,
 		case mt_dump_hex:
 			pr_cont("%p %lX ", node->slot[i], node->pivot[i]);
 			break;
-		default:
 		case mt_dump_dec:
 			pr_cont("%p %lu ", node->slot[i], node->pivot[i]);
 		}
@@ -7083,7 +7081,6 @@ static void mt_dump_range64(const struct maple_tree *mt, void *entry,
 				pr_err("node %p last (%lx) > max (%lx) at pivot %d!\n",
 					node, last, max, i);
 				break;
-			default:
 			case mt_dump_dec:
 				pr_err("node %p last (%lu) > max (%lu) at pivot %d!\n",
 					node, last, max, i);
@@ -7108,7 +7105,6 @@ static void mt_dump_arange64(const struct maple_tree *mt, void *entry,
 		case mt_dump_hex:
 			pr_cont("%lx ", node->gap[i]);
 			break;
-		default:
 		case mt_dump_dec:
 			pr_cont("%lu ", node->gap[i]);
 		}
@@ -7119,7 +7115,6 @@ static void mt_dump_arange64(const struct maple_tree *mt, void *entry,
 		case mt_dump_hex:
 			pr_cont("%p %lX ", node->slot[i], node->pivot[i]);
 			break;
-		default:
 		case mt_dump_dec:
 			pr_cont("%p %lu ", node->slot[i], node->pivot[i]);
 		}
