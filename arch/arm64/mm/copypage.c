f1f0713c906cd73ee33fc89e55b516bc4a295edc
Tong Tiangen <tongtiangen@huawei.com>
2024-04-15 15:39:22 +0800

    Revert "arm64: support copy_mc_[user]_highpage()"
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I9GCZS
    CVE: NA
    
    -------------------------------
    
    This reverts commit 9e7e36876260c81bb34c1ef7141a50fb22abfd24.
    
    Signed-off-by: Tong Tiangen <tongtiangen@huawei.com>

--- arch/arm64/mm/copypage.c
+++ arch/arm64/mm/copypage.c
@@ -14,25 +14,6 @@
 #include <asm/cpufeature.h>
 #include <asm/mte.h>
 
-static int do_mte(struct page *to, struct page *from, void *kto, void *kfrom, bool mc)
-{
-	int ret = 0;
-
-	if (system_supports_mte() && page_mte_tagged(from)) {
-		/* It's a new page, shouldn't have been tagged yet */
-		WARN_ON_ONCE(!try_page_mte_tagging(to));
-		if (mc)
-			ret = mte_copy_mc_page_tags(kto, kfrom);
-		else
-			mte_copy_page_tags(kto, kfrom);
-
-		if (!ret)
-			set_page_mte_tagged(to);
-	}
-
-	return ret;
-}
-
 void copy_highpage(struct page *to, struct page *from)
 {
 	void *kto = page_address(to);
@@ -43,7 +24,12 @@ void copy_highpage(struct page *to, struct page *from)
 	if (kasan_hw_tags_enabled())
 		page_kasan_tag_reset(to);
 
-	do_mte(to, from, kto, kfrom, false);
+	if (system_supports_mte() && page_mte_tagged(from)) {
+		/* It's a new page, shouldn't have been tagged yet */
+		WARN_ON_ONCE(!try_page_mte_tagging(to));
+		mte_copy_page_tags(kto, kfrom);
+		set_page_mte_tagged(to);
+	}
 }
 EXPORT_SYMBOL(copy_highpage);
 
@@ -54,43 +40,3 @@ void copy_user_highpage(struct page *to, struct page *from,
 	flush_dcache_page(to);
 }
 EXPORT_SYMBOL_GPL(copy_user_highpage);
-
-#ifdef CONFIG_ARCH_HAS_COPY_MC
-/*
- * Return -EFAULT if anything goes wrong while copying page or mte.
- */
-int copy_mc_highpage(struct page *to, struct page *from)
-{
-	void *kto = page_address(to);
-	void *kfrom = page_address(from);
-	int ret;
-
-	ret = copy_mc_page(kto, kfrom);
-	if (ret)
-		return -EFAULT;
-
-	if (kasan_hw_tags_enabled())
-		page_kasan_tag_reset(to);
-
-	ret = do_mte(to, from, kto, kfrom, true);
-	if (ret)
-		return -EFAULT;
-
-	return 0;
-}
-EXPORT_SYMBOL(copy_mc_highpage);
-
-int copy_mc_user_highpage(struct page *to, struct page *from,
-			unsigned long vaddr, struct vm_area_struct *vma)
-{
-	int ret;
-
-	ret = copy_mc_highpage(to, from);
-
-	if (!ret)
-		flush_dcache_page(to);
-
-	return ret;
-}
-EXPORT_SYMBOL_GPL(copy_mc_user_highpage);
-#endif
