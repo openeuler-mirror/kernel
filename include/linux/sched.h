85cfe67f8d6b0399619017139e30ed44692a815e
Ma Wupeng <mawupeng1@huawei.com>
2024-04-23 18:44:28 +0800

    mm: oom_kill: fix KABI broken by "oom_kill.c: futex: delay the OOM reaper to allow time for proper futex cleanup"
    hulk inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I9HSOM
    CVE: NA
    
    -------------------------------
    
    Move oom_reaper_timer from task_struct to task_struct_resvd to fix KABI
    broken.
    
    Signed-off-by: Ma Wupeng <mawupeng1@huawei.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -629,6 +629,9 @@ struct task_struct_resvd {
 	 */
 	struct task_struct	*task;
 
+#ifdef CONFIG_MMU
+	struct timer_list	oom_reaper_timer;
+#endif
 #ifdef CONFIG_QOS_SCHED_SMART_GRID
 	struct sched_grid_qos	*grid_qos;
 #endif
@@ -1236,7 +1239,6 @@ struct task_struct {
 	int				pagefault_disabled;
 #ifdef CONFIG_MMU
 	struct task_struct		*oom_reaper_list;
-	struct timer_list		oom_reaper_timer;
 #endif
 #ifdef CONFIG_VMAP_STACK
 	struct vm_struct		*stack_vm_area;
