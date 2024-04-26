a8d801d44d51251713b08562413c777a48c0f3e3
Christoph Hellwig <hch@lst.de>
2024-04-26 10:24:14 +0800

    iomap: treat inline data in iomap_writepage_map as an I/O error
    mainline inclusion
    from mainline-v6.9-rc1
    commit 80d012e98894ac9112cbcddf2fbf276c2e4be0ec
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I9DN5Z
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=80d012e98894ac9112cbcddf2fbf276c2e4be0ec
    
    --------------------------------
    
    iomap_writepage_map aready warns about inline data, but then just ignores
    it.  Treat it as an error and return -EIO.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Link: https://lore.kernel.org/r/20231207072710.176093-3-hch@lst.de
    Signed-off-by: Christian Brauner <brauner@kernel.org>
    Signed-off-by: Zhang Yi <yi.zhang@huawei.com>

--- fs/iomap/buffered-io.c
+++ fs/iomap/buffered-io.c
@@ -1805,8 +1805,10 @@ iomap_writepage_map(struct iomap_writepage_ctx *wpc,
 		if (error)
 			break;
 		trace_iomap_writepage_map(inode, &wpc->iomap);
-		if (WARN_ON_ONCE(wpc->iomap.type == IOMAP_INLINE))
-			continue;
+		if (WARN_ON_ONCE(wpc->iomap.type == IOMAP_INLINE)) {
+			error = -EIO;
+			break;
+		}
 		if (wpc->iomap.type == IOMAP_HOLE)
 			continue;
 		iomap_add_to_ioend(inode, pos, folio, ifs, wpc, wbc,
