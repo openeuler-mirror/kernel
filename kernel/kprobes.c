3d3b9d650223f14c6a769c396201fc385631c64b
Li Huafei <lihuafei1@huawei.com>
2024-05-07 03:04:41 +0000

    kprobes: Fix check for probe enabled in kill_kprobe()
    mainline inclusion
    from mainline-v6.2-rc1
    commit 0c76ef3f26d5ef2ac2c21b47e7620cff35809fbb
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I9MXH3
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=0c76ef3f26d5
    
    --------------------------------
    
    In kill_kprobe(), the check whether disarm_kprobe_ftrace() needs to be
    called always fails. This is because before that we set the
    KPROBE_FLAG_GONE flag for kprobe so that "!kprobe_disabled(p)" is always
    false.
    
    The disarm_kprobe_ftrace() call introduced by commit:
    
      0cb2f1372baa ("kprobes: Fix NULL pointer dereference at kprobe_ftrace_handler")
    
    to fix the NULL pointer reference problem. When the probe is enabled, if
    we do not disarm it, this problem still exists.
    
    Fix it by putting the probe enabled check before setting the
    KPROBE_FLAG_GONE flag.
    
    Link: https://lore.kernel.org/all/20221126114316.201857-1-lihuafei1@huawei.com/
    
    Fixes: 3031313eb3d54 ("kprobes: Fix to check probe enabled before disarm_kprobe_ftrace()")
    Signed-off-by: Li Huafei <lihuafei1@huawei.com>
    Acked-by: Masami Hiramatsu (Google) <mhiramat@kernel.org>
    Reviewed-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    Signed-off-by: Masami Hiramatsu (Google) <mhiramat@kernel.org>
    Signed-off-by: Pu Lehui <pulehui@huawei.com>
    (cherry picked from commit f19e20dd4a1b33a7fe7abdccb98680f3ae9b2278)

--- kernel/kprobes.c
+++ kernel/kprobes.c
@@ -2266,6 +2266,14 @@ static void kill_kprobe(struct kprobe *p)
 	if (WARN_ON_ONCE(kprobe_gone(p)))
 		return;
 
+	/*
+	 * The module is going away. We should disarm the kprobe which
+	 * is using ftrace, because ftrace framework is still available at
+	 * MODULE_STATE_GOING notification.
+	 */
+	if (kprobe_ftrace(p) && !kprobe_disabled(p) && !kprobes_all_disarmed)
+		disarm_kprobe_ftrace(p);
+
 	p->flags |= KPROBE_FLAG_GONE;
 	if (kprobe_aggrprobe(p)) {
 		/*
@@ -2282,14 +2290,6 @@ static void kill_kprobe(struct kprobe *p)
 	 * the original probed function (which will be freed soon) any more.
 	 */
 	arch_remove_kprobe(p);
-
-	/*
-	 * The module is going away. We should disarm the kprobe which
-	 * is using ftrace, because ftrace framework is still available at
-	 * MODULE_STATE_GOING notification.
-	 */
-	if (kprobe_ftrace(p) && !kprobe_disabled(p) && !kprobes_all_disarmed)
-		disarm_kprobe_ftrace(p);
 }
 
 /* Disable one kprobe */
