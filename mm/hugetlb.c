75510e3e301b156485fbb7ac0fea4e86bd78ba76
Peter Xu <peterx@redhat.com>
2024-05-09 20:25:34 +0800

    mm/hugetlb: fix missing hugetlb_lock for resv uncharge
    stable inclusion
    from stable-v6.6.30
    commit f6c5d21db16a0910152ec8aa9d5a7aed72694505
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I9NYY7
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=f6c5d21db16a0910152ec8aa9d5a7aed72694505
    
    --------------------------------
    
    commit b76b46902c2d0395488c8412e1116c2486cdfcb2 upstream.
    
    There is a recent report on UFFDIO_COPY over hugetlb:
    
    https://lore.kernel.org/all/000000000000ee06de0616177560@google.com/
    
    350:	lockdep_assert_held(&hugetlb_lock);
    
    Should be an issue in hugetlb but triggered in an userfault context, where
    it goes into the unlikely path where two threads modifying the resv map
    together.  Mike has a fix in that path for resv uncharge but it looks like
    the locking criteria was overlooked: hugetlb_cgroup_uncharge_folio_rsvd()
    will update the cgroup pointer, so it requires to be called with the lock
    held.
    
    Link: https://lkml.kernel.org/r/20240417211836.2742593-3-peterx@redhat.com
    Fixes: 79aa925bf239 ("hugetlb_cgroup: fix reservation accounting")
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Reported-by: syzbot+4b8077a5fccc61c385a1@syzkaller.appspotmail.com
    Reviewed-by: Mina Almasry <almasrymina@google.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Conflicts:
    	mm/hugetlb.c
    [ Context conflicts with commit d5ea6f5f46c3 because it adds a new param
      in hugepage_subpool_put_pages() and hugetlb_acct_memory(). ]
    Signed-off-by: Liu Shixin <liushixin2@huawei.com>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -3255,9 +3255,12 @@ out:
 
 		rsv_adjust = hugepage_subpool_put_pages(spool, 1, info);
 		hugetlb_acct_memory(h, -rsv_adjust, info);
-		if (deferred_reserve)
+		if (deferred_reserve) {
+			spin_lock_irq(&hugetlb_lock);
 			hugetlb_cgroup_uncharge_folio_rsvd(hstate_index(h),
 					pages_per_huge_page(h), folio);
+			spin_unlock_irq(&hugetlb_lock);
+		}
 	}
 
 	if (!memcg_charge_ret)
