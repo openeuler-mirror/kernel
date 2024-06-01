8580d935cf095dae95cdbf7c2a6fe538557e88da
Yanteng Si <siyanteng@loongson.cn>
2024-06-01 09:44:45 +0800

    net: stmmac: Add multi-channel support
    maillist inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I9RPAR
    
    Reference: https://lore.kernel.org/all/cover.1714046812.git.siyanteng@loongson.cn
    
    --------------------------------
    
    DW GMAC v3.x multi-channels feature is implemented as multiple
    sets of the same CSRs. Here is only preliminary support, it will
    be useful for the driver further evolution and for the users
    having multi-channel DWGMAC v3.x devices.
    
    Signed-off-by: Feiyang Chen <chenfeiyang@loongson.cn>
    Signed-off-by: Yinggang Gu <guyinggang@loongson.cn>
    Signed-off-by: Yanteng Si <siyanteng@loongson.cn>
    Signed-off-by: Ming Wang <wangming01@loongson.cn>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -2505,7 +2505,7 @@ static bool stmmac_xdp_xmit_zc(struct stmmac_priv *priv, u32 queue, u32 budget)
 				       true, priv->mode, true, true,
 				       xdp_desc.len);
 
-		stmmac_enable_dma_transmission(priv, priv->ioaddr);
+		stmmac_enable_dma_transmission(priv, priv->ioaddr, queue);
 
 		tx_q->cur_tx = STMMAC_GET_ENTRY(tx_q->cur_tx, priv->dma_conf.dma_tx_size);
 		entry = tx_q->cur_tx;
@@ -4623,7 +4623,7 @@ static netdev_tx_t stmmac_xmit(struct sk_buff *skb, struct net_device *dev)
 
 	netdev_tx_sent_queue(netdev_get_tx_queue(dev, queue), skb->len);
 
-	stmmac_enable_dma_transmission(priv, priv->ioaddr);
+	stmmac_enable_dma_transmission(priv, priv->ioaddr, queue);
 
 	stmmac_flush_tx_descriptors(priv, queue);
 	stmmac_tx_timer_arm(priv, queue);
@@ -4842,7 +4842,7 @@ static int stmmac_xdp_xmit_xdpf(struct stmmac_priv *priv, int queue,
 		u64_stats_update_end(&txq_stats->q_syncp);
 	}
 
-	stmmac_enable_dma_transmission(priv, priv->ioaddr);
+	stmmac_enable_dma_transmission(priv, priv->ioaddr, queue);
 
 	entry = STMMAC_GET_ENTRY(entry, priv->dma_conf.dma_tx_size);
 	tx_q->cur_tx = entry;
