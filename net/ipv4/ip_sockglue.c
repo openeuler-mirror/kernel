f551825772491095116592ae43c4ac0b09cdc758
Geliang Tang <tanggeliang@kylinos.cn>
2024-06-07 14:58:58 +0800

    inet: implement lockless IP_TOS
    mainline inclusion
    from mainline-v6.7-rc1
    commit e08d0b3d172311e2bb500865c0d0038533d0ff11
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I9VYQ9
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git/commit/?id=e08d0b3d172311e2bb500865c0d0038533d0ff11
    
    --------------------------------
    
    Some reads of inet->tos are racy.
    
    Add needed READ_ONCE() annotations and convert IP_TOS option lockless.
    
    v2: missing changes in include/net/route.h (David Ahern)
    rebased: on "mptcp: fix setsockopt(IP_TOS) subflow locking" (Geliang)
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: David Ahern <dsahern@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>

--- net/ipv4/ip_sockglue.c
+++ net/ipv4/ip_sockglue.c
@@ -587,12 +587,14 @@ out:
 
 void __ip_sock_set_tos(struct sock *sk, int val)
 {
+	u8 old_tos = inet_sk(sk)->tos;
+
 	if (sk->sk_type == SOCK_STREAM) {
 		val &= ~INET_ECN_MASK;
-		val |= inet_sk(sk)->tos & INET_ECN_MASK;
+		val |= old_tos & INET_ECN_MASK;
 	}
-	if (inet_sk(sk)->tos != val) {
-		inet_sk(sk)->tos = val;
+	if (old_tos != val) {
+		WRITE_ONCE(inet_sk(sk)->tos, val);
 		WRITE_ONCE(sk->sk_priority, rt_tos2priority(val));
 		sk_dst_reset(sk);
 	}
@@ -1050,6 +1052,9 @@ int do_ip_setsockopt(struct sock *sk, int level, int optname,
 		return 0;
 	case IP_MTU_DISCOVER:
 		return ip_sock_set_mtu_discover(sk, val);
+	case IP_TOS:	/* This sets both TOS and Precedence */
+		ip_sock_set_tos(sk, val);
+		return 0;
 	}
 
 	err = 0;
@@ -1103,9 +1108,6 @@ int do_ip_setsockopt(struct sock *sk, int level, int optname,
 				inet_clear_bit(CHECKSUM, sk);
 			}
 		}
-		break;
-	case IP_TOS:	/* This sets both TOS and Precedence */
-		__ip_sock_set_tos(sk, val);
 		break;
 	case IP_UNICAST_IF:
 	{
@@ -1595,6 +1597,9 @@ int do_ip_getsockopt(struct sock *sk, int level, int optname,
 	case IP_MTU_DISCOVER:
 		val = READ_ONCE(inet->pmtudisc);
 		goto copyval;
+	case IP_TOS:
+		val = READ_ONCE(inet->tos);
+		goto copyval;
 	}
 
 	if (needs_rtnl)
@@ -1631,9 +1636,6 @@ int do_ip_getsockopt(struct sock *sk, int level, int optname,
 			return -EFAULT;
 		return 0;
 	}
-	case IP_TOS:
-		val = inet->tos;
-		break;
 	case IP_MTU:
 	{
 		struct dst_entry *dst;
