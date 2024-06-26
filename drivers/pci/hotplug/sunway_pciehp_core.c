66d1ff2ee8d4a55ac5908e5bf2c20abaeaa611b4
Chen Wang <chenwang@wxiat.com>
2024-06-26 14:15:01 +0800

    sw64: bypass sunway pcie hotplug driver in non-physical scenario
    Sunway inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBDJNZ
    
    --------------------------------
    
    Sunway PCIe hotplug driver is specifically designed for physical
    hardware, and should not be activated in guest and emulator.
    
    Signed-off-by: Chen Wang <chenwang@wxiat.com>
    Reviewed-by: He Sheng <hesheng@wxiat.com>
    Signed-off-by: Gu Zitao <guzitao@wxiat.com>

--- drivers/pci/hotplug/sunway_pciehp_core.c
+++ drivers/pci/hotplug/sunway_pciehp_core.c
@@ -248,6 +248,11 @@ static int __init sunway_pciehp_init(void)
 	struct pci_dev *pdev = NULL;
 	struct pci_controller *hose = NULL;
 
+	if (is_guest_or_emul()) {
+		pr_info(DRIVER_DESC " does not support for VM and emulator.\n");
+		return -ENODEV;
+	}
+
 	pr_info(DRIVER_DESC " version: " DRIVER_VERSION "\n");
 
 	for (hose = hose_head; hose; hose = hose->next) {
