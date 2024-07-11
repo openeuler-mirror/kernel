torvalds/linux

commit b3f0907c71e006e12fde74ea9a745b6096b6f90f

x86/mm: Add .bss..decrypted section to hold shared variables
kvmclock defines few static variables which are shared with the
hypervisor during the kvmclock initialization.

When SEV is active, memory is encrypted with a guest-specific key, and
if the guest OS wants to share the memory region with the hypervisor
then it must clear the C-bit before sharing it.

Currently, we use kernel_physical_mapping_init() to split large pages
before clearing the C-bit on shared pages. But it fails when called from
the kvmclock initialization (mainly because the memblock allocator is
not ready that early during boot).

Add a __bss_decrypted section attribute which can be used when defining
such shared variable. The so-defined variables will be placed in the
.bss..decrypted section. This section will be mapped with C=0 early
during boot.

The .bss..decrypted section has a big chunk of memory that may be unused
when memory encryption is not active, free it when memory encryption is
not active.

Suggested-by: Thomas Gleixner <tglx@linutronix.de>
Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>
Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
Cc: Tom Lendacky <thomas.lendacky@amd.com>
Cc: Borislav Petkov <bp@suse.de>
Cc: "H. Peter Anvin" <hpa@zytor.com>
Cc: Paolo Bonzini <pbonzini@redhat.com>
Cc: Sean Christopherson <sean.j.christopherson@intel.com>
Cc: Radim Krčmář<rkrcmar@redhat.com>
Cc: kvm@vger.kernel.org
Link: https://lkml.kernel.org/r/1536932759-12905-2-git-send-email-brijesh.singh@amd.com

arch/x86/kernel/head64.c
@@ -112,6 +112,7 @@ static bool __head check_la57_support(unsigned long physaddr)
 unsigned long __head __startup_64(unsigned long physaddr,
 				  struct boot_params *bp)
 {
+	unsigned long vaddr, vaddr_end;
 	unsigned long load_delta, *p;
 	unsigned long pgtable_flags;
 	pgdval_t *pgd;
@@ -233,6 +234,21 @@ unsigned long __head __startup_64(unsigned long physaddr,
 
 	/* Encrypt the kernel and related (if SME is active) */
 	sme_encrypt_kernel(bp);
+
+	/*
+	 * Clear the memory encryption mask from the .bss..decrypted section.
+	 * The bss section will be memset to zero later in the initialization so
+	 * there is no need to zero it after changing the memory encryption
+	 * attribute.
+	 */
+	if (mem_encrypt_active()) {
+		vaddr = (unsigned long)__start_bss_decrypted;
+		vaddr_end = (unsigned long)__end_bss_decrypted;
+		for (; vaddr < vaddr_end; vaddr += PMD_SIZE) {
+			i = pmd_index(vaddr);
+			pmd[i] -= sme_get_me_mask();
+		}
+	}
 
 	/*
 	 * Return the SME encryption mask (if SME is active) to be used as a

arch/x86/include/asm/mem_encrypt.h
@@ -48,9 +48,12 @@ int __init early_set_memory_encrypted(unsigned long vaddr, unsigned long size);
 
 /* Architecture __weak replacement functions */
 void __init mem_encrypt_init(void);
+void __init mem_encrypt_free_decrypted_mem(void);
 
 bool sme_active(void);
 bool sev_active(void);
+
+#define __bss_decrypted __attribute__((__section__(".bss..decrypted")))
 
 #else	/* !CONFIG_AMD_MEM_ENCRYPT */
 
@@ -77,6 +80,8 @@ early_set_memory_decrypted(unsigned long vaddr, unsigned long size) { return 0; 
 static inline int __init
 early_set_memory_encrypted(unsigned long vaddr, unsigned long size) { return 0; }
 
+#define __bss_decrypted
+
 #endif	/* CONFIG_AMD_MEM_ENCRYPT */
 
 /*
@@ -87,6 +92,8 @@ early_set_memory_encrypted(unsigned long vaddr, unsigned long size) { return 0; 
  */
 #define __sme_pa(x)		(__pa(x) | sme_me_mask)
 #define __sme_pa_nodebug(x)	(__pa_nodebug(x) | sme_me_mask)
+
+extern char __start_bss_decrypted[], __end_bss_decrypted[], __start_bss_decrypted_unused[];
 
 #endif	/* __ASSEMBLY__ */
 

arch/x86/kernel/vmlinux.lds.S
@@ -65,6 +65,23 @@ jiffies_64 = jiffies;
 #define ALIGN_ENTRY_TEXT_BEGIN	. = ALIGN(PMD_SIZE);
 #define ALIGN_ENTRY_TEXT_END	. = ALIGN(PMD_SIZE);
 
+/*
+ * This section contains data which will be mapped as decrypted. Memory
+ * encryption operates on a page basis. Make this section PMD-aligned
+ * to avoid splitting the pages while mapping the section early.
+ *
+ * Note: We use a separate section so that only this section gets
+ * decrypted to avoid exposing more than we wish.
+ */
+#define BSS_DECRYPTED						\
+	. = ALIGN(PMD_SIZE);					\
+	__start_bss_decrypted = .;				\
+	*(.bss..decrypted);					\
+	. = ALIGN(PAGE_SIZE);					\
+	__start_bss_decrypted_unused = .;			\
+	. = ALIGN(PMD_SIZE);					\
+	__end_bss_decrypted = .;				\
+
 #else
 
 #define X86_ALIGN_RODATA_BEGIN
@@ -74,6 +91,7 @@ jiffies_64 = jiffies;
 
 #define ALIGN_ENTRY_TEXT_BEGIN
 #define ALIGN_ENTRY_TEXT_END
+#define BSS_DECRYPTED
 
 #endif
 
@@ -355,6 +373,7 @@ SECTIONS
 		__bss_start = .;
 		*(.bss..page_aligned)
 		*(.bss)
+		BSS_DECRYPTED
 		. = ALIGN(PAGE_SIZE);
 		__bss_stop = .;
 	}

arch/x86/mm/init.c
@@ -815,9 +815,13 @@ void free_kernel_image_pages(void *begin, void *end)
 		set_memory_np_noalias(begin_ul, len_pages);
 }
 
+void __weak mem_encrypt_free_decrypted_mem(void) { }
+
 void __ref free_initmem(void)
 {
 	e820__reallocate_tables();
+
+	mem_encrypt_free_decrypted_mem();
 
 	free_kernel_image_pages(&__init_begin, &__init_end);
 }

arch/x86/mm/mem_encrypt.c
@@ -348,6 +348,30 @@ bool sev_active(void)
 EXPORT_SYMBOL(sev_active);
 
 /* Architecture __weak replacement functions */
+void __init mem_encrypt_free_decrypted_mem(void)
+{
+	unsigned long vaddr, vaddr_end, npages;
+	int r;
+
+	vaddr = (unsigned long)__start_bss_decrypted_unused;
+	vaddr_end = (unsigned long)__end_bss_decrypted;
+	npages = (vaddr_end - vaddr) >> PAGE_SHIFT;
+
+	/*
+	 * The unused memory range was mapped decrypted, change the encryption
+	 * attribute from decrypted to encrypted before freeing it.
+	 */
+	if (mem_encrypt_active()) {
+		r = set_memory_encrypted(vaddr, npages);
+		if (r) {
+			pr_warn("failed to free unused decrypted pages\n");
+			return;
+		}
+	}
+
+	free_init_pages("unused decrypted", vaddr, vaddr_end);
+}
+
 void __init mem_encrypt_init(void)
 {
 	if (!sme_me_mask)

openeuler-mirror/kernel

commit ca0c1fa1ec4b6d9040cec7e543d50a93fc88d4c6

x86/head/64: Flush caches for .bss..decrypted section after CR3 switches to early_top_pgt
hygon inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/I98WPG
CVE: NA

---------------------------

The memory region of .bss..decrypted section maybe mapped with encryption
before early boot stage of Linux. If the correspond stale caches lives in
earlier stage were not flushed before we access that memory region in
later stages, then Linux will crash because the stale caches will pollute
the memory.

Fix this issue by flush the caches with encrypted mapping before we
access .bss..decrypted section.

Fixes: b3f0907c71e0 ("x86/mm: Add .bss..decrypted section to hold shared variables")
Signed-off-by: hanliyang <hanliyang@hygon.cn>

arch/x86/kernel/head64.c
@@ -318,6 +318,115 @@ unsigned long __head __startup_64(unsigned long physaddr,
 	return sme_postprocess_startup(bp, pmd);
 }
 
+#ifdef CONFIG_AMD_MEM_ENCRYPT
+
+extern bool bsp_flush_bss_decrypted_section_handled;
+
+/* Get CPUID data through GHCB MSR protocol */
+static int __cpuid_msr_protocol(u32 fn, int reg_idx, u32 *reg)
+{
+	unsigned int msr_idx = (unsigned int)MSR_AMD64_SEV_ES_GHCB;
+	struct msr m;
+
+	m.q = GHCB_CPUID_REQ(fn, reg_idx);
+
+	asm volatile("wrmsr" : : "c" (msr_idx), "a"(m.l), "d" (m.h) : "memory");
+	VMGEXIT();
+	asm volatile("rdmsr" : "=a" (m.l), "=d" (m.h) : "c" (msr_idx));
+
+	if (GHCB_RESP_CODE(m.q) != GHCB_MSR_CPUID_RESP)
+		return -EIO;
+
+	*reg = m.h;
+
+	return 0;
+}
+
+static bool __should_do_clflush(void)
+{
+	u32 eax, ebx, ecx, edx;
+	int ret;
+
+	/* Check if this is a Hygon CSV guest or an AMD SEV guest */
+	if (!sme_get_me_mask() ||
+	    !(RIP_REL_REF(sev_status) & MSR_AMD64_SEV_ENABLED))
+		return false;
+
+	/* Get cpuid vendor info, if cannot get vendor info, then return false */
+	eax = 0x0;
+	if (!(RIP_REL_REF(sev_status) & MSR_AMD64_SEV_ES_ENABLED)) {
+		native_cpuid(&eax, &ebx, &ecx, &edx);
+	} else {
+		/*
+		 * Hygon CSV2 guest or AMD SEV-ES guest should use GHCB MSR
+		 * protocol to get cpu vendor info.
+		 */
+		ret =         __cpuid_msr_protocol(eax, GHCB_CPUID_REQ_EBX, &ebx);
+		ret = ret ? : __cpuid_msr_protocol(eax, GHCB_CPUID_REQ_ECX, &ecx);
+		ret = ret ? : __cpuid_msr_protocol(eax, GHCB_CPUID_REQ_EDX, &edx);
+		if (ret)
+			return false;
+	}
+
+	/* Check if this is a Hygon CSV guest */
+#define STRING_Hygo 0x6f677948
+#define STRING_uine 0x656e6975
+#define STRING_nGen 0x6e65476e
+
+	if (ebx != STRING_Hygo || ecx != STRING_uine || edx != STRING_nGen)
+		return false;
+
+	return true;
+}
+
+void __ref early_clflush_bss_decrypted_section(void)
+{
+	unsigned long vaddr, vaddr_end;
+	char *cl, *start, *end;
+
+	/* Only allow bsp flush these caches and the bsp must at early boot stage */
+	if (bsp_flush_bss_decrypted_section_handled)
+		return;
+
+	if (read_cr3_pa() != __pa_nodebug(early_top_pgt))
+		return;
+
+	/* Only Hygon CSV guest should do the clflush */
+	if (!__should_do_clflush())
+		goto handled;
+
+	/*
+	 * The memory region of .bss..decrypted section maybe mapped
+	 * with encryption in earlier stage. If the correspond stale
+	 * caches lives in earlier stage were not flushed before we
+	 * access that memory region, then Linux will crash later
+	 * because the stale caches will pollute the memory. So we
+	 * need flush the caches with encrypted mapping before we
+	 * access .bss..decrypted section.
+	 *
+	 * The function __startup_64() have already filled the
+	 * encrypted mapping for .bss..decrypted section, use that
+	 * mapping here.
+	 */
+	vaddr = (unsigned long)__start_bss_decrypted -
+				__START_KERNEL_map + phys_base;
+	vaddr_end = (unsigned long)__end_bss_decrypted -
+				__START_KERNEL_map + phys_base;
+
+	/* Hardcode cl-size to 64 at this stage. */
+	start = (char *)(vaddr & ~63);
+	end   = (char *)((vaddr_end + 63) & ~63);
+
+	asm volatile("mfence" : : : "memory");
+	for (cl = start; cl != end; cl += 64)
+		clflush(cl);
+	asm volatile("mfence" : : : "memory");
+
+handled:
+	bsp_flush_bss_decrypted_section_handled = true;
+}
+#endif
+
 /* Wipe all early page tables except for the kernel symbol map */
 static void __init reset_early_page_tables(void)
 {

arch/x86/kernel/head_64.S
@@ -375,6 +375,14 @@ SYM_INNER_LABEL(secondary_startup_64_no_verify, SYM_L_GLOBAL)
 	shrq	$32, %rdx
 	wrmsr
 
+#ifdef CONFIG_AMD_MEM_ENCRYPT
+	/*
+	 * Ensure .bss.decrypted memory's stale caches which lived in earlier
+	 * stage to be flushed.
+	 */
+	call	early_clflush_bss_decrypted_section
+#endif
+
 	/* Setup and Load IDT */
 	call	early_setup_idt
 
@@ -511,6 +519,8 @@ SYM_CODE_END(vc_boot_ghcb)
 SYM_DATA(initial_code,	.quad x86_64_start_kernel)
 #ifdef CONFIG_AMD_MEM_ENCRYPT
 SYM_DATA(initial_vc_handler,	.quad handle_vc_boot_ghcb)
+SYM_DATA(bsp_flush_bss_decrypted_section_handled,	.byte 0x0)
+	.balign	8
 #endif
 
 SYM_DATA(trampoline_lock, .quad 0);