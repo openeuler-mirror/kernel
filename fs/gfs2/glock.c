2d407ee26f19d9bb29da16627804829ab73990a9
Andreas Gruenbacher <agruenba@redhat.com>
2024-07-11 21:53:09 +0800

    gfs2: Rename sd_{ glock => kill }_wait
    mainline inclusion
    from mainline-v6.6-rc1
    commit 3c69c437bf9832d2201702c5ccc3b8a77a7e0aa3
    category: bugfix
    bugzilla: https://gitee.com/src-openeuler/kernel/issues/IA6SA1
    CVE: CVE-2024-38570
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=3c69c437bf9832d2201702c5ccc3b8a77a7e0aa3
    
    --------------------------------
    
    Rename sd_glock_wait to sd_kill_wait: we'll use it for other things
    related to "killing" a filesystem on unmount soon (kill_sb).
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>
    Conflicts:
    	fs/gfs2/glock.c
    [Resolve conflicts due to several refactor patches not merged.]
    Signed-off-by: Zeng Heng <zengheng4@huawei.com>

--- fs/gfs2/glock.c
+++ fs/gfs2/glock.c
@@ -169,7 +169,7 @@ void gfs2_glock_free(struct gfs2_glock *gl)
 	wake_up_glock(gl);
 	call_rcu(&gl->gl_rcu, gfs2_glock_dealloc);
 	if (atomic_dec_and_test(&sdp->sd_glock_disposal))
-		wake_up(&sdp->sd_glock_wait);
+		wake_up(&sdp->sd_kill_wait);
 }
 
 /**
@@ -1097,7 +1097,7 @@ out_free:
 	kfree(gl->gl_lksb.sb_lvbptr);
 	kmem_cache_free(cachep, gl);
 	if (atomic_dec_and_test(&sdp->sd_glock_disposal))
-		wake_up(&sdp->sd_glock_wait);
+		wake_up(&sdp->sd_kill_wait);
 
 out:
 	return ret;
@@ -2010,7 +2010,7 @@ void gfs2_gl_hash_clear(struct gfs2_sbd *sdp)
 	flush_workqueue(glock_workqueue);
 	glock_hash_walk(clear_glock, sdp);
 	flush_workqueue(glock_workqueue);
-	wait_event_timeout(sdp->sd_glock_wait,
+	wait_event_timeout(sdp->sd_kill_wait,
 			   atomic_read(&sdp->sd_glock_disposal) == 0,
 			   HZ * 600);
 	glock_hash_walk(dump_glock_func, sdp);
