7dce08b99115534ea6a15a52b3c6148eb1620741
Scott Mayhew <smayhew@redhat.com>
2024-07-16 21:01:42 +0800

    nfs: ensure correct writeback errors are returned on close()
    mainline inclusion
    from mainline-v5.9-rc1
    commit 67dd23f9e6fbaf163431912ef5599c5e0693476c
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/IACBGS
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=67dd23f9e6fbaf163431912ef5599c5e0693476c
    
    --------------------------------
    
    nfs_wb_all() calls filemap_write_and_wait(), which uses
    filemap_check_errors() to determine the error to return.
    filemap_check_errors() only looks at the mapping->flags and will
    therefore only return either -ENOSPC or -EIO.  To ensure that the
    correct error is returned on close(), nfs{,4}_file_flush() should call
    filemap_check_wb_err() which looks at the errseq value in
    mapping->wb_err without consuming it.
    
    Fixes: 6fbda89b257f ("NFS: Replace custom error reporting mechanism with
    generic one")
    Signed-off-by: Scott Mayhew <smayhew@redhat.com>
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>
    Signed-off-by: Wang Zhaolong <wangzhaolong1@huawei.com>

--- fs/nfs/nfs4file.c
+++ fs/nfs/nfs4file.c
@@ -113,6 +113,7 @@ static int
 nfs4_file_flush(struct file *file, fl_owner_t id)
 {
 	struct inode	*inode = file_inode(file);
+	errseq_t since;
 
 	dprintk("NFS: flush(%pD2)\n", file);
 
@@ -128,7 +129,9 @@ nfs4_file_flush(struct file *file, fl_owner_t id)
 		return filemap_fdatawrite(file->f_mapping);
 
 	/* Flush writes to the server and return any errors */
-	return nfs_wb_all(inode);
+	since = filemap_sample_wb_err(file->f_mapping);
+	nfs_wb_all(inode);
+	return filemap_check_wb_err(file->f_mapping, since);
 }
 
 #ifdef CONFIG_NFS_V4_2
