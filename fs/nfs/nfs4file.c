e6e68bb2d90d39b490c08af25c72b116bf6594dc
Wang Zhaolong <wangzhaolong1@huawei.com>
2024-07-17 14:09:11 +0800

    nfs: Ensure write and flush consume writeback errors
    hulk inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/IACBGS
    
    --------------------------------
    
    When the file is closed and the ->flush is executed, the writeback
    error is not consumed. As a result, when the file is written next time,
    the writeback error still exists, causing a new write failure.
    
    Another problem is that in the write process, only special writeback
    error codes such as -EDQUOT, -EFBIG, and -ENOSPC are detected, and the
    error codes are consumed. In this case, other error codes may be left.
    
    The writeback error code should be consumed unconditionally in the
    write(), fsync(), and close() to avoid sampling residual expired
    codes that are left over from the last invoking.
    
    In the fsync process, the error code can be consumed. Therefore, this
    patch ensures that write and flush can consume error codes.
    
    Fixes: 6fbda89b257f ("NFS: Replace custom error reporting mechanism with generic one")
    Signed-off-by: Wang Zhaolong <wangzhaolong1@huawei.com>

--- fs/nfs/nfs4file.c
+++ fs/nfs/nfs4file.c
@@ -113,7 +113,6 @@ static int
 nfs4_file_flush(struct file *file, fl_owner_t id)
 {
 	struct inode	*inode = file_inode(file);
-	errseq_t since;
 
 	dprintk("NFS: flush(%pD2)\n", file);
 
@@ -129,9 +128,8 @@ nfs4_file_flush(struct file *file, fl_owner_t id)
 		return filemap_fdatawrite(file->f_mapping);
 
 	/* Flush writes to the server and return any errors */
-	since = filemap_sample_wb_err(file->f_mapping);
 	nfs_wb_all(inode);
-	return filemap_check_wb_err(file->f_mapping, since);
+	return file_check_and_advance_wb_err(file);
 }
 
 #ifdef CONFIG_NFS_V4_2
