481811226d045ef9d7d28cf0c2c3fb744b8834a9
Chen Ridong <chenridong@huawei.com>
2024-07-24 10:24:18 +0000

    cgroup/cpuset: remove child_ecpus_count
    mainline inclusion
    from mainline-v6.12-rc1
    commit d6326047576266991d88639e1e9739a9a9a20ef4
    category: cleanup
    bugzilla: https://atomgit.com/openeuler/kernel/issues/9899
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=d6326047576266991d88639e1e9739a9a9a20ef4
    
    --------------------------------
    
    The child_ecpus_count variable was previously used to update
    sibling cpumask when parent's effective_cpus is updated. However, it became
    obsolete after commit e2ffe502ba45 ("cgroup/cpuset: Add
    cpuset.cpus.exclusive for v2"). It should be removed.
    
    tj: Restored {} for style consistency.
    
    Signed-off-by: Chen Ridong <chenridong@huawei.com>
    Acked-by: Waiman Long <longman@redhat.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    
    Conflicts:
    	kernel/cgroup/cpuset.c
    [
    The upstream context of the remote_partition_enable() hunk contains
    isolcpus_updated assignment and update_unbound_workqueue_cpumask()
    calls from 72c6303acfa1, which is not backported (the
    partition_xcpus_add() helper keeps its void form from the
    11e5f407b64a backport); those lines are dropped. In
    cpuset_css_online(), OLK-6.6 keeps the v6.6-era in-block
    CS_SCHED_LOAD_BALANCE clearing inside is_in_v2_mode(), so only the
    parent->child_ecpus_count++ line is removed there.
    
    Placed at its upstream chronological position (v6.12-rc1, before
    e55f45b4bafe) since the 11e5f407b64a backport at the head of this
    series provides the new_prs parameter context its hunks need.
    ]
    Signed-off-by: Zhang Qiao <zhangqiao22@huawei.com>

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -196,10 +196,8 @@ struct cpuset {
 	/*
 	 * Default hierarchy only:
 	 * use_parent_ecpus - set if using parent's effective_cpus
-	 * child_ecpus_count - # of children with use_parent_ecpus set
 	 */
 	int use_parent_ecpus;
-	int child_ecpus_count;
 
 	/*
 	 * number of SCHED_DEADLINE tasks attached to this cpuset, so that we
@@ -1660,7 +1658,6 @@ static void reset_partition_data(struct cpuset *cs)
 	if (!cpumask_and(cs->effective_cpus,
 			 parent->effective_cpus, cs->cpus_allowed)) {
 		cs->use_parent_ecpus = true;
-		parent->child_ecpus_count++;
 		cpumask_copy(cs->effective_cpus, parent->effective_cpus);
 	}
 }
@@ -1799,12 +1796,8 @@ static int remote_partition_enable(struct cpuset *cs, int new_prs,
 	spin_lock_irq(&callback_lock);
 	partition_xcpus_add(new_prs, NULL, tmp->new_cpus);
 	list_add(&cs->remote_sibling, &remote_children);
-	if (cs->use_parent_ecpus) {
-		struct cpuset *parent = parent_cs(cs);
-
+	if (cs->use_parent_ecpus)
 		cs->use_parent_ecpus = false;
-		parent->child_ecpus_count--;
-	}
 	spin_unlock_irq(&callback_lock);
 
 	/*
@@ -2421,14 +2414,10 @@ static void update_cpumasks_hier(struct cpuset *cs, struct tmpmasks *tmp,
 		 */
 		if (is_in_v2_mode() && !remote && cpumask_empty(tmp->new_cpus)) {
 			cpumask_copy(tmp->new_cpus, parent->effective_cpus);
-			if (!cp->use_parent_ecpus) {
+			if (!cp->use_parent_ecpus)
 				cp->use_parent_ecpus = true;
-				parent->child_ecpus_count++;
-			}
 		} else if (cp->use_parent_ecpus) {
 			cp->use_parent_ecpus = false;
-			WARN_ON_ONCE(!parent->child_ecpus_count);
-			parent->child_ecpus_count--;
 		}
 
 		if (remote)
@@ -4256,7 +4245,6 @@ static int cpuset_css_online(struct cgroup_subsys_state *css)
 		cpumask_copy(cs->effective_cpus, parent->effective_cpus);
 		cs->effective_mems = parent->effective_mems;
 		cs->use_parent_ecpus = true;
-		parent->child_ecpus_count++;
 		/*
 		 * Clear CS_SCHED_LOAD_BALANCE if parent is isolated
 		 */
@@ -4338,12 +4326,8 @@ static void cpuset_css_offline(struct cgroup_subsys_state *css)
 	    is_sched_load_balance(cs))
 		update_flag(CS_SCHED_LOAD_BALANCE, cs, 0);
 
-	if (cs->use_parent_ecpus) {
-		struct cpuset *parent = parent_cs(cs);
-
+	if (cs->use_parent_ecpus)
 		cs->use_parent_ecpus = false;
-		parent->child_ecpus_count--;
-	}
 
 	cpuset_dec();
 	clear_bit(CS_ONLINE, &cs->flags);
