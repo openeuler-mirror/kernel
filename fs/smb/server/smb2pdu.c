bcc8044d981102fb5c1ff39520fc7e06825a0ecf
Namjae Jeon <linkinjeon@kernel.org>
2024-08-05 08:57:03 +0900

    ksmbd: override fsids for smb2_query_info()
    stable inclusion
    from stable-v6.6.52
    commit 78bce66914dba6fb7a589dd8f3aa3a42c0f78248
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/IAYXOD
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=78bce66914dba6fb7a589dd8f3aa3a42c0f78248
    
    --------------------------------
    
    [ Upstream commit f6bd41280a44dcc2e0a25ed72617d25f586974a7 ]
    
    Sangsoo reported that a DAC denial error occurred when accessing
    files through the ksmbd thread. This patch override fsids for
    smb2_query_info().
    
    Reported-by: Sangsoo Lee <constant.lee@samsung.com>
    Signed-off-by: Namjae Jeon <linkinjeon@kernel.org>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Wen Zhiwei <wenzhiwei@kylinos.cn>

--- fs/smb/server/smb2pdu.c
+++ fs/smb/server/smb2pdu.c
@@ -5603,6 +5603,11 @@ int smb2_query_info(struct ksmbd_work *work)
 
 	ksmbd_debug(SMB, "GOT query info request\n");
 
+	if (ksmbd_override_fsids(work)) {
+		rc = -ENOMEM;
+		goto err_out;
+	}
+
 	switch (req->InfoType) {
 	case SMB2_O_INFO_FILE:
 		ksmbd_debug(SMB, "GOT SMB2_O_INFO_FILE\n");
@@ -5621,6 +5626,7 @@ int smb2_query_info(struct ksmbd_work *work)
 			    req->InfoType);
 		rc = -EOPNOTSUPP;
 	}
+	ksmbd_revert_fsids(work);
 
 	if (!rc) {
 		rsp->StructureSize = cpu_to_le16(9);
@@ -5630,6 +5636,7 @@ int smb2_query_info(struct ksmbd_work *work)
 					le32_to_cpu(rsp->OutputBufferLength));
 	}
 
+err_out:
 	if (rc < 0) {
 		if (rc == -EACCES)
 			rsp->hdr.Status = STATUS_ACCESS_DENIED;
