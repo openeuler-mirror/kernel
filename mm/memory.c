d41df9976f63f195cec775475738e930475dcfe1
Ryan Roberts <ryan.roberts@arm.com>
2024-09-14 09:15:23 +0800

    mm: fix failure to unmap pte on highmem systems
    mainline inclusion
    from mainline-v6.5-rc1
    commit 3b65f437d9e8dd696a2b88e7afcd51385532ab35
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/IAQT9Q
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=3b65f437d9e8dd696a2b88e7afcd51385532ab35
    
    --------------------------------
    
    The loser of a race to service a pte for a device private entry in the
    swap path previously unlocked the ptl, but failed to unmap the pte.  This
    only affects highmem systems since unmapping a pte is a noop on
    non-highmem systems.
    
    Link: https://lkml.kernel.org/r/20230602092949.545577-5-ryan.roberts@arm.com
    Fixes: 16ce101db85d ("mm/memory.c: fix race when faulting a device private page")
    Signed-off-by: Ryan Roberts <ryan.roberts@arm.com>
    Reviewed-by: Zi Yan <ziy@nvidia.com>
    Reviewed-by: Mike Rapoport (IBM) <rppt@kernel.org>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Lorenzo Stoakes <lstoakes@gmail.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: SeongJae Park <sj@kernel.org>
    Cc: Uladzislau Rezki (Sony) <urezki@gmail.com>
    Cc: Yu Zhao <yuzhao@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Ma Wupeng <mawupeng1@huawei.com>

--- mm/memory.c
+++ mm/memory.c
@@ -3424,10 +3424,8 @@ vm_fault_t do_swap_page(struct vm_fault *vmf)
 			vmf->page = device_private_entry_to_page(entry);
 			vmf->pte = pte_offset_map_lock(vma->vm_mm, vmf->pmd,
 					vmf->address, &vmf->ptl);
-			if (unlikely(!pte_same(*vmf->pte, vmf->orig_pte))) {
-				spin_unlock(vmf->ptl);
-				goto out;
-			}
+			if (unlikely(!pte_same(*vmf->pte, vmf->orig_pte)))
+				goto unlock;
 
 			/*
 			 * Get a page reference while we know the page can't be
