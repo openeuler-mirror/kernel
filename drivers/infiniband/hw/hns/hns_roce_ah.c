59e32cc5fb721ff0372c602471a3cf6ceb7fff95
Junxian Huang <huangjunxian6@hisilicon.com>
2024-09-27 18:15:10 +0800

    RDMA/hns: Fix ah error counter in sw stat not increasing
    maillist inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/IATE99
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/rdma/rdma.git/commit/?id=39c047d4047a1242aeefa87513174b56a91080ab
    
    ----------------------------------------------------------------------
    
    There are several error cases where hns_roce_create_ah() returns
    directly without jumping to sw stat path, thus leading to a problem
    that the ah error counter does not increase.
    
    Fixes: 5e7ecc38d89b ("RDMA/hns: Support SW stats with debugfs")
    Signed-off-by: Junxian Huang <huangjunxian6@hisilicon.com>
    Link: https://patch.msgid.link/20240912115700.2016443-1-huangjunxian6@hisilicon.com
    Signed-off-by: Leon Romanovsky <leon@kernel.org>
    Signed-off-by: Xinghai Cen <cenxinghai@h-partners.com>

--- drivers/infiniband/hw/hns/hns_roce_ah.c
+++ drivers/infiniband/hw/hns/hns_roce_ah.c
@@ -65,8 +65,10 @@ int hns_roce_create_ah(struct ib_ah *ibah, struct rdma_ah_init_attr *init_attr,
 	u32 sl_num;
 	int ret;
 
-	if (hr_dev->pci_dev->revision == PCI_REVISION_ID_HIP08 && udata)
-		return -EOPNOTSUPP;
+	if (hr_dev->pci_dev->revision == PCI_REVISION_ID_HIP08 && udata) {
+		ret = -EOPNOTSUPP;
+		goto err_out;
+	}
 
 	ah->av.port = rdma_ah_get_port_num(ah_attr);
 	ah->av.gid_index = grh->sgid_index;
@@ -98,7 +100,8 @@ int hns_roce_create_ah(struct ib_ah *ibah, struct rdma_ah_init_attr *init_attr,
 		ibdev_err_ratelimited(&hr_dev->ib_dev,
 			  "failed to set sl, sl (%u) shouldn't be larger than %u.\n",
 			  ah->av.sl, sl_num);
-		return -EINVAL;
+		ret = -EINVAL;
+		goto err_out;
 	}
 
 	memcpy(ah->av.dgid, grh->dgid.raw, HNS_ROCE_GID_SIZE);
