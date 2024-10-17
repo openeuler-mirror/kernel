6f3a7c45998ada49ca9c31ed232db14ba56f1578
Jian Shen <shenjian15@huawei.com>
2024-10-17 16:58:55 +0800

    net: hns3: add mac tunnel number query
    driver inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBV2JA
    CVE: NA
    
    ----------------------------------------------------------------------
    
    The mac tunnel number was missed when query the dev_info
    by debugfs, so add it.
    
    Fixes: c929bc2ac36e ("net: hns3: refactor dev capability and dev spec of debugfs")
    Signed-off-by: Jian Shen <shenjian15@huawei.com>
    Signed-off-by: Hao Chen <chenhao418@huawei.com>

--- drivers/net/ethernet/hisilicon/hns3/hns3_debugfs.c
+++ drivers/net/ethernet/hisilicon/hns3/hns3_debugfs.c
@@ -1107,6 +1107,8 @@ hns3_dbg_dev_specs(struct hnae3_handle *h, char *buf, int len, int *pos)
 	*pos += scnprintf(buf + *pos, len - *pos,
 			  "TX timeout threshold: %d seconds\n",
 			  dev->watchdog_timeo / HZ);
+	*pos += scnprintf(buf + *pos, len - *pos, "mac tunnel number: %u\n",
+			  dev_specs->tnl_num);
 	*pos += scnprintf(buf + *pos, len - *pos, "Hilink Version: %u\n",
 			  dev_specs->hilink_version);
 	*pos += scnprintf(buf + *pos, len - *pos, "total rx buffer size: %u\n",
