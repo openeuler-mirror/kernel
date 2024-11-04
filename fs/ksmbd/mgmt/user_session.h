8d0729e0829e31ec4204a03eaad0432c353424d7
Namjae Jeon <linkinjeon@kernel.org>
2024-11-04 19:07:35 +0800

    ksmbd: fix user-after-free from session log off
    stable inclusion
    from stable-v6.6.57
    commit 5511999e9615e4318e9142d23b29bd1597befc08
    category: bugfix
    bugzilla: https://gitee.com/src-openeuler/kernel/issues/IB0ENJ
    CVE: CVE-2024-50086
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?h=tags/v6.6.58&id=5511999e9615e4318e9142d23b29bd1597befc08
    
    --------------------------------
    
    commit 7aa8804c0b67b3cb263a472d17f2cb50d7f1a930 upstream.
    
    There is racy issue between smb2 session log off and smb2 session setup.
    It will cause user-after-free from session log off.
    This add session_lock when setting SMB2_SESSION_EXPIRED and referece
    count to session struct not to free session while it is being used.
    
    Cc: stable@vger.kernel.org # v5.15+
    Reported-by: zdi-disclosures@trendmicro.com # ZDI-CAN-25282
    Signed-off-by: Namjae Jeon <linkinjeon@kernel.org>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Conflicts:
    	fs/ksmbd/mgmt/user_session.c
    	fs/ksmbd/mgmt/user_session.h
    	fs/ksmbd/server.c
    	fs/ksmbd/smb2pdu.c
    	fs/smb/server/mgmt/user_session.c
    	fs/smb/server/user_session.h
    	fs/smb/server/server.c
    	fs/smb/server/smb2pdu.c
    [Conflicts due to ksmbd rename to smb/server ]
    Signed-off-by: Long Li <leo.lilong@huawei.com>

--- fs/ksmbd/mgmt/user_session.h
+++ fs/ksmbd/mgmt/user_session.h
@@ -60,6 +60,7 @@ struct ksmbd_session {
 
 	struct ksmbd_file_table		file_table;
 	unsigned long			last_active;
+	atomic_t			refcnt;
 };
 
 static inline int test_session_flag(struct ksmbd_session *sess, int bit)
@@ -100,4 +101,6 @@ void ksmbd_release_tree_conn_id(struct ksmbd_session *sess, int id);
 int ksmbd_session_rpc_open(struct ksmbd_session *sess, char *rpc_name);
 void ksmbd_session_rpc_close(struct ksmbd_session *sess, int id);
 int ksmbd_session_rpc_method(struct ksmbd_session *sess, int id);
+void ksmbd_user_session_get(struct ksmbd_session *sess);
+void ksmbd_user_session_put(struct ksmbd_session *sess);
 #endif /* __USER_SESSION_MANAGEMENT_H__ */
