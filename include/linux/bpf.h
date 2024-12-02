c6e22ccd746f1b807ae7e9895437fddfb6dd8329
Joanne Koong <joannelkoong@gmail.com>
2024-12-02 07:00:58 +0000

    bpf: Add MEM_UNINIT as a bpf_type_flag
    mainline inclusion
    from mainline-v5.19-rc1
    commit 16d1e00c7e8a4950e914223b3112144289a82913
    category: bugfix
    bugzilla: https://gitee.com/src-openeuler/kernel/issues/IAYQOP
    CVE: CVE-2024-49861
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=16d1e00c7e8a
    
    --------------------------------
    
    Instead of having uninitialized versions of arguments as separate
    bpf_arg_types (eg ARG_PTR_TO_UNINIT_MEM as the uninitialized version
    of ARG_PTR_TO_MEM), we can instead use MEM_UNINIT as a bpf_type_flag
    modifier to denote that the argument is uninitialized.
    
    Doing so cleans up some of the logic in the verifier. We no longer
    need to do two checks against an argument type (eg "if
    (base_type(arg_type) == ARG_PTR_TO_MEM || base_type(arg_type) ==
    ARG_PTR_TO_UNINIT_MEM)"), since uninitialized and initialized
    versions of the same argument type will now share the same base type.
    
    In the near future, MEM_UNINIT will be used by dynptr helper functions
    as well.
    
    Signed-off-by: Joanne Koong <joannelkoong@gmail.com>
    Acked-by: Andrii Nakryiko <andrii@kernel.org>
    Acked-by: David Vernet <void@manifault.com>
    Link: https://lore.kernel.org/r/20220509224257.3222614-2-joannelkoong@gmail.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Conflicts:
    	include/linux/bpf.h
    	kernel/bpf/helpers.c
    	kernel/bpf/verifier.c
    [The conflicts were due to not merge some bpf_type_flag]
    Signed-off-by: Xiaomeng Zhang <zhangxiaomeng13@huawei.com>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -290,7 +290,10 @@ enum bpf_type_flag {
 	 */
 	MEM_ALLOC		= BIT(2 + BPF_BASE_TYPE_BITS),
 
-	__BPF_TYPE_LAST_FLAG	= MEM_ALLOC,
+	MEM_UNINIT		= BIT(7 + BPF_BASE_TYPE_BITS),
+
+	__BPF_TYPE_FLAG_MAX,
+	__BPF_TYPE_LAST_FLAG	= __BPF_TYPE_FLAG_MAX - 1,
 };
 
 /* Max number of base types. */
@@ -309,16 +312,11 @@ enum bpf_arg_type {
 	ARG_CONST_MAP_PTR,	/* const argument used as pointer to bpf_map */
 	ARG_PTR_TO_MAP_KEY,	/* pointer to stack used as map key */
 	ARG_PTR_TO_MAP_VALUE,	/* pointer to stack used as map value */
-	ARG_PTR_TO_UNINIT_MAP_VALUE,	/* pointer to valid memory used to store a map value */
 
-	/* the following constraints used to prototype bpf_memcmp() and other
-	 * functions that access data on eBPF program stack
+	/* Used to prototype bpf_memcmp() and other functions that access data
+	 * on eBPF program stack
 	 */
 	ARG_PTR_TO_MEM,		/* pointer to valid memory (stack, packet, map value) */
-	ARG_PTR_TO_UNINIT_MEM,	/* pointer to memory does not need to be initialized,
-				 * helper function must fill all bytes or clear
-				 * them in error case.
-				 */
 
 	ARG_CONST_SIZE,		/* number of bytes accessed from memory */
 	ARG_CONST_SIZE_OR_ZERO,	/* number of bytes accessed from memory or 0 */
@@ -343,6 +341,10 @@ enum bpf_arg_type {
 	ARG_PTR_TO_CTX_OR_NULL		= PTR_MAYBE_NULL | ARG_PTR_TO_CTX,
 	ARG_PTR_TO_SOCKET_OR_NULL	= PTR_MAYBE_NULL | ARG_PTR_TO_SOCKET,
 	ARG_PTR_TO_ALLOC_MEM_OR_NULL	= PTR_MAYBE_NULL | ARG_PTR_TO_ALLOC_MEM,
+	/* pointer to memory does not need to be initialized, helper function must fill
+	 * all bytes or clear them in error case.
+	 */
+	ARG_PTR_TO_UNINIT_MEM		= MEM_UNINIT | ARG_PTR_TO_MEM,
 
 	/* This must be the last entry. Its purpose is to ensure the enum is
 	 * wide enough to hold the higher bits reserved for bpf_type_flag.
