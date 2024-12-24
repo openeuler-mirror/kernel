65bd372785866d12ec0dbe2067344504d56584ae
Liu Shixin <liushixin2@huawei.com>
2024-12-24 21:00:55 +0800

    dhugetlb: keep refcount to 0 while page is actually free in pool
    hulk inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBDF90
    
    --------------------------------
    
    While we split/merge page in dynamic pool, the refcount of page may be
    increased to 1 due to prep_new_page(), and we then set the refcount to 0.
    That means there's a time window that the refcount of the free page is
    not 0.
    
    split_huge_pages_set() will scan all pages and try to get the refcount
    If try to get refcount in the above time window and put_page() after
    the refcount is set to 0, the refcount will be decreased to -1.
    
    It's dangerous if the refcount for page is not 0 when split/merge, so try
    not to get refcount there.
    
    Fixes: 0bc0d0d57eda ("dhugetlb: backport dynamic hugetlb feature")
    Signed-off-by: Liu Shixin <liushixin2@huawei.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -2100,6 +2100,32 @@ void prep_new_page(struct page *page, unsigned int order, gfp_t gfp_flags,
 		clear_page_pfmemalloc(page);
 }
 
+#ifdef CONFIG_DYNAMIC_HUGETLB
+/* Similar with prep_new_page without set refcount to 1 */
+void prep_new_frozen_free_page(struct page *page, unsigned int order,
+			       gfp_t gfp_flags)
+{
+	int i;
+
+	set_page_private(page, 0);
+
+	arch_alloc_page(page, order);
+	kernel_map_pages(page, 1 << order, 1);
+	kasan_alloc_pages(page, order);
+	kernel_poison_pages(page, 1 << order, 1);
+	set_page_owner(page, order, gfp_flags);
+
+	if (!free_pages_prezeroed() && (gfp_flags & __GFP_ZERO))
+		for (i = 0; i < (1 << order); i++)
+			clear_highpage(page + i);
+
+	if (order && (gfp_flags & __GFP_COMP))
+		prep_compound_page(page, order);
+
+	clear_page_pfmemalloc(page);
+}
+#endif
+
 /*
  * Go through the free lists for the given migratetype and remove
  * the smallest available page from the freelists
