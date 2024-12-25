65c1682aca74d111db66c0a345a416267a0bff39
Jeffle Xu <jefflexu@linux.alibaba.com>
2024-12-25 10:10:21 +0800

    cachefiles: notify the user daemon when withdrawing cookie
    anolis inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IB5UKT
    
    Reference: https://gitee.com/anolis/cloud-kernel/commit/d665b925541c
    
    --------------------------------
    
    ANBZ: #1666
    
    commit 324b954ac80cff0d11ddb6bde9b6631e45e98620 upstream.
    
    Notify the user daemon that cookie is going to be withdrawn, providing a
    hint that the associated anonymous fd can be closed.
    
    Be noted that this is only a hint. The user daemon may close the
    associated anonymous fd when receiving the CLOSE request, then it will
    receive another anonymous fd when the cookie gets looked up. Or it may
    ignore the CLOSE request, and keep writing data through the anonymous
    fd. However the next time the cookie gets looked up, the user daemon
    will still receive another new anonymous fd.
    
    Signed-off-by: Jeffle Xu <jefflexu@linux.alibaba.com>
    Acked-by: David Howells <dhowells@redhat.com>
    Link: https://lore.kernel.org/r/20220425122143.56815-5-jefflexu@linux.alibaba.com
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Signed-off-by: Huang Jianan <jnhuang@linux.alibaba.com>
    Reviewed-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Reviewed-by: Jeffle Xu <jefflexu@linux.alibaba.com>
    Signed-off-by: Baokun Li <libaokun1@huawei.com>

--- fs/cachefiles/ondemand.c
+++ fs/cachefiles/ondemand.c
@@ -241,6 +241,14 @@ ssize_t cachefiles_ondemand_daemon_read(struct cachefiles_cache *cache,
 			ret = -EFAULT;
 			goto err_put_fd;
 		}
+
+		/* CLOSE request has no reply */
+		if (msg->opcode == CACHEFILES_OP_CLOSE) {
+			xa_lock(&cache->reqs);
+			radix_tree_delete(&cache->reqs, id);
+			xa_unlock(&cache->reqs);
+			complete(&req->done);
+		}
 		return n;
 	}
 	xa_unlock(&cache->reqs);
@@ -321,6 +329,13 @@ static int cachefiles_ondemand_send_req(struct cachefiles_object *object,
 	/* coupled with the barrier in cachefiles_flush_reqs() */
 	smp_mb();
 
+	if (opcode != CACHEFILES_OP_OPEN && object->ondemand_id <= 0) {
+		WARN_ON_ONCE(object->ondemand_id == 0);
+		xa_unlock(&cache->reqs);
+		ret = -EIO;
+		goto out;
+	}
+
 	while (radix_tree_insert(&cache->reqs,
 				 id = atomic64_read(&global_index), req))
 		atomic64_inc(&global_index);
@@ -368,6 +383,25 @@ static int cachefiles_ondemand_init_open_req(struct cachefiles_req *req,
 	return 0;
 }
 
+static int cachefiles_ondemand_init_close_req(struct cachefiles_req *req,
+					      void *private)
+{
+	struct cachefiles_object *object = req->object;
+	int object_id = object->ondemand_id;
+
+	/*
+	 * It's possible that object id is still 0 if the cookie looking up
+	 * phase failed before OPEN request has ever been sent. Also avoid
+	 * sending CLOSE request for CACHEFILES_ONDEMAND_ID_CLOSED, which means
+	 * anon_fd has already been closed.
+	 */
+	if (object_id <= 0)
+		return -ENOENT;
+
+	req->msg.object_id = object_id;
+	return 0;
+}
+
 int cachefiles_ondemand_init_object(struct cachefiles_object *object)
 {
 	struct fscache_cookie *cookie = object->fscache.cookie;
@@ -388,4 +422,10 @@ int cachefiles_ondemand_init_object(struct cachefiles_object *object)
 
 	return cachefiles_ondemand_send_req(object, CACHEFILES_OP_OPEN,
 			data_len, cachefiles_ondemand_init_open_req, NULL);
+}
+
+void cachefiles_ondemand_clean_object(struct cachefiles_object *object)
+{
+	cachefiles_ondemand_send_req(object, CACHEFILES_OP_CLOSE, 0,
+			cachefiles_ondemand_init_close_req, NULL);
 }
