c8794c1fd1b907e293ba3cb63c22e9005bc8df06
Yang Shi <shy828301@gmail.com>
2024-12-27 16:16:04 +0800

    mm: shmem: don't truncate page if memory failure happens
    stable inclusion
    from stable-v5.15.80
    commit 94fa250ea55cd4eea8049011ea4045343547ebd8
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBE6C6
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=94fa250ea55cd4eea8049011ea4045343547ebd8
    
    --------------------------------
    
    commit a7605426666196c5a460dd3de6f8dac1d3c21f00 upstream.
    
    The current behavior of memory failure is to truncate the page cache
    regardless of dirty or clean.  If the page is dirty the later access
    will get the obsolete data from disk without any notification to the
    users.  This may cause silent data loss.  It is even worse for shmem
    since shmem is in-memory filesystem, truncating page cache means
    discarding data blocks.  The later read would return all zero.
    
    The right approach is to keep the corrupted page in page cache, any
    later access would return error for syscalls or SIGBUS for page fault,
    until the file is truncated, hole punched or removed.  The regular
    storage backed filesystems would be more complicated so this patch is
    focused on shmem.  This also unblock the support for soft offlining
    shmem THP.
    
    [akpm@linux-foundation.org: coding style fixes]
    [arnd@arndb.de: fix uninitialized variable use in me_pagecache_clean()]
      Link: https://lkml.kernel.org/r/20211022064748.4173718-1-arnd@kernel.org
    [Fix invalid pointer dereference in shmem_read_mapping_page_gfp() with a
     slight different implementation from what Ajay Garg <ajaygargnsit@gmail.com>
     and Muchun Song <songmuchun@bytedance.com> proposed and reworked the
     error handling of shmem_write_begin() suggested by Linus]
      Link: https://lore.kernel.org/linux-mm/20211111084617.6746-1-ajaygargnsit@gmail.com/
    
    Link: https://lkml.kernel.org/r/20211020210755.23964-6-shy828301@gmail.com
    Link: https://lkml.kernel.org/r/20211116193247.21102-1-shy828301@gmail.com
    Signed-off-by: Yang Shi <shy828301@gmail.com>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Naoya Horiguchi <naoya.horiguchi@nec.com>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: Peter Xu <peterx@redhat.com>
    Cc: Ajay Garg <ajaygargnsit@gmail.com>
    Cc: Muchun Song <songmuchun@bytedance.com>
    Cc: Andy Lavr <andy.lavr@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Naoya Horiguchi <naoya.horiguchi@linux.dev>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Conflicts:
    	mm/memory-failure.c
    	mm/shmem.c
    	mm/userfaultfd.c
    [context conflict and userfaultfd do not merge 153132571f02("userfaultfd/
    shmem: support UFFDIO_CONTINUE for shmem")]
    Signed-off-by: Tong Tiangen <tongtiangen@huawei.com>

--- mm/memory-failure.c
+++ mm/memory-failure.c
@@ -59,6 +59,7 @@
 #include <linux/kfifo.h>
 #include <linux/ratelimit.h>
 #include <linux/page-isolation.h>
+#include <linux/shmem_fs.h>
 #include "internal.h"
 #include "ras/ras_event.h"
 
@@ -715,6 +716,7 @@ static int me_pagecache_clean(struct page_state *ps, struct page *p)
 {
 	int ret;
 	struct address_space *mapping;
+	bool extra_pins;
 
 	delete_from_lru_cache(p);
 
@@ -744,16 +746,22 @@ static int me_pagecache_clean(struct page_state *ps, struct page *p)
 	}
 
 	/*
+	 * The shmem page is kept in page cache instead of truncating
+	 * so is expected to have an extra refcount after error-handling.
+	 */
+	extra_pins = shmem_mapping(mapping);
+
+	/*
 	 * Truncation is a bit tricky. Enable it per file system for now.
 	 *
 	 * Open: to take i_mutex or not for this? Right now we don't.
 	 */
 	ret = truncate_error_page(p, page_to_pfn(p), mapping);
+	if (has_extra_refcount(ps, p, extra_pins))
+		ret = MF_FAILED;
+
 out:
 	unlock_page(p);
-
-	if (has_extra_refcount(ps, p, false))
-		ret = MF_FAILED;
 
 	return ret;
 }
