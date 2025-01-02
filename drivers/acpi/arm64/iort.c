af552f9d957def499065ed6e194bcd5f0c75dbc5
Jia Qingtong <jiaqingtong@huawei.com>
2025-01-02 17:25:57 +0800

    irqchip/gic-v3-its: Move build_devid_pools from its to acpi iort init
    virt inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBFBK2
    CVE: NA
    
    --------------------------------
    
    Move build_devid_pools' call to iort init as  VIRT_PLAT_DEV depedent
    on ACPI IORT table's DeviceID info.
    
    Signed-off-by: Jia Qingtong <jiaqingtong@huawei.com>

--- drivers/acpi/arm64/iort.c
+++ drivers/acpi/arm64/iort.c
@@ -1816,6 +1816,10 @@ static void __init iort_init_platform_devices(void)
 	}
 }
 
+#ifdef CONFIG_VIRT_PLAT_DEV
+void build_devid_pools(void);
+#endif
+
 void __init acpi_iort_init(void)
 {
 	acpi_status status;
@@ -1836,6 +1840,10 @@ void __init acpi_iort_init(void)
 	}
 
 	iort_init_platform_devices();
+
+#ifdef CONFIG_VIRT_PLAT_DEV
+	build_devid_pools();
+#endif
 }
 
 #ifdef CONFIG_ZONE_DMA
