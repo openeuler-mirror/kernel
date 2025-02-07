8eaa31bfc121ec09c283bfc00bbe370dfc84cd1d
Biju Das <biju.das.jz@bp.renesas.com>
2025-02-07 02:26:25 +0000

    drm: adv7511: Fix use-after-free in adv7533_attach_dsi()
    stable inclusion
    from stable-v5.10.234
    commit acec80d9f126cd3fa764bbe3d96bc0cb5cd2b087
    category: bugfix
    bugzilla: https://gitee.com/src-openeuler/kernel/issues/IBID2S
    CVE: CVE-2024-57887
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=acec80d9f126cd3fa764bbe3d96bc0cb5cd2b087
    
    --------------------------------
    
    [ Upstream commit 81adbd3ff21c1182e06aa02c6be0bfd9ea02d8e8 ]
    
    The host_node pointer was assigned and freed in adv7533_parse_dt(), and
    later, adv7533_attach_dsi() uses the same. Fix this use-after-free issue
    by dropping of_node_put() in adv7533_parse_dt() and calling of_node_put()
    in error path of probe() and also in the remove().
    
    Fixes: 1e4d58cd7f88 ("drm/bridge: adv7533: Create a MIPI DSI device")
    Cc: stable@vger.kernel.org
    Reviewed-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Signed-off-by: Biju Das <biju.das.jz@bp.renesas.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20241119192040.152657-2-biju.das.jz@bp.renesas.com
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    
    Conflicts:
        drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
        drivers/gpu/drm/bridge/adv7511/adv7533.c
    [Context differences.]
    Signed-off-by: Zheng Zucheng <zhengzucheng@huawei.com>
    (cherry picked from commit 29776fa6b7c4c679b634b59a3a8db252ee8cf7bb)

--- drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
+++ drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
@@ -1222,7 +1222,7 @@ static int adv7511_probe(struct i2c_client *i2c, const struct i2c_device_id *id)
 	ret = adv7511_init_regulators(adv7511);
 	if (ret) {
 		dev_err(dev, "failed to init regulators\n");
-		return ret;
+		goto err_of_node_put;
 	}
 
 	/*
@@ -1332,6 +1332,8 @@ err_i2c_unregister_edid:
 	i2c_unregister_device(adv7511->i2c_edid);
 uninit_regulators:
 	adv7511_uninit_regulators(adv7511);
+err_of_node_put:
+	of_node_put(adv7511->host_node);
 
 	return ret;
 }
@@ -1346,6 +1348,7 @@ static int adv7511_remove(struct i2c_client *i2c)
 	if (adv7511->cec_clk)
 		clk_disable_unprepare(adv7511->cec_clk);
 
+	of_node_put(adv7511->host_node);
 	adv7511_uninit_regulators(adv7511);
 
 	drm_bridge_remove(&adv7511->bridge);
