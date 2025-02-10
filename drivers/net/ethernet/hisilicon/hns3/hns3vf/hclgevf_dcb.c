012143b2ce7049e9ab763c47c1681695b0f23aad
Peiyang Wang <wangpeiyang1@huawei.com>
2025-02-10 16:43:47 +0800

    net: hns3: the multi-tcs VF can be configured only when the interface is down
    driver inclusion
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8432
    
    ----------------------------------------------------------------------
    
    The multi-tcs VF can be configured only when the interface is down
    
    Signed-off-by: Peiyang Wang <wangpeiyang1@huawei.com>
    Signed-off-by: Donghua Huang <huangdonghua3@h-partners.com>

--- drivers/net/ethernet/hisilicon/hns3/hns3vf/hclgevf_dcb.c
+++ drivers/net/ethernet/hisilicon/hns3/hns3vf/hclgevf_dcb.c
@@ -234,8 +234,10 @@ static bool hclgevf_ets_not_need_config(struct hclgevf_dev *hdev,
 static int hclgevf_ieee_setets(struct hnae3_handle *h, struct ieee_ets *ets)
 {
 	struct hclgevf_dev *hdev = hclgevf_ae_get_hdev(h);
-	int ret1;
 	int ret;
+
+	if (!test_bit(HCLGEVF_STATE_DOWN, &hdev->state))
+		return -EBUSY;
 
 	ret = hclgevf_ets_validate(hdev, ets);
 	if (ret)
@@ -244,23 +246,7 @@ static int hclgevf_ieee_setets(struct hnae3_handle *h, struct ieee_ets *ets)
 	if (hclgevf_ets_not_need_config(hdev, ets))
 		return 0;
 
-	ret = hclgevf_notify_client(hdev, HNAE3_DOWN_CLIENT);
-	if (ret)
-		return ret;
-	ret = hclgevf_notify_client(hdev, HNAE3_UNINIT_CLIENT);
-	if (ret)
-		return ret;
-
-	ret1 = hclgevf_set_vf_multi_tc(hdev, ets);
-
-	ret = hclgevf_notify_client(hdev, HNAE3_INIT_CLIENT);
-	if (ret)
-		return ret;
-
-	ret = hclgevf_notify_client(hdev, HNAE3_UP_CLIENT);
-	if (ret)
-		return ret;
-	return ret1;
+	return hclgevf_set_vf_multi_tc(hdev, ets);
 }
 
 static void hclgevf_tm_info_to_ieee_ets(struct hclgevf_dev *hdev,
