2fb0542afb24456d9be5f144cf7ae5bb058b6df3
Wenhui Fan <fanwh@hygon.cn>
2025-02-18 14:42:20 +0800

    EDAC/amd64: Calculate instance id for hygon family 18h model 7h
    hygon inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBN1FG
    CVE: NA
    
    ---------------------------
    
    The instance id for Hygon family 18h model 7h and 10h is also get from
    IPID[23:20] and IPID[13].
    
    Signed-off-by: Wenhui Fan <fanwh@hygon.cn>

--- drivers/edac/amd64_edac.c
+++ drivers/edac/amd64_edac.c
@@ -2777,7 +2777,7 @@ static void decode_umc_error(int node_id, struct mce *m)
 
 	err.csrow = m->synd & 0x7;
 
-	if (hygon_f18h_m4h() && boot_cpu_data.x86_model == 0x6)
+	if (hygon_f18h_m4h() && boot_cpu_data.x86_model >= 0x6)
 		umc = (err.channel << 1) + ((m->ipid & BIT(13)) >> 13);
 	else
 		umc = err.channel;
