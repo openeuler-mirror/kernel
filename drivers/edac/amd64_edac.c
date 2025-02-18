a2060fafdd9e491badbe66d31d5036bbbe5c0908
Wenhui Fan <fanwh@hygon.cn>
2025-02-18 15:33:41 +0800

    EDAC/amd64: Get correct memory type for Hygon family 18h model 10h
    hygon inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBN2NP
    CVE: NA
    
    ---------------------------
    
    Get the correct DDR memory types for Hygon family 18h model 10h.
    
    Signed-off-by: Wenhui Fan <fanwh@hygon.cn>

--- drivers/edac/amd64_edac.c
+++ drivers/edac/amd64_edac.c
@@ -1512,7 +1512,9 @@ static void determine_memory_type_df(struct amd64_pvt *pvt)
 		 * Check if the system supports the "DDR Type" field in UMC Config
 		 * and has DDR5 DIMMs in use.
 		 */
-		if ((pvt->flags.zn_regs_v2 || hygon_f18h_m4h()) &&
+		if ((pvt->flags.zn_regs_v2 ||
+		     hygon_f18h_m4h() ||
+		     hygon_f18h_m10h()) &&
 		    ((umc->umc_cfg & GENMASK(2, 0)) == 0x1)) {
 			if (umc->dimm_cfg & BIT(5))
 				umc->dram_type = MEM_LRDDR5;
