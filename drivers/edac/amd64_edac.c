openeuler-mirror/kernel

commit d0d1ed37697f039bdc7c992d84da4d08df16de23

EDAC/amd64: Adjust UMC channel for Hygon family 18h model 6h
hygon inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/I92NKS
CVE: NA

---------------------------

Hygon family 18h model 6h has 2 cs mapped to 1 umc, so adjust for it.

Fixes: b38615308659 ("EDAC/amd64: Add support for Hygon family 18h model 6h")
Signed-off-by: Pu Wen <puwen@hygon.cn>

drivers/edac/amd64_edac.c
@@ -2971,6 +2971,7 @@ static void decode_umc_error(int node_id, struct mce *m)
 	struct amd64_pvt *pvt;
 	struct err_info err;
 	u64 sys_addr;
+	u8 umc;
 
 	mci = edac_mc_find(node_id);
 	if (!mci)
@@ -3001,7 +3002,12 @@ static void decode_umc_error(int node_id, struct mce *m)
 
 	err.csrow = m->synd & 0x7;
 
-	if (umc_normaddr_to_sysaddr(m->addr, pvt->mc_node_id, err.channel, &sys_addr)) {
+	if (hygon_f18h_m4h() && boot_cpu_data.x86_model == 0x6)
+		umc = err.channel << 1;
+	else
+		umc = err.channel;
+
+	if (umc_normaddr_to_sysaddr(m->addr, pvt->mc_node_id, umc, &sys_addr)) {
 		err.err_code = ERR_NORM_ADDR;
 		goto log_error;
 	}

commit bdcb915b2475dd71ab9a7e6ab4641ec85e189e49

EDAC/amd64: Fix the calculation of instance id for Hygon family 18h model 6h
hygon inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/IBN075
CVE: NA

---------------------------

On Hygon family 18h model 6h platform, each DDR has two subchannels,
and each subchannel has a mca bank with a cs corresponded. In the
process of address translation, it needs to know the instance id
of cs to access df register correctly. The instance id of cs can be
calculated from IPID[23:20] and IPID[13]: IPID[23:20] represents
which umc controller it belongs to, and IPID[13] represents which
subchannel it is.

Fixes: d0d1ed37697f ("EDAC/amd64: Adjust UMC channel for Hygon family 18h model 6h")
Signed-off-by: Wenhui Fan <fanwh@hygon.cn>

drivers/edac/amd64_edac.c
@@ -2778,7 +2778,7 @@ static void decode_umc_error(int node_id, struct mce *m)
 	err.csrow = m->synd & 0x7;
 
 	if (hygon_f18h_m4h() && boot_cpu_data.x86_model == 0x6)
-		umc = err.channel << 1;
+		umc = (err.channel << 1) + ((m->ipid & BIT(13)) >> 13);
 	else
 		umc = err.channel;
 