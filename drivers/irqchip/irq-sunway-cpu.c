8f17bc9213a5669df43dce62d6d036f333996eab
Wu Liliu <wuliliu@wxiat.com>
2025-03-05 10:08:07 +0800

    sw64: fix nmi_[enter/exit] error in CORE3B
    Sunway inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IC94VY
    
    --------------------------------
    
    In commit 3daffda21330 ("sw64: add NMI support"), since CORE3B does not
    have regs->cause, if some random case happens to take the value of
    -2, then it enters the wrong judgment.
    
    Fixes: 3daffda21330 ("sw64: add NMI support")
    Signed-off-by: Wu Liliu <wuliliu@wxiat.com>
    Reviewed-by: He Sheng <hesheng@wxiat.com>
    Signed-off-by: Gu Zitao <guzitao@wxiat.com>

--- drivers/irqchip/irq-sunway-cpu.c
+++ drivers/irqchip/irq-sunway-cpu.c
@@ -87,10 +87,10 @@ asmlinkage void noinstr do_entInt(struct pt_regs *regs)
 	/* restart idle routine if it is interrupted */
 	if (regs->pc > (u64)__idle_start && regs->pc < (u64)__idle_end)
 		regs->pc = (u64)__idle_start;
-	if (regs->cause != -2)
-		irq_enter();
-	else
+	if (unlikely(regs->cause == -2 && IS_ENABLED(CONFIG_SUBARCH_C4)))
 		nmi_enter();
+	else
+		irq_enter();
 	old_regs = set_irq_regs(regs);
 
 #ifdef CONFIG_PM
@@ -176,10 +176,10 @@ asmlinkage void noinstr do_entInt(struct pt_regs *regs)
 
 out:
 	set_irq_regs(old_regs);
-	if (regs->cause != -2)
-		irq_exit();
-	else
+	if (unlikely(regs->cause == -2 && IS_ENABLED(CONFIG_SUBARCH_C4)))
 		nmi_exit();
+	else
+		irq_exit();
 }
 EXPORT_SYMBOL(do_entInt);
 
