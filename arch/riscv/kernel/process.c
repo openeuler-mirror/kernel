cd6e76224b482b7621a832da8f07bf15527bb01b
Charlie Jenkins <charlie@rivosinc.com>
2025-03-20 10:29:22 -0700

    riscv: entry: Split ret_from_fork() into user and kernel
    mainline inclusion
    from mainline-6.16
    commit 5b3d6103b343d59e19bd641e4c31df519f4d250d
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/9771
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=5b3d6103b343d59e19bd641e4c31df519f4d250d
    
    ----------------------------------------------------------------------
    
    This function was unified into a single function in commit ab9164dae273
    ("riscv: entry: Consolidate ret_from_kernel_thread into ret_from_fork").
    However that imposed a performance degradation.
    
    Partially reverting this commit to have ret_from_fork() split again,
    results in a 1% increase on the number of times fork is able to be called
    per second.
    
    Signed-off-by: Charlie Jenkins <charlie@rivosinc.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Alexandre Ghiti <alexghiti@rivosinc.com>
    Link: https://lore.kernel.org/all/20250320-riscv_optimize_entry-v6-2-63e187e26041@rivosinc.com
    Conflicts:
    	arch/riscv/kernel/entry.S
    	arch/riscv/kernel/process.c
    [Adapt to this tree's older RISC-V entry and copy_thread() context.]
    Signed-off-by: Dong Chenchen <dongchenchen2@huawei.com>

--- arch/riscv/kernel/process.c
+++ arch/riscv/kernel/process.c
@@ -34,7 +34,8 @@ unsigned long __stack_chk_guard __read_mostly;
 EXPORT_SYMBOL(__stack_chk_guard);
 #endif
 
-extern asmlinkage void ret_from_fork_asm(void);
+extern asmlinkage void ret_from_fork_kernel_asm(void);
+extern asmlinkage void ret_from_fork_user_asm(void);
 
 void arch_cpu_idle(void)
 {
@@ -173,12 +174,16 @@ int arch_dup_task_struct(struct task_struct *dst, struct task_struct *src)
 	return 0;
 }
 
-asmlinkage void ret_from_fork(void *fn_arg, int (*fn)(void *),
-			      struct pt_regs *regs)
+asmlinkage void ret_from_fork_kernel(void *fn_arg, int (*fn)(void *),
+				     struct pt_regs *regs)
 {
-	if (unlikely(fn))
-		fn(fn_arg);
+	fn(fn_arg);
 
+	syscall_exit_to_user_mode(regs);
+}
+
+asmlinkage void ret_from_fork_user(struct pt_regs *regs)
+{
 	syscall_exit_to_user_mode(regs);
 }
 
@@ -200,6 +205,7 @@ int copy_thread(struct task_struct *p, const struct kernel_clone_args *args)
 
 		p->thread.s[0] = (unsigned long)args->fn;
 		p->thread.s[1] = (unsigned long)args->fn_arg;
+		p->thread.ra = (unsigned long)ret_from_fork_kernel_asm;
 	} else {
 		*childregs = *(current_pt_regs());
 		/* Turn off status.VS */
@@ -209,9 +215,8 @@ int copy_thread(struct task_struct *p, const struct kernel_clone_args *args)
 		if (clone_flags & CLONE_SETTLS)
 			childregs->tp = tls;
 		childregs->a0 = 0; /* Return value of fork() */
-		p->thread.s[0] = 0;
+		p->thread.ra = (unsigned long)ret_from_fork_user_asm;
 	}
-	p->thread.ra = (unsigned long)ret_from_fork_asm;
 	p->thread.sp = (unsigned long)childregs; /* kernel sp */
 	return 0;
 }
