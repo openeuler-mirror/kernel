d81a0df54d6586e1069f56e5d86ef8594e854f00
Josh Poimboeuf <jpoimboe@kernel.org>
2025-04-08 14:47:32 -0700

    x86/bugs: Fix RSB clearing in indirect_branch_prediction_barrier()
    mainline inclusion
    from mainline-v6.15-rc2
    commit b1b19cfcf4656c75088dc06b7499f493e0dec3e5
    bugzilla: https://gitee.com/openeuler/kernel/issues/ICOVRS
    
    ----------------------------------------
    
    commit b1b19cfcf4656c75088dc06b7499f493e0dec3e5 upstream
    
    IBPB is expected to clear the RSB.  However, if X86_BUG_IBPB_NO_RET is
    set, that doesn't happen.  Make indirect_branch_prediction_barrier()
    take that into account by calling write_ibpb() which clears RSB on
    X86_BUG_IBPB_NO_RET:
    
    	/* Make sure IBPB clears return stack preductions too. */
    	FILL_RETURN_BUFFER %rax, RSB_CLEAR_LOOPS, X86_BUG_IBPB_NO_RET
    
    Note that, as of the previous patch, write_ibpb() also reads
    'x86_pred_cmd' in order to use SBPB when applicable:
    
    	movl	_ASM_RIP(x86_pred_cmd), %eax
    
    Therefore that existing behavior in indirect_branch_prediction_barrier()
    is not lost.
    
    [Backport Changes]
    
    1. In file arch/x86/include/asm/nospec-branch.h, within the function
    indirect_branch_prediction_barrier(), in a call to function ALTERNATIVE
    the exiting parameter "call entry_ibpb" was used instead of parameter
    "call write_ibpb", since global renaming of the function entry_ibpb() to
    write_ibpb() is not applied in the current source tree.
    
    Fixes: 50e4b3b94090 ("x86/entry: Have entry_ibpb() invalidate return predictions")
    Signed-off-by: Josh Poimboeuf <jpoimboe@kernel.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Nikolay Borisov <nik.borisov@suse.com>
    Link: https://lore.kernel.org/r/bba68888c511743d4cd65564d1fc41438907523f.1744148254.git.jpoimboe@kernel.org
    Signed-off-by: malathi <amalathi@amd.com>
    Signed-off-by: mohanasv <mohanasv@amd.com>

--- arch/x86/kernel/cpu/bugs.c
+++ arch/x86/kernel/cpu/bugs.c
@@ -57,7 +57,6 @@ DEFINE_PER_CPU(u64, x86_spec_ctrl_current);
 EXPORT_SYMBOL_GPL(x86_spec_ctrl_current);
 
 u64 x86_pred_cmd __ro_after_init = PRED_CMD_IBPB;
-EXPORT_SYMBOL_GPL(x86_pred_cmd);
 
 static DEFINE_MUTEX(spec_ctrl_mutex);
 
