867dba920be3d6f1781bbf72506cf527918a80f6
Chang S. Bae <chang.seok.bae@intel.com>
2025-04-15 19:16:51 -0700

    x86/cpufeatures: Add X86_FEATURE_APX
    mainline inclusion
    from mainline-v6.16-rc1
    commit b02dc185ee86836cf1d8a37b81349374e4018ee0
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8507
    CVE: N/A
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=b02dc185ee86836cf1d8a37b81349374e4018ee0
    
    (cherry-pick from OLK-6.6 commit b0c9b6439ef8)
    
    -------------------------------------
    
    Intel-SIG: commit b02dc185ee86 ("x86/cpufeatures: Add X86_FEATURE_APX")
    
    -------------------------------------
    
    x86/cpufeatures: Add X86_FEATURE_APX
    
    Intel Advanced Performance Extensions (APX) introduce a new set of
    general-purpose registers, managed as an extended state component via the
    xstate management facility.
    
    Before enabling this new xstate, define a feature flag to clarify the
    dependency in xsave_cpuid_features[]. APX is enumerated under CPUID level
    7 with EDX=1. Since this CPUID leaf is not yet allocated, place the flag
    in a scattered feature word.
    
    While this feature is intended only for userspace, exposing it via
    /proc/cpuinfo is unnecessary. Instead, the existing arch_prctl(2)
    mechanism with the ARCH_GET_XCOMP_SUPP option can be used to query the
    feature availability.
    
    Finally, clarify that APX depends on XSAVE.
    
    Signed-off-by: Chang S. Bae <chang.seok.bae@intel.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Sohil Mehta <sohil.mehta@intel.com>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: H. Peter Anvin <hpa@zytor.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Link: https://lore.kernel.org/r/20250416021720.12305-2-chang.seok.bae@intel.com
    Signed-off-by: Quanxian Wang <quanxian.wang@intel.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -509,6 +509,7 @@
 #define X86_FEATURE_TSA_SQ_NO		(21*32+11) /* AMD CPU not vulnerable to TSA-SQ */
 #define X86_FEATURE_TSA_L1_NO		(21*32+12) /* AMD CPU not vulnerable to TSA-L1 */
 #define X86_FEATURE_CLEAR_CPU_BUF_VM	(21*32+13) /* Clear CPU buffers using VERW before VMRUN */
+#define X86_FEATURE_APX			(21*32+ 9) /* Advanced Performance Extensions */
 
 /*
  * BUG word(s)
