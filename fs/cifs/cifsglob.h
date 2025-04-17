1446df8664d1547441ef826ad905c3528a92a2a6
Wang Zhaolong <wangzhaolong1@huawei.com>
2025-04-17 17:00:54 +0800

    smb:client: smb: client: Add reverse mapping from tcon to superblocks
    mainline inclusion
    from mainline-v6.15-rc1
    commit a091d9711bdee46a76fa14fad31cb261a6dad74a
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/IC1OPD
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=a091d9711bdee46a76fa14fad31cb261a6dad74a
    
    --------------------------------
    
    Currently, when a SMB connection is reset and renegotiated with the
    server, there's no way to update all related mount points with new
    negotiated sizes. This is because while superblocks (cifs_sb_info)
    maintain references to tree connections (tcon) through tcon_link
    structures, there is no reverse mapping from a tcon back to all the
    superblocks using it.
    
    This patch adds a bidirectional relationship between tcon and
    cifs_sb_info structures by:
    
    1. Adding a cifs_sb_list to tcon structure with appropriate locking
    2. Adding tcon_sb_link to cifs_sb_info to join the list
    3. Managing the list entries during mount and umount operations
    
    The bidirectional relationship enables future functionality to locate and
    update all superblocks connected to a specific tree connection, such as:
    
    - Updating negotiated parameters after reconnection
    - Efficiently notifying all affected mounts of capability changes
    
    This is the first part of a series to improve connection resilience
    by keeping all mount parameters in sync with server capabilities
    after reconnection.
    
    Signed-off-by: Wang Zhaolong <wangzhaolong1@huawei.com>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Conflicts:
    	fs/cifs/cifs_fs_sb.h
    	fs/cifs/cifsglob.h
    	fs/cifs/connect.c
    	fs/cifs/misc.c
    	fs/smb/client/cifs_fs_sb.h
    	fs/smb/client/cifsglob.h
    	fs/smb/client/connect.c
    	fs/smb/client/misc.c
    [The mainline code has already undergone multiple refactoring sessions.]
    Signed-off-by: Wang Zhaolong <wangzhaolong1@huawei.com>

--- fs/cifs/cifsglob.h
+++ fs/cifs/cifsglob.h
@@ -1174,7 +1174,8 @@ struct cifs_tcon {
 #endif
 	struct list_head pending_opens;	/* list of incomplete opens */
 	struct cached_fid crfid; /* Cached root fid */
-	/* BB add field for back pointer to sb struct(s)? */
+	struct list_head cifs_sb_list;
+	spinlock_t sb_list_lock;
 #ifdef CONFIG_CIFS_DFS_UPCALL
 	char *dfs_path;
 	int remap:2;
