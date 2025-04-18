c6327d1130ba43d200a7db08544d84053431066f
Zheng Qixing <zhengqixing@huawei.com>
2025-04-18 13:15:43 +0800

    block: fix resource leak in blk_register_queue() error path
    hulk inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/IBWSWQ
    
    --------------------------------
    
    When registering a queue fails after __blk_mq_register_dev() is
    successful but the function later encounters an error, we need
    to clean up the resources.
    
    Add the missing blk_mq_unregister_dev() call in the error path
    to properly clean up these resources and prevent a memory leak.
    
    Fixes: 320ae51feed5 ("blk-mq: new multi-queue block IO queueing mechanism")
    Signed-off-by: Zheng Qixing <zhengqixing@huawei.com>

--- block/blk-sysfs.c
+++ block/blk-sysfs.c
@@ -989,6 +989,8 @@ int blk_register_queue(struct gendisk *disk)
 		ret = elv_register_queue(q, false);
 		if (ret) {
 			mutex_unlock(&q->sysfs_lock);
+			if (q->mq_ops)
+				blk_mq_unregister_dev(dev, q);
 			mutex_unlock(&queue_to_wrapper(q)->sysfs_dir_lock);
 			kobject_del(&q->kobj);
 			blk_trace_remove_sysfs(dev);
