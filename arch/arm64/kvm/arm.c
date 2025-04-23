b3e814f573d9c9dd841084be0db120422ec37e37
Xiang Chen <chenxiang66@hisilicon.com>
2025-04-23 17:16:27 +0800

    kvm: arm64: Add interface KVM_CAP_ARM_IPIV_MODE
    virt inclusion
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8370
    
    ------------------------------------------------------------------------
    
    Before IPIV feature, it gets mpidr from vcpu id, but after the feature,
    we need to know if IPIV mode is enabled. And new IPIV modes may be added
    later.
    
    Signed-off-by: Xiang Chen <chenxiang66@hisilicon.com>
    Signed-off-by: Jinqian Yang <yangjinqian1@huawei.com>

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -299,6 +299,8 @@ void kvm_arch_destroy_vm(struct kvm *kvm)
 #endif
 }
 
+extern struct static_key_false ipiv_enable;
+
 int kvm_vm_ioctl_check_extension(struct kvm *kvm, long ext)
 {
 	int r;
@@ -371,6 +373,12 @@ int kvm_vm_ioctl_check_extension(struct kvm *kvm, long ext)
 		r = static_key_enabled(&kvm_cvm_is_available);
 		break;
 #endif
+	case KVM_CAP_ARM_IPIV_MODE:
+		if (static_branch_unlikely(&ipiv_enable))
+			r = 1;
+		else
+			r = 0;
+		break;
 	default:
 		r = kvm_arch_vm_ioctl_check_extension(kvm, ext);
 		break;
