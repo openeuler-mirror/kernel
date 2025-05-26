40b8b84157cf389dde5b7ad14429101fe3cd8e41
Michal Koutný <mkoutny@suse.com>
2025-05-26 14:38:04 +0800

    io_uring/sqpoll: Do not set PF_NO_SETAFFINITY on sqpoll threads
    mainline inclusion
    from mainline-v6.3-rc3
    commit a5fc1441af7719e93dc7a638a960befb694ade89
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/IC6ES1
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=a5fc1441af7719e93dc7a638a960befb694ade89
    
    --------------------------------
    
    Users may specify a CPU where the sqpoll thread would run. This may
    conflict with cpuset operations because of strict PF_NO_SETAFFINITY
    requirement. That flag is unnecessary for polling "kernel" threads, see
    the reasoning in commit 01e68ce08a30 ("io_uring/io-wq: stop setting
    PF_NO_SETAFFINITY on io-wq workers"). Drop the flag on poll threads too.
    
    Fixes: 01e68ce08a30 ("io_uring/io-wq: stop setting PF_NO_SETAFFINITY on io-wq workers")
    Link: https://lore.kernel.org/all/20230314162559.pnyxdllzgw7jozgx@blackpad/
    Signed-off-by: Michal Koutný <mkoutny@suse.com>
    Link: https://lore.kernel.org/r/20230314183332.25834-1-mkoutny@suse.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Conflicts:
      io_uring/sqpoll.c
    [Commit 17437f311490 ("io_uring: move SQPOLL related handling into its own
    file") move io_sq_thread from io_uring.c to sqpoll.c]
    Signed-off-by: Li Lingfeng <lilingfeng3@huawei.com>

--- io_uring/io_uring.c
+++ io_uring/io_uring.c
@@ -7542,7 +7542,6 @@ static int io_sq_thread(void *data)
 		set_cpus_allowed_ptr(current, cpumask_of(sqd->sq_cpu));
 	else
 		set_cpus_allowed_ptr(current, cpu_online_mask);
-	current->flags |= PF_NO_SETAFFINITY;
 
 	mutex_lock(&sqd->lock);
 	while (1) {
