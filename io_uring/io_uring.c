8c9aade9312a5b1ccd13f05025a2489e41269ffa
Al Viro <viro@zeniv.linux.org.uk>
2025-05-26 14:38:06 +0800

    io_uring: kiocb_done() should *not* trust ->ki_pos if ->{read,write}_iter() failed
    mainline inclusion
    from mainline-v6.7-rc1
    commit 1939316bf988f3e49a07d9c4dd6f660bf4daa53d
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/IC6ES1
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=1939316bf988f3e49a07d9c4dd6f660bf4daa53d
    
    --------------------------------
    
    ->ki_pos value is unreliable in such cases.  For an obvious example,
    consider O_DSYNC write - we feed the data to page cache and start IO,
    then we make sure it's completed.  Update of ->ki_pos is dealt with
    by the first part; failure in the second ends up with negative value
    returned _and_ ->ki_pos left advanced as if sync had been successful.
    In the same situation write(2) does not advance the file position
    at all.
    
    Reviewed-by: Christian Brauner <brauner@kernel.org>
    Reviewed-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Conflicts:
      io_uring/rw.c
    [Commit f3b44f92e59a ("io_uring: move read/write related opcodes to its
    own file") move kiocb_done from io_uring.c to rw.c]
    Signed-off-by: Li Lingfeng <lilingfeng3@huawei.com>

--- io_uring/io_uring.c
+++ io_uring/io_uring.c
@@ -3073,7 +3073,7 @@ static void kiocb_done(struct kiocb *kiocb, ssize_t ret,
 {
 	struct io_kiocb *req = container_of(kiocb, struct io_kiocb, rw.kiocb);
 
-	if (req->flags & REQ_F_CUR_POS)
+	if (ret >= 0 && req->flags & REQ_F_CUR_POS)
 		req->file->f_pos = kiocb->ki_pos;
 	if (ret >= 0 && (kiocb->ki_complete == io_complete_rw)) {
 		if (!__io_complete_rw_common(req, ret)) {
