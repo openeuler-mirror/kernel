85abf88bed4ffc50faf2885c01c00b9622b4f0b3
Gao Chen <gaochen@wxiat.com>
2025-05-28 22:30:40 +0800

    sw64: optimize unaligned access
    Sunway inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/ICC5ID
    
    --------------------------------
    
    Since junzhang_v2 supports hardware unalinged access, this patch selects
    HAVE_EFFICIENT_UNALIGNED_ACCESS and enables hw_una_enabled, which can
    improve the efficiency of unaligned access.
    
    Signed-off-by: Gao Chen <gaochen@wxiat.com>
    Reviewed-by: He Sheng <hesheng@wxiat.com>
    Signed-off-by: Gu Zitao <guzitao@wxiat.com>

--- arch/sw_64/kernel/setup.c
+++ arch/sw_64/kernel/setup.c
@@ -588,6 +588,12 @@ cmd_handle:
 	}
 }
 
+static void __init setup_cpu_caps(void)
+{
+	if (!IS_ENABLED(CONFIG_SUBARCH_C3B) && !is_junzhang_v1())
+		static_branch_enable(&hw_una_enabled);
+}
+
 static void __init setup_legacy_io(void)
 {
 	if (is_guest_or_emul()) {
@@ -690,6 +696,9 @@ setup_arch(char **cmdline_p)
 
 	/* Early initialization for device tree */
 	setup_firmware_fdt();
+
+	/* Now we know who we are, setup caps */
+	setup_cpu_caps();
 
 	/* Now we get the final boot_command_line */
 	*cmdline_p = boot_command_line;
