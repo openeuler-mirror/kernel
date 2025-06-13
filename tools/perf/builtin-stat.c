488a9f96f2173155fb94fa43dba03fb2d017763e
Song Liu <songliubraving@fb.com>
2025-06-13 09:42:41 +0000

    perf stat: Measure 't0' and 'ref_time' after enable_counters()
    mainline inclusion
    from mainline-v5.13-rc1
    commit 435b46ef1d9fd904089199da16a21ade0701537f
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/ICDP8O
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=435b46ef1d9fd904089199da16a21ade0701537f
    
    --------------------------------
    
    Take measurements of 't0' and 'ref_time' after enable_counters(), so
    that they only measure the time consumed when the counters are enabled.
    
    Signed-off-by: Song Liu <songliubraving@fb.com>
    Acked-by: Andi Kleen <andi@firstfloor.org>
    Acked-by: Jiri Olsa <jolsa@kernel.org>
    Acked-by: Namhyung Kim <namhyung@kernel.org>
    Cc: kernel-team@fb.com
    Link: http://lore.kernel.org/lkml/20210316211837.910506-3-songliubraving@fb.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Signed-off-by: Xiaomeng Zhang <zhangxiaomeng13@huawei.com>

--- tools/perf/builtin-stat.c
+++ tools/perf/builtin-stat.c
@@ -920,14 +920,14 @@ try_again_reset:
 	/*
 	 * Enable counters and exec the command:
 	 */
-	t0 = rdclock();
-	clock_gettime(CLOCK_MONOTONIC, &ref_time);
-
 	if (forks) {
 		perf_evlist__start_workload(evsel_list);
 		err = enable_counters();
 		if (err)
 			return -1;
+
+		t0 = rdclock();
+		clock_gettime(CLOCK_MONOTONIC, &ref_time);
 
 		if (interval || timeout || evlist__ctlfd_initialized(evsel_list))
 			status = dispatch_events(forks, timeout, interval, &times);
@@ -949,6 +949,10 @@ try_again_reset:
 		err = enable_counters();
 		if (err)
 			return -1;
+
+		t0 = rdclock();
+		clock_gettime(CLOCK_MONOTONIC, &ref_time);
+
 		status = dispatch_events(forks, timeout, interval, &times);
 	}
 
