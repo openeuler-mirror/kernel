5b09ad67a1f2bca99567a7b3dcb71ab822b2990c
Jinqian Yang <yangjinqian1@huawei.com>
2025-06-17 19:31:02 +0800

    KVM: arm64: check if IPIV is enabled in BIOS
    virt inclusion
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8370
    
    ------------------------------------------------------------------------
    
    GICD_MISC_CTRL bit19(cfg_ipiv_en) is read-only in EL2.
    The write of GICD_MISC_CTRL bit19=1 is done by BIOS.
    Therefore, need to check whether the BIOS has enabled
    ipiv in OS.
    
    Fixes: 5634a3dfafb7 ("kvm: hisi_virt: Probe and configure IPIV capacity on HIP12")
    Signed-off-by: Jinqian Yang <yangjinqian1@huawei.com>

--- arch/arm64/kvm/hisilicon/hisi_virt.h
+++ arch/arm64/kvm/hisilicon/hisi_virt.h
@@ -109,5 +109,6 @@ void kvm_hisi_reload_lsudvmbm(struct kvm *kvm);
 
 #ifdef CONFIG_ARM64_HISI_IPIV
 extern bool gic_dist_enable_ipiv(void);
+extern bool gic_get_ipiv_status(void);
 #endif /* CONFIG_ARM64_HISI_IPIV */
 #endif /* __HISI_VIRT_H__ */
