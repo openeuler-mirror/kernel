6cfc5e86a785015d85f72aee77e783808d3cb5af
Pu Lehui <pulehui@huawei.com>
2025-07-02 02:03:17 +0000

    bpf: Add XDP_HISOCK_REDIRECT action
    hulk inclusion
    category: featrue
    bugzilla: https://gitee.com/openeuler/kernel/issues/ICJ716
    
    --------------------------------
    
    Add XDP_HISOCK_REDIRECT xdp action to bypass net filter rules
    for specific connections. XDP_HISOCK_REDIRECT action only
    support for generic XDP.
    
    Signed-off-by: Pu Lehui <pulehui@huawei.com>

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -4485,6 +4485,7 @@ enum xdp_action {
 	XDP_PASS,
 	XDP_TX,
 	XDP_REDIRECT,
+	XDP_HISOCK_REDIRECT = 100,
 };
 
 /* user accessible metadata for XDP packet hook
--- tools/include/uapi/linux/bpf.h
+++ tools/include/uapi/linux/bpf.h
@@ -4489,6 +4489,7 @@ enum xdp_action {
 	XDP_PASS,
 	XDP_TX,
 	XDP_REDIRECT,
+	XDP_HISOCK_REDIRECT = 100,
 };
 
 /* user accessible metadata for XDP packet hook
