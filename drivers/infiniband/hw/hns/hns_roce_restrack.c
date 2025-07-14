e53fe96b19f25e3bf668d49579101e1e9d453780
wenglianfa <wenglianfa@huawei.com>
2025-07-14 11:15:37 +0800

    RDMA/hns: Support querying DIP SCC context
    driver inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/ICOQ9B
    
    ----------------------------------------------------------------------
    
    In scenarios where the DIP flow control algorithm is used,
    all QPs establishing connections with the same destination IP
    share the same SCC, which is indexed by dip_idx. Therefore,
    dip_idx should be used instead of qpn to query the sccc.
    
    Signed-off-by: wenglianfa <wenglianfa@huawei.com>
    Signed-off-by: Donghua Huang <huangdonghua3@h-partners.com>

--- drivers/infiniband/hw/hns/hns_roce_restrack.c
+++ drivers/infiniband/hw/hns/hns_roce_restrack.c
@@ -102,6 +102,7 @@ int hns_roce_fill_res_qp_entry_raw(struct sk_buff *msg, struct ib_qp *ib_qp)
 		struct hns_roce_v2_qp_context qpc;
 		struct hns_roce_v2_scc_context sccc;
 	} context = {};
+	u32 sccn = hr_qp->qpn;
 	int ret;
 
 	if (!hr_dev->hw->query_qpc)
@@ -115,11 +116,17 @@ int hns_roce_fill_res_qp_entry_raw(struct sk_buff *msg, struct ib_qp *ib_qp)
 	    !hr_dev->hw->query_sccc)
 		goto out;
 
+	if (hr_qp->congest_type == HNS_ROCE_CONGEST_TYPE_DIP) {
+		if (!hr_qp->dip)
+			goto out;
+		sccn = hr_qp->dip->dip_idx;
+	}
+
 	/* The scc may not exist. If the scc query fails,
 	 * only the qpc content is displayed, and the
 	 * scc value is all 0.
 	 */
-	ret = hr_dev->hw->query_sccc(hr_dev, hr_qp->qpn, &context.sccc);
+	ret = hr_dev->hw->query_sccc(hr_dev, sccn, &context.sccc);
 	if (ret)
 		ibdev_warn_ratelimited(&hr_dev->ib_dev,
 				       "failed to query SCCC, ret = %d.\n",
