intel-lab-lkp/linux

x86/bugs: Move the X86_FEATURE_USE_IBPB check into callers
indirect_branch_prediction_barrier() only performs the MSR write if
X86_FEATURE_USE_IBPB is set, using alternative_msr_write(). In
preparation for removing X86_FEATURE_USE_IBPB, move the feature check
into the callers so that they can be addressed one-by-one, and use
X86_FEATURE_IBPB instead to guard the MSR write.

Signed-off-by: Yosry Ahmed <yosry.ahmed@linux.dev>
Acked-by: Josh Poimboeuf <jpoimboe@kernel.org>

arch/x86/kvm/vmx/nested.c
@@ -5026,7 +5026,8 @@ void nested_vmx_vmexit(struct kvm_vcpu *vcpu, u32 vm_exit_reason,
 	 * doesn't isolate different VMCSs, i.e. in this case, doesn't provide
 	 * separate modes for L2 vs L1.
 	 */
-	if (guest_cpu_cap_has(vcpu, X86_FEATURE_SPEC_CTRL))
+	if (guest_cpu_cap_has(vcpu, X86_FEATURE_SPEC_CTRL) &&
+	    cpu_feature_enabled(X86_FEATURE_USE_IBPB))
 		indirect_branch_prediction_barrier();
 
 	/* Update any VMCS fields that might have changed while L2 ran */

openeuler-mirror/kernel

mainline inclusion
from mainline-v6.15-rc1
commit 549435aab49ae83d60a08795de6cf0e866f3b2ec
bugzilla: https://gitee.com/openeuler/kernel/issues/ICOVRS
----------------------------------------
commit 549435aab49ae83d60a08795de6cf0e866f3b2ec upstream
[Backport Changes]
1. In file arch/x86/kvm/vmx/nested.c, within the function
nested_vmx_vmexit(), in the if block the exiting function call
guest_cpuid_has() was retained, instead of  guest_cpu_cap_has(),
since global renaming of the function guest_cpuid_has() to
guest_cpu_cap_has() is not applied in the current source tree.
Signed-off-by: Ingo Molnar <mingo@kernel.org>
Acked-by: Sean Christopherson <seanjc@google.com>
Link: https://lore.kernel.org/r/20250227012712.3193063-2-yosry.ahmed@linux.dev
Reviewed-by: Jason Zeng <jason.zeng@intel.com>
Signed-off-by: malathi <amalathi@amd.com>
Signed-off-by: mohanasv <mohanasv@amd.com>

arch/x86/kvm/vmx/nested.c
@@ -4522,7 +4522,8 @@ void nested_vmx_vmexit(struct kvm_vcpu *vcpu, u32 vm_exit_reason,
 	 * doesn't isolate different VMCSs, i.e. in this case, doesn't provide
 	 * separate modes for L2 vs L1.
 	 */
-	if (guest_cpuid_has(vcpu, X86_FEATURE_SPEC_CTRL))
+	if (guest_cpuid_has(vcpu, X86_FEATURE_SPEC_CTRL) &&
+	    cpu_feature_enabled(X86_FEATURE_USE_IBPB))
 		indirect_branch_prediction_barrier();
 
 	/* Update any VMCS fields that might have changed while L2 ran */