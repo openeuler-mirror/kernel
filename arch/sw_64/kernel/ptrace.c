500d1114f90ca983aba48563dbd1c20406c5bbed
Gu Yuchen <guyuchen@wxiat.com>
2025-08-04 14:00:42 +0800

    sw64: ptrace: return -EIO in invalid PEEKUSR/POKEUSR requests
    Sunway inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/ICR45H
    
    --------------------------------
    
    When PTRACE_PEEKUSR or PTRACE_POKEUSR operations are called with an
    out-of-bounds or unaligned address, the system should return -EIO
    instead of 0. This commit fixes the issue.
    
    Signed-off-by: Gu Yuchen <guyuchen@wxiat.com>
    Reviewed-by: He Sheng <hesheng@wxiat.com>
    Signed-off-by: Gu Zitao <guzitao@wxiat.com>

--- arch/sw_64/kernel/ptrace.c
+++ arch/sw_64/kernel/ptrace.c
@@ -129,7 +129,9 @@ get_reg_addr(struct task_struct *task, unsigned long regno)
 unsigned long
 get_reg(struct task_struct *task, unsigned long regno)
 {
-	return *get_reg_addr(task, regno);
+	unsigned long addr = *get_reg_addr(task, regno);
+
+	return (addr) ? addr : -EIO;
 }
 
 /*
@@ -138,8 +140,9 @@ get_reg(struct task_struct *task, unsigned long regno)
 static int
 put_reg(struct task_struct *task, unsigned long regno, unsigned long data)
 {
-	*get_reg_addr(task, regno) = data;
-	return 0;
+	unsigned long addr = *get_reg_addr(task, regno);
+
+	return (addr) ? 0 : -EIO;
 }
 
 static inline int
