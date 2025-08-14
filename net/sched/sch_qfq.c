ba3ef7b1ec7d0015776ed98f863d6ff2b638f259
Xiang Mei <xmei5@asu.edu>
2025-08-14 21:07:22 +0800

    net/sched: sch_qfq: Avoid triggering might_sleep in atomic context in qfq_delete_class
    mainline inclusion
    from mainline-v6.16-rc6
    commit cf074eca0065bc5142e6004ae236bb35a2687fdf
    category: bugfix
    bugzilla: https://gitee.com/src-openeuler/kernel/issues/ICPD9I
    CVE: CVE-2025-38477
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=cf074eca0065bc5142e6004ae236bb35a2687fdf
    
    --------------------------------
    
    might_sleep could be trigger in the atomic context in qfq_delete_class.
    
    qfq_destroy_class was moved into atomic context locked
    by sch_tree_lock to avoid a race condition bug on
    qfq_aggregate. However, might_sleep could be triggered by
    qfq_destroy_class, which introduced sleeping in atomic context (path:
    qfq_destroy_class->qdisc_put->__qdisc_destroy->lockdep_unregister_key
    ->might_sleep).
    
    Considering the race is on the qfq_aggregate objects, keeping
    qfq_rm_from_agg in the lock but moving the left part out can solve
    this issue.
    
    Fixes: 5e28d5a3f774 ("net/sched: sch_qfq: Fix race condition on qfq_aggregate")
    Reported-by: Dan Carpenter <dan.carpenter@linaro.org>
    Signed-off-by: Xiang Mei <xmei5@asu.edu>
    Link: https://patch.msgid.link/4a04e0cc-a64b-44e7-9213-2880ed641d77@sabinyo.mountain
    Reviewed-by: Cong Wang <xiyou.wangcong@gmail.com>
    Reviewed-by: Dan Carpenter <dan.carpenter@linaro.org>
    Link: https://patch.msgid.link/20250717230128.159766-1-xmei5@asu.edu
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Conflicts:
    	net/sched/sch_qfq.c
    [commit 4dd78a73738a and 86bd446b5ceb are not backport]
    Signed-off-by: Dong Chenchen <dongchenchen2@huawei.com>

--- net/sched/sch_qfq.c
+++ net/sched/sch_qfq.c
@@ -543,9 +543,6 @@ destroy_class:
 
 static void qfq_destroy_class(struct Qdisc *sch, struct qfq_class *cl)
 {
-	struct qfq_sched *q = qdisc_priv(sch);
-
-	qfq_rm_from_agg(q, cl);
 	gen_kill_estimator(&cl->rate_est);
 	qdisc_destroy(cl->qdisc);
 	kfree(cl);
@@ -563,10 +560,11 @@ static int qfq_delete_class(struct Qdisc *sch, unsigned long arg)
 
 	qfq_purge_queue(cl);
 	qdisc_class_hash_remove(&q->clhash, &cl->common);
-	qfq_destroy_class(sch, cl);
+	qfq_rm_from_agg(q, cl);
 
 	sch_tree_unlock(sch);
 
+	qfq_destroy_class(sch, cl);
 	return 0;
 }
 
@@ -1521,6 +1519,7 @@ static void qfq_destroy_qdisc(struct Qdisc *sch)
 	for (i = 0; i < q->clhash.hashsize; i++) {
 		hlist_for_each_entry_safe(cl, next, &q->clhash.hash[i],
 					  common.hnode) {
+			qfq_rm_from_agg(q, cl);
 			qfq_destroy_class(sch, cl);
 		}
 	}
