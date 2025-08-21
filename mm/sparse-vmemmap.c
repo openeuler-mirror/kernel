55270f133ac52e8793ffc7d0e17e5e448c91a8e7
Nanyong Sun <sunnanyong@huawei.com>
2025-08-21 16:06:23 +0800

    mm: HVO: fix hard lockup in split_vmemmap_huge_pmd under x86
    hulk inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/ICTXL2
    
    -------------------------------
    
    The commit 94238c5ff554 ("[Huawei] arm64: mm: HVO: make spin_lock irq
    safe") is improper for X86 because it disable irq before TLB flush in
    split_vmemmap_huge_pmd, which need sending IPI under x86. The comments
    of smp_call_function_many_cond() has pointed out that deadlock can
    happen when called with interrupts disabled.
    
    Reverting to spin_lock for archs besides arm64.
    
    Fixes: 4529b88488e4 ("arm64: mm: HVO: support BBM of vmemmap pgtable safely")
    Signed-off-by: Nanyong Sun <sunnanyong@huawei.com>

--- mm/sparse-vmemmap.c
+++ mm/sparse-vmemmap.c
@@ -70,6 +70,14 @@ static inline void vmemmap_update_pte(unsigned long addr,
 }
 #endif
 
+#ifndef vmemmap_split_lock
+#define vmemmap_split_lock(lock)	spin_lock(lock)
+#endif
+
+#ifndef vmemmap_split_unlock
+#define vmemmap_split_unlock(lock)	spin_unlock(lock)
+#endif
+
 #ifndef vmemmap_flush_tlb_all
 static inline void vmemmap_flush_tlb_all(void)
 {
@@ -107,7 +115,7 @@ static int __split_vmemmap_huge_pmd(pmd_t *pmd, unsigned long start)
 		set_pte_at(&init_mm, addr, pte, entry);
 	}
 
-	spin_lock_irq(&init_mm.page_table_lock);
+	vmemmap_split_lock(&init_mm.page_table_lock);
 	if (likely(pmd_leaf(*pmd))) {
 		/*
 		 * Higher order allocations from buddy allocator must be able to
@@ -124,7 +132,7 @@ static int __split_vmemmap_huge_pmd(pmd_t *pmd, unsigned long start)
 	} else {
 		pte_free_kernel(&init_mm, pgtable);
 	}
-	spin_unlock_irq(&init_mm.page_table_lock);
+	vmemmap_split_unlock(&init_mm.page_table_lock);
 
 	return 0;
 }
