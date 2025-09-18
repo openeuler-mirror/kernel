a6315f4ff2f5ff219f7b72d6fc51b454af8d11b4
Bibo Mao <maobibo@loongson.cn>
2025-09-18 19:44:22 +0800

    LoongArch: KVM: Fix VM migration failure with PTW enabled
    loongarch inclusion
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/9150
    
    ----------------------------------------
    
    With PTW disabled system, bit _PAGE_DIRTY is a HW bit for page writing.
    However with PTW enabled system, bit _PAGE_WRITE is also a "HW bit" for
    page writing, because hardware synchronizes _PAGE_WRITE to _PAGE_DIRTY
    automatically. Previously, _PAGE_WRITE is treated as a SW bit to record
    the page writeable attribute for the fast page fault handling in the
    secondary MMU, however with PTW enabled machine, this bit is used by HW
    already (so setting it will silence the TLB modify exception).
    
    Here define KVM_PAGE_WRITEABLE with the SW bit _PAGE_MODIFIED, so that
    it can work on both PTW disabled and enabled machines. And for HW write
    bits, both _PAGE_DIRTY and _PAGE_WRITE are set or clear together.
    
    Signed-off-by: Bibo Mao <maobibo@loongson.cn>
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>
    Signed-off-by: Xianglai Li <lixianglai@loongson.cn>

--- arch/loongarch/kvm/mmu.c
+++ arch/loongarch/kvm/mmu.c
@@ -540,7 +540,7 @@ bool kvm_set_spte_gfn(struct kvm *kvm, struct kvm_gfn_range *range)
 	prot_bits |= __WRITEABLE & *ptep & pte_val(range->arg.pte);
 	if (kvm_pte_dirty(prot_bits)) {
 		prot_bits = kvm_pte_mkclean(prot_bits);
-		prot_bits = kvm_pte_mkwrite(prot_bits);
+		prot_bits = kvm_pte_mkwriteable(prot_bits);
 	}
 	kvm_set_pte(ptep, kvm_pfn_pte(pfn, __pgprot(prot_bits)));
 
@@ -609,7 +609,7 @@ static int kvm_map_page_fast(struct kvm_vcpu *vcpu, unsigned long gpa, bool writ
 
 	/* call kvm_set_pfn_accessed() after unlock */
 	if (write && !kvm_pte_dirty(new)) {
-		if (!kvm_pte_write(new)) {
+		if (!kvm_pte_writeable(new)) {
 			ret = -EFAULT;
 			goto out;
 		}
@@ -912,7 +912,7 @@ retry:
 		 * to record it to restore the write attribute of the page entry,
 		 * in the fast path kvm_map_page_fast for page table processing
 		 */
-		prot_bits = kvm_pte_mkwrite(prot_bits);
+		prot_bits = kvm_pte_mkwriteable(prot_bits);
 		if (write || !kvm_slot_dirty_track_enabled(memslot))
 			prot_bits = kvm_pte_mkdirty(prot_bits);
 	}
@@ -958,7 +958,7 @@ retry:
 	kvm_set_pte(ptep, new_pte);
 	spin_unlock(&kvm->mmu_lock);
 
-	if (prot_bits & _PAGE_DIRTY) {
+	if (kvm_pte_dirty(prot_bits)) {
 		mark_page_dirty_in_slot(kvm, memslot, gfn);
 		kvm_set_pfn_dirty(pfn);
 	}
