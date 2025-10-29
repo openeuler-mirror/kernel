675b387450b029c98317b34874265f40aa771160
Zhang Qiao <zhangqiao22@huawei.com>
2025-10-29 16:54:15 +0800

    sched: Inhibit soft domain scheduling when overutil
    hulk inclusion
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/9546
    
    (cherry-pick from OLK-6.6 commit 597b83fcd292)
    
    ------------------
    
    Soft domain scheduling affects task migration. When a CPU is in
    overutilized state(default 85%, tuned via
    /sys/kernel/debug/sched/soft_domain_overutil_pct), tasks may struggle to
    migrate to idle LLCs, leading to scheduling latency degradation. To
    mitigate this, allow tasks to migrate to idle CPUs when the src CPU's
    LLC is overutilized; meanwhile, to prevent tasks from migrating back to
    the prefer LLC during wakeup, temporarily inhibit soft domain
    scheduling.
    
    Signed-off-by: Zhang Qiao <zhangqiao22@huawei.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- kernel/sched/soft_domain.c
+++ kernel/sched/soft_domain.c
@@ -38,7 +38,7 @@ static int __init soft_domain_switch_setup(char *str)
 }
 __setup("sched_soft_domain=", soft_domain_switch_setup);
 
-static bool soft_domain_enabled(void)
+bool soft_domain_enabled(void)
 {
 	return static_branch_likely(&__soft_domain_switch);
 }
