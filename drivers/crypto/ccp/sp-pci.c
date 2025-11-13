e08f3fc2ecb586179916ba8cfa5f695b319c1ead
chench <chench@hygon.cn>
2025-11-13 19:44:29 +0800

    crypto: ccp: add new pci device id for HYGON C86-4G
    hygon inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/ID13Y9
    CVE: NA
    
    ---------------------------
    
    add new psp/ccp device id for hygon C86-4G
    
    Signed-off-by: chench <chench@hygon.cn>

--- drivers/crypto/ccp/sp-pci.c
+++ drivers/crypto/ccp/sp-pci.c
@@ -442,6 +442,8 @@ static const struct pci_device_id sp_pci_table[] = {
 	{ PCI_VDEVICE(HYGON, 0x1486), (kernel_ulong_t)&hygon_dev_vdata[2] },
 	{ PCI_VDEVICE(HYGON, 0x14b8), (kernel_ulong_t)&hygon_dev_vdata[1] },
 	{ PCI_VDEVICE(HYGON, 0x14a6), (kernel_ulong_t)&hygon_dev_vdata[2] },
+	{ PCI_VDEVICE(HYGON, 0x14d8), (kernel_ulong_t)&hygon_dev_vdata[1] },
+	{ PCI_VDEVICE(HYGON, 0x14c6), (kernel_ulong_t)&hygon_dev_vdata[2] },
 	/* Last entry must be zero */
 	{ 0, }
 };
