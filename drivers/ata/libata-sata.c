94e6951c82790a655c7b8f6d469f8d59918ae66b
LeoLiu-oc <leoliu-oc@zhaoxin.com>
2025-11-17 11:38:35 +0800

    Revert "ata: Add support for PxSCT.LPM set based on actual LPM capability"
    zhaoxin inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/ID6ZW2
    CVE: NA
    
    --------------------
    
    This reverts commit 8ed2460e27379629dfc109fb2facbfd02ac50ec3.
    
    The upstream mainline community has incorporated the-version patch
    24e0e61db3cb ("ata: libata: disallow dev-initiated LPM transitions to
    unsupported states").
    
    Therefore, the old version is withdraw here to facilitate subsequent
    updating to the new version in the mainline.
    
    Signed-off-by: leoliu-oc <leoliu-oc@zhaoxin.com>

--- drivers/ata/libata-sata.c
+++ drivers/ata/libata-sata.c
@@ -8,7 +8,6 @@
  */
 
 #include <linux/kernel.h>
-#include <linux/pci.h>
 #include <linux/module.h>
 #include <scsi/scsi_cmnd.h>
 #include <scsi/scsi_device.h>
@@ -369,10 +368,6 @@ int sata_link_scr_lpm(struct ata_link *link, enum ata_lpm_policy policy,
 		      bool spm_wakeup)
 {
 	struct ata_eh_context *ehc = &link->eh_context;
-	struct ata_port *ap = ata_is_host_link(link) ? link->ap : NULL;
-	struct device *dev = ap ? ap->host->dev : NULL;
-	struct pci_dev *pdev = (!dev || !dev_is_pci(dev)) ? NULL : to_pci_dev(dev);
-
 	bool woken_up = false;
 	u32 scontrol;
 	int rc;
@@ -399,21 +394,10 @@ int sata_link_scr_lpm(struct ata_link *link, enum ata_lpm_policy policy,
 	case ATA_LPM_MED_POWER_WITH_DIPM:
 	case ATA_LPM_MIN_POWER_WITH_PARTIAL:
 	case ATA_LPM_MIN_POWER:
-		if (ata_link_nr_enabled(link) > 0) {
+		if (ata_link_nr_enabled(link) > 0)
 			/* no restrictions on LPM transitions */
 			scontrol &= ~(0x7 << 8);
-
-			/* if controller does not support partial, then disallows it,
-			 * the same for slumber
-			 */
-			if (pdev && pdev->vendor == PCI_VENDOR_ID_ZHAOXIN) {
-				if (!(link->ap->host->flags & ATA_HOST_PART))
-					scontrol |= (0x1 << 8);
-
-				if (!(link->ap->host->flags & ATA_HOST_SSC))
-					scontrol |= (0x2 << 8);
-			}
-		} else {
+		else {
 			/* empty port, power off */
 			scontrol &= ~0xf;
 			scontrol |= (0x1 << 2);
