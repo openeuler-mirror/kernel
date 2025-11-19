10b663c676e7cb7c55590b8d7a44c89de0e4a265
Qiuxu Zhuo <qiuxu.zhuo@intel.com>
2025-11-19 12:11:40 -0800

    EDAC/{skx_common,imh}: Add EDAC driver for Intel Diamond Rapids servers
    mainline inclusion
    from mainline-v6.19-rc1
    commit 9fc67b11703fe9d8a5617ccacec2a452e455fa52
    category: feature
    bugzilla: https://atomgit.com/openeuler/intel-kernel/issues/187
    CVE: NA
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=9fc67b11703fe9d8a5617ccacec2a452e455fa52
    
    -------------------------------------------------
    
    Intel-SIG: commit 9fc67b11703f EDAC/{skx_common,imh}: Add EDAC driver for Intel Diamond Rapids servers
    Backport EDAC support for DMR
    
    Intel Diamond Rapids CPUs include Integrated Memory and I/O Hubs (IMH).
    The memory controllers within the IMHs provide memory stacks to the
    processor. Create a new driver for this IMH-based memory controllers
    rather than applying additional patches to the existing i10nm_edac.c
    for the following reasons:
    
    1) The memory controllers are not presented as PCI devices; instead,
       the detection and all their registers have been transitioned to
       MMIO-based memory spaces.
    
    2) Validation processes are costly. Modifications to i10nm_edac would
       require extensive validation checks against multiple platforms,
       including Ice Lake, Sapphire Rapids, Emerald Rapids, Granite Rapids,
       Sierra Forest, and Grand Ridge.
    
    3) Future Intel CPUs will likely only need patches on top of this new
       EDAC driver. Validation can be limited to Diamond Rapids servers
       and future Intel CPU generations.
    
    [Tony: Fix kerneldoc for struct local_reg]
    [randconfig: Added dependencies on NFIT and DMI]
    
    Tested-by: Yi Lai <yi1.lai@intel.com>
    Signed-off-by: Qiuxu Zhuo <qiuxu.zhuo@intel.com>
    Signed-off-by: Tony Luck <tony.luck@intel.com>
    Link: https://patch.msgid.link/20251119134132.2389472-5-qiuxu.zhuo@intel.com
    [ Zhang Rui: amend commit log ]
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>

--- drivers/edac/skx_common.c
+++ drivers/edac/skx_common.c
@@ -122,7 +122,7 @@ void skx_adxl_put(void)
 }
 EXPORT_SYMBOL_GPL(skx_adxl_put);
 
-static void skx_init_mc_mapping(struct skx_dev *d)
+void skx_init_mc_mapping(struct skx_dev *d)
 {
 	/*
 	 * By default, the BIOS presents all memory controllers within each
@@ -133,6 +133,7 @@ static void skx_init_mc_mapping(struct skx_dev *d)
 	for (int i = 0; i < NUM_IMC; i++)
 		d->mc_mapping[i] = i;
 }
+EXPORT_SYMBOL_GPL(skx_init_mc_mapping);
 
 void skx_set_mc_mapping(struct skx_dev *d, u8 pmc, u8 lmc)
 {
@@ -809,6 +810,9 @@ void skx_remove(void)
 			if (d->imc[i].mbase)
 				iounmap(d->imc[i].mbase);
 
+			if (d->imc[i].dev)
+				put_device(d->imc[i].dev);
+
 			for (j = 0; j < d->imc[i].num_channels; j++) {
 				if (d->imc[i].chan[j].cdev)
 					pci_dev_put(d->imc[i].chan[j].cdev);
@@ -832,7 +836,7 @@ EXPORT_SYMBOL_GPL(skx_remove);
 /*
  * Debug feature.
  * Exercise the address decode logic by writing an address to
- * /sys/kernel/debug/edac/{skx,i10nm}_test/addr.
+ * /sys/kernel/debug/edac/{skx,i10nm,imh}_test/addr.
  */
 static struct dentry *skx_test;
 
