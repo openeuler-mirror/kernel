7e023d431b3ddba6406a9f8a47429ba656f8a2d3
Paolo Abeni <pabeni@redhat.com>
2025-11-25 17:59:11 +0100

    mptcp: clear scheduled subflows on retransmit
    stable inclusion
    from stable-v6.6.119
    commit ce318ec9864bc3dbaa37d71ec4813f0358f0d44a
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8839
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=ce318ec9864bc3dbaa37d71ec4813f0358f0d44a
    
    (cherry-pick from OLK-6.6 commit 160f206bede9)
    
    --------------------------------
    
    commit 27fd02860164bfa78cec2640dfad630d832e302c upstream.
    
    When __mptcp_retrans() kicks-in, it schedules one or more subflows for
    retransmission, but such subflows could be actually left alone if there
    is no more data to retransmit and/or in case of concurrent fallback.
    
    Scheduled subflows could be processed much later in time, i.e. when new
    data will be transmitted, leading to bad subflow selection.
    
    Explicitly clear all scheduled subflows before leaving the
    retransmission function.
    
    Fixes: ee2708aedad0 ("mptcp: use get_retrans wrapper")
    Cc: stable@vger.kernel.org
    Reported-by: Filip Pokryvka <fpokryvk@redhat.com>
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Reviewed-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>
    Link: https://patch.msgid.link/20251125-net-mptcp-clear-sched-rtx-v1-1-1cea4ad2165f@kernel.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit ce318ec9864bc3dbaa37d71ec4813f0358f0d44a)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- net/mptcp/protocol.c
+++ net/mptcp/protocol.c
@@ -2709,7 +2709,7 @@ static void __mptcp_retrans(struct sock *sk)
 		}
 
 		if (!mptcp_send_head(sk))
-			return;
+			goto clear_scheduled;
 
 		goto reset_timer;
 	}
@@ -2743,7 +2743,7 @@ static void __mptcp_retrans(struct sock *sk)
 			    !msk->allow_subflows) {
 				spin_unlock_bh(&msk->fallback_lock);
 				release_sock(ssk);
-				return;
+				goto clear_scheduled;
 			}
 
 			while (info.sent < info.limit) {
@@ -2775,6 +2775,15 @@ reset_timer:
 
 	if (!mptcp_rtx_timer_pending(sk))
 		mptcp_reset_rtx_timer(sk);
+
+clear_scheduled:
+	/* If no rtx data was available or in case of fallback, there
+	 * could be left-over scheduled subflows; clear them all
+	 * or later xmit could use bad ones
+	 */
+	mptcp_for_each_subflow(msk, subflow)
+		if (READ_ONCE(subflow->scheduled))
+			mptcp_subflow_set_scheduled(subflow, false);
 }
 
 /* schedule the timeout timer for the relevant event: either close timeout
