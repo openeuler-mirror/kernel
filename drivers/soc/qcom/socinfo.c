06ba5a231185fdd771cb0a33ef73d51d5a0fc105
Yao Kai <yaokai34@huawei.com>
2025-12-02 09:41:39 +0800

    soc: qcom: socinfo: Avoid out of bounds read of serial number
    mainline inclusion
    from mainline-v6.14-rc1
    commit 22cf4fae6660b6e1a583a41cbf84e3046ca9ccd0
    category: bugfix
    bugzilla: https://gitee.com/src-openeuler/kernel/issues/IBPC4I
    CVE: CVE-2024-58007
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=22cf4fae6660b6e1a583a41cbf84e3046ca9ccd0
    
    --------------------------------
    
    On MSM8916 devices, the serial number exposed in sysfs is constant and does
    not change across individual devices. It's always:
    
      db410c:/sys/devices/soc0$ cat serial_number
      2644893864
    
    The firmware used on MSM8916 exposes SOCINFO_VERSION(0, 8), which does not
    have support for the serial_num field in the socinfo struct. There is an
    existing check to avoid exposing the serial number in that case, but it's
    not correct: When checking the item_size returned by SMEM, we need to make
    sure the *end* of the serial_num is within bounds, instead of comparing
    with the *start* offset. The serial_number currently exposed on MSM8916
    devices is just an out of bounds read of whatever comes after the socinfo
    struct in SMEM.
    
    Fix this by changing offsetof() to offsetofend(), so that the size of the
    field is also taken into account.
    
    Cc: stable@vger.kernel.org
    Fixes: efb448d0a3fc ("soc: qcom: Add socinfo driver")
    Signed-off-by: Stephan Gerhold <stephan.gerhold@linaro.org>
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Link: https://lore.kernel.org/r/20241230-qcom-socinfo-serialno-oob-v1-1-9b7a890da3da@linaro.org
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>
    Conflicts:
    	drivers/soc/qcom/socinfo.c
    [context conflicts]
    Signed-off-by: Yao Kai <yaokai34@huawei.com>

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -507,7 +507,7 @@ static int qcom_socinfo_probe(struct platform_device *pdev)
 	qs->attr.revision = devm_kasprintf(&pdev->dev, GFP_KERNEL, "%u.%u",
 					   SOCINFO_MAJOR(le32_to_cpu(info->ver)),
 					   SOCINFO_MINOR(le32_to_cpu(info->ver)));
-	if (offsetof(struct socinfo, serial_num) <= item_size)
+	if (offsetofend(struct socinfo, serial_num) <= item_size)
 		qs->attr.serial_number = devm_kasprintf(&pdev->dev, GFP_KERNEL,
 							"%u",
 							le32_to_cpu(info->serial_num));
