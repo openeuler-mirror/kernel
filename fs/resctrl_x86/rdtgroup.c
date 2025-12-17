800d4d8c55696ce49a18459bdc0b5896d72fd36c
Tony Luck <tony.luck@intel.com>
2025-12-17 09:20:56 -0800

    x86,fs/resctrl: Rename some L3 specific functions
    mainline inclusion
    from mainline-v7.0-rc1
    commit 9c214d10c50990c7a61b95887493df9ae713eec5
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/8967
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=9c214d10c50990c7a61b95887493df9ae713eec5
    
    (cherry-pick from OLK-6.6 commit 71097d30388f)
    
    --------------------------------
    
    With the arrival of monitor events tied to new domains associated with a
    different resource it would be clearer if the L3 resource specific functions
    are more accurately named.
    
    Rename three groups of functions:
    
    Functions that allocate/free architecture per-RMID MBM state information:
    arch_domain_mbm_alloc()		-> l3_mon_domain_mbm_alloc()
    mon_domain_free()		-> l3_mon_domain_free()
    
    Functions that allocate/free filesystem per-RMID MBM state information:
    domain_setup_mon_state()	-> domain_setup_l3_mon_state()
    domain_destroy_mon_state()	-> domain_destroy_l3_mon_state()
    
    Initialization/exit:
    rdt_get_mon_l3_config()		-> rdt_get_l3_mon_config()
    resctrl_mon_resource_init()	-> resctrl_l3_mon_resource_init()
    resctrl_mon_resource_exit()	-> resctrl_l3_mon_resource_exit()
    
    Ensure kernel-doc descriptions of these functions' return values are present
    and correctly formatted.
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Reviewed-by: Reinette Chatre <reinette.chatre@intel.com>
    Link: https://lore.kernel.org/20251217172121.12030-1-tony.luck@intel.com
    
     Conflicts:
    	fs/resctrl_x86/rdtgroup.c
    [jz: resolve context conflict]
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- fs/resctrl_x86/rdtgroup.c
+++ fs/resctrl_x86/rdtgroup.c
@@ -4181,7 +4181,7 @@ static void rdtgroup_setup_default(void)
 	mutex_unlock(&rdtgroup_mutex);
 }
 
-static void domain_destroy_mon_state(struct rdt_l3_mon_domain *d)
+static void domain_destroy_l3_mon_state(struct rdt_l3_mon_domain *d)
 {
 	int idx;
 
@@ -4236,13 +4236,13 @@ void resctrl_offline_mon_domain(struct rdt_resource *r, struct rdt_domain_hdr *h
 		cancel_delayed_work(&d->cqm_limbo);
 	}
 
-	domain_destroy_mon_state(d);
+	domain_destroy_l3_mon_state(d);
 out_unlock:
 	mutex_unlock(&rdtgroup_mutex);
 }
 
 /**
- * domain_setup_mon_state() -  Initialise domain monitoring structures.
+ * domain_setup_l3_mon_state() -  Initialise domain monitoring structures.
  * @r:	The resource for the newly online domain.
  * @d:	The newly online domain.
  *
@@ -4250,11 +4250,11 @@ out_unlock:
  * Called when the first CPU of a domain comes online, regardless of whether
  * the filesystem is mounted.
  * During boot this may be called before global allocations have been made by
- * resctrl_mon_resource_init().
+ * resctrl_l3_mon_resource_init().
  *
- * Returns 0 for success, or -ENOMEM.
+ * Return: 0 for success, or -ENOMEM.
  */
-static int domain_setup_mon_state(struct rdt_resource *r, struct rdt_l3_mon_domain *d)
+static int domain_setup_l3_mon_state(struct rdt_resource *r, struct rdt_l3_mon_domain *d)
 {
 	u32 idx_limit = resctrl_arch_system_num_rmid_idx();
 	size_t tsize = sizeof(*d->mbm_states[0]);
@@ -4321,7 +4321,7 @@ int resctrl_online_mon_domain(struct rdt_resource *r, struct rdt_domain_hdr *hdr
 		goto out_unlock;
 
 	d = container_of(hdr, struct rdt_l3_mon_domain, hdr);
-	err = domain_setup_mon_state(r, d);
+	err = domain_setup_l3_mon_state(r, d);
 	if (err)
 		goto out_unlock;
 
@@ -4436,13 +4436,13 @@ int resctrl_init(void)
 
 	thread_throttle_mode_init();
 
-	ret = resctrl_mon_resource_init();
+	ret = resctrl_l3_mon_resource_init();
 	if (ret)
 		return ret;
 
 	ret = sysfs_create_mount_point(fs_kobj, "resctrl");
 	if (ret) {
-		resctrl_mon_resource_exit();
+		resctrl_l3_mon_resource_exit();
 		return ret;
 	}
 
@@ -4477,7 +4477,7 @@ int resctrl_init(void)
 
 cleanup_mountpoint:
 	sysfs_remove_mount_point(fs_kobj, "resctrl");
-	resctrl_mon_resource_exit();
+	resctrl_l3_mon_resource_exit();
 
 	return ret;
 }
@@ -4513,7 +4513,7 @@ static bool resctrl_online_domains_exist(void)
  * When called by the architecture code, all CPUs and resctrl domains must be
  * offline. This ensures the limbo and overflow handlers are not scheduled to
  * run, meaning the data structures they access can be freed by
- * resctrl_mon_resource_exit().
+ * resctrl_l3_mon_resource_exit().
  *
  * After resctrl_exit() returns, the architecture code should return an
  * error from all resctrl_arch_ functions that can do this.
@@ -4540,5 +4540,5 @@ void resctrl_exit(void)
 	 * it can be used to umount resctrl.
 	 */
 
-	resctrl_mon_resource_exit();
+	resctrl_l3_mon_resource_exit();
 }
