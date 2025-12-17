2a82834cd5927211a21c805f42838545b963bfa7
Kumar Kartikeya Dwivedi <memxor@gmail.com>
2025-12-17 11:57:40 +0000

    bpf: Allow calling static subprogs while holding a bpf_spin_lock
    mainline inclusion
    from mainline-v6.9-rc1
    commit a44b1334aadd82203f661adb9adb41e53ad0e8d1
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IDC9YK
    
    Reference: https://web.git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=a44b1334aadd82203f661adb9adb41e53ad0e8d1
    
    --------------------------------
    
    Currently, calling any helpers, kfuncs, or subprogs except the graph
    data structure (lists, rbtrees) API kfuncs while holding a bpf_spin_lock
    is not allowed. One of the original motivations of this decision was to
    force the BPF programmer's hand into keeping the bpf_spin_lock critical
    section small, and to ensure the execution time of the program does not
    increase due to lock waiting times. In addition to this, some of the
    helpers and kfuncs may be unsafe to call while holding a bpf_spin_lock.
    
    However, when it comes to subprog calls, atleast for static subprogs,
    the verifier is able to explore their instructions during verification.
    Therefore, it is similar in effect to having the same code inlined into
    the critical section. Hence, not allowing static subprog calls in the
    bpf_spin_lock critical section is mostly an annoyance that needs to be
    worked around, without providing any tangible benefit.
    
    Unlike static subprog calls, global subprog calls are not safe to permit
    within the critical section, as the verifier does not explore them
    during verification, therefore whether the same lock will be taken
    again, or unlocked, cannot be ascertained.
    
    Therefore, allow calling static subprogs within a bpf_spin_lock critical
    section, and only reject it in case the subprog linkage is global.
    
    Acked-by: Yonghong Song <yonghong.song@linux.dev>
    Acked-by: David Vernet <void@manifault.com>
    Signed-off-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>
    Link: https://lore.kernel.org/r/20240204222349.938118-2-memxor@gmail.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Conflicts:
    	kernel/bpf/verifier.c
    [Only pick the part modified by this mainline commit, and ignore the
    conflict part caused by higher version.]
    Signed-off-by: Zicheng Qu <quzicheng@huawei.com>
    Signed-off-by: cheliequan <cheliequan@inspur.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -9501,6 +9501,13 @@ static int check_func_call(struct bpf_verifier_env *env, struct bpf_insn *insn,
 	if (subprog_is_global(env, subprog)) {
 		const char *sub_name = subprog_name(env, subprog);
 
+		/* Only global subprogs cannot be called with a lock held. */
+		if (env->cur_state->active_lock.ptr) {
+			verbose(env, "global function calls are not allowed while holding a lock,\n"
+				     "use static function instead\n");
+			return -EINVAL;
+		}
+
 		if (err) {
 			verbose(env, "Caller passes invalid args into func#%d ('%s')\n",
 				subprog, sub_name);
@@ -17470,7 +17477,6 @@ static int do_check(struct bpf_verifier_env *env)
 
 				if (env->cur_state->active_lock.ptr) {
 					if ((insn->src_reg == BPF_REG_0 && insn->imm != BPF_FUNC_spin_unlock) ||
-					    (insn->src_reg == BPF_PSEUDO_CALL) ||
 					    (insn->src_reg == BPF_PSEUDO_KFUNC_CALL &&
 					     (insn->off != 0 || !is_bpf_graph_api_kfunc(insn->imm)))) {
 						verbose(env, "function calls are not allowed while holding a lock\n");
@@ -17512,9 +17518,7 @@ static int do_check(struct bpf_verifier_env *env)
 					verbose(env, "BPF_EXIT uses reserved fields\n");
 					return -EINVAL;
 				}
-
-				if (env->cur_state->active_lock.ptr &&
-				    !in_rbtree_lock_required_cb(env)) {
+				if (env->cur_state->active_lock.ptr && !env->cur_state->curframe) {
 					verbose(env, "bpf_spin_unlock is missing\n");
 					return -EINVAL;
 				}
