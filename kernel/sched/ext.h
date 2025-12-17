f1d5bdebd5ba3ce67e31bce5fe6dc65b8d9c5740
Andrea Righi <arighi@nvidia.com>
2025-12-17 12:02:50 +0000

    sched_ext: Make scx_kf_allowed_if_unlocked() available outside ext.c
    mainline inclusion
    from mainline-v6.16-rc1
    commit 617a77018f683905ed4e8cd693df51243908353f
    category: feature
    bugzilla:  https://gitee.com/openeuler/kernel/issues/ID7HLY
    
    Reference: https://web.git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=617a77018f683905ed4e8cd693df51243908353f
    
    --------------------------------
    
    Relocate the scx_kf_allowed_if_unlocked(), so it can be used from other
    source files (e.g., ext_idle.c).
    
    No functional change.
    
    Signed-off-by: Andrea Righi <arighi@nvidia.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Zicheng Qu <quzicheng@huawei.com>
    Signed-off-by: cheliequan <cheliequan@inspur.com>

--- kernel/sched/ext.h
+++ kernel/sched/ext.h
@@ -8,6 +8,11 @@
  */
 #ifdef CONFIG_SCHED_CLASS_EXT
 
+static inline bool scx_kf_allowed_if_unlocked(void)
+{
+	return !current->scx.kf_mask;
+}
+
 DECLARE_STATIC_KEY_FALSE(scx_ops_allow_queued_wakeup);
 
 DECLARE_PER_CPU(struct rq *, scx_locked_rq_state);
