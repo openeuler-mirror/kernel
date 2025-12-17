f3123c88c37e4ccfac1cfee5d5b50a611bd790ef
Yiwei Lin <s921975628@gmail.com>
2025-12-17 11:59:40 +0000

    sched/fair: Remove unused 'curr' argument from pick_next_entity()
    mainline inclusion
    from mainline-v6.7-rc1
    commit 4c456c9ad334a940e354da1002184bc19f4493ef
    category: feature
    bugzilla:  https://gitee.com/openeuler/kernel/issues/ID7HLY
    
    Reference: https://web.git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=4c456c9ad334a940e354da1002184bc19f4493ef
    
    --------------------------------
    
    The 'curr' argument of pick_next_entity() has become unused after
    the EEVDF changes.
    
    [ mingo: Updated the changelog. ]
    
    Signed-off-by: Yiwei Lin <s921975628@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/20231020055617.42064-1-s921975628@gmail.com
    Signed-off-by: Zicheng Qu <quzicheng@huawei.com>
    Signed-off-by: cheliequan <cheliequan@inspur.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5694,7 +5694,7 @@ set_next_entity(struct cfs_rq *cfs_rq, struct sched_entity *se)
  * 4) do not run the "skip" process, if something else is available
  */
 static struct sched_entity *
-pick_next_entity(struct cfs_rq *cfs_rq, struct sched_entity *curr)
+pick_next_entity(struct cfs_rq *cfs_rq)
 {
 	/*
 	 * Enabling NEXT_BUDDY will affect latency but not fairness.
@@ -10202,7 +10202,7 @@ again:
 				goto again;
 		}
 
-		se = pick_next_entity(cfs_rq, curr);
+		se = pick_next_entity(cfs_rq);
 		cfs_rq = group_cfs_rq(se);
 	} while (cfs_rq);
 
@@ -10288,7 +10288,7 @@ again:
 			}
 		}
 
-		se = pick_next_entity(cfs_rq, curr);
+		se = pick_next_entity(cfs_rq);
 		cfs_rq = group_cfs_rq(se);
 #ifdef CONFIG_QOS_SCHED
 		if (check_qos_cfs_rq(cfs_rq)) {
@@ -10338,7 +10338,7 @@ qos_simple:
 		put_prev_task(rq, prev);
 
 	do {
-		se = pick_next_entity(cfs_rq, NULL);
+		se = pick_next_entity(cfs_rq);
 		if (check_qos_cfs_rq(group_cfs_rq(se))) {
 			cfs_rq = &rq->cfs;
 			if (!cfs_rq->nr_running)
@@ -10365,7 +10365,7 @@ simple:
 		put_prev_task(rq, prev);
 
 	do {
-		se = pick_next_entity(cfs_rq, NULL);
+		se = pick_next_entity(cfs_rq);
 		set_next_entity(cfs_rq, se);
 		cfs_rq = group_cfs_rq(se);
 	} while (cfs_rq);
