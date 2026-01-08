849f40e6af185383ef3a7accfb574f687d4d29ea
Tony Luck <tony.luck@intel.com>
2026-01-08 09:42:26 -0800

    x86/resctrl: Discover hardware telemetry events
    mainline inclusion
    from mainline-v7.0-rc1
    commit 1fb2daa60de640efb13f907d43d72d28763f696c
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/8967
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=1fb2daa60de640efb13f907d43d72d28763f696c
    
    (cherry-pick from OLK-6.6 commit 5cc58e23fd33)
    
    --------------------------------
    
    Each CPU collects data for telemetry events that it sends to the nearest
    telemetry event aggregator either when the value of MSR_IA32_PQR_ASSOC.RMID
    changes, or when a two millisecond timer expires.
    
    There is a feature type ("energy" or "perf"), GUID, and MMIO region associated
    with each aggregator. This combination links to an XML description of the
    set of telemetry events tracked by the aggregator. XML files are published
    by Intel in a GitHub repository¹.
    
    The telemetry event aggregators maintain per-RMID per-event counts of the
    total seen for all the CPUs. There may be multiple telemetry event aggregators
    per package.
    
    There are separate sets of aggregators for each feature type. Aggregators
    in a set may have different GUIDs. All aggregators with the same feature
    type and GUID are symmetric keeping counts for the same set of events for
    the CPUs that provide data to them.
    
    The XML file for each aggregator provides the following information:
    0) Feature type of the events ("perf" or "energy")
    1) Which telemetry events are tracked by the aggregator.
    2) The order in which the event counters appear for each RMID.
    3) The value type of each event counter (integer or fixed-point).
    4) The number of RMIDs supported.
    5) Which additional aggregator status registers are included.
    6) The total size of the MMIO region for an aggregator.
    
    Introduce struct event_group that condenses the relevant information from
    an XML file. Hereafter an "event group" refers to a group of events of a
    particular feature type (event_group::pfname set to "energy" or "perf") with
    a particular GUID.
    
    Use event_group::pfname to determine the feature id needed to obtain the
    aggregator details. It will later be used in console messages and with the
    rdt= boot parameter.
    
    The INTEL_PMT_TELEMETRY driver enumerates support for telemetry events.
    This driver provides intel_pmt_get_regions_by_feature() to list all available
    telemetry event aggregators of a given feature type. The list includes the
    "guid", the base address in MMIO space for the region where the event counters
    are exposed, and the package id where the all the CPUs that report to this
    aggregator are located.
    
    Call INTEL_PMT_TELEMETRY's intel_pmt_get_regions_by_feature() for each event
    group to obtain a private copy of that event group's aggregator data. Duplicate
    the aggregator data between event groups that have the same feature type
    but different GUID. Further processing on this private copy will be unique
    to the event group.
    
      ¹https://github.com/intel/Intel-PMT
    
      [ bp: Zap text explaining the code, s/guid/GUID/g ]
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Reviewed-by: Reinette Chatre <reinette.chatre@intel.com>
    Link: https://lore.kernel.org/20251217172121.12030-1-tony.luck@intel.com
    
     Conflicts:
    	arch/x86/Kconfig
    [jz: resolve context conflict]
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- arch/x86/kernel/cpu/resctrl/core.c
+++ arch/x86/kernel/cpu/resctrl/core.c
@@ -732,6 +732,8 @@ static int resctrl_arch_offline_cpu(unsigned int cpu)
 
 void resctrl_arch_pre_mount(void)
 {
+	if (!intel_aet_get_events())
+		return;
 }
 
 enum {
@@ -1089,6 +1091,8 @@ late_initcall(resctrl_arch_late_init);
 
 static void __exit resctrl_arch_exit(void)
 {
+	intel_aet_exit();
+
 	cpuhp_remove_state(rdt_online);
 
 	resctrl_exit();
