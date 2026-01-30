8b6d0c3131f41aba55c9c18716891b1fa222e149
Sabrina Dubroca <sd@queasysnail.net>
2026-01-30 14:31:27 +0800

    xfrm: also call xfrm_state_delete_tunnel at destroy time for states that were never added
    mainline inclusion
    from mainline-v6.18-rc7
    commit 10deb69864840ccf96b00ac2ab3a2055c0c04721
    category: bugfix
    bugzilla: https://atomgit.com/src-openeuler/kernel/issues/11221
    CVE: CVE-2025-40215
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=10deb69864840ccf96b00ac2ab3a2055c0c04721
    
    --------------------------------
    
    In commit b441cf3f8c4b ("xfrm: delete x->tunnel as we delete x"), I
    missed the case where state creation fails between full
    initialization (->init_state has been called) and being inserted on
    the lists.
    
    In this situation, ->init_state has been called, so for IPcomp
    tunnels, the fallback tunnel has been created and added onto the
    lists, but the user state never gets added, because we fail before
    that. The user state doesn't go through __xfrm_state_delete, so we
    don't call xfrm_state_delete_tunnel for those states, and we end up
    leaking the FB tunnel.
    
    There are several codepaths affected by this: the add/update paths, in
    both net/key and xfrm, and the migrate code (xfrm_migrate,
    xfrm_state_migrate). A "proper" rollback of the init_state work would
    probably be doable in the add/update code, but for migrate it gets
    more complicated as multiple states may be involved.
    
    At some point, the new (not-inserted) state will be destroyed, so call
    xfrm_state_delete_tunnel during xfrm_state_gc_destroy. Most states
    will have their fallback tunnel cleaned up during __xfrm_state_delete,
    which solves the issue that b441cf3f8c4b (and other patches before it)
    aimed at. All states (including FB tunnels) will be removed from the
    lists once xfrm_state_fini has called flush_work(&xfrm_state_gc_work).
    
    Reported-by: syzbot+999eb23467f83f9bf9bf@syzkaller.appspotmail.com
    Closes: https://syzkaller.appspot.com/bug?extid=999eb23467f83f9bf9bf
    Fixes: b441cf3f8c4b ("xfrm: delete x->tunnel as we delete x")
    Signed-off-by: Sabrina Dubroca <sd@queasysnail.net>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>
    Conflicts:
    	net/xfrm/xfrm_state.c
    [commit 2a198bbec691 is not backport]
    Signed-off-by: Dong Chenchen <dongchenchen2@huawei.com>

--- net/xfrm/xfrm_state.c
+++ net/xfrm/xfrm_state.c
@@ -484,6 +484,7 @@ void xfrm_state_free(struct xfrm_state *x)
 }
 EXPORT_SYMBOL(xfrm_state_free);
 
+static void xfrm_state_delete_tunnel(struct xfrm_state *x);
 static void ___xfrm_state_destroy(struct xfrm_state *x)
 {
 	hrtimer_cancel(&x->mtimer);
@@ -496,6 +497,7 @@ static void ___xfrm_state_destroy(struct xfrm_state *x)
 	kfree(x->coaddr);
 	kfree(x->replay_esn);
 	kfree(x->preplay_esn);
+	xfrm_state_delete_tunnel(x);
 	if (x->type_offload)
 		xfrm_put_type_offload(x->type_offload);
 	if (x->type) {
@@ -661,7 +663,6 @@ void __xfrm_state_destroy(struct xfrm_state *x, bool sync)
 }
 EXPORT_SYMBOL(__xfrm_state_destroy);
 
-static void xfrm_state_delete_tunnel(struct xfrm_state *x);
 int __xfrm_state_delete(struct xfrm_state *x)
 {
 	struct net *net = xs_net(x);
