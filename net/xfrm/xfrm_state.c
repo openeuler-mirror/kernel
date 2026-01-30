6ea1185ce3cc210ab35d8d4a3397e28a805cbae0
Sabrina Dubroca <sd@queasysnail.net>
2026-01-30 14:31:22 +0800

    xfrm: xfrm_alloc_spi shouldn't use 0 as SPI
    mainline inclusion
    from mainline-v6.17
    commit cd8ae32e4e4652db55bce6b9c79267d8946765a9
    category: bugfix
    bugzilla: https://atomgit.com/src-openeuler/kernel/issues/12409
    CVE: CVE-2025-39797
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=cd8ae32e4e4652db55bce6b9c79267d8946765a9
    
    --------------------------------
    
    x->id.spi == 0 means "no SPI assigned", but since commit
    94f39804d891 ("xfrm: Duplicate SPI Handling"), we now create states
    and add them to the byspi list with this value.
    
    __xfrm_state_delete doesn't remove those states from the byspi list,
    since they shouldn't be there, and this shows up as a UAF the next
    time we go through the byspi list.
    
    Reported-by: syzbot+a25ee9d20d31e483ba7b@syzkaller.appspotmail.com
    Closes: https://syzkaller.appspot.com/bug?extid=a25ee9d20d31e483ba7b
    Fixes: 94f39804d891 ("xfrm: Duplicate SPI Handling")
    Signed-off-by: Sabrina Dubroca <sd@queasysnail.net>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>
    Conflicts:
    	net/xfrm/xfrm_state.c
    [conflicts due to merged 3ef7f75526f2 ("[Backport] xfrm: Duplicate SPI Handling")]
    Signed-off-by: Dong Chenchen <dongchenchen2@huawei.com>

--- net/xfrm/xfrm_state.c
+++ net/xfrm/xfrm_state.c
@@ -2093,6 +2093,8 @@ int xfrm_alloc_spi(struct xfrm_state *x, u32 low, u32 high)
 
 	for (h = 0; h < range; h++) {
 		u32 spi = (low == high) ? low : low + prandom_u32() % (high - low + 1);
+		if (spi == 0)
+			goto next;
 		newspi = htonl(spi);
 
 		spin_lock_bh(&net->xfrm.xfrm_state_lock);
@@ -2108,6 +2110,7 @@ int xfrm_alloc_spi(struct xfrm_state *x, u32 low, u32 high)
 		xfrm_state_put(x0);
 		spin_unlock_bh(&net->xfrm.xfrm_state_lock);
 
+next:
 		if (signal_pending(current)) {
 			err = -ERESTARTSYS;
 			goto unlock;
