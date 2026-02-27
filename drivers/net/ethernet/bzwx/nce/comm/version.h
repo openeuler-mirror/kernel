45126395e2a29a1d1da6f983bfef3c3df5e37b78
lpj1911 <liaopengju@bzwx-kj.com>
2026-02-27 15:17:25 +0800

    drivers/net: Update vendor name of Chengdu NorthLink Technology N5/N6 Series Network Card Drivers
    dirver inclusion
    category: cleanup
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8523
    CVE: N/A
    
    -------------------------------------------------
    
    Update the vendor name display from the old/incorrect name to the correct
    company name "Chengdu NorthLink Technology" in driver information and
    device listings.
    
    Signed-off-by: lpj1911 <liaopengju@bzwx-kj.com>

--- drivers/net/ethernet/bzwx/nce/comm/version.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-/* Copyright(c) 2020 - 2023, Chengdu BeiZhongWangXin Technology Co., Ltd. */
-
-#ifndef _VERSION_H
-#define _VERSION_H
-
-#define VERSION   "1.0.4"
-
-#endif
---
+++ drivers/net/ethernet/northlink/nce/comm/version.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright(c) 2020 - 2023, Chengdu NorthLink Technology Co., Ltd. */
+
+#ifndef _VERSION_H
+#define _VERSION_H
+
+#define VERSION   "1.0.6"
+
+#endif
