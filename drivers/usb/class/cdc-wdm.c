0e3658964aa5aaa9e104f2eeb99d66a0b1ee34a5
Oliver Neukum <oneukum@suse.com>
2026-03-04 14:01:12 +0100

    usb: class: cdc-wdm: fix reordering issue in read code path
    mainline inclusion
    from mainline-v7.0-rc4
    commit 8df672bfe3ec2268c2636584202755898e547173
    category: bugfix
    bugzilla: https://atomgit.com/src-openeuler/kernel/issues/14985
    CVE: CVE-2026-43427
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=8df672bfe3ec2268c2636584202755898e547173
    
    --------------------------------
    
    Quoting the bug report:
    
    Due to compiler optimization or CPU out-of-order execution, the
    desc->length update can be reordered before the memmove. If this
    happens, wdm_read() can see the new length and call copy_to_user() on
    uninitialized memory. This also violates LKMM data race rules [1].
    
    Fix it by using WRITE_ONCE and memory barriers.
    
    Fixes: afba937e540c9 ("USB: CDC WDM driver")
    Cc: stable <stable@kernel.org>
    Signed-off-by: Oliver Neukum <oneukum@suse.com>
    Closes: https://lore.kernel.org/linux-usb/CALbr=LbrUZn_cfp7CfR-7Z5wDTHF96qeuM=3fO2m-q4cDrnC4A@mail.gmail.com/
    Reported-by: Gui-Dong Han <hanguidong02@gmail.com>
    Reviewed-by: Gui-Dong Han <hanguidong02@gmail.com>
    Link: https://patch.msgid.link/20260304130116.1721682-1-oneukum@suse.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Conflicts:
         drivers/usb/class/cdc-wdm.c
    [ Context conflict ]
    Signed-off-by: Xinyu Zheng <zhengxinyu6@huawei.com>

--- drivers/usb/class/cdc-wdm.c
+++ drivers/usb/class/cdc-wdm.c
@@ -208,7 +208,8 @@ static void wdm_in_callback(struct urb *urb)
 		/* we may already be in overflow */
 		if (!test_bit(WDM_OVERFLOW, &desc->flags)) {
 			memmove(desc->ubuf + desc->length, desc->inbuf, length);
-			desc->length += length;
+			smp_wmb(); /* against wdm_read() */
+			WRITE_ONCE(desc->length, desc->length + length);
 			desc->reslength = length;
 		}
 	}
@@ -515,6 +516,7 @@ static ssize_t wdm_read
 		return -ERESTARTSYS;
 
 	cntr = READ_ONCE(desc->length);
+	smp_rmb(); /* against wdm_in_callback() */
 	if (cntr == 0) {
 		desc->read = 0;
 retry:
