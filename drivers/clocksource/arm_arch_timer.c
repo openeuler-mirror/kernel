57a68ef9e7538c998785beb6924ac8b64748fe0e
Yifan Wu <wuyifan50@huawei.com>
2026-03-10 12:57:21 +0800

    gic: Add hip09 support for HISILICON_ERRATUM_165010801
    driver inclusion
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/7023
    
    ----------------------------------------------------------------------
    
    Add hip09 support for HISILICON_ERRATUM_165010801
    
    Fixes: f0f9be237c22 ("gic: increase the arch_timer priority to avoid hardlockup")
    Signed-off-by: Yifan Wu <wuyifan50@huawei.com>
    Signed-off-by: Zeng Heng <zengheng4@huawei.com>

--- drivers/clocksource/arm_arch_timer.c
+++ drivers/clocksource/arm_arch_timer.c
@@ -342,6 +342,11 @@ static struct ate_acpi_oem_info hisi_161010101_oem_info[] = {
 static struct ate_acpi_oem_info hisi_165010801_oem_info[] = {
 	{
 		.oem_id		= "HISI  ",
+		.oem_table_id	= "HIP09   ",
+		.oem_revision	= 0,
+	},
+	{
+		.oem_id		= "HISI  ",
 		.oem_table_id	= "HIP12   ",
 		.oem_revision	= 0,
 	},
