d8d441a83be30dee4efa23bfb4b7b00ffcf738ff
Liao Xuan <liaoxuan@hygon.cn>
2026-03-16 11:42:42 +0800

    EDAC/amd64: Add support for Hygon family 18h model 18h
    hygon inclusion
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8724
    CVE: N/A
    
    Reference: https://gitee.com/anolis/cloud-kernel/commit/eb162a81e5007122e42bab182a9807dbe34fc029
    
    (cherry-pick from OLK-6.6 commit d9bc6305f986)
    
    ------------------------------------------
    
    ANBZ: #30343
    
    Add Hygon family 18h model 18h processor support for amd64_edac. For Hygon
    family 18h model 0x18h-0x1fh processors, the UMC base are identical, so
    modify them uniformly.
    
    Meanwhile, code for Hygon UMC base calculations has been moved to a
    dedicated function.
    
    Hygon-SIG: commit none hygon anolis: EDAC/amd64: Add support for Hygon
    family 18h model 18h
    
    Hygon-SIG: commit eb162a81e500 anolis anolis: EDAC/amd64: Add support for Hygon family 18h model 18h
    Backport from anolis to support Hygon family 18h model 18h
    
    Signed-off-by: Liao Xuan <liaoxuan@hygon.cn>
    Cc: hygon-arch@list.openanolis.cn
    Reviewed-by: Xiaochen Shen <shenxiaochen@hygon.cn>
    Reviewed-by: Ruidong Tian <tianruidong@linux.alibaba.com>
    Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6454
    [ YuntongJin : amend commit log ]
    Signed-off-by: YuntongJin <jinyuntong@hygon.cn>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- drivers/edac/amd64_edac.c
+++ drivers/edac/amd64_edac.c
@@ -108,6 +108,29 @@ static u32 get_umc_base_f18h_m4h(u16 node, u8 channel)
 	return get_umc_base(channel) + (0x80000000 + (0x10000000 * df_id));
 }
 
+static u32 get_umc_base_f18h_m18h(u8 channel)
+{
+	return 0x70000000 + (channel << 20);
+}
+
+static u32 hygon_get_umc_base(struct amd64_pvt *pvt, u8 channel)
+{
+	u32 umc_base;
+
+	if (hygon_f18h_m4h())
+		umc_base = get_umc_base_f18h_m4h(pvt->mc_node_id, channel);
+	/*
+	 * For Hygon family 18h model 0x18h-0x1fh processors, the UMC base
+	 * are identical.
+	 */
+	else if (hygon_f18h_m10h() && boot_cpu_data.x86_model >= 0x18)
+		umc_base = get_umc_base_f18h_m18h(channel);
+	else
+		umc_base = get_umc_base(channel);
+
+	return umc_base;
+}
+
 /*
  * Select DCT to which PCI cfg accesses are routed
  */
@@ -1848,8 +1871,8 @@ static void umc_read_base_mask(struct amd64_pvt *pvt)
 		if (!hygon_umc_channel_enabled(pvt, umc))
 			continue;
 
-		if (hygon_f18h_m4h())
-			umc_base = get_umc_base_f18h_m4h(pvt->mc_node_id, umc);
+		if (boot_cpu_data.x86_vendor == X86_VENDOR_HYGON)
+			umc_base = hygon_get_umc_base(pvt, umc);
 		else
 			umc_base = get_umc_base(umc);
 
@@ -3343,8 +3366,8 @@ static void umc_read_mc_regs(struct amd64_pvt *pvt)
 		if (!hygon_umc_channel_enabled(pvt, i))
 			continue;
 
-		if (hygon_f18h_m4h())
-			umc_base = get_umc_base_f18h_m4h(pvt->mc_node_id, i);
+		if (boot_cpu_data.x86_vendor == X86_VENDOR_HYGON)
+			umc_base = hygon_get_umc_base(pvt, i);
 		else
 			umc_base = get_umc_base(i);
 
