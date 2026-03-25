5e4b6d9938d2bb4769a0b3aba5ffddfcb5b3106c
Jia Qingtong <jiaqingtong@huawei.com>
2026-03-25 08:56:47 +0800

    KVM: arm64: Provide VCPU attributes for pvtimer status
    virt inclusion
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8901
    
    (cherry-pick from OLK-6.6 commit d18cafd04384)
    
    ------------------------------------------------------------------------
    
    Add KVM device attribute support for pvtimer status, allowing
    userspace to get/set the shared memory base address via
    KVM_ARM_VCPU_PVTIMER_STATUS_CTRL.
    
    This enables userspace introspection and migration support.
    
    Signed-off-by: Jia Qingtong <jiaqingtong@huawei.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- arch/arm64/kvm/pvtimer_status.c
+++ arch/arm64/kvm/pvtimer_status.c
@@ -19,5 +19,62 @@ gpa_t kvm_init_pvtimer_status(struct kvm_vcpu *vcpu)
 	return vcpu->arch.pvtimer_status.base;
 }
 
+int kvm_arm_pvtimer_status_set_attr(struct kvm_vcpu *vcpu,
+			    struct kvm_device_attr *attr)
+{
+	u64 __user *user = (u64 __user *)attr->addr;
+	struct kvm *kvm = vcpu->kvm;
+	u64 ipa;
+	int ret = 0;
+	int idx;
+
+	if (attr->attr != KVM_ARM_VCPU_PVTIMER_STATUS_IPA)
+		return -ENXIO;
+
+	if (get_user(ipa, user))
+		return -EFAULT;
+	if (!IS_ALIGNED(ipa, 64))
+		return -EINVAL;
+	if (vcpu->arch.pvtimer_status.base != INVALID_GPA)
+		return -EEXIST;
+
+	/* Check the address is in a valid memslot */
+	idx = srcu_read_lock(&kvm->srcu);
+	if (kvm_is_error_hva(gfn_to_hva(kvm, ipa >> PAGE_SHIFT)))
+		ret = -EINVAL;
+	srcu_read_unlock(&kvm->srcu, idx);
+
+	if (!ret)
+		vcpu->arch.pvtimer_status.base = ipa;
+
+	return ret;
+}
+
+int kvm_arm_pvtimer_status_get_attr(struct kvm_vcpu *vcpu,
+			    struct kvm_device_attr *attr)
+{
+	u64 __user *user = (u64 __user *)attr->addr;
+	u64 ipa;
+
+	if (attr->attr != KVM_ARM_VCPU_PVTIMER_STATUS_IPA)
+		return -ENXIO;
+
+	ipa = vcpu->arch.pvtimer_status.base;
+
+	if (put_user(ipa, user))
+		return -EFAULT;
+	return 0;
+}
+
+int kvm_arm_pvtimer_status_has_attr(struct kvm_vcpu *vcpu,
+			    struct kvm_device_attr *attr)
+{
+	switch (attr->attr) {
+	case KVM_ARM_VCPU_PVTIMER_STATUS_IPA:
+		return vtimer_is_irqbypass() ? 0 : -ENXIO;
+	}
+	return -ENXIO;
+}
+
 #endif
 
