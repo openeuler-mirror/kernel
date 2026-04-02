17e44d94322e2325c1be389928283963306335f0
Haiqing Fang <fanghaiqing@huawei.com>
2026-04-02 11:00:45 +0800

    net: unic: The unsupported VL rate limit value is displayed as 0
    drivers inclusion
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/9157
    CVE: NA
    
    (cherry-pick from OLK-6.6 commit 8c66c312fb94)
    
    --------------------------------------------------------------------------
    
    The dcb tool always queries the rate limit of TCs 0 to 7.
    However, the range of TCs supported by the driver may be
    smaller than 0 to 7. For TCs not supported by the driver,
    the current implementation is that the dcb query result
    is the same as the dcb configuration result.
    When the user sets the rate limit of all TCs to 0, the
    value 0 is specially processed as the maximum rate limit
    supported by the TCs supported by the driver. For TCs not
    supported by the driver, the value 0 remains unchanged.
    In this case, the query result is different and difficult
    to understand.
    This patch adjusts the policy for querying the rate limit
    of TCs in the dcb tool. For TCs not supported by the
    driver, the value 0 is always displayed.
    
    Fixes: c20baa942215 ("net: unic: support config/query the mapping between dscp and tc")
    Signed-off-by: Haiqing Fang <fanghaiqing@huawei.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- drivers/net/ub/unic/unic_dcbnl.c
+++ drivers/net/ub/unic/unic_dcbnl.c
@@ -563,6 +563,8 @@ static int unic_ieee_setmaxrate(struct net_device *ndev,
 	struct unic_dev *unic_dev = netdev_priv(ndev);
 	struct unic_vl *vl = &unic_dev->channels.vl;
 	u64 tc_maxrate[IEEE_8021QAZ_MAX_TCS];
+	struct auxiliary_device *adev;
+	struct ubase_caps *caps;
 	int ret;
 
 	if (!unic_dev_ets_supported(unic_dev) ||
@@ -581,7 +583,9 @@ static int unic_ieee_setmaxrate(struct net_device *ndev,
 	if (ret)
 		return ret;
 
-	memcpy(vl->vl_maxrate, tc_maxrate, sizeof(tc_maxrate));
+	adev = unic_dev->comdev.adev;
+	caps = ubase_get_dev_caps(adev);
+	memcpy(vl->vl_maxrate, tc_maxrate, caps->vl_num * sizeof(u64));
 
 	return 0;
 }
