9021092947c9bcff3a3f3d76e29c8ca45acf11e5
Eric Dumazet <edumazet@google.com>
2026-04-02 19:55:13 +0800

    tcp: secure_seq: add back ports to TS offset
    mainline inclusion
    from mainline-v7.0-rc3
    commit 165573e41f2f66ef98940cf65f838b2cb575d9d1
    category: bugfix
    bugzilla: https://atomgit.com/src-openeuler/kernel/issues/13867
    CVE: CVE-2026-23247
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=165573e41f2f66ef98940cf65f838b2cb575d9d1
    
    (cherry-pick from OLK-6.6 commit 0ce323b573da)
    
    --------------------------------
    
    This reverts 28ee1b746f49 ("secure_seq: downgrade to per-host timestamp offsets")
    
    tcp_tw_recycle went away in 2017.
    
    Zhouyan Deng reported off-path TCP source port leakage via
    SYN cookie side-channel that can be fixed in multiple ways.
    
    One of them is to bring back TCP ports in TS offset randomization.
    
    As a bonus, we perform a single siphash() computation
    to provide both an ISN and a TS offset.
    
    Fixes: 28ee1b746f49 ("secure_seq: downgrade to per-host timestamp offsets")
    Reported-by: Zhouyan Deng <dengzhouyan_nwpu@163.com>
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: Kuniyuki Iwashima <kuniyu@google.com>
    Acked-by: Florian Westphal <fw@strlen.de>
    Link: https://patch.msgid.link/20260302205527.1982836-1-edumazet@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Conflicts:
    	include/net/secure_seq.h
    	include/net/tcp.h
    	net/core/secure_seq.c
    	net/ipv4/syncookies.c
    	net/ipv4/tcp_input.c
    	net/ipv4/tcp_ipv4.c
    	net/ipv6/syncookies.c
    	net/ipv6/tcp_ipv6.c
    [conflicts due to merged
    bf8317ce3910 ("net: use __GENKSYMS__ to revert the kabi change") and not merge
    2a63dd0edf38 ("net: Retire DCCP socket.") and
    6dc4c2526f6d ("tcp: use EXPORT_IPV6_MOD[_GPL]()") and
    8e7bab6b9652 ("tcp: Factorise cookie-independent fields initialisation in cookie_v[46]_check()") and
    cdbab6236605 ("tcp: fix fastopen code vs usec TS") and
    449f68f8fffa ("net: Convert proto callbacks from sockaddr to sockaddr_unsized").]
    Signed-off-by: Li Xiasong <lixiasong1@huawei.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -44,6 +44,7 @@
 #ifndef __GENKSYMS__
 #include <net/xfrm.h>
 #endif
+#include <net/secure_seq.h>
 
 #include <linux/seq_file.h>
 #include <linux/memcontrol.h>
@@ -2180,8 +2181,9 @@ struct tcp_request_sock_ops {
 				       struct sk_buff *skb,
 				       struct flowi *fl,
 				       struct request_sock *req);
-	u32 (*init_seq)(const struct sk_buff *skb);
-	u32 (*init_ts_off)(const struct net *net, const struct sk_buff *skb);
+	union tcp_seq_and_ts_off (*init_seq_and_ts_off)(
+					const struct net *net,
+					const struct sk_buff *skb);
 	int (*send_synack)(const struct sock *sk, struct dst_entry *dst,
 			   struct flowi *fl, struct request_sock *req,
 			   struct tcp_fastopen_cookie *foc,
