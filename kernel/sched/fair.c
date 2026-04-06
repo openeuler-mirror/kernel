674bcbb6d755f19c9ccb4ef1b2b21689983fb0fa
Peter Zijlstra <peterz@infradead.org>
2026-04-06 10:40:06 +0800

    sched: Allow sched_class::dequeue_task() to fail
    mainline inclusion
    from mainline-v6.12-rc1
    commit 863ccdbb918a77e3f011571f943020bf7f0b114b
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8335
    CVE: NA
    
    Reference: https://web.git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=863ccdbb918a77e3f011571f943020bf7f0b114b
    
    (cherry-pick from OLK-6.6 commit b1cb7f0760a9)
    
    --------------------------------
    
    Change the function signature of sched_class::dequeue_task() to return
    a boolean, allowing future patches to 'fail' dequeue.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Valentin Schneider <vschneid@redhat.com>
    Tested-by: Valentin Schneider <vschneid@redhat.com>
    Link: https://lkml.kernel.org/r/20240727105028.864630153@infradead.org
    Conflicts:
    	kernel/sched/deadline.c
    	kernel/sched/fair.c
    	kernel/sched/sched.h
    [The mainline and OLK both touch `dequeue_task_dl()`, but mainline only
    changes the return-value path while OLK already moved the surrounding
    logic into other functions.]
    Signed-off-by: Zicheng Qu <quzicheng@huawei.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7738,7 +7738,7 @@ static void set_next_buddy(struct sched_entity *se);
  * decreased. We remove the task from the rbtree and
  * update the fair scheduling stats:
  */
-static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
+static bool dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 {
 	struct cfs_rq *cfs_rq;
 	struct sched_entity *se = &p->se;
@@ -7817,6 +7817,8 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 dequeue_throttle:
 	util_est_update(&rq->cfs, p, task_sleep);
 	hrtick_update(rq);
+
+	return true;
 }
 
 #ifdef CONFIG_SMP
--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1543,7 +1543,7 @@ enqueue_task_rt(struct rq *rq, struct task_struct *p, int flags)
 		enqueue_pushable_task(rq, p);
 }
 
-static void dequeue_task_rt(struct rq *rq, struct task_struct *p, int flags)
+static bool dequeue_task_rt(struct rq *rq, struct task_struct *p, int flags)
 {
 	struct sched_rt_entity *rt_se = &p->rt;
 
@@ -1551,6 +1551,8 @@ static void dequeue_task_rt(struct rq *rq, struct task_struct *p, int flags)
 	dequeue_rt_entity(rt_se, flags);
 
 	dequeue_pushable_task(rq, p);
+
+	return true;
 }
 
 /*
