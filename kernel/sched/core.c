a90d8ac7ea9ac1a11a3a02ed0fcf7757a11d0c36
Peter Zijlstra <peterz@infradead.org>
2026-04-06 10:40:34 +0800

    sched: Combine the last put_prev_task() and the first set_next_task()
    mainline inclusion
    from mainline-v6.12-rc1
    commit 436f3eed5c69c1048a5754df6e3dbb291e5cccbd
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8335
    CVE: NA
    
    Reference: https://web.git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=436f3eed5c69c1048a5754df6e3dbb291e5cccbd
    
    (cherry-pick from OLK-6.6 commit eb9ef1ebbbb4)
    
    --------------------------------
    
    Ensure the last put_prev_task() and the first set_next_task() always
    go together.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20240813224016.158454756@infradead.org
    Conflicts:
        	kernel/sched/core.c
    	kernel/sched/fair.c
    [The mainline and OLK both changed the task-switch callsites in
    __pick_next_task(), but both sides can be kept together.]
    Signed-off-by: Zicheng Qu <quzicheng@huawei.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5961,8 +5961,7 @@ __pick_next_task(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 		/* Assume the next prioritized class is idle_sched_class */
 		if (!p) {
 			p = pick_task_idle(rq);
-			put_prev_task(rq, prev);
-			set_next_task_first(rq, p);
+			put_prev_set_next_task(rq, prev, p);
 		}
 
 		return p;
@@ -5986,9 +5985,8 @@ restart:
 			if (p) {
 				const struct sched_class *prev_class = prev->sched_class;
 
-				put_prev_task(rq, prev);
-				set_next_task_first(rq, p);
-
+				put_prev_set_next_task(rq, prev, p);
+				
 				if (class != prev_class && prev_class->switch_class)
 					prev_class->switch_class(rq, p);
 				return p;
@@ -6079,13 +6077,8 @@ pick_next_task(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 		WRITE_ONCE(rq->core_sched_seq, rq->core->core_pick_seq);
 
 		next = rq->core_pick;
-		if (next != prev) {
-			put_prev_task(rq, prev);
-			set_next_task_first(rq, next);
-		}
-
 		rq->core_pick = NULL;
-		goto out;
+		goto out_set_next;
 	}
 
 	prev_balance(rq, prev, rf);
@@ -6255,9 +6248,7 @@ pick_next_task(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 	}
 
 out_set_next:
-	put_prev_task(rq, prev);
-	set_next_task_first(rq, next);
-out:
+	put_prev_set_next_task(rq, prev, next);
 	if (rq->core->core_forceidle_count && next == rq->idle)
 		queue_core_balance(rq);
 
