c9a3007a614df3b7d2609e46fb2f8b40aa714bbb
Peter Zijlstra <peterz@infradead.org>
2026-04-06 10:40:33 +0800

    sched: Split up put_prev_task_balance()
    mainline inclusion
    from mainline-v6.12-rc1
    commit 260598f142c34811d226fdde5ab0346b48181439
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8335
    CVE: NA
    
    Reference: https://web.git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=260598f142c34811d226fdde5ab0346b48181439
    
    --------------------------------
    
    With the goal of pushing put_prev_task() after pick_task() / into
    pick_next_task().
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20240813224015.943143811@infradead.org
    Conflicts:
    	kernel/sched/core.c
    [The mainline moves put_prev_task() out of the balance helper while OLK
    had already added SCX-specific start-class selection and class-range
    walking there.]
    Signed-off-by: Zicheng Qu <quzicheng@huawei.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5902,8 +5902,8 @@ static inline void schedule_debug(struct task_struct *prev, bool preempt)
 	schedstat_inc(this_rq()->sched_count);
 }
 
-static void put_prev_task_balance(struct rq *rq, struct task_struct *prev,
-				  struct rq_flags *rf)
+static void prev_balance(struct rq *rq, struct task_struct *prev,
+			 struct rq_flags *rf)
 {
 	const struct sched_class *start_class = prev->sched_class;
 	const struct sched_class *class;
@@ -5931,7 +5931,6 @@ static void put_prev_task_balance(struct rq *rq, struct task_struct *prev,
 			break;
 	}
 
-	put_prev_task(rq, prev);
 }
 
 /*
@@ -5969,7 +5968,8 @@ __pick_next_task(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 	}
 
 restart:
-	put_prev_task_balance(rq, prev, rf);
+	prev_balance(rq, prev, rf);
+	put_prev_task(rq, prev);
 
 	for_each_active_class(class) {
 		p = class->pick_next_task(rq);
@@ -6074,7 +6074,8 @@ pick_next_task(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 		goto out;
 	}
 
-	put_prev_task_balance(rq, prev, rf);
+	prev_balance(rq, prev, rf);
+	put_prev_task(rq, prev);
 
 	smt_mask = cpu_smt_mask(cpu);
 	need_sync = !!rq->core->core_cookie;
