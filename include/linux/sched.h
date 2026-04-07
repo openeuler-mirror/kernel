1df99e94c84758cd7f6d9d272bac8385f3832a75
Zicheng Qu <quzicheng@huawei.com>
2026-04-07 10:18:24 +0800

    Revert "sched: Fix kabi breakage of struct task_struct for saved_state"
    hulk inclusion
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8335
    CVE: NA
    
    (cherry-pick from OLK-6.6 commit 936973622517)
    
    --------------------------------
    
    This reverts commit f1b45e14fb61f425f143968f2ad78991f54d98a4.
    
    Signed-off-by: Zicheng Qu <quzicheng@huawei.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -809,6 +809,9 @@ struct task_struct {
 #endif
 	unsigned int			__state;
 
+	/* saved state for "spinlock sleepers" */
+	unsigned int			saved_state;
+
 	/*
 	 * This begins the randomizable portion of task_struct. Only
 	 * scheduling-critical items should be added above here.
@@ -1637,10 +1640,8 @@ struct task_struct {
 #else
 	KABI_RESERVE(1)
 #endif
-
 	KABI_RESERVE(2)
-	/* saved state for "spinlock sleepers" */
-	KABI_USE2(3, unsigned kabi_reserved_int, unsigned int saved_state)
+	KABI_RESERVE(3)
 	KABI_RESERVE(4)
 	KABI_RESERVE(5)
 	KABI_RESERVE(6)
