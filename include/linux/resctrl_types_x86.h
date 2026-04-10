0cfc53c865a55f836d8c8ecd23574b04f0c8b91b
Jason Zeng <jason.zeng@intel.com>
2026-04-10 06:46:43 -0700

    x86: Decouple resctrl with resctrl-mpam
    Intel inclusion
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8967
    CVE: NA
    
    (cherry-pick from OLK-6.6 commit 0654728ca3e9)
    
    --------------------------------
    
    There are so many conflicts between upstream resctrl code and current
    OLK-6.6 resctrl code.
    
    The solution is to separate upstream code to another directory, so
    backporting can be done individually against current OLK-6.6 resctrl
    code.
    
    The code is reverted to the state of commit 4cee14bcb1488, where the
    OOT patches started to integrate into OLK-6.6. At this point, the
    resctrl code is only in arch/x86/kernel/cpu/resctrl/.
    
    Meanwhile, the state of following files are also decoupled:
      include/linux/resctrl.h -> include/linux/resctrl_x86.h
      include/linux/resctrl_types.h -> include/linux/resctrl_types_x86.h
    
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

---
+++ include/linux/resctrl_types_x86.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2025 Arm Ltd.
+ * Based on arch/x86/kernel/cpu/resctrl/internal.h
+ */
+
+#ifndef __LINUX_RESCTRL_TYPES_X86_H
+#define __LINUX_RESCTRL_TYPES_X86_H
+
+#endif /* __LINUX_RESCTRL_TYPES_X86_H */
