edc62a3796561bfccea11d5d20912786fd455f41
Jason Zeng <jason.zeng@intel.com>
2026-04-10 06:46:43 -0700

    x86: Decouple resctrl with resctrl-mpam
    Intel inclusion
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8967
    CVE: NA
    
    (cherry-pick from OLK-6.6 commit 0654728ca3e9)
    
    --------------------------------
    
    There are so many conflicts between upstream resctrl code and current
    OLK-6.6 resctrl code.
    
    The solution is to separate upstream code to another directory, so
    backporting can be done individually against current OLK-6.6 resctrl
    code.
    
    The code is reverted to the state of commit 4cee14bcb1488, where the
    OOT patches started to integrate into OLK-6.6. At this point, the
    resctrl code is only in arch/x86/kernel/cpu/resctrl/.
    
    Meanwhile, the state of following files are also decoupled:
      include/linux/resctrl.h -> include/linux/resctrl_x86.h
      include/linux/resctrl_types.h -> include/linux/resctrl_types_x86.h
    
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- include/linux/resctrl.h
+++ include/linux/resctrl.h
@@ -2,6 +2,12 @@
 #ifndef _RESCTRL_H
 #define _RESCTRL_H
 
+#ifdef CONFIG_X86_CPU_RESCTRL
+
+#include <linux/resctrl_x86.h>
+
+#else
+
 #include <linux/cpu.h>
 #include <linux/kernel.h>
 #include <linux/list.h>
@@ -474,4 +480,5 @@ resctrl_arch_match_iommu_closid_rmid(struct iommu_group *group,
 	return false;
 }
 #endif /* CONFIG_RESCTRL_IOMMU */
+#endif /* CONFIG_X86_CPU_RESCTRL */
 #endif /* _RESCTRL_H */
