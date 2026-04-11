918426e9e63f70fbb9708351f5e99932321218f6
Xia Fukun <xiafukun@huawei.com>
2026-04-11 07:23:47 +0000

    sched: fix kabi broken in struct sched_statistics
    hulk inclusion
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8903
    
    (cherry-pick from OLK-6.6 commit 8d339a46855f)
    
    --------------------------------
    
    Fix kabi broken in struct sched_statistics.
    
    Fixes: 551add71e231 ("sched: Unify runtime accounting across classes")
    Signed-off-by: Xia Fukun <xiafukun@huawei.com>
    Signed-off-by: Zicheng Qu <quzicheng@huawei.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -533,7 +533,7 @@ struct sched_statistics {
 	u64				block_max;
 	s64				sum_block_runtime;
 
-	s64				exec_max;
+	KABI_REPLACE(u64 exec_max, s64 exec_max)
 	u64				slice_max;
 
 	u64				nr_migrations_cold;
