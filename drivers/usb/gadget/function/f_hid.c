bf3221264373097a92e07c7f8e7b14eb5327f41b
Michael Zimmermann <sigmaepsilon92@gmail.com>
2026-04-11 21:02:47 -0400

    usb: gadget: f_hid: move list and spinlock inits from bind to alloc
    stable inclusion
    from stable-v6.6.135
    commit 8ec6a58586f195a88479edcdb0b8027c39f12d03
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=8ec6a58586f195a88479edcdb0b8027c39f12d03
    
    (cherry-pick from OLK-6.6 commit 2558f0525f46)
    
    --------------------------------
    
    commit 8ec6a58586f195a88479edcdb0b8027c39f12d03 upstream.
    
    [ Upstream commit 4e0a88254ad59f6c53a34bf5fa241884ec09e8b2 ]
    
    There was an issue when you did the following:
    - setup and bind an hid gadget
    - open /dev/hidg0
    - use the resulting fd in EPOLL_CTL_ADD
    - unbind the UDC
    - bind the UDC
    - use the fd in EPOLL_CTL_DEL
    
    When CONFIG_DEBUG_LIST was enabled, a list_del corruption was reported
    within remove_wait_queue (via ep_remove_wait_queue). After some
    debugging I found out that the queues, which f_hid registers via
    poll_wait were the problem. These were initialized using
    init_waitqueue_head inside hidg_bind. So effectively, the bind function
    re-initialized the queues while there were still items in them.
    
    The solution is to move the initialization from hidg_bind to hidg_alloc
    to extend their lifetimes to the lifetime of the function instance.
    
    Additionally, I found many other possibly problematic init calls in the
    bind function, which I moved as well.
    
    Signed-off-by: Michael Zimmermann <sigmaepsilon92@gmail.com>
    Cc: stable <stable@kernel.org>
    Link: https://patch.msgid.link/20260331184844.2388761-1-sigmaepsilon92@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- drivers/usb/gadget/function/f_hid.c
+++ drivers/usb/gadget/function/f_hid.c
@@ -1001,13 +1001,8 @@ static int hidg_bind(struct usb_configuration *c, struct usb_function *f)
 	if (status)
 		goto fail;
 
-	spin_lock_init(&hidg->write_spinlock);
 	hidg->write_pending = 1;
 	hidg->req = NULL;
-	spin_lock_init(&hidg->read_spinlock);
-	init_waitqueue_head(&hidg->write_queue);
-	init_waitqueue_head(&hidg->read_queue);
-	INIT_LIST_HEAD(&hidg->completed_out_req);
 
 	/* create char device */
 	hidg->cdev = cdev_alloc();
@@ -1281,6 +1276,12 @@ static struct usb_function *hidg_alloc(struct usb_function_instance *fi)
 	opts = container_of(fi, struct f_hid_opts, func_inst);
 
 	mutex_lock(&opts->lock);
+
+	spin_lock_init(&hidg->write_spinlock);
+	spin_lock_init(&hidg->read_spinlock);
+	init_waitqueue_head(&hidg->write_queue);
+	init_waitqueue_head(&hidg->read_queue);
+	INIT_LIST_HEAD(&hidg->completed_out_req);
 
 	device_initialize(&hidg->dev);
 	hidg->dev.release = hidg_release;
