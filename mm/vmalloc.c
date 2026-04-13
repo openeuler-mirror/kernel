1aa026f05af886281e22009b68c3eec4894599f3
Uladzislau Rezki (Sony) <urezki@gmail.com>
2026-04-13 21:26:46 +0200

    mm/vmalloc: take vmap_purge_lock in shrinker
    mainline inclusion
    from mainline-v7.1-rc1
    commit ec05f51f1e65bce95528543eb73fda56fd201d94
    category: bugfix
    bugzilla: https://atomgit.com/src-openeuler/kernel/issues/15335
    CVE: CVE-2026-46093
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=ec05f51f1e65bce95528543eb73fda56fd201d94
    
    (cherry-pick from OLK-6.6 commit e3a50a3eb929)
    
    --------------------------------
    
    decay_va_pool_node() can be invoked concurrently from two paths:
    __purge_vmap_area_lazy() when pools are being purged, and the shrinker via
    vmap_node_shrink_scan().
    
    However, decay_va_pool_node() is not safe to run concurrently, and the
    shrinker path currently lacks serialization, leading to races and possible
    leaks.
    
    Protect decay_va_pool_node() by taking vmap_purge_lock in the shrinker
    path to ensure serialization with purge users.
    
    Link: https://lore.kernel.org/20260413192646.14683-1-urezki@gmail.com
    Fixes: 7679ba6b36db ("mm: vmalloc: add a shrinker to drain vmap pools")
    Signed-off-by: Uladzislau Rezki (Sony) <urezki@gmail.com>
    Reviewed-by: Baoquan He <baoquan.he@linux.dev>
    Cc: chenyichong <chenyichong@uniontech.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Conflicts:
         mm/vmalloc.c
    [ Context conflict ]
    Signed-off-by: Xi Qi <xiqi2@huawei.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -5302,6 +5302,7 @@ vmap_node_shrink_scan(struct shrinker_v2 *shrink, struct shrink_control *sc)
 {
 	int i;
 
+	guard(mutex)(&vmap_purge_lock);
 	for (i = 0; i < nr_vmap_nodes; i++)
 		decay_va_pool_node(&vmap_nodes[i], true);
 
