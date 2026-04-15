20d929e94a30345f46cb16063733a36bc5ae0e80
leoliu-oc <leoliu-oc@zhaoxin.com>
2026-04-15 20:01:46 +0800

    x86/mce/zhaoxin: Enable mcelog to decode PCIE, ZDI/ZPI, and DRAM errors
    zhaoxin inclusion
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8949
    CVE: NA
    
    -------------------
    
    The mcelog cannot decode PCIE, ZDI/ZPI, and DRAM errors in the FFM
    (Firmware First Mode).
    The purpose of this patch is to enable mcelog to decode PCIE, ZDI/ZPI, and
    DRAM errors that occur on Zhaoxin processors, so that the cause of these
    errors can be quickly located.
    
    Signed-off-by: leoliu-oc <leoliu-oc@zhaoxin.com>

--- arch/x86/kernel/acpi/apei.c
+++ arch/x86/kernel/acpi/apei.c
@@ -40,8 +40,34 @@ int arch_apei_enable_cmcff(struct acpi_hest_header *hest_hdr, void *data)
 void arch_apei_report_mem_error(int sev, struct cper_sec_mem_err *mem_err)
 {
 #ifdef CONFIG_X86_MCE
-	apei_mce_report_mem_error(sev, mem_err);
+	if (boot_cpu_data.x86_vendor == X86_VENDOR_ZHAOXIN ||
+	    boot_cpu_data.x86_vendor == X86_VENDOR_CENTAUR)
+		zx_apei_mce_report_mem_error(mem_err);
+	else
+		apei_mce_report_mem_error(sev, mem_err);
 #endif
+}
+
+void arch_apei_report_pcie_error(int sev, struct cper_sec_pcie *pcie_err)
+{
+#ifdef CONFIG_X86_MCE
+	if (boot_cpu_data.x86_vendor == X86_VENDOR_ZHAOXIN ||
+	    boot_cpu_data.x86_vendor == X86_VENDOR_CENTAUR)
+		zx_apei_mce_report_pcie_error(sev, pcie_err);
+#endif
+}
+
+bool arch_apei_report_zdi_error(guid_t *sec_type, struct cper_sec_proc_generic *zdi_err)
+{
+#ifdef CONFIG_X86_MCE
+	if ((boot_cpu_data.x86_vendor == X86_VENDOR_CENTAUR ||
+	     boot_cpu_data.x86_vendor == X86_VENDOR_ZHAOXIN) &&
+	    (guid_equal(sec_type, &CPER_SEC_PROC_GENERIC))) {
+		zx_apei_mce_report_zdi_error(zdi_err);
+		return true;
+	}
+#endif
+	return false;
 }
 
 int arch_apei_report_x86_error(struct cper_ia_proc_ctx *ctx_info, u64 lapic_id)
