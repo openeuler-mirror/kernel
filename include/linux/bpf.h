f88f3b4f790a6df9fab39f436a74fd011acd3dce
Peter Zijlstra <peterz@infradead.org>
2026-04-16 12:41:58 +0000

    x86/cfi,bpf: Fix BPF JIT call
    mainline inclusion
    from mainline-v6.8-rc1
    commit 4f9087f16651aca4a5f32da840a53f6660f0579a
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8335
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=4f9087f16651aca4a5f32da840a53f6660f0579a
    
    (cherry-pick from OLK-6.6 commit 9c163ff4834e)
    
    ----------------------------------------------------------------------
    
    The current BPF call convention is __nocfi, except when it calls !JIT things,
    then it calls regular C functions.
    
    It so happens that with FineIBT the __nocfi and C calling conventions are
    incompatible. Specifically __nocfi will call at func+0, while FineIBT will have
    endbr-poison there, which is not a valid indirect target. Causing #CP.
    
    Notably this only triggers on IBT enabled hardware, which is probably why this
    hasn't been reported (also, most people will have JIT on anyway).
    
    Implement proper CFI prologues for the BPF JIT codegen and drop __nocfi for
    x86.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20231215092707.345270396@infradead.org
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Conflicts:
    	arch/x86/net/bpf_jit_comp.c
    	include/linux/bpf.h
    	kernel/bpf/core.c
    [Fix conflict because of kabi and cev patches db775a8ebf140, and
    pre-patches fd5d27b70188 is not existed.]
    Signed-off-by: Luo Gengkun <luogengkun2@huawei.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -30,6 +30,7 @@
 #include <linux/static_call.h>
 #include <linux/memcontrol.h>
 #include <linux/kabi.h>
+#include <linux/cfi.h>
 
 struct bpf_verifier_env;
 struct bpf_verifier_log;
@@ -1278,7 +1279,12 @@ struct bpf_dispatcher {
 	KABI_RESERVE(4)
 };
 
-static __always_inline __nocfi unsigned int bpf_dispatcher_nop_func(
+#ifndef __bpfcall
+#define __bpfcall __nocfi
+#endif
+
+static __always_inline __bpfcall unsigned int bpf_dispatcher_nop_func(
+
 	const void *ctx,
 	const struct bpf_insn *insnsi,
 	bpf_func_t bpf_func)
@@ -1372,7 +1378,7 @@ int arch_prepare_bpf_dispatcher(void *image, void *buf, s64 *funcs, int num_func
 
 #define DEFINE_BPF_DISPATCHER(name)					\
 	__BPF_DISPATCHER_SC(name);					\
-	noinline __nocfi unsigned int bpf_dispatcher_##name##_func(	\
+	noinline __bpfcall unsigned int bpf_dispatcher_##name##_func(	\
 		const void *ctx,					\
 		const struct bpf_insn *insnsi,				\
 		bpf_func_t bpf_func)					\
@@ -1523,6 +1529,9 @@ struct bpf_prog_aux {
 	struct bpf_kfunc_desc_tab *kfunc_tab;
 	struct bpf_kfunc_btf_tab *kfunc_btf_tab;
 	u32 size_poke_tab;
+#ifdef CONFIG_FINEIBT
+	struct bpf_ksym ksym_prefix;
+#endif
 	struct bpf_ksym ksym;
 	const struct bpf_prog_ops *ops;
 	struct bpf_map **used_maps;
