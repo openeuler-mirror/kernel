10afa15cedc9b50e5dbaaf4f404b5fd18e6897d4
Andrii Nakryiko <andrii@kernel.org>
2026-04-16 12:43:24 +0000

    selftests/bpf: test passing iterator to a kfunc
    mainline inclusion
    from mainline-v6.12-rc1
    commit b0cd726f9a8279b33faa5b4b0011df14f331fb33
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8335
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=b0cd726f9a8279b33faa5b4b0011df14f331fb33
    
    (cherry-pick from OLK-6.6 commit 6eca157e3849)
    
    ----------------------------------------------------------------------
    
    Define BPF iterator "getter" kfunc, which accepts iterator pointer as
    one of the arguments. Make sure that argument passed doesn't have to be
    the very first argument (unlike new-next-destroy combo).
    
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Link: https://lore.kernel.org/r/20240808232230.2848712-4-andrii@kernel.org
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Conflicts:
    	tools/testing/selftests/bpf/bpf_testmod/bpf_testmod.c
    [Fix conflicts caused by context diff.]
    Signed-off-by: Luo Gengkun <luogengkun2@huawei.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- tools/testing/selftests/bpf/bpf_testmod/bpf_testmod.c
+++ tools/testing/selftests/bpf/bpf_testmod/bpf_testmod.c
@@ -112,13 +112,12 @@ bpf_testmod_test_mod_kfunc(int i)
 
 __bpf_kfunc int bpf_iter_testmod_seq_new(struct bpf_iter_testmod_seq *it, s64 value, int cnt)
 {
-	if (cnt < 0) {
-		it->cnt = 0;
+	it->cnt = cnt;
+
+	if (cnt < 0)
 		return -EINVAL;
-	}
 
 	it->value = value;
-	it->cnt = cnt;
 
 	return 0;
 }
@@ -131,6 +130,14 @@ __bpf_kfunc s64 *bpf_iter_testmod_seq_next(struct bpf_iter_testmod_seq* it)
 	it->cnt--;
 
 	return &it->value;
+}
+
+__bpf_kfunc s64 bpf_iter_testmod_seq_value(int val, struct bpf_iter_testmod_seq* it__iter)
+{
+	if (it__iter->cnt < 0)
+		return 0;
+
+	return val + it__iter->value;
 }
 
 __bpf_kfunc void bpf_iter_testmod_seq_destroy(struct bpf_iter_testmod_seq *it)
@@ -376,6 +383,7 @@ BTF_ID_FLAGS(func, bpf_iter_testmod_seq_next, KF_ITER_NEXT | KF_RET_NULL)
 BTF_ID_FLAGS(func, bpf_iter_testmod_seq_destroy, KF_ITER_DESTROY)
 BTF_ID_FLAGS(func, bpf_testmod_ctx_create, KF_ACQUIRE | KF_RET_NULL)
 BTF_ID_FLAGS(func, bpf_testmod_ctx_release, KF_RELEASE)
+BTF_ID_FLAGS(func, bpf_iter_testmod_seq_value)
 BTF_KFUNCS_END(bpf_testmod_common_kfunc_ids)
 
 BTF_ID_LIST(bpf_testmod_dtor_ids)
