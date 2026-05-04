7bbf4782700fa443b0be3093518d5ea1ff4176c1
Manivannan Sadhasivam <manivannan.sadhasivam@oss.qualcomm.com>
2026-05-04 04:22:33 -0400

    net: qrtr: ns: Limit the total number of nodes
    stable inclusion
    from stable-v6.6.140
    commit 4c46413661431aa60fb134cd4ecdf8beaa39f824
    category: bugfix
    bugzilla: https://atomgit.com/src-openeuler/kernel/issues/15254
    CVE: CVE-2026-46003
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=4c46413661431aa60fb134cd4ecdf8beaa39f824
    
    --------------------------------
    
    [ Upstream commit 27d5e84e810b0849d08b9aec68e48570461ce313 ]
    
    Currently, the nameserver doesn't limit the number of nodes it handles.
    This can be an attack vector if a malicious client starts registering
    random nodes, leading to memory exhaustion.
    
    Hence, limit the maximum number of nodes to 64. Note that, limit of 64 is
    chosen based on the current platform requirements. If requirement changes
    in the future, this limit can be increased.
    
    Cc: stable@vger.kernel.org
    Fixes: 0c2204a4ad71 ("net: qrtr: Migrate nameservice to kernel from userspace")
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@oss.qualcomm.com>
    Link: https://patch.msgid.link/20260409-qrtr-fix-v3-4-00a8a5ff2b51@oss.qualcomm.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    [ dropped comment/define changes for missing QRTR_NS_MAX_SERVERS/LOOKUPS prereqs and kept plain kzalloc instead of kzalloc_obj ]
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Conflicts:
    	net/qrtr/ns.c
    [Simple context conflicts, not affect this patch.]
    Signed-off-by: Wang Yan <wangyan01@kylinos.cn>

--- net/qrtr/ns.c
+++ net/qrtr/ns.c
@@ -80,6 +80,13 @@ struct qrtr_node {
  */
 #define QRTR_NS_MAX_LOOKUPS 64
 
+/* Max nodes limit is chosen based on the current platform requirements.
+ * If the requirement changes in the future, this value can be increased.
+ */
+#define QRTR_NS_MAX_NODES   64
+
+static u8 node_count;
+
 static struct qrtr_node *node_get(unsigned int node_id)
 {
 	struct qrtr_node *node;
@@ -87,6 +94,11 @@ static struct qrtr_node *node_get(unsigned int node_id)
 	node = radix_tree_lookup(&nodes, node_id);
 	if (node)
 		return node;
+
+	if (node_count >= QRTR_NS_MAX_NODES) {
+		pr_err_ratelimited("QRTR clients exceed max node limit!\n");
+		return NULL;
+	}
 
 	/* If node didn't exist, allocate and insert it to the tree */
 	node = kzalloc(sizeof(*node), GFP_KERNEL);
@@ -100,6 +112,8 @@ static struct qrtr_node *node_get(unsigned int node_id)
 		kfree(node);
 		return NULL;
 	}
+
+	node_count++;
 
 	return node;
 }
@@ -404,6 +418,7 @@ static int ctrl_cmd_bye(struct sockaddr_qrtr *from)
 delete_node:
 	xa_erase(&nodes, from->sq_node);
 	kfree(node);
+	node_count--;
 
 	return ret;
 }
