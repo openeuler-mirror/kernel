d1120da5ec2c3432fec06e1325d78da40f014c07
Xuanyu Pu <puxuanyu@huawei.com>
2026-05-11 17:33:12 +0800

    ub: ubase: Add DTU capability bit printing
    drivers inclusion
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/9157
    CVE: NA
    
    (cherry-pick from OLK-6.6 commit e3c2930e4291)
    
    --------------------------------------------------------------------------
    
    The DTU capability bit is not printed in the current ubase.
    This patch implements the following functions:
    1. Add DTU capability bit printing in debugfs;
    2. The order of printing existing capability bits is adjusted
    to be consistent with the bit order of the capability bits.
    
    Fixes: f0c538c05de8 ("ub: ubase: support DTU feature")
    Signed-off-by: Xuanyu Pu <puxuanyu@huawei.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- drivers/ub/ubase/debugfs/ubase_debugfs.c
+++ drivers/ub/ubase/debugfs/ubase_debugfs.c
@@ -60,9 +60,10 @@ static void ubase_dbg_dump_caps_bits(struct seq_file *s, struct ubase_dev *udev)
 		PRINT_CAP(ip_over_urma_utp, ubase_ip_over_urma_utp_supported);
 	PRINT_CAP(activate_proxy, ubase_activate_proxy_supported);
 	PRINT_CAP(utp, ubase_utp_supported);
-	PRINT_CAP(pmu_irq, ubase_pmu_irq_supported);
+	PRINT_CAP(dtu, ubase_dev_dtu_supported);
 	PRINT_CAP(usc, ubase_dev_usc_supported);
 	PRINT_CAP(ucp, ubase_ucp_supported);
+	PRINT_CAP(pmu_irq, ubase_pmu_irq_supported);
 }
 
 static void ubase_dbg_dump_caps_info(struct seq_file *s, struct ubase_dev *udev)
