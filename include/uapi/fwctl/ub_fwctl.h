32169f4dbae564af9eb660d3a915a3afa77f04f4
Jiaqi Cheng <chengjiaqi3@huawei.com>
2026-05-12 14:09:29 +0800

    ub: ub_fwctl: supports the configuration and query of packet statistics on the UBOE interface based on the NL-layer VL.
    driver inclusion
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/9149
    CVE: NA
    
    --------------------------------------------------------------------------
    
    Supports the configuration and query of packet statistics on the UBOE
    interface based on the NL-layer VL.
    
    Fixes: f2923dd8d91e ("ub: ub_fwctl: Modify TP/TA/SCC register query process.")
    
    Signed-off-by: Jiaqi Cheng <chengjiaqi3@huawei.com>

--- include/uapi/fwctl/ub_fwctl.h
+++ include/uapi/fwctl/ub_fwctl.h
@@ -74,6 +74,14 @@ enum ub_fwctl_cmdrpc_type {
 	 * @UTOOL_CMD_QUERY_NL_SSU_P2P: Query ssu_p2p queue non-empty dfx statistics
 	 */
 	UTOOL_CMD_QUERY_NL_SSU_P2P = 0x0007,
+	/**
+	 * @UTOOL_CMD_CONF_NL_SSU_VL_PKT: Config NL layer ssu_vl_pkt related registers
+	 */
+	UTOOL_CMD_CONF_NL_SSU_VL_PKT = 0x0008,
+	/**
+	 * @UTOOL_CMD_QUERY_NL_SSU_VL_PKT: Query NL layer ssu_vl_pkt related registers
+	 */
+	UTOOL_CMD_QUERY_NL_SSU_VL_PKT = 0x0009,
 
 	/**
 	 * @UTOOL_CMD_QUERY_TP: Query all registers at the TP layer
