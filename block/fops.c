5fd465f8a9a23c2b920279e9aaec4fc99253cc63
John Garry <john.g.garry@oracle.com>
2026-05-13 15:10:37 +0800

    block: Don't allow an atomic write be truncated in blkdev_write_iter()
    mainline inclusion
    from mainline-v6.12-rc4
    commit 2cbd51f1f8739fd2fdf4bae1386bcf75ce0176ba
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8862
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=2cbd51f1f8739fd2fdf4bae1386bcf75ce0176ba
    
    (cherry-pick from OLK-6.6 commit 155b5ba8f0ed)
    
    --------------------------------
    
    A write which goes past the end of the bdev in blkdev_write_iter() will
    be truncated. Truncating cannot tolerated for an atomic write, so error
    that condition.
    
    Fixes: caf336f81b3a ("block: Add fops atomic write support")
    Signed-off-by: John Garry <john.g.garry@oracle.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Link: https://lore.kernel.org/r/20241127092318.632790-1-john.g.garry@oracle.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Long Li <leo.lilong@huawei.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- block/fops.c
+++ block/fops.c
@@ -663,6 +663,7 @@ static ssize_t blkdev_write_iter(struct kiocb *iocb, struct iov_iter *from)
 	struct file *file = iocb->ki_filp;
 	struct block_device *bdev = I_BDEV(file->f_mapping->host);
 	struct inode *bd_inode = bdev->bd_inode;
+	bool atomic = iocb->ki_flags & IOCB_ATOMIC;
 	loff_t size = bdev_nr_bytes(bdev);
 	size_t shorted = 0;
 	ssize_t ret;
@@ -682,7 +683,7 @@ static ssize_t blkdev_write_iter(struct kiocb *iocb, struct iov_iter *from)
 	if ((iocb->ki_flags & (IOCB_NOWAIT | IOCB_DIRECT)) == IOCB_NOWAIT)
 		return -EOPNOTSUPP;
 
-	if (iocb->ki_flags & IOCB_ATOMIC) {
+	if (atomic) {
 		ret = generic_atomic_write_valid(iocb, from);
 		if (ret)
 			return ret;
@@ -690,6 +691,8 @@ static ssize_t blkdev_write_iter(struct kiocb *iocb, struct iov_iter *from)
 
 	size -= iocb->ki_pos;
 	if (iov_iter_count(from) > size) {
+		if (atomic)
+			return -EINVAL;
 		shorted = iov_iter_count(from) - size;
 		iov_iter_truncate(from, size);
 	}
