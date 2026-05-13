804263750484a6aabbf6379bc2cc0bab68907e0e
Prasad Singamsetty <prasad.singamsetty@oracle.com>
2026-05-13 15:10:18 +0800

    fs: Add initial atomic write support info to statx
    mainline inclusion
    from mainline-v6.10-rc3
    commit 0f9ca80fa4f9670ba09721e4e36b8baf086a500c
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8862
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=0f9ca80fa4f9670ba09721e4e36b8baf086a500c
    
    --------------------------------
    
    Extend statx system call to return additional info for atomic write support
    support for a file.
    
    Helper function generic_fill_statx_atomic_writes() can be used by FSes to
    fill in the relevant statx fields. For now atomic_write_segments_max will
    always be 1, otherwise some rules would need to be imposed on iovec length
    and alignment, which we don't want now.
    
    Signed-off-by: Prasad Singamsetty <prasad.singamsetty@oracle.com>
    jpg: relocate bdev support to another patch
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: John Garry <john.g.garry@oracle.com>
    Acked-by: Darrick J. Wong <djwong@kernel.org>
    Link: https://lore.kernel.org/r/20240620125359.2684798-5-john.g.garry@oracle.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Conflicts:
    	include/linux/stat.h
    	include/uapi/linux/stat.h
    	fs/stat.c
    Signed-off-by: Long Li <leo.lilong@huawei.com>

--- include/linux/stat.h
+++ include/linux/stat.h
@@ -54,6 +54,9 @@ struct kstat {
 	u32		dio_mem_align;
 	u32		dio_offset_align;
 	u64		change_cookie;
+	u32		atomic_write_unit_min;
+	u32		atomic_write_unit_max;
+	u32		atomic_write_segments_max;
 
 	KABI_RESERVE(1)
 	KABI_RESERVE(2)
