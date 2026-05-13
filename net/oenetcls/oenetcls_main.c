1ba3409dc458523d8519957825ae1dc5d82c1b4a
Yue Haibing <yuehaibing@huawei.com>
2026-05-13 18:23:51 +0800

    net/oenetcls: Limit rxqs against combined_channels
    hulk inclusion
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/9102
    
    (cherry-pick from OLK-6.6 commit fd76f98c114f)
    
    --------------------------------
    
    Only use NIC current hardware channels for flow steer.
    
    Signed-off-by: Yue Haibing <yuehaibing@huawei.com>
    Signed-off-by: Li Xiasong <lixiasong1@huawei.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- net/oenetcls/oenetcls_main.c
+++ net/oenetcls/oenetcls_main.c
@@ -255,6 +255,19 @@ err_out:
 	return ret;
 }
 
+static noinline_for_stack int ethtool_get_channels(struct net_device *dev,
+						   void *useraddr)
+{
+	struct ethtool_channels channels = { .cmd = ETHTOOL_GCHANNELS };
+
+	if (!dev->ethtool_ops->get_channels)
+		return -EOPNOTSUPP;
+
+	dev->ethtool_ops->get_channels(dev, &channels);
+	memcpy_r(useraddr, &channels, sizeof(channels));
+	return 0;
+}
+
 static int ethtool_get_value(struct net_device *dev, char *useraddr,
 			     u32 cmd, u32 (*actor)(struct net_device *))
 {
@@ -308,6 +321,7 @@ static int dev_ethtool_kern(struct net *net, struct ifreq *ifr)
 	case ETHTOOL_GRXCLSRLCNT:
 	case ETHTOOL_GRXCLSRULE:
 	case ETHTOOL_GRXCLSRLALL:
+	case ETHTOOL_GCHANNELS:
 		break;
 	default:
 		if (check_cap && !ns_capable(net->user_ns, CAP_NET_ADMIN))
@@ -340,6 +354,9 @@ static int dev_ethtool_kern(struct net *net, struct ifreq *ifr)
 	case ETHTOOL_SRXCLSRLDEL:
 	case ETHTOOL_SRXCLSRLINS:
 		rc = ethtool_set_rxnfc(dev, ethcmd, useraddr);
+		break;
+	case ETHTOOL_GCHANNELS:
+		rc = ethtool_get_channels(dev, useraddr);
 		break;
 	default:
 		rc = -EOPNOTSUPP;
@@ -403,8 +420,20 @@ static bool check_irq_name(const char *irq_name, struct oecls_netdev_info *oecls
 static void get_netdev_queue_info(struct oecls_netdev_info *oecls_dev)
 {
 	struct oecls_netdev_queue_info *rxq_info;
+	struct ethtool_channels echannels = {0};
+	int irq, cpu, ret, combined_channels;
+	struct cmd_context ctx = {0};
 	struct irq_desc *desc;
-	int irq, cpu;
+
+	strscpy(ctx.netdev, oecls_dev->dev_name, IFNAMSIZ);
+	echannels.cmd = ETHTOOL_GCHANNELS;
+	ret = send_ethtool_ioctl(&ctx, &echannels);
+	if (ret) {
+		oecls_error("get %s channels fail ret:%d\n", oecls_dev->dev_name, ret);
+		return;
+	}
+
+	combined_channels = echannels.combined_count;
 
 	for_each_irq_desc(irq, desc) {
 		if (!desc->action)
@@ -415,6 +444,10 @@ static void get_netdev_queue_info(struct oecls_netdev_info *oecls_dev)
 			continue;
 		if (oecls_dev->rxq_num >= OECLS_MAX_RXQ_NUM_PER_DEV)
 			break;
+		if (oecls_dev->rxq_num > combined_channels - 1)
+			break;
+		oecls_debug("rxq_num:%d channels:%d\n", oecls_dev->rxq_num, combined_channels);
+
 		rxq_info = &oecls_dev->rxq[oecls_dev->rxq_num++];
 		rxq_info->irq = irq;
 		cpu = cpumask_first(irq_data_get_effective_affinity_mask(&desc->irq_data));
