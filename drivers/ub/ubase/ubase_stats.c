OERV-RVCI/rvck-olk

commit 549bcad76eb5c889e7bb3bb1799c05dc08d76d03

ub: ubase: support to query ub dl pkt stats and real-time peak bandwidth
drivers inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8514
CVE: NA

----------------------------------------------------------

This patch does three things:
1. Supports querying filt statistics for ub dl packets;
2. Supports querying real-time peak bandwidth;
3. Changes the real-time traffic bandwidth period in EVB
scenarios to 100ms to reduce the impact of traffic jitter on the
iperf tool.

Fixes: 9c1792c8e559 ("ub: ubase: adds the function to get ub port stats for unic and other aux driver")
Signed-off-by: Zihao Sheng <shengzihao1@huawei.com>

drivers/ub/ubase/ubase_stats.c
@@ -7,6 +7,29 @@
 #include "ubase_cmd.h"
 #include "ubase_stats.h"
 
+static int ubase_check_port_bitmap(struct ubase_dev *udev, u64 *port_bitmap,
+				   u32 data_size)
+{
+	unsigned long logic_port_bitmap = udev->caps.dev_caps.logic_port_bitmap;
+
+	if (*port_bitmap) {
+		if (data_size < bitmap_weight((unsigned long *)port_bitmap,
+					      UBASE_MAX_PORT_NUM) ||
+		    !bitmap_subset((unsigned long *)port_bitmap,
+				   &logic_port_bitmap, UBASE_MAX_PORT_NUM))
+			return -EINVAL;
+	} else {
+		if (data_size != UBASE_MAX_PORT_NUM ||
+		    data_size < bitmap_weight((unsigned long *)&logic_port_bitmap,
+					     UBASE_MAX_PORT_NUM))
+			return -EINVAL;
+
+		*port_bitmap = logic_port_bitmap;
+	}
+
+	return 0;
+}
+
 static int ubase_update_mac_stats(struct ubase_dev *udev, u16 port_id, u64 *data,
 				  u16 size, bool is_accumulate)
 {
@@ -105,6 +128,106 @@ int ubase_get_ub_port_stats(struct auxiliary_device *adev, u16 port_id,
 				      sizeof(*data) / sizeof(u64), false);
 }
 EXPORT_SYMBOL(ubase_get_ub_port_stats);
+
+static int ubase_query_dl_pkt_stats(struct ubase_dev *udev, u16 port_id,
+				    struct ubase_query_dl_pkt_stats_cmd *resp)
+{
+	struct ubase_query_dl_pkt_stats_cmd req = {0};
+	struct ubase_cmd_buf in, out;
+	int ret;
+
+	req.logic_port_id = cpu_to_le16(port_id);
+
+	__ubase_fill_inout_buf(&in, UBASE_OPC_QUERY_UB_DL_PKT_STATS, true,
+			       sizeof(req), &req);
+	__ubase_fill_inout_buf(&out, UBASE_OPC_QUERY_UB_DL_PKT_STATS, false,
+			       sizeof(*resp), resp);
+
+	ret = __ubase_cmd_send_inout(udev, &in, &out);
+	if (ret && ret != -EPERM)
+
+
+		ubase_err(udev, "failed to query ub dl pkt stats, ret = %d.\n",
+			  ret);
+
+	return ret == -EPERM ? -EOPNOTSUPP : ret;
+}
+
+static int __ubase_get_ub_dl_pkt_stats(struct ubase_dev *udev, u64 port_bitmap,
+				       struct ubase_ub_dl_pkt_stats_result *data,
+				       u32 data_size)
+{
+#define UBASE_FLIT_TO_BYTE	20
+
+	struct ubase_query_dl_pkt_stats_cmd resp;
+	unsigned long port_num, k;
+	u64 pkt_filts;
+	int ret;
+	u16 i;
+
+	if (!test_bit(UBASE_STATE_INITED_B, &udev->state_bits) ||
+	    test_bit(UBASE_STATE_RST_HANDLING_B, &udev->state_bits))
+		return -EBUSY;
+
+	ret = ubase_check_port_bitmap(udev, &port_bitmap, data_size);
+	if (ret)
+		return ret;
+
+	port_num = bitmap_weight((unsigned long *)&port_bitmap,
+				 UBASE_MAX_PORT_NUM);
+
+	for (i = 0, k = 0; i < UBASE_MAX_PORT_NUM && k < port_num; i++) {
+		if (!test_bit(i, (unsigned long *)&port_bitmap))
+			continue;
+
+		memset(&resp, 0, sizeof(resp));
+		ret = ubase_query_dl_pkt_stats(udev, i, &resp);
+		if (ret)
+			return ret;
+
+		pkt_filts = ubase_size_gen(le32_to_cpu(resp.tx_flit_num_h),
+					   le32_to_cpu(resp.tx_flit_num_l));
+		data[k].tx_pkt_bytes = pkt_filts * UBASE_FLIT_TO_BYTE;
+		pkt_filts = ubase_size_gen(le32_to_cpu(resp.rx_flit_num_h),
+					   le32_to_cpu(resp.rx_flit_num_l));
+		data[k].rx_pkt_bytes = pkt_filts * UBASE_FLIT_TO_BYTE;
+		data[k].port_id = i;
+		data[k].valid = 1;
+
+		k++;
+	}
+
+	return ret;
+}
+
+/**
+ * ubase_get_ub_dl_pkt_stats() - get ub dl pkt stats
+ * @adev: auxiliary device
+ * @port_bitmap: port bitmap
+ * @data: ub date dl pkt stats
+ * @data_size: valid size of data
+ *
+ * The function is used to get ub dl pkt stats.
+ *
+ * Context: Process context. Takes and releases <lock>, BH-safe. Sleep.
+ * Return: 0 on success, negative error code otherwise
+ */
+int ubase_get_ub_dl_pkt_stats(struct auxiliary_device *adev, u64 port_bitmap,
+			      struct ubase_ub_dl_pkt_stats_result *data,
+			      u32 data_size)
+{
+	struct ubase_dev *udev;
+
+	if (!adev || !data || !data_size)
+		return -EINVAL;
+
+	udev = __ubase_get_udev_by_adev(adev);
+	if (!(ubase_dev_ubl_supported(udev) || ubase_dev_fwctl_supported(udev)))
+		return -EOPNOTSUPP;
+
+	return __ubase_get_ub_dl_pkt_stats(udev, port_bitmap, data, data_size);
+}
+EXPORT_SYMBOL(ubase_get_ub_dl_pkt_stats);
 
 int __ubase_get_eth_port_stats(struct ubase_dev *udev,
 			       struct ubase_eth_mac_stats *data)

drivers/ub/ubase/debugfs/ubase_debugfs.c
@@ -312,6 +312,8 @@ static void ubase_dbg_fill_single_port(struct seq_file *s,
 	seq_printf(s, "\tport_id: %u\n", stats->port_id);
 	seq_printf(s, "\tport_tx_bw: %u(kbps)\n", le32_to_cpu(stats->tx_port_bw));
 	seq_printf(s, "\tport_rx_bw: %u(kbps)\n", le32_to_cpu(stats->rx_port_bw));
+	seq_printf(s, "\tport_tx_max_bw: %u(kbps)\n", le32_to_cpu(stats->tx_max_port_bw));
+	seq_printf(s, "\tport_rx_max_bw: %u(kbps)\n", le32_to_cpu(stats->rx_max_port_bw));
 	seq_puts(s, "\tvl   tx_bw(kbps)          rx_bw(kbps)\n");
 
 	for (i = 0; i < UBASE_STATS_MAX_VL_NUM; i++) {
@@ -326,7 +328,7 @@ static void ubase_dbg_fill_single_port(struct seq_file *s,
 static int ubase_dbg_dump_perf_stats_ub(struct seq_file *s,
 					struct ubase_dev *udev)
 {
-#define UBASE_UB_PERF_STATS_PERIOD	10
+#define UBASE_UB_PERF_STATS_PERIOD	100
 #define UBASE_QUERY_ALL_BITMAP	0
 
 	struct ubase_perf_stats_result *stats;

drivers/ub/ubase/ubase.h
@@ -73,5 +73,6 @@ static inline u32 ubase_read_reg(u8 __iomem *base, u32 reg)
 	ubase_read_reg((a)->io_base.addr, reg)
 
 #define ubase_addr_gen(addr_h, addr_l) ((u64)(addr_h) << 32 | (addr_l))
+#define ubase_size_gen(size_h, size_l) ((u64)(size_h) << 32 | (size_l))
 
 #endif

drivers/ub/ubase/ubase_cmd.h
@@ -140,7 +140,13 @@ struct ubase_stop_perf_stats_cmd {
 	__le32	rx_port_bw;
 	__le32	tx_vl_bw[UBASE_STATS_MAX_VL_NUM];
 	__le32	rx_vl_bw[UBASE_STATS_MAX_VL_NUM];
-	u8	rsv1[8];
+	u32	tx_max_port_bw;
+	u32	rx_max_port_bw;
+
+	KABI_RESERVE(1)
+	KABI_RESERVE(2)
+	KABI_RESERVE(3)
+	KABI_RESERVE(4)
 };
 
 struct ubase_cfg_ets_vl_sch_cmd {
@@ -148,6 +154,16 @@ struct ubase_cfg_ets_vl_sch_cmd {
 	u8 rsvd[2];
 	u8 vl_bw[UBASE_MAX_VL_NUM];
 	__le32 port_bitmap;
+};
+
+struct ubase_query_dl_pkt_stats_cmd {
+	__le16	logic_port_id;
+	u8	rsvd[2];
+	__le32	tx_flit_num_l;
+	__le32	tx_flit_num_h;
+	__le32	rx_flit_num_l;
+	__le32	rx_flit_num_h;
+	u8	rsvd1[4];
 };
 
 struct ubase_cfg_tm_vl_sch_cmd {

drivers/ub/ubase/ubase_hw.c
@@ -1025,6 +1025,8 @@ int __ubase_perf_stats(struct ubase_dev *udev, u64 port_bitmap, u32 period,
 
 		data[k].tx_port_bw = le32_to_cpu(resp.tx_port_bw);
 		data[k].rx_port_bw = le32_to_cpu(resp.rx_port_bw);
+		data[k].tx_max_port_bw = le32_to_cpu(resp.tx_max_port_bw);
+		data[k].rx_max_port_bw = le32_to_cpu(resp.rx_max_port_bw);
 		data[k].port_id = i;
 		data[k].valid = 1;
 

include/ub/ubase/ubase_comm_cmd.h
@@ -91,6 +91,7 @@ enum ubase_opcode_type {
 	UBASE_OPC_START_PERF_STATS	= 0x5103,
 	UBASE_OPC_STOP_PERF_STATS	= 0x5104,
 	UBASE_OPC_QUERY_UB_PORT_BITMAP	= 0x5105,
+	UBASE_OPC_QUERY_UB_DL_PKT_STATS	= 0x5106,
 
 	/* PHY commands */
 	UBASE_OPC_CONFIG_SPEED_DUP	= 0x6100,

include/ub/ubase/ubase_comm_stats.h
@@ -267,6 +267,32 @@ struct ubase_perf_stats_result {
 	KABI_RESERVE(4)
 };
 
+/**
+ * struct ubase_ub_dl_pkt_stats_result - ub dl packet bytes statistics results
+ * @valid: data valid flag, 0-invalid, 1-valid
+ * @resv0: reserved bits
+ * @port_id: port id
+ * @resv1: reserved bits
+ * @tx_pkt_bytes: tx dl packet bytes
+ * @rx_pkt_bytes: rx dl packet bytes
+ */
+struct ubase_ub_dl_pkt_stats_result {
+	u8	valid : 1;
+	u8	resv0 : 7;
+	u8	port_id;
+	u8	resv1[2];
+
+	u64	tx_pkt_bytes;
+	u64	rx_pkt_bytes;
+
+	KABI_RESERVE(1)
+	KABI_RESERVE(2)
+	KABI_RESERVE(3)
+	KABI_RESERVE(4)
+	KABI_RESERVE(5)
+	KABI_RESERVE(6)
+};
+
 void ubase_clear_eth_port_stats(struct auxiliary_device *adev);
 int ubase_get_ub_port_stats(struct auxiliary_device *adev, u16 port_id,
 			    struct ubase_ub_dl_stats *data);
@@ -274,5 +300,8 @@ int ubase_get_eth_port_stats(struct auxiliary_device *adev,
 			     struct ubase_eth_mac_stats *data);
 int ubase_perf_stats(struct auxiliary_device *adev, u64 port_bitmap, u32 period,
 		     struct ubase_perf_stats_result *data, u32 data_size);
+int ubase_get_ub_dl_pkt_stats(struct auxiliary_device *adev, u64 port_bitmap,
+			      struct ubase_ub_dl_pkt_stats_result *data,
+			      u32 data_size);
 
 #endif /* _UBASE_COMM_STATS_H */

openeuler-mirror/kernel

commit 32cb77691072bff9038dfdaf1e0f17b98aeeb02b

ub: ubase: Fix the problem of frequent log printing
drivers inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/9157
CVE: NA

--------------------------------------------------------------------------

Avoid frequent printing within a short period of time and modify
the error logs of the ubase_query_dl_pkt_stats interface to limit
the printing frequency.

Fixes: 549bcad76eb5 ("ub: ubase: support to query ub dl pkt stats and real-time peak bandwidth")
Signed-off-by: Guangwei Zhang <zhangguangwei6@huawei.com>

drivers/ub/ubase/ubase_stats.c
@@ -145,8 +145,9 @@ static int ubase_query_dl_pkt_stats(struct ubase_dev *udev, u16 port_id,
 
 	ret = __ubase_cmd_send_inout(udev, &in, &out);
 	if (ret && ret != -EPERM)
-		ubase_err(udev, "failed to query ub dl pkt stats, ret = %d.\n",
-			  ret);
+		dev_err_ratelimited(udev->dev,
+				    "failed to query ub dl pkt stats, ret = %d.\n",
+				    ret);
 
 	return ret == -EPERM ? -EOPNOTSUPP : ret;
 }