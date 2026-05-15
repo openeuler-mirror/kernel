openeuler-mirror/kernel

commit 1d51cbd118e3bdc2aa061a5d809beaac7c2544ee

sched: Add sched_class->switching_to() and expose check_class_changing/changed()
mainline inclusion
from mainline-v6.12-rc1
commit d8c7bc2e209170ff78b88b85258546aa6e337af9
category: feature
bugzilla: https://atomgit.com/openeuler/kernel/issues/8335
CVE: NA

Reference: https://web.git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=d8c7bc2e209170ff78b88b85258546aa6e337af9

--------------------------------

When a task switches to a new sched_class, the prev and new classes are
notified through ->switched_from() and ->switched_to(), respectively, after
the switching is done.

A new BPF extensible sched_class will have callbacks that allow the BPF
scheduler to keep track of relevant task states (like priority and cpumask).
Those callbacks aren't called while a task is on a different sched_class.
When a task comes back, we wanna tell the BPF progs the up-to-date state
before the task gets enqueued, so we need a hook which is called before the
switching is committed.

This patch adds ->switching_to() which is called during sched_class switch
through check_class_changing() before the task is restored. Also, this patch
exposes check_class_changing/changed() in kernel/sched/sched.h. They will be
used by the new BPF extensible sched_class to implement implicit sched_class
switching which is used e.g. when falling back to CFS when the BPF scheduler
fails or unloads.

This is a prep patch and doesn't cause any behavior changes. The new
operation and exposed functions aren't used yet.

v3: Refreshed on top of tip:sched/core.

v2: Improve patch description w/ details on planned use.

Signed-off-by: Tejun Heo <tj@kernel.org>
Reviewed-by: David Vernet <dvernet@meta.com>
Acked-by: Josh Don <joshdon@google.com>
Acked-by: Hao Luo <haoluo@google.com>
Acked-by: Barret Rhoden <brho@google.com>
Conflicts:
	kernel/sched/sched.h
	kernel/sched/syscalls.c
[Context difference, only git am reports conflicts, but git cherry-pick
works good.]

kernel/sched/sched.h
@@ -2493,6 +2493,7 @@ struct sched_class {
 	 * cannot assume the switched_from/switched_to pair is serialized by
 	 * rq->lock. They are however serialized by p->pi_lock.
 	 */
+	void (*switching_to) (struct rq *this_rq, struct task_struct *task);
 	void (*switched_from)(struct rq *this_rq, struct task_struct *task);
 	void (*switched_to)  (struct rq *this_rq, struct task_struct *task);
 	void (*prio_changed) (struct rq *this_rq, struct task_struct *task,
@@ -3877,6 +3878,8 @@ extern void set_load_weight(struct task_struct *p, bool update_load);
 extern void enqueue_task(struct rq *rq, struct task_struct *p, int flags);
 extern void dequeue_task(struct rq *rq, struct task_struct *p, int flags);
 
+extern void check_class_changing(struct rq *rq, struct task_struct *p,
+				 const struct sched_class *prev_class);
 extern void check_class_changed(struct rq *rq, struct task_struct *p,
 				const struct sched_class *prev_class,
 				int oldprio);

kernel/sched/core.c
@@ -2042,6 +2042,17 @@ inline int task_curr(const struct task_struct *p)
 }
 
 /*
+ * ->switching_to() is called with the pi_lock and rq_lock held and must not
+ * mess with locking.
+ */
+void check_class_changing(struct rq *rq, struct task_struct *p,
+			  const struct sched_class *prev_class)
+{
+	if (prev_class != p->sched_class && p->sched_class->switching_to)
+		p->sched_class->switching_to(rq, p);
+}
+
+/*
  * switched_from, switched_to and prio_changed must _NOT_ drop rq->lock,
  * use the balance_callback list if you want balancing.
  *
@@ -7024,6 +7035,7 @@ void rt_mutex_setprio(struct task_struct *p, struct task_struct *pi_task)
 	}
 
 	__setscheduler_prio(p, prio);
+	check_class_changing(rq, p, prev_class);
 
 	if (queued)
 		enqueue_task(rq, p, queue_flag);

kernel/sched/syscalls.c
@@ -820,6 +820,7 @@ change:
 		__setscheduler_dl_pi(newprio, policy, p, &queue_flags);
 	}
 	__setscheduler_uclamp(p, attr);
+	check_class_changing(rq, p, prev_class);
 
 	if (queued) {
 		/*

commit 42c763bdc6dc4152f1a8351d1442ccf76866f69b

sched: Fix kabi for switching_to in struct sched_class
hulk inclusion
category: feature
bugzilla: https://atomgit.com/openeuler/kernel/issues/8335
CVE: NA

--------------------------------

sched: Fix kabi for the method pointer switching_to in struct
sched_class.

Fixes: 1d51cbd118e3 ("sched: Add sched_class->switching_to() and expose check_class_changing/changed()")
Signed-off-by: Zicheng Qu <quzicheng@huawei.com>

kernel/sched/sched.h
@@ -2493,7 +2493,6 @@ struct sched_class {
 	 * cannot assume the switched_from/switched_to pair is serialized by
 	 * rq->lock. They are however serialized by p->pi_lock.
 	 */
-	void (*switching_to) (struct rq *this_rq, struct task_struct *task);
 	void (*switched_from)(struct rq *this_rq, struct task_struct *task);
 	void (*switched_to)  (struct rq *this_rq, struct task_struct *task);
 	void (*prio_changed) (struct rq *this_rq, struct task_struct *task,
@@ -2513,7 +2512,7 @@ struct sched_class {
 #endif
 	KABI_USE(1, void (*reweight_task)(struct rq *this_rq,
 		struct task_struct *task, const struct load_weight *lw))
-	KABI_RESERVE(2)
+	KABI_USE(2, void (*switching_to) (struct rq *this_rq, struct task_struct *task))
 };
 
 static inline void put_prev_task(struct rq *rq, struct task_struct *prev)