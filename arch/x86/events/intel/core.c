03383f467408da9005429d5b269eb6f39a9f62d8
Dapeng Mi <dapeng1.mi@linux.intel.com>
2026-05-15 14:11:43 +0800

    perf/x86/intel: Update event constraints and cache_extra_regsfor CWF
    mainline inclusion
    from mainline-v7.2-rc1
    commit 66cc29745f2f5815482587bb9fbc1e8a3e6fcf00
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8410
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=66cc29745f2f5815482587bb9fbc1e8a3e6fcf00
    
    (cherry-pick from OLK-6.6 commit 2d89a9c8ac7e)
    
    ---------------------------
    
    Update perf hard-coded event constraints and cache_extra_regs[] for
    Clearwater Forest according to the latest CWF perfmon events (V1.02).
    
    An important difference is that CWF introduce new extra register values
    for the L3 cache OCR events, so define darkmont specific
    dkt_hw_cache_extra_regs[] array.
    
    CWF perfmon events:
    https://github.com/intel/perfmon/blob/main/CWF/events/clearwaterforest_core.json
    
    Signed-off-by: Dapeng Mi <dapeng1.mi@linux.intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://patch.msgid.link/20260515061143.338553-12-dapeng1.mi@linux.intel.com
    
     Conflicts:
    	arch/x86/events/intel/core.c
    [jz: resolve context conflicts]
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- arch/x86/events/intel/core.c
+++ arch/x86/events/intel/core.c
@@ -2539,6 +2539,22 @@ static __initconst const u64 skt_hw_cache_extra_regs
 	},
 };
 
+static __initconst const u64 dkt_hw_cache_extra_regs
+				[PERF_COUNT_HW_CACHE_MAX]
+				[PERF_COUNT_HW_CACHE_OP_MAX]
+				[PERF_COUNT_HW_CACHE_RESULT_MAX] = {
+	[C(LL)] = {
+		[C(OP_READ)] = {
+			[C(RESULT_ACCESS)]	= 0x10001,		/* OCR.DEMAND_DATA_RD.ANY_RESPONSE */
+			[C(RESULT_MISS)]	= 0x33FBFC00001,	/* OCR.DEMAND_DATA_RD.L3_MISS */
+		},
+		[C(OP_WRITE)] = {
+			[C(RESULT_ACCESS)]	= 0x10002,		/* OCR.DEMAND_RFO.ANY_RESPONSE */
+			[C(RESULT_MISS)]	= 0x33FBFC00002,	/* OCR.DEMAND_RFO.L3_MISS */
+		},
+	},
+};
+
 EVENT_ATTR_STR(topdown-fe-bound,       td_fe_bound_tnt,        "event=0x71,umask=0x0");
 EVENT_ATTR_STR(topdown-retiring,       td_retiring_tnt,        "event=0xc2,umask=0x0");
 EVENT_ATTR_STR(topdown-bad-spec,       td_bad_spec_tnt,        "event=0x73,umask=0x6");
@@ -7686,6 +7702,18 @@ static __always_inline void intel_pmu_init_dkt_hybrid(struct pmu *pmu)
 	hybrid(pmu, pebs_constraints) = intel_dkt_pebs_event_constraints;
 }
 
+/*
+ * Darkmont is used by the CWF and PTL E-cores, but their L3 OCR
+ * events require different extra MSR values. Keep a separate init
+ * function for the non-hybrid server variant.
+ */
+static __always_inline void intel_pmu_init_dkt(struct pmu *pmu)
+{
+	intel_pmu_init_dkt_hybrid(pmu);
+	memcpy(hybrid_var(pmu, hw_cache_extra_regs),
+	       dkt_hw_cache_extra_regs, sizeof(hw_cache_extra_regs));
+}
+
 __init int intel_pmu_init(void)
 {
 	struct attribute **extra_skl_attr = &empty_attrs;
@@ -7997,7 +8025,7 @@ __init int intel_pmu_init(void)
 		break;
 
 	case INTEL_ATOM_DARKMONT_X:
-		intel_pmu_init_skt(NULL);
+		intel_pmu_init_dkt(NULL);
 		intel_pmu_pebs_data_source_cmt();
 		x86_pmu.pebs_latency_data = cmt_latency_data;
 		x86_pmu.get_event_constraints = cmt_get_event_constraints;
