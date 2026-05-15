84148ff063dea8a25fb223725a17434f1b44e1f0
Jinjie Ruan <ruanjinjie@huawei.com>
2026-05-15 14:26:44 +0800

    sched: Add qos_sched_enabled() helper for future expansion
    hulk inclusion
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8929
    
    (cherry-pick from OLK-6.6 commit a161fc4bf5d2)
    
    ----------------------------------------
    
    Using the qos_sched_enabled() helper to decouple QOS_SCHED_SMT_EXPELLER
    and QOS_SCHED scheduling logic, providing a hook for future extensions.
    
    No functional changes.
    
    Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>
    Signed-off-by: Zicheng Qu <quzicheng@huawei.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9432,6 +9432,13 @@ out:
 }
 #endif
 
+#ifdef CONFIG_QOS_SCHED
+static __always_inline bool qos_sched_enabled(void)
+{
+	return true;
+}
+#endif
+
 /*
  * select_task_rq_fair: Select target runqueue for the waking task in domains
  * that have the relevant SD flag set. In practice, this is SD_BALANCE_WAKE,
@@ -9707,7 +9714,7 @@ static void check_preempt_wakeup_fair(struct rq *rq, struct task_struct *p, int 
 		return;
 
 #ifdef CONFIG_QOS_SCHED
-	if (unlikely(is_offline_task(curr) && !is_offline_task(p)))
+	if (qos_sched_enabled() && unlikely(is_offline_task(curr) && !is_offline_task(p)))
 		goto preempt;
 #endif
 
@@ -9957,6 +9964,9 @@ static int unthrottle_qos_cfs_rqs(int cpu)
 
 static bool check_qos_cfs_rq(struct cfs_rq *cfs_rq)
 {
+	if (!qos_sched_enabled())
+		return false;
+
 	if (unlikely(__this_cpu_read(qos_cpu_overload)))
 		return false;
 
@@ -10061,6 +10071,9 @@ static void start_qos_hrtimer(int cpu)
 	ktime_t time;
 	struct hrtimer *hrtimer = &(per_cpu(qos_overload_timer, cpu));
 
+	if (!qos_sched_enabled())
+		return;
+
 	time = ktime_add_ms(hrtimer->base->get_time(), (u64)sysctl_overload_detect_period);
 	hrtimer_set_expires(hrtimer, time);
 	hrtimer_start_expires(hrtimer, HRTIMER_MODE_ABS_PINNED);
@@ -10069,6 +10082,9 @@ static void start_qos_hrtimer(int cpu)
 void init_qos_hrtimer(int cpu)
 {
 	struct hrtimer *hrtimer = &(per_cpu(qos_overload_timer, cpu));
+
+	if (!qos_sched_enabled())
+		return;
 
 	hrtimer_init(hrtimer, CLOCK_MONOTONIC, HRTIMER_MODE_ABS_PINNED);
 	hrtimer->function = qos_overload_timer_handler;
@@ -10137,7 +10153,7 @@ static bool qos_sched_idle_cpu(int this_cpu)
 
 static bool qos_smt_expelled(int this_cpu)
 {
-	if (!static_branch_likely(&qos_smt_expell_switch))
+	if (!static_branch_likely(&qos_smt_expell_switch) || !qos_sched_enabled())
 		return false;
 
 	/*
@@ -10196,7 +10212,7 @@ static void qos_smt_send_ipi(int this_cpu)
 
 static void qos_smt_expel(int this_cpu, struct task_struct *p)
 {
-	if (!static_branch_likely(&qos_smt_expell_switch))
+	if (!static_branch_likely(&qos_smt_expell_switch) || !qos_sched_enabled())
 		return;
 
 	if (qos_smt_update_status(p))
@@ -10205,7 +10221,7 @@ static void qos_smt_expel(int this_cpu, struct task_struct *p)
 
 static inline bool qos_smt_enabled(void)
 {
-	if (!static_branch_likely(&qos_smt_expell_switch))
+	if (!static_branch_likely(&qos_smt_expell_switch) || !qos_sched_enabled())
 		return false;
 
 	if (!sched_smt_active())
@@ -10335,7 +10351,7 @@ again:
 #ifdef CONFIG_FAIR_GROUP_SCHED
 	if (prev->sched_class != &fair_sched_class) {
 #ifdef CONFIG_QOS_SCHED
-		if (cfs_rq->idle_h_nr_running != 0 && rq->online)
+		if (qos_sched_enabled() && cfs_rq->idle_h_nr_running != 0 && rq->online)
 			goto qos_simple;
 		else
 #endif
