bd9981dad88bf2f8149e15bb856ddb18df0108c8
Bi Shulei <bishulei@huawei.com>
2026-05-20 14:44:01 +0800

    urma: add rate limit for modify error logs
    urma inclusion
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/9207
    
    ----------------------------------------------
    
    Prevent log flooding by using rate-limited logging
    for frequent modify errors in both uburma
    and ubcore.
    
    Fixes: d9eceb15bc7b ("urma: Kernel-mode specific implementations file for Jetty extension interface")
    Signed-off-by: Shulei Bi <bishulei@huawei.com>

--- drivers/ub/urma/uburma/uburma_cmd.c
+++ drivers/ub/urma/uburma/uburma_cmd.c
@@ -491,7 +491,7 @@ static int uburma_cmd_modify_jfs(struct ubcore_device *ubc_dev,
 
 	uobj = uobj_get_write(UOBJ_CLASS_JFS, arg.in.handle, file);
 	if (IS_ERR_OR_NULL(uobj)) {
-		uburma_log_err("failed to find jfs.\n");
+		uburma_log_err_rl("failed to find jfs.\n");
 		return -EINVAL;
 	}
 
@@ -499,7 +499,7 @@ static int uburma_cmd_modify_jfs(struct ubcore_device *ubc_dev,
 	ret = ubcore_modify_jfs(jfs, &attr, &udata);
 	if (ret != 0) {
 		uobj_put_write(uobj);
-		uburma_log_err("modify jfs failed, ret:%d.\n", ret);
+		uburma_log_err_rl("modify jfs failed, ret:%d.\n", ret);
 		return ret;
 	}
 
@@ -1216,7 +1216,7 @@ static int uburma_cmd_modify_jfr(struct ubcore_device *ubc_dev,
 
 	uobj = uobj_get_write(UOBJ_CLASS_JFR, arg.in.handle, file);
 	if (IS_ERR_OR_NULL(uobj)) {
-		uburma_log_err("failed to find jfr.\n");
+		uburma_log_err_rl("failed to find jfr.\n");
 		return -EINVAL;
 	}
 
@@ -1224,7 +1224,7 @@ static int uburma_cmd_modify_jfr(struct ubcore_device *ubc_dev,
 	ret = ubcore_modify_jfr(jfr, &attr, &udata);
 	if (ret != 0) {
 		uobj_put_write(uobj);
-		uburma_log_err("modify jfr failed, ret:%d.\n", ret);
+		uburma_log_err_rl("modify jfr failed, ret:%d.\n", ret);
 		return ret;
 	}
 
@@ -1859,7 +1859,7 @@ static int uburma_cmd_modify_jfc(struct ubcore_device *ubc_dev,
 
 	uobj = uobj_get_write(UOBJ_CLASS_JFC, arg.in.handle, file);
 	if (IS_ERR_OR_NULL(uobj)) {
-		uburma_log_err("failed to find jfc.\n");
+		uburma_log_err_rl("failed to find jfc.\n");
 		return -EINVAL;
 	}
 
@@ -1867,7 +1867,7 @@ static int uburma_cmd_modify_jfc(struct ubcore_device *ubc_dev,
 	ret = ubcore_modify_jfc(jfc, &attr, &udata);
 	if (ret != 0) {
 		uobj_put_write(uobj);
-		uburma_log_err("modify jfc failed, ret:%d.\n", ret);
+		uburma_log_err_rl("modify jfc failed, ret:%d.\n", ret);
 		return ret;
 	}
 
@@ -2505,7 +2505,7 @@ static int uburma_cmd_modify_jetty(struct ubcore_device *ubc_dev,
 
 	uobj = uobj_get_write(UOBJ_CLASS_JETTY, arg.in.handle, file);
 	if (IS_ERR_OR_NULL(uobj)) {
-		uburma_log_err("failed to find jetty.\n");
+		uburma_log_err_rl("failed to find jetty.\n");
 		return -EINVAL;
 	}
 
@@ -2513,7 +2513,7 @@ static int uburma_cmd_modify_jetty(struct ubcore_device *ubc_dev,
 	ret = ubcore_modify_jetty(jetty, &attr, &udata);
 	if (ret != 0) {
 		uobj_put_write(uobj);
-		uburma_log_err("modify jetty failed, ret:%d.\n", ret);
+		uburma_log_err_rl("modify jetty failed, ret:%d.\n", ret);
 		return ret;
 	}
 
