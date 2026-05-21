4ddc12e48396268174eb8616445af132be69c91c
Wang Hai <wanghai38@huawei.com>
2026-05-21 16:45:46 +0800

    Revert "x86/resctrl: Move mbm_cfg_mask to struct rdt_resource"
    HULK inclusion
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/9208
    
    ----------------------------------------------------------------------
    
    This reverts commit 18f45c2063d44aeafe82cd32394501b8b7223a01.
    
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- include/linux/resctrl_x86.h
+++ include/linux/resctrl_x86.h
@@ -217,8 +217,6 @@ enum resctrl_schema_fmt {
  * @name:		Name to use in "schemata" file.
  * @schema_fmt:		Which format string and parser is used for this schema.
  * @evt_list:		List of monitoring events
- * @mbm_cfg_mask:	Bandwidth sources that can be tracked when bandwidth
- *			monitoring events can be configured.
  * @cdp_capable:	Is the CDP feature available on this resource
  */
 struct rdt_resource {
@@ -235,7 +233,6 @@ struct rdt_resource {
 	char			*name;
 	enum resctrl_schema_fmt	schema_fmt;
 	struct list_head	evt_list;
-	unsigned int		mbm_cfg_mask;
 	bool			cdp_capable;
 };
 
