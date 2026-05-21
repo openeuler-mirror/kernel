820a902180f958526117a5f211b828a0345b67ae
Wang Hai <wanghai38@huawei.com>
2026-05-21 16:45:49 +0800

    Revert "x86/resctrl: Expose resctrl fs's init function to the rest of the kernel"
    HULK inclusion
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/9208
    
    ----------------------------------------------------------------------
    
    This reverts commit 43075bb7b20c4ad8557a2018cd525293b4d5e7eb.
    
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- arch/x86/kernel/cpu/resctrl/rdtgroup.c
+++ arch/x86/kernel/cpu/resctrl/rdtgroup.c
@@ -4235,14 +4235,14 @@ out_unlock:
 }
 
 /*
- * resctrl_init - resctrl filesystem initialization
+ * rdtgroup_init - rdtgroup initialization
  *
  * Setup resctrl file system including set up root, create mount point,
- * register resctrl filesystem, and initialize files under root directory.
+ * register rdtgroup filesystem, and initialize files under root directory.
  *
  * Return: 0 on success or -errno
  */
-int __init resctrl_init(void)
+int __init rdtgroup_init(void)
 {
 	int ret = 0;
 
@@ -4290,7 +4290,7 @@ cleanup_mountpoint:
 	return ret;
 }
 
-void __exit resctrl_exit(void)
+void __exit rdtgroup_exit(void)
 {
 	debugfs_remove_recursive(debugfs_resctrl);
 	unregister_filesystem(&rdt_fs_type);
