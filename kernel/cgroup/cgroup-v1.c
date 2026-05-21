f781a75a1880346614abd8c8d47b7eb77a9e2b2a
Wang Hai <wanghai38@huawei.com>
2026-05-21 16:45:40 +0800

    Revert "kernfs: Use RCU to access kernfs_node::parent."
    HULK inclusion
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/9208
    
    ----------------------------------------------------------------------
    
    This reverts commit 74e9e7d73c8fec625d22beb149871a3d6572ee82.
    
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- kernel/cgroup/cgroup-v1.c
+++ kernel/cgroup/cgroup-v1.c
@@ -851,7 +851,7 @@ static int cgroup1_rename(struct kernfs_node *kn, struct kernfs_node *new_parent
 
 	if (kernfs_type(kn) != KERNFS_DIR)
 		return -ENOTDIR;
-	if (rcu_access_pointer(kn->__parent) != new_parent)
+	if (kn->parent != new_parent)
 		return -EIO;
 
 	/*
