a6db2a47e660941df062bec04b4236a9b8b42870
Wang Hai <wanghai38@huawei.com>
2026-05-21 16:46:09 +0800

    Revert "x86/resctrl: Remove redundant variable in mbm_config_write_domain()"
    HULK inclusion
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/9208
    
    ----------------------------------------------------------------------
    
    This reverts commit 1c7065f73a07c2c007d6fa255a209e2e7c40bbd4.
    
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- arch/x86/kernel/cpu/resctrl/rdtgroup.c
+++ arch/x86/kernel/cpu/resctrl/rdtgroup.c
@@ -1614,10 +1614,11 @@ static void mon_event_config_write(void *info)
 	wrmsr(MSR_IA32_EVT_CFG_BASE + index, mon_info->mon_config, 0);
 }
 
-static void mbm_config_write_domain(struct rdt_resource *r,
-				    struct rdt_domain *d, u32 evtid, u32 val)
+static int mbm_config_write_domain(struct rdt_resource *r,
+				   struct rdt_domain *d, u32 evtid, u32 val)
 {
 	struct mon_config_info mon_info = {0};
+	int ret = 0;
 
 	/*
 	 * Read the current config value first. If both are the same then
@@ -1626,7 +1627,7 @@ static void mbm_config_write_domain(struct rdt_resource *r,
 	mon_info.evtid = evtid;
 	mondata_config_read(d, &mon_info);
 	if (mon_info.mon_config == val)
-		return;
+		goto out;
 
 	mon_info.mon_config = val;
 
@@ -1649,6 +1650,9 @@ static void mbm_config_write_domain(struct rdt_resource *r,
 	 * mbm_local and mbm_total counts for all the RMIDs.
 	 */
 	resctrl_arch_reset_rmid_all(r, d);
+
+out:
+	return ret;
 }
 
 static int mon_config_write(struct rdt_resource *r, char *tok, u32 evtid)
@@ -1657,6 +1661,7 @@ static int mon_config_write(struct rdt_resource *r, char *tok, u32 evtid)
 	char *dom_str = NULL, *id_str;
 	unsigned long dom_id, val;
 	struct rdt_domain *d;
+	int ret = 0;
 
 next:
 	if (!tok || tok[0] == '\0')
@@ -1685,7 +1690,9 @@ next:
 
 	list_for_each_entry(d, &r->domains, list) {
 		if (d->id == dom_id) {
-			mbm_config_write_domain(r, d, evtid, val);
+			ret = mbm_config_write_domain(r, d, evtid, val);
+			if (ret)
+				return -EINVAL;
 			goto next;
 		}
 	}
