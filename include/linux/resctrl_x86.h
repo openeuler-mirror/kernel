c50e8b50cc1d7c89b58b50acd23b4f7041baa057
Wang Hai <wanghai38@huawei.com>
2026-05-21 16:46:01 +0800

    Revert "x86/resctrl: Prepare to split rdt_domain structure"
    HULK inclusion
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/9208
    
    ----------------------------------------------------------------------
    
    This reverts commit f74cb293e1117a60edad2d05547f8c0eafc7adb0.
    
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- include/linux/resctrl_x86.h
+++ include/linux/resctrl_x86.h
@@ -59,20 +59,10 @@ struct resctrl_staged_config {
 };
 
 /**
- * struct rdt_domain_hdr - common header for different domain types
+ * struct rdt_domain - group of CPUs sharing a resctrl resource
  * @list:		all instances of this resource
  * @id:			unique id for this instance
  * @cpu_mask:		which CPUs share this resource
- */
-struct rdt_domain_hdr {
-	struct list_head		list;
-	int				id;
-	struct cpumask			cpu_mask;
-};
-
-/**
- * struct rdt_domain - group of CPUs sharing a resctrl resource
- * @hdr:		common header for different domain types
  * @rmid_busy_llc:	bitmap of which limbo RMIDs are above threshold
  * @mbm_total:		saved state for MBM total bandwidth
  * @mbm_local:		saved state for MBM local bandwidth
@@ -87,7 +77,9 @@ struct rdt_domain_hdr {
  *			by closid
  */
 struct rdt_domain {
-	struct rdt_domain_hdr		hdr;
+	struct list_head		list;
+	int				id;
+	struct cpumask			cpu_mask;
 	unsigned long			*rmid_busy_llc;
 	struct mbm_state		*mbm_total;
 	struct mbm_state		*mbm_local;
