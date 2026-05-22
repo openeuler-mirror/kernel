10b6909d17b83854a266b7747f42094098f5912e
Lizhi He <helizhi1@huawei.com>
2026-05-22 14:52:25 +0800

    ub/ubmempfd: update note and optimize format
    ubmempfd inclusion
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/9223
    
    ----------------------------------------------
    
    Add more annotation explanations and optimize formats.
    
    Fixes: 086a741d7b71 ("ub: ubmempfd: supports for D2H mapping and demapping")
    
    Signed-off-by: Lizhi He <helizhi1@huawei.com>

--- include/uapi/ub/ubmempfd/ubmempfd.h
+++ include/uapi/ub/ubmempfd/ubmempfd.h
@@ -1,5 +1,8 @@
 /* SPDX-License-Identifier: GPL-2.0+ WITH Linux-syscall-note */
-/* Copyright (c) 2025 HiSilicon Technologies Co., Ltd. All rights reserved. */
+/*
+ * Copyright (c) 2025 HiSilicon Technologies Co., Ltd. All rights reserved.
+ * Description: ubmempfd defines and function prototypes
+ */
 
 #ifndef _UAPI_UB_UBMEMPFD_UBMEMPFD_H_
 #define _UAPI_UB_UBMEMPFD_UBMEMPFD_H_
