1b832eb50aef52bc363e18b2d84bd479567a92ef
Liu Kai <liukai284@huawei.com>
2026-06-05 14:30:19 +0800

    fbdev: defio: fix KABI breakage in struct fb_deferred_io and struct fb_info
    hulk inclusion
    category: bugfix
    bugzilla: https://atomgit.com/src-openeuler/kernel/issues/15309
    
    --------------------------------
    
    Fix the KABI breakage in 'struct fb_deferred_io' and 'struct fb_info'
    caused by the commit e5d43517c4c1 ("fbdev: defio: Disconnect deferred
    I/O from the lifetime of struct fb_info")
    
    Fixes: e5d43517c4c1 ("fbdev: defio: Disconnect deferred I/O from the lifetime of struct fb_info")
    Signed-off-by: Liu Kai <liukai284@huawei.com>

--- include/linux/fb.h
+++ include/linux/fb.h
@@ -215,6 +215,7 @@ struct fb_deferred_io {
 	unsigned long delay;
 	bool sort_pagereflist; /* sort pagelist by offset */
 	int open_count; /* number of opened files; protected by fb_info lock */
+	KABI_BROKEN_REMOVE(struct mutex lock)
 	struct list_head pagereflist; /* list of pagerefs for touched pages */
 	/* callback */
 	void (*deferred_io)(struct fb_info *info, struct list_head *pagelist);
@@ -480,7 +481,6 @@ struct fb_info {
 	unsigned long npagerefs;
 	struct fb_deferred_io_pageref *pagerefs;
 	struct fb_deferred_io *fbdefio;
-	struct fb_deferred_io_state *fbdefio_state;
 #endif
 
 	const struct fb_ops *fbops;
@@ -506,7 +506,11 @@ struct fb_info {
 	void *par;
 
 	bool skip_vt_switch; /* no VT switch on suspend/resume required */
+#ifdef CONFIG_FB_DEFERRED_IO
+	KABI_USE(1, struct fb_deferred_io_state *fbdefio_state)
+#else
 	KABI_RESERVE(1)
+#endif
 	KABI_RESERVE(2)
 	KABI_RESERVE(3)
 	KABI_RESERVE(4)
