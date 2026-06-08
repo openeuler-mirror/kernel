cd28226a593b7f6397f24a533b4a23e9aa6ab797
Xianglai Li <lixianglai@loongson.cn>
2026-06-08 10:05:31 +0800

    LoongArch: KVM: Revert "LoongArch: KVM: Make vcpu_is_preempted() as a macro rather than function"
    loongarch inclusion
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/9352
    
    (cherry-pick from OLK-6.6 commit 9f5d2170ce86)
    
    ------------------------------------------
    
    This reverts commit 79c38ad43426975e7442a121c1e9891e88e62906.
    
    The paravirt preempt feature patch may cause system hangs on 3C5000
    machines with a certain probability. Revert this feature for now.
    
    Fixes: fb0cdb92ead2 ("LoongArch: KVM: Add paravirt preempt feature in hypervisor side")
    Signed-off-by: Xianglai Li <lixianglai@loongson.cn>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- arch/loongarch/include/asm/qspinlock.h
+++ arch/loongarch/include/asm/qspinlock.h
@@ -2,13 +2,11 @@
 #ifndef _ASM_LOONGARCH_QSPINLOCK_H
 #define _ASM_LOONGARCH_QSPINLOCK_H
 
-#include <asm/kvm_para.h>
 #include <linux/jump_label.h>
 
 #ifdef CONFIG_PARAVIRT
-DECLARE_STATIC_KEY_FALSE(virt_preempt_key);
+
 DECLARE_STATIC_KEY_FALSE(virt_spin_lock_key);
-DECLARE_PER_CPU(struct kvm_steal_time, steal_time);
 
 #define virt_spin_lock virt_spin_lock
 
@@ -36,25 +34,9 @@ __retry:
 	return true;
 }
 
-/*
- * Macro is better than inline function here
- * With macro, parameter cpu is parsed only when it is used.
- * With inline function, parameter cpu is parsed even though it is not used.
- * This may cause cache line thrashing across NUMA nodes.
- */
-#define vcpu_is_preempted(cpu)							\
-({										\
-	bool __val;								\
-										\
-	if (!static_branch_unlikely(&virt_preempt_key))				\
-		__val = false;							\
-	else {									\
-		struct kvm_steal_time *src;					\
-		src = &per_cpu(steal_time, cpu);				\
-		__val = !!(READ_ONCE(src->preempted) & KVM_VCPU_PREEMPTED);	\
-	}									\
-	__val;									\
-})
+#define vcpu_is_preempted vcpu_is_preempted
+
+bool vcpu_is_preempted(int cpu);
 
 #endif /* CONFIG_PARAVIRT */
 
