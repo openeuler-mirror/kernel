175e6a3a4e65859a7b98ee4a63628f6298e7529b
Wang Hang <wanghang73@huawei.com>
2026-06-12 17:00:13 +0800

    urma: refactor ubagg connect and message handling
    urma inclusion
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/9252
    CVE: NA
    
    (cherry-pick from OLK-6.6 commit b5ab3f22786d)
    
    ----------------------------------------------------------
    
    This patch cleans up the tail of the ubagg connect_bonding refactor.
    It renames the connect_bonding module to a generic ubagg_connect
    interface, moves bonding message dispatch and version/length checking
    into a dedicated ubagg_msg module, and removes the obsolete user
    payload path from ubagg netlink.
    
    With the connect handlers registered through a common descriptor table,
    ubagg init and exit now manage connect and message modules explicitly,
    which makes the bonding message path clearer and easier to extend.
    
    Fixes: 909724a292b1 ("urma: refactor connect_bonding from ubcore to ubagg")
    Signed-off-by: Wang Hang <wanghang73@huawei.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

---
+++ drivers/ub/urma/ubagg/ubagg_connect.h
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2026-2026. All rights reserved.
+ *
+ * Description: ubagg connect header file
+ * Author: Chen Chongyu
+ * Create: 2026-05-13
+ * Note:
+ * History: 2026-05-13: create file
+ */
+
+#ifndef UBAGG_CONNECT_H
+#define UBAGG_CONNECT_H
+
+#include <linux/types.h>
+#include <ub/urma/ubcore_types.h>
+
+#include "ubagg_types.h"
+
+int ubagg_connect_init(void);
+void ubagg_connect_uninit(void);
+
+int ubagg_connect_xchg_seg(struct ubcore_seg *seg, uint32_t ue_idx,
+			   struct ubcore_device *dev,
+			   struct ubagg_seg_info *seg_info);
+
+int ubagg_connect_xchg_jetty(struct ubcore_tjetty_cfg *cfg, uint32_t ue_idx,
+			     bool is_jfr, struct ubcore_device *dev,
+			     struct ubagg_jetty_exchange_info *jetty_info);
+
+#endif /* UBAGG_CONNECT_H */
--- drivers/ub/urma/ubagg/ubagg_netlink.h
+++ drivers/ub/urma/ubagg/ubagg_netlink.h
@@ -8,11 +8,6 @@
 #ifndef UBAGG_NETLINK_H
 #define UBAGG_NETLINK_H
 
-#include <ub/urma/ubcore_uapi.h>
-
-void ubagg_nl_bonding_user_msg_handler(struct ubcore_device *dev,
-	struct ubcore_comm_msg *msg, void *conn);
-
 int ubagg_genl_register_family(void);
 void ubagg_genl_unregister_family(void);
 
