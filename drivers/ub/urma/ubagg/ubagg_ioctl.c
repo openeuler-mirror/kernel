openeuler-mirror/kernel

commit 8e700301346ddfbbbd033644154d42d4765c60d3

urma: fix well-known jetty create/delete hook up with main ue eid event
urma inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/9339

----------------------------------------------

Hook up well-known jetty create/delete with main ue eid events.
Fix EID iteration boundary to use actual eid_cnt instead of max_eid_cnt.
Fix primary EID detection logic to find real primary EID correctly.

Fixes: fa39902c0e1d ("urma: Introduce EID patricia trie for primary EID lookup")
Signed-off-by: Shulei Bi <bishulei@huawei.com>

drivers/ub/urma/ubcore/ubcore_topo_info.c
@@ -95,41 +95,6 @@ bool is_eid_valid(const char *eid)
 	return false;
 }
 
-static int find_cur_node_index(struct ubcore_topo_map *topo_map,
-				   uint32_t *node_index)
-{
-	int i;
-
-	for (i = 0; i < topo_map->node_num; i++) {
-		if (topo_map->topo_infos[i].is_current) {
-			*node_index = i;
-			break;
-		}
-	}
-	if (i == topo_map->node_num) {
-		ubcore_log_err("can't find cur node\n");
-		return -EINVAL;
-	}
-	return 0;
-}
-
-static bool is_eid_match(const char *eid1, const char *eid2)
-{
-	return memcmp(eid1, eid2, EID_LEN) == 0;
-}
-
-struct ubcore_topo_node *
-ubcore_get_cur_topo_info(struct ubcore_topo_map *topo_map)
-{
-	uint32_t cur_node_index = 0;
-
-	if (find_cur_node_index(topo_map, &cur_node_index) != 0) {
-		ubcore_log_err("find cur node index failed\n");
-		return NULL;
-	}
-	return &(topo_map->topo_infos[cur_node_index]);
-}
-
 static int update_dev_info(struct ubcore_topo_node *new_topo_info,
 				struct ubcore_topo_node *old_topo_info)
 {
@@ -274,88 +239,6 @@ void ubcore_show_topo_map(struct ubcore_topo_map *topo_map)
 	}
 	ubcore_log_info(
 		"========================== topo map end =============================\n");
-}
-
-static int find_primary_eid_in_ues(struct ubcore_topo_agg_dev *agg_dev,
-								const char *eid_raw,
-								union ubcore_eid *primary_eid,
-								uint32_t *entity_id,
-								uint32_t *chip_id)
-{
-	int iodie_id, port_id;
-
-	for (iodie_id = 0; iodie_id < IODIE_NUM; iodie_id++) {
-		if (is_eid_match(agg_dev->ues[iodie_id].primary_eid,
-			eid_raw)) {
-			(void)memcpy(
-				primary_eid,
-				agg_dev->ues[iodie_id].primary_eid,
-				EID_LEN);
-			*entity_id = agg_dev->ues[iodie_id].entity_id;
-			*chip_id = agg_dev->ues[iodie_id].chip_id;
-			ubcore_log_info(
-				"find primary eid: "EID_FMT", entity_id: %u, chip_id: %u.\n",
-				EID_ARGS(*primary_eid), *entity_id, *chip_id);
-			return 0;
-		}
-
-		for (port_id = 0; port_id < PORT_NUM; port_id++) {
-			if (is_eid_match(
-				agg_dev->ues[iodie_id].port_eid[port_id],
-				eid_raw)) {
-				(void)memcpy(
-					primary_eid,
-					agg_dev->ues[iodie_id].primary_eid,
-					EID_LEN);
-				*entity_id = agg_dev->ues[iodie_id].entity_id;
-				*chip_id = agg_dev->ues[iodie_id].chip_id;
-				ubcore_log_info(
-					"find primary eid by port eid, port_eid: "EID_FMT
-					", ", EID_ARGS(*(union ubcore_eid *)eid_raw));
-				ubcore_log_info(
-					"primary eid: "EID_FMT", entity_id: %u, chip_id: %u.\n",
-					EID_ARGS(*primary_eid), *entity_id, *chip_id);
-				return 0;
-			}
-		}
-	}
-
-	return -EINVAL;
-}
-
-int ubcore_get_primary_eid(union ubcore_eid *eid, union ubcore_eid *primary_eid,
-	uint32_t *entity_id, uint32_t *chip_id, uint32_t *nd_id)
-{
-	int node_id, dev_id;
-	struct ubcore_topo_node *cur_node_info;
-
-	if (!g_ubcore_topo_map) {
-		ubcore_log_info("ubcore topo map doesn't exist, eid is primary_eid.\n");
-		(void)memcpy(primary_eid, eid, EID_LEN);
-		return 0;
-	}
-
-	for (node_id = 0; node_id < g_ubcore_topo_map->node_num; node_id++) {
-		cur_node_info = g_ubcore_topo_map->topo_infos + node_id;
-		for (dev_id = 0; dev_id < DEV_NUM; dev_id++) {
-			if (is_eid_match(cur_node_info->agg_devs[dev_id].agg_eid,
-							(char *)eid->raw)) {
-				ubcore_log_err("input eid is bonding eid!\n");
-				return -EINVAL;
-			}
-
-			if (find_primary_eid_in_ues(
-				&cur_node_info->agg_devs[dev_id],
-				(char *)eid->raw,
-				primary_eid, entity_id, chip_id) == 0) {
-				*nd_id = (uint32_t)node_id;
-				return 0;
-			}
-		}
-	}
-
-	ubcore_log_err("can't find primary eid\n");
-	return -EINVAL;
 }
 
 static struct ubcore_topo_node *

drivers/ub/urma/ubcore/ubcore_uvs_cmd.c
@@ -20,7 +20,6 @@
 #include "ubcore_main_ue_eid.h"
 #include "ubcore_topo_info.h"
 #include "net/ubcore_cm.h"
-#include "ubmgr/ubmgr_topo.h"
 #include "ubcore_uvs_cmd.h"
 
 int ubcore_get_path_set(union ubcore_eid *src_bonding_eid,
@@ -157,94 +156,6 @@ int ubcore_update_eidtbl_by_idx(struct ubcore_device *dev,
 	return ret;
 }
 
-static int ubcore_get_eid_index(struct ubcore_device *dev,
-				union ubcore_eid *eid, uint32_t *eid_index)
-{
-	uint32_t idx, ret = 0;
-
-	spin_lock(&dev->eid_table.lock);
-	for (idx = 0; idx < dev->eid_table.eid_cnt; idx++) {
-		if (memcmp(&dev->eid_table.eid_entries[idx].eid, eid,
-			   sizeof(union ubcore_eid)) == 0) {
-			*eid_index = idx;
-			break;
-		}
-	}
-	if (idx == dev->eid_table.eid_cnt)
-		ret = -1;
-	spin_unlock(&dev->eid_table.lock);
-	return ret;
-}
-
-static int ubcore_create_jetty_rsrc(struct ubcore_topo_map *topo_map)
-{
-	struct ubcore_device *dev;
-	struct ubcore_eid_info eid_info = { 0 };
-	struct ubcore_topo_node *cur_node_info;
-	bool has_any_primary_eid = false;
-	int dev_idx, die_idx;
-	int ret;
-
-	cur_node_info = ubcore_get_cur_topo_info(topo_map);
-	if (cur_node_info == NULL) {
-		ubcore_log_err("Failed to get current node info\n");
-		return -EINVAL;
-	}
-
-	for (dev_idx = 0; dev_idx < DEV_NUM; dev_idx++) {
-		for (die_idx = 0; die_idx < IODIE_NUM; die_idx++) {
-			if (!is_eid_valid(
-				cur_node_info->agg_devs[dev_idx].ues[die_idx].primary_eid))
-				continue;
-
-			has_any_primary_eid = true;
-			(void)memcpy(&eid_info.eid,
-					cur_node_info->agg_devs[dev_idx].ues[die_idx].primary_eid,
-					sizeof(union ubcore_eid));
-
-			dev = ubcore_get_device_by_eid(&eid_info.eid,
-								   UBCORE_TRANSPORT_UB);
-			if (dev == NULL) {
-				ubcore_log_err(
-				"primary dev not exist, node %d dev %d die %d, eid: " EID_FMT
-				"\n",
-				cur_node_info->node_id, dev_idx, die_idx,
-				EID_RAW_ARGS(
-				cur_node_info->agg_devs[dev_idx].ues[die_idx].primary_eid
-				));
-				return -1;
-			}
-
-			ret = ubcore_get_eid_index(dev, &eid_info.eid,
-						&eid_info.eid_index);
-			if (ret != 0) {
-				ubcore_log_err("Failed to get eid index\n");
-				ubcore_put_device(dev);
-				return ret;
-			}
-
-			ret = ubcore_call_cm_eid_ops(dev, &eid_info,
-						UBCORE_MGMT_EVENT_EID_ADD);
-			if (ret != 0) {
-				ubcore_log_err("Failed to call cm eid ops\n");
-				ubcore_put_device(dev);
-				return ret;
-			}
-
-			ubcore_log_info(
-				"Created jetty rsrc: node %d dev %d primary die %d, eid: " EID_FMT
-				", idx: %d\n",
-				cur_node_info->node_id, dev_idx, die_idx,
-				EID_RAW_ARGS(
-				cur_node_info->agg_devs[dev_idx].ues[die_idx].primary_eid),
-				eid_info.eid_index);
-			ubcore_put_device(dev);
-		}
-	}
-
-	return has_any_primary_eid ? 0 : -1;
-}
-
 static int ubcore_cmd_set_topo(struct ubcore_global_file *file,
 			       struct ubcore_cmd_hdr *hdr)
 {
@@ -282,14 +193,6 @@ static int ubcore_cmd_set_topo(struct ubcore_global_file *file,
 	}
 	ubcore_show_topo_map(topo_map);
 
-	ret = ubcore_create_jetty_rsrc(topo_map);
-	if (ret != 0) {
-		ubcore_log_err("Failed to create jetty rsrc\n");
-		ubcore_delete_global_topo_map();
-		return ret;
-	}
-
-	ubmgr_notify_set_topo();
 	return 0;
 }
 

drivers/ub/urma/ubcore/Makefile
@@ -35,7 +35,6 @@ ubcm/ubmad_datapath.o \
 ubcore_umem.o \
 ubmgr/ubmgr.o \
 ubmgr/ubmgr_ping.o \
-ubmgr/ubmgr_topo.o \
 ubcore_dmac.o \
 ubcore_main_ue_eid.o
 

drivers/ub/urma/ubcore/ubcm/ub_mad.c
@@ -167,6 +167,34 @@ static int ubmad_check_eid_in_dev(struct ubcore_device *dev,
 	return -1;
 }
 
+static int ubmad_get_eid_info_by_eid(struct ubcore_device *dev,
+				     const union ubcore_eid *eid,
+				     struct ubcore_eid_info *eid_info)
+{
+	int i;
+
+	if (IS_ERR_OR_NULL(dev) || IS_ERR_OR_NULL(eid) ||
+	    IS_ERR_OR_NULL(eid_info) || IS_ERR_OR_NULL(dev->eid_table.eid_entries))
+		return -EINVAL;
+
+	spin_lock(&dev->eid_table.lock);
+	for (i = 0; i < dev->eid_table.eid_cnt; i++) {
+		if (!dev->eid_table.eid_entries[i].valid)
+			continue;
+
+		if (memcmp(&dev->eid_table.eid_entries[i].eid, eid,
+			   sizeof(union ubcore_eid)) == 0) {
+			eid_info->eid = dev->eid_table.eid_entries[i].eid;
+			eid_info->eid_index =
+				dev->eid_table.eid_entries[i].eid_index;
+			spin_unlock(&dev->eid_table.lock);
+			return 0;
+		}
+	}
+	spin_unlock(&dev->eid_table.lock);
+	return -ENOENT;
+}
+
 static int
 ubmad_update_device_priv_resources(struct ubmad_device_priv *dev_priv,
 				   struct ubcore_eid_info *eid_info)
@@ -295,6 +323,47 @@ static int ubmad_ubc_eid_ops(struct ubcore_device *dev,
 	}
 	mutex_unlock(&g_ubc_eid_lock);
 	return 0;
+}
+
+static void ubmad_on_main_ue_eid_event(
+	const union ubcore_eid *main_ue_eid,
+	enum ubcore_main_ue_eid_event_type event_type)
+{
+	struct ubcore_device *dev;
+	struct ubcore_eid_info eid_info = { 0 };
+	enum ubcore_mgmt_event_type mgmt_event_type;
+	int ret;
+
+	switch (event_type) {
+	case UBCORE_MAIN_UE_EID_FIRST_ADD:
+		mgmt_event_type = UBCORE_MGMT_EVENT_EID_ADD;
+		break;
+	case UBCORE_MAIN_UE_EID_LAST_DEL:
+		mgmt_event_type = UBCORE_MGMT_EVENT_EID_RMV;
+		break;
+	default:
+		return;
+	}
+
+	dev = ubcore_get_device_by_eid((union ubcore_eid *)main_ue_eid,
+				       UBCORE_TRANSPORT_UB);
+	if (dev == NULL)
+		return;
+
+	ret = ubmad_get_eid_info_by_eid(dev, main_ue_eid, &eid_info);
+	if (ret != 0) {
+		ubcore_put_device(dev);
+		return;
+	}
+
+	mutex_lock(&g_ubc_eid_lock);
+	ret = ubmad_ubc_eid_ops_inner(dev, &eid_info, mgmt_event_type);
+	mutex_unlock(&g_ubc_eid_lock);
+	if (ret != 0)
+		ubcore_log_err("Failed to handle main ue eid event, eid " EID_FMT
+			       ", event_type: %d, ret: %d.\n",
+			       EID_ARGS(*main_ue_eid), event_type, ret);
+	ubcore_put_device(dev);
 }
 
 /* jetty ops */
@@ -1049,12 +1118,9 @@ ubmad_create_device_priv_resources(struct ubmad_device_priv *dev_priv)
 static void
 ubmad_destroy_device_priv_resources(struct ubmad_device_priv *dev_priv)
 {
-	if (!dev_priv->valid) {
-		ubcore_log_warn(
-			"dev_priv rsrc not inited. No need to uninit. dev_name: %s\n",
-			dev_priv->device->dev_name);
+	if (!dev_priv->valid)
 		return;
-	}
+
 	dev_priv->valid = false;
 
 	ubmad_uninit_jetty_rsrc_array(dev_priv->jetty_rsrc);
@@ -1112,7 +1178,7 @@ void ubmad_put_device_priv(struct ubmad_device_priv *dev_priv)
 	kref_put(&dev_priv->kref, ubmad_release_device_priv);
 }
 
-// init dev_priv rsrc fail won't cause this func ret err
+/* device resources are created lazily on main ue eid events */
 static int ubmad_open_device(struct ubcore_device *device)
 {
 	struct ubmad_device_priv *dev_priv;
@@ -1127,20 +1193,12 @@ static int ubmad_open_device(struct ubcore_device *device)
 	dev_priv->handler.event_callback = ubmad_event_cb;
 	ubcore_register_event_handler(device, &dev_priv->handler);
 
-	/* rsrc */
-	if (ubmad_create_device_priv_resources(dev_priv) != 0) {
-		// It could be due to eid not added. Wait for ubcore add eid event to init again.
-		ubcore_log_warn("fail to create dev_priv rsrc. dev_name: %s\n",
-			      device->dev_name);
-	}
-
 	/* reliable communication */
 	dev_priv->rt_wq = alloc_workqueue("%s",
 		WQ_UNBOUND | WQ_HIGHPRI | WQ_MEM_RECLAIM, 0, "ubmad rt_wq");
 	if (IS_ERR_OR_NULL(dev_priv->rt_wq)) {
 		ubcore_log_err("create rt_wq failed. dev_name: %s\n",
 			     device->dev_name);
-		ubmad_destroy_device_priv_resources(dev_priv);
 		ubcore_unregister_event_handler(dev_priv->device,
 						&dev_priv->handler);
 		kfree(dev_priv);
@@ -1154,7 +1212,6 @@ static int ubmad_open_device(struct ubcore_device *device)
 			     device->dev_name);
 		drain_workqueue(dev_priv->rt_wq);
 		destroy_workqueue(dev_priv->rt_wq);
-		ubmad_destroy_device_priv_resources(dev_priv);
 		ubcore_unregister_event_handler(dev_priv->device,
 						&dev_priv->handler);
 		kfree(dev_priv);
@@ -1204,6 +1261,11 @@ static void ubmad_notify_close(struct ubcore_device *device)
 	if (dev_priv == NULL) {
 		ubcore_log_err("Failed to get dev_priv, dev_name: %s\n",
 			     device->dev_name);
+		return;
+	}
+
+	if (!dev_priv->valid) {
+		ubmad_put_device_priv(dev_priv);
 		return;
 	}
 
@@ -1279,10 +1341,19 @@ int ubmad_init(void)
 	INIT_LIST_HEAD(&g_ubmad_device_list);
 	INIT_LIST_HEAD(&g_ubmad_agent_list);
 
+	ret = ubcore_register_main_ue_eid_event_cb(ubmad_on_main_ue_eid_event);
+	if (ret != 0) {
+		ubcore_log_err("Failed to register main ue eid event cb, ret: %d.\n",
+			       ret);
+		return ret;
+	}
+
 	ret = ubcore_register_client(&g_ubmad_client);
 	if (ret != 0) {
 		ubcore_log_err("Failed to register ub_mad client, ret: %d.\n",
 			     ret);
+		(void)ubcore_unregister_main_ue_eid_event_cb(
+			ubmad_on_main_ue_eid_event);
 		return ret;
 	}
 
@@ -1293,6 +1364,7 @@ int ubmad_init(void)
 
 void ubmad_uninit(void)
 {
+	(void)ubcore_unregister_main_ue_eid_event_cb(ubmad_on_main_ue_eid_event);
 	ubcore_unregister_client(&g_ubmad_client);
 }
 

drivers/ub/urma/ubcore/ubcm/ubmad_datapath.c
@@ -1114,12 +1114,15 @@ int ubmad_post_send(struct ubcore_device *device,
 
 	/* import well-known jetty */
 	// unimport in ubmad_uninit_jetty_rsrc()
+	ubcore_log_err("lookup primary eid for dst eid " EID_FMT "\n",
+		     EID_ARGS(send_buf->dst_eid));
 	ret = ubcore_lookup_main_ue_eid(&send_buf->dst_eid, &dst_primary_eid);
 	if (ret != 0) {
-		ubcore_log_err("get primary eid failed\n");
+		ubcore_log_err("get primary eid failed, ret = %d\n", ret);
 		goto put_device_priv;
 	}
-
+	ubcore_log_err("dst_primary_eid " EID_FMT "\n",
+		     EID_ARGS(dst_primary_eid));
 	hash = jhash(&dst_primary_eid, sizeof(union ubcore_eid), 0) %
 		UBMAD_MAX_TJETTY_NUM;
 	spin_lock_irqsave(&rsrc->tjetty_hlist_lock, flag);

drivers/ub/urma/ubcore/ubcore_device.c
@@ -31,7 +31,6 @@
 #include "ubcore_priv.h"
 #include "net/ubcore_session.h"
 #include "net/ubcore_cm.h"
-#include "ubmgr/ubmgr_topo.h"
 
 #define UBCORE_MAX_MUE_NUM 16
 #define UBCORE_DEVICE_NAME "ubcore"
@@ -2453,8 +2452,6 @@ void ubcore_dispatch_mgmt_event(struct ubcore_mgmt_event *event)
 			"Failed to update eid table, index: %u, type: %d.\n",
 			eid_info->eid_index, event->event_type);
 
-	ubmgr_notify_mgmt_event(event);
-
 	if (ubcore_call_cm_eid_ops(event->ub_dev, event->element.eid_info,
 				   event->event_type) != 0)
 		ubcore_log_err_rl("cast eid to ubcm failed.\n");
@@ -2492,4 +2489,3 @@ struct ubcore_device *ubcore_get_device_by_eid(union ubcore_eid *eid,
 	return target;
 }
 EXPORT_SYMBOL(ubcore_get_device_by_eid);
-

commit 7e7bb1ac03cfa1ab2eb779596b5c701c08475039

urma: add set_share_topo host EID programming path
urma inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/9389

--------------------------

Add the set_share_topo configuration path used to program host EID
mappings derived from topology. User space reads the accumulated ubagg
topology, computes port EID to host EID mappings from share_topo, and
programs the host_info trie through a UVS global command.

Allow ubagg and ubcore topology updates to append newly received topo
nodes while keeping the existing merge behavior for repeated node
updates. Add a ubagg get-topo command so user space can read the
accumulated topology before processing share_topo input.

Add a UVS global command and TLV descriptor for host_eid batch insert.
Insert host_eid batch payloads through the host_info trie and validate
batch entry counts before programming the table. Keep share_topo mapping
calculation in user space so ubcore does not need to maintain separate
share_topo state.

Fixes: 8e700301346d ("urma: fix well-known jetty create/delete hook up with main ue eid event")

Signed-off-by: Weijie Li <liweijie39@huawei.com>

drivers/ub/urma/ubagg/ubagg_ioctl.c
@@ -1266,10 +1266,13 @@ static int ubagg_update_topo_info(struct ubagg_topo_map *new_topo_map,
 	}
 
 	for (i = 0; i < new_topo_map->node_num; i++) {
+		bool found = false;
+
 		new_node = &new_topo_map->topo_infos[i];
 		for (j = 0; j < old_topo_map->node_num; j++) {
 			old_node = &old_topo_map->topo_infos[j];
 			if (new_node->node_id == old_node->node_id) {
+				found = true;
 				if (update_link_info(new_node, old_node)) {
 					ubagg_log_err("update link info fail.");
 					return -EINVAL;
@@ -1278,7 +1281,17 @@ static int ubagg_update_topo_info(struct ubagg_topo_map *new_topo_map,
 					ubagg_log_err("update dev info fail.");
 					return -EINVAL;
 				}
+				break;
 			}
+		}
+		if (!found) {
+			if (old_topo_map->node_num >= MAX_NODE_NUM) {
+				ubagg_log_err("topo map is full\n");
+				return -ENOSPC;
+			}
+			(void)memcpy(&old_topo_map->topo_infos[old_topo_map->node_num],
+				     new_node, sizeof(*new_node));
+			old_topo_map->node_num++;
 		}
 	}
 
@@ -1735,6 +1748,60 @@ static int ubagg_cmd_set_topo_info(struct ubagg_cmd_hdr *hdr)
 	return 0;
 }
 
+static int ubagg_cmd_get_topo_info(struct ubagg_cmd_hdr *hdr)
+{
+	struct ubagg_topo_info_out *topo_info_out = NULL;
+	struct ubagg_get_topo_info_arg __user *uarg =
+		(void __user *)hdr->args_addr;
+	struct ubagg_get_topo_info_arg arg;
+	int ret;
+
+	if (hdr->args_len != sizeof(struct ubagg_get_topo_info_arg)) {
+		ubagg_log_err(
+			"get topo info, args_len is invalid, args_len:%u\n",
+			hdr->args_len);
+		return -EINVAL;
+	}
+
+	ret = copy_from_user(&arg, (void __user *)hdr->args_addr,
+			     hdr->args_len);
+	if (ret != 0) {
+		ubagg_log_err("copy_from_user fail.");
+		return -EFAULT;
+	}
+	if (arg.out.topo == NULL) {
+		ubagg_log_err("Invalid get_topo_info param\n");
+		return -EINVAL;
+	}
+
+	topo_info_out = get_topo_info();
+	if (!topo_info_out) {
+		ubagg_log_err("ubagg topo info does not exist\n");
+		return -ENXIO;
+	}
+
+	ret = copy_to_user((void __user *)arg.out.topo,
+			   (void *)topo_info_out->topo_info,
+			   sizeof(struct ubagg_topo_node) *
+			   topo_info_out->node_num);
+	if (ret != 0) {
+		ubagg_log_err("copy to user fail, ret:%d", ret);
+		vfree(topo_info_out);
+		return -EFAULT;
+	}
+
+	arg.out.topo_num = topo_info_out->node_num;
+	ret = copy_to_user(&uarg->out.topo_num, &arg.out.topo_num,
+			   sizeof(arg.out.topo_num));
+	vfree(topo_info_out);
+	if (ret != 0) {
+		ubagg_log_err("copy topo num to user fail, ret:%d", ret);
+		return -EFAULT;
+	}
+
+	return 0;
+}
+
 void ubagg_delete_topo_map(void)
 {
 	delete_global_ubagg_topo_map();
@@ -2082,6 +2149,8 @@ long ubagg_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		return 0;
 	case UBAGG_CMD_SET_TOPO_INFO:
 		return ubagg_cmd_set_topo_info(&hdr);
+	case UBAGG_CMD_GET_TOPO_INFO:
+		return ubagg_cmd_get_topo_info(&hdr);
 	case UBAGG_CMD_CREATE_DEV:
 		return ubagg_cmd_create_dev(&hdr);
 	case UBAGG_CMD_DELETE_DEV:

drivers/ub/urma/ubagg/ubagg_ioctl.h
@@ -22,12 +22,20 @@ enum ubagg_cmd {
 	UBAGG_CMD_CREATE_DEV,
 	UBAGG_CMD_DELETE_DEV,
 	UBAGG_CMD_GET_DEV_NAME,
+	UBAGG_CMD_GET_TOPO_INFO,
 };
 
 struct ubagg_cmd_hdr {
 	uint32_t command;
 	uint32_t args_len;
 	uint64_t args_addr;
+};
+
+struct ubagg_get_topo_info_arg {
+	struct {
+		void *topo;
+		uint32_t topo_num;
+	} out;
 };
 
 #define UBAGG_CMD_MAGIC 'B'

drivers/ub/urma/ubcore/ubcore_cmd_tlv.c
@@ -170,6 +170,15 @@ static void ubcore_insert_main_ue_eid_batch_fill_spec_in(void *arg_addr,
 	SPEC(s++, INSERT_MAIN_UE_EID_BATCH_IN_ENTRY, arg->in);
 }
 
+static void ubcore_insert_host_eid_batch_fill_spec_in(void *arg_addr,
+	struct ubcore_cmd_spec *spec)
+{
+	struct ubcore_cmd_host_eid_batch *arg = arg_addr;
+	struct ubcore_cmd_spec *s = spec;
+
+	SPEC(s++, INSERT_HOST_EID_BATCH_IN_ENTRY, arg->in);
+}
+
 static struct ubcore_tlv_handler
 	g_global_tlv_handler[] = {
 		[0] = { 0 },
@@ -224,6 +233,12 @@ static struct ubcore_tlv_handler
 		[UBCORE_CMD_INSERT_MAIN_UE_EID_BATCH] = {
 			ubcore_insert_main_ue_eid_batch_fill_spec_in,
 			INSERT_MAIN_UE_EID_BATCH_IN_NUM,
+			NULL,
+			0,
+		},
+		[UBCORE_CMD_INSERT_HOST_EID_BATCH] = {
+			ubcore_insert_host_eid_batch_fill_spec_in,
+			INSERT_HOST_EID_BATCH_IN_NUM,
 			NULL,
 			0,
 		},

drivers/ub/urma/ubcore/ubcore_cmd_tlv.h
@@ -960,6 +960,11 @@ enum ubcore_insert_main_ue_eid_batch_type {
 	INSERT_MAIN_UE_EID_BATCH_IN_NUM,
 };
 
+enum ubcore_insert_host_eid_batch_type {
+	INSERT_HOST_EID_BATCH_IN_ENTRY,
+	INSERT_HOST_EID_BATCH_IN_NUM,
+};
+
 int ubcore_mue_tlv_parse(struct ubcore_cmd_hdr *hdr, void *arg);
 int ubcore_mue_tlv_append(struct ubcore_cmd_hdr *hdr, void *arg);
 int ubcore_global_tlv_parse(struct ubcore_cmd_hdr *hdr, void *arg);

drivers/ub/urma/ubcore/ubcore_main.c
@@ -19,6 +19,7 @@
 #include "net/ubcore_session.h"
 #include "ubcore_connect_adapter.h"
 #include "ubcore_genl.h"
+#include "ubcore_topo_info.h"
 #include "ubcm/ub_cm.h"
 #include "ubmgr/ubmgr.h"
 

drivers/ub/urma/ubcore/ubcore_topo_info.c
@@ -154,10 +154,13 @@ int ubcore_update_topo_map(struct ubcore_topo_map *new_topo_map,
 	}
 
 	for (i = 0; i < new_topo_map->node_num; i++) {
+		bool found = false;
+
 		new_node = &new_topo_map->topo_infos[i];
 		for (j = 0; j < old_topo_map->node_num; j++) {
 			old_node = &old_topo_map->topo_infos[j];
 			if (new_node->node_id == old_node->node_id) {
+				found = true;
 				if (update_link_info(new_node, old_node)) {
 					ubcore_log_err("update link info failed\n");
 					return -EINVAL;
@@ -166,7 +169,17 @@ int ubcore_update_topo_map(struct ubcore_topo_map *new_topo_map,
 					ubcore_log_err("update dev info failed\n");
 					return -EINVAL;
 				}
+				break;
 			}
+		}
+		if (!found) {
+			if (old_topo_map->node_num >= MAX_NODE_NUM) {
+				ubcore_log_err("topo map is full\n");
+				return -ENOSPC;
+			}
+			(void)memcpy(&old_topo_map->topo_infos[old_topo_map->node_num],
+				     new_node, sizeof(*new_node));
+			old_topo_map->node_num++;
 		}
 	}
 

drivers/ub/urma/ubcore/ubcore_uvs_cmd.c
@@ -18,6 +18,7 @@
 #include "ubcore_priv.h"
 #include "ubcore_cmd_tlv.h"
 #include "ubcore_main_ue_eid.h"
+#include "ubcore_host_info.h"
 #include "ubcore_topo_info.h"
 #include "net/ubcore_cm.h"
 #include "ubcore_uvs_cmd.h"
@@ -277,6 +278,33 @@ static int ubcore_cmd_get_path_set(struct ubcore_global_file *file,
 	return ret;
 }
 
+static int ubcore_insert_host_eid_batch(
+	const union ubcore_eid *host_eid, uint32_t eid_num,
+	const union ubcore_eid *eids)
+{
+	struct ubcore_host_info host_info = {0};
+	uint32_t i;
+	int ret;
+
+	if (host_eid == NULL || eids == NULL || eid_num == 0 ||
+	    eid_num > UBCORE_HOST_EID_BATCH_EID_MAX) {
+		ubcore_log_err("invalid host eid batch.\n");
+		return -EINVAL;
+	}
+
+	host_info.eid = *host_eid;
+	for (i = 0; i < eid_num; i++) {
+		ret = ubcore_insert_host_info(&eids[i], &host_info);
+		if (ret != 0) {
+			ubcore_log_err("insert host eid batch failed, idx: %u, ret: %d\n",
+				       i, ret);
+			return ret;
+		}
+	}
+
+	return 0;
+}
+
 static int ubcore_insert_main_ue_eid_batch(
 	const union ubcore_eid *main_ue_eid, uint32_t eid_num,
 	const union ubcore_eid *eids)
@@ -292,8 +320,11 @@ static int ubcore_insert_main_ue_eid_batch(
 
 	for (i = 0; i < eid_num; i++) {
 		ret = ubcore_insert_main_ue_eid(&eids[i], main_ue_eid);
-		if (ret != 0)
+		if (ret != 0) {
+			ubcore_log_err("insert main ue eid batch failed, idx: %u, ret: %d\n",
+				       i, ret);
 			return ret;
+		}
 	}
 
 	return 0;
@@ -323,7 +354,11 @@ static int ubcore_cmd_delete_main_ue_eid(struct ubcore_global_file *file,
 	if (ret != 0)
 		return ret;
 
-	return ubcore_delete_main_ue_eid(&arg.in.eid);
+	ret = ubcore_delete_main_ue_eid(&arg.in.eid);
+	if (ret != 0)
+		ubcore_log_err("delete main ue eid failed, ret: %d\n", ret);
+
+	return ret;
 }
 
 static int ubcore_cmd_lookup_main_ue_eid(struct ubcore_global_file *file,
@@ -338,8 +373,10 @@ static int ubcore_cmd_lookup_main_ue_eid(struct ubcore_global_file *file,
 
 	ret = ubcore_lookup_main_ue_eid(&arg.in.eid,
 		&arg.out.main_ue_eid);
-	if (ret != 0)
+	if (ret != 0) {
+		ubcore_log_err("lookup main ue eid failed, ret: %d\n", ret);
 		return ret;
+	}
 
 	if (ubcore_global_tlv_append(hdr, (void *)&arg) != 0)
 		return -EPERM;
@@ -378,6 +415,26 @@ static int ubcore_cmd_insert_main_ue_eid_batch(
 	return ret;
 }
 
+static int ubcore_cmd_insert_host_eid_batch(
+	struct ubcore_global_file *file, struct ubcore_cmd_hdr *hdr)
+{
+	struct ubcore_cmd_host_eid_batch *arg;
+	int ret;
+
+	arg = kzalloc(sizeof(*arg), GFP_KERNEL);
+	if (arg == NULL)
+		return -ENOMEM;
+
+	ret = ubcore_global_tlv_parse(hdr, (void *)arg);
+	if (ret == 0)
+		ret = ubcore_insert_host_eid_batch(&arg->in.host_eid,
+						   arg->in.eid_num,
+						   arg->in.eids);
+
+	kfree(arg);
+	return ret;
+}
+
 typedef int (*ubcore_uvs_global_cmd_handler)(struct ubcore_global_file *file,
 					     struct ubcore_cmd_hdr *hdr);
 struct ubcore_uvs_global_cmd_func {
@@ -401,6 +458,8 @@ static struct ubcore_uvs_global_cmd_func g_ubcore_uvs_global_cmd_funcs[] = {
 		ubcore_cmd_flush_main_ue_eid, true },
 	[UBCORE_CMD_INSERT_MAIN_UE_EID_BATCH] = {
 		ubcore_cmd_insert_main_ue_eid_batch, true },
+	[UBCORE_CMD_INSERT_HOST_EID_BATCH] = {
+		ubcore_cmd_insert_host_eid_batch, true },
 };
 
 int ubcore_uvs_global_cmd_parse(struct ubcore_global_file *file,

drivers/ub/urma/ubcore/ubcore_uvs_cmd.h
@@ -27,6 +27,7 @@
 #define UBCORE_MAX_EID_CONFIG_CNT 32
 #define UBCORE_MAX_DSCP_VL_NUM 64
 #define UBCORE_CMD_MAX_MUE_NUM 128
+#define UBCORE_HOST_EID_BATCH_EID_MAX 32
 
 enum ubcore_uvs_global_cmd {
 	UBCORE_CMD_SET_TOPO = 1,
@@ -38,6 +39,7 @@ enum ubcore_uvs_global_cmd {
 	UBCORE_CMD_LOOKUP_MAIN_UE_EID = 7,
 	UBCORE_CMD_FLUSH_MAIN_UE_EID = 8,
 	UBCORE_CMD_INSERT_MAIN_UE_EID_BATCH = 9,
+	UBCORE_CMD_INSERT_HOST_EID_BATCH = 10,
 	UBCORE_CMD_GLOBAL_LAST
 };
 
@@ -102,6 +104,14 @@ struct ubcore_cmd_main_ue_eid_batch {
 		union ubcore_eid main_ue_eid;
 		uint32_t eid_num;
 		union ubcore_eid eids[UBCORE_MAIN_UE_EID_BATCH_EID_MAX];
+	} in;
+};
+
+struct ubcore_cmd_host_eid_batch {
+	struct {
+		union ubcore_eid host_eid;
+		uint32_t eid_num;
+		union ubcore_eid eids[UBCORE_HOST_EID_BATCH_EID_MAX];
 	} in;
 };
 