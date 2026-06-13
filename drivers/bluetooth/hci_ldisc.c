754a7c09e281d7bfc6bbaee3aa037cfb998d7ed4
Pauli Virtanen <pav@iki.fi>
2026-06-13 21:43:37 +0300

    Bluetooth: hci_uart: clear HCI_UART_SENDING when write_work is canceled
    mainline inclusion
    from mainline-v7.2-rc3
    commit 1b0d946d6f08bd39211385bc703a440911b41e46
    category: bugfix
    bugzilla: https://atomgit.com/src-openeuler/kernel/issues/15540
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=1b0d946d6f08bd39211385bc703a440911b41e46
    
    (cherry-pick from OLK-6.6 commit bb67ca752dd3)
    
    --------------------------------
    
    HCI_UART_SENDING bit in tx_state means write_work is pending and blocks
    queueing it again.  Currently this bit is not cleared when canceling the
    work in hci_uart_close(), which blocks future writes when device is
    reopened later if write_work was pending.
    
    Fix by clearing HCI_UART_SENDING when canceling the work.
    
    Also make clearing of tx_skb safe by using disable_work_sync +
    enable_work instead of just cancel_work_sync.  hci_uart_flush() purges
    the proto tx queue so we can cancel the pending write_work there,
    instead of doing it just in hci_uart_close().  Re-enable and possibly
    requeue the work after queue flush.
    
    Fixes: c1bb9336ae6b ("Bluetooth: hci_uart: fix UAFs and race conditions in close and init paths")
    Link: https://lore.kernel.org/linux-bluetooth/07e0a28650773abec711ee492fdb1bf5d21a6c98.camel@iki.fi/
    Cc: stable@vger.kernel.org
    Signed-off-by: Pauli Virtanen <pav@iki.fi>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    Signed-off-by: Qingshuang Fu <fuqingshuang@kylinos.cn>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- drivers/bluetooth/hci_ldisc.c
+++ drivers/bluetooth/hci_ldisc.c
@@ -281,11 +281,7 @@ static int hci_uart_open(struct hci_dev *hdev)
 /* Close device */
 static int hci_uart_close(struct hci_dev *hdev)
 {
-	struct hci_uart *hu = hci_get_drvdata(hdev);
-
 	BT_DBG("hdev %p", hdev);
-
-	cancel_work_sync(&hu->write_work);
 
 	hci_uart_flush(hdev);
 	hdev->flush = NULL;
