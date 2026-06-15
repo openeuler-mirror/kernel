9f31ad62c6d7dfe268bc356491b296eee4e58f59
Jijie Shao <shaojijie@huawei.com>
2026-06-15 16:23:42 +0800

    Revert "net: hns3: support tc flow for vf"
    driver inclusion
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/9473
    
    ----------------------------------------------------------------------
    
    This reverts commit 1e3975b039dde899431fd15da52fbf73b499cf0a.
    
    Signed-off-by: Jijie Shao <shaojijie@huawei.com>
    Signed-off-by: Haibing Xie <xiehaibing7@h-partners.com>

--- drivers/net/ethernet/hisilicon/hns3/hns3_enet.c
+++ drivers/net/ethernet/hisilicon/hns3/hns3_enet.c
@@ -2831,10 +2831,9 @@ static int hns3_setup_tc_cls_flower(struct hns3_nic_priv *priv,
 static int hns3_setup_tc_block_cb(enum tc_setup_type type, void *type_data,
 				  void *cb_priv)
 {
-	struct flow_cls_common_offload *common = type_data;
 	struct hns3_nic_priv *priv = cb_priv;
 
-	if (!tc_can_offload_extack(priv->netdev, common->extack))
+	if (!tc_cls_can_offload_and_chain0(priv->netdev, type_data))
 		return -EOPNOTSUPP;
 
 	switch (type) {
