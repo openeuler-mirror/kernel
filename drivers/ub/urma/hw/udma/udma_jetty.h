00775902af79b52fa6b64b343ef8cfdce9ea3e4b
Wang Hai <wanghai38@huawei.com>
2026-06-25 23:35:18 +0800

    ub: udma: fix issues of safe mode
    driver inclusion
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/9416
    CVE: NA
    
    (cherry-pick from OLK-6.6 commit 9352061da409)
    
    -----------------------------------------------------------
    
    This patch fix issues of context verification in safe mode
    1. only the ver_0 version requires performing the pre-flush steps.
    2. the overflow is only validated when db_status is 0.
    
    Fixes: 8b50280f90bd ("ub: udma: Supports Safe Mode.")
    
    Signed-off-by: Jiawei Kang <kangjiawei2@huawei.com>
    
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- drivers/ub/urma/hw/udma/udma_jetty.h
+++ drivers/ub/urma/hw/udma/udma_jetty.h
@@ -262,36 +262,20 @@ static inline struct udma_jetty *to_udma_jetty_from_queue(struct udma_jetty_queu
 	return container_of(queue, struct udma_jetty, sq);
 }
 
-static inline uint32_t udma_get_ta_timeout(uint8_t gear)
-{
-#define GEAR_0	0
-#define GEAR_1	1
-#define GEAR_2	2
-#define GEAR_3	3
-
-	switch (gear) {
-	case GEAR_0: return UDMA_TA_TIMEOUT_128MS;
-	case GEAR_1: return UDMA_TA_TIMEOUT_1000MS;
-	case GEAR_2: return UDMA_TA_TIMEOUT_8000MS;
-	case GEAR_3: return UDMA_TA_TIMEOUT_64000MS;
-	default: return UDMA_TA_TIMEOUT_64000MS;
-	}
-}
-
 static inline uint8_t udma_get_ta_timeout_gear(struct udma_dev *udev, uint32_t err_timeout)
 {
 #define TA_TIMEOUT_DIVISOR 8
 #define UDMA_TA_TIMEOUT_MAX_INDEX 3
 
 	uint8_t ta_timeout_gear = err_timeout / TA_TIMEOUT_DIVISOR;
-	uint32_t hw_ver = ubase_get_hw_ver(udev->comdev.adev);
-
 	if ((ta_timeout_gear >= UDMA_TA_TIMEOUT_MAX_INDEX) &&
-	    ((hw_ver == UBASE_HW_VER_A_0) || (hw_ver == UBASE_HW_VER_K_0)))
+		((udev->hw_ver == UBASE_HW_VER_A_0) || (udev->hw_ver == UBASE_HW_VER_K_0)))
 		ta_timeout_gear = (UDMA_TA_TIMEOUT_MAX_INDEX - 1);
 
 	return ta_timeout_gear;
 }
+
+uint32_t udma_get_ta_timeout(uint8_t gear);
 
 static inline void udma_set_query_flush_time(struct udma_dev *udev, struct udma_jetty_queue *sq,
 					     uint8_t err_timeout)
