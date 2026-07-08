cf05d7a3785909b909e1f229794facafc2ba3eea
Amir Goldstein <amir73il@gmail.com>
2026-07-08 14:49:09 +0800

    fs: constify file ptr in backing_file accessor helpers
    stable inclusion
    from stable-v6.12.95
    commit 28f020bce41540e6cb27ee2565929c75d11e28d1
    category: bugfix
    bugzilla: https://atomgit.com/src-openeuler/kernel/issues/15316
    CVE: CVE-2026-46054
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=28f020bce41540e6cb27ee2565929c75d11e28d1
    
    --------------------------------
    
    [ Upstream commit 4e301d858af17ae2ce56886296e5458c5a08219a ]
    
    Add internal helper backing_file_set_user_path() for the only
    two cases that need to modify backing_file fields.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Link: https://lore.kernel.org/20250607115304.2521155-2-amir73il@gmail.com
    Signed-off-by: Christian Brauner <brauner@kernel.org>
    Signed-off-by: Cai Xinchen <caixinchen1@huawei.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Conflicts:
    	fs/backing-file.c
    	fs/file_table.c
    	include/linux/fs.h
    [The function backing_tmpfile_open is not backport in OLK-6.6. Others
    are context conflicts.]
    Signed-off-by: Cai Xinchen <caixinchen1@huawei.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2657,7 +2657,7 @@ struct file *dentry_open(const struct path *path, int flags,
 			 const struct cred *creds);
 struct file *dentry_create(const struct path *path, int flags, umode_t mode,
 			   const struct cred *cred);
-struct path *backing_file_user_path(struct file *f);
+struct path *backing_file_user_path(const struct file *f);
 
 /*
  * file_user_path - get the path to display for memory mapped file
@@ -2668,7 +2668,7 @@ struct path *backing_file_user_path(struct file *f);
  * /proc/<pid>/maps), this helper should be used to get the path to display
  * to the user, which is the path of the fd that user has requested to map.
  */
-static inline const struct path *file_user_path(struct file *f)
+static inline const struct path *file_user_path(const struct file *f)
 {
 	if (unlikely(f->f_mode & FMODE_BACKING))
 		return backing_file_user_path(f);
