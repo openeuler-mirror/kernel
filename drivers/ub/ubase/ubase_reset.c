openeuler-mirror/kernel

commit 2368191f131b24fac87496337aea5079250c6ba1

ub: ubase&unic&cdma&udma: Supporting the restart optimization of the management and control process
drivers inclusion
category: feature
bugzilla: https://atomgit.com/openeuler/kernel/issues/9110
CVE: NA

--------------------------------------------------------------------------

This patch does the following:
1.ubase driver add UBASE_STATE_RST_TIMEOUT_RETY_B to indicates the
timeout in communication with the MAMI when elr resetting and need
retry.
2.ubase driver add UBASE_RESET_STAGE_ABORT to instruct the auxiliary
device to prepare for the next elr reset.
3.If the elr reset fails due to a timeout in communication with the
MAMI, the elr reset is restarted after 5s.
4.ubase driver add UBASE_STATE_PROBE_AGAIN_B to indicates the probe
state of udev.
5.ubase provide a interface called ubase_update_adev_status for the
auxiliary device to update the probe status of auxiliary device.
6.unic driver set probe type PROBE_FORCE_SYNCHRONOUS.
7.unic will use the ubase_update_adev_status to update the adev status
when connection with mami preocess timeout.

The modifications to the cdma driver are as follows:
The device is running properly. Only when the management
and control process is restarted, the software table data
retained in the process is lost. The corresponding backup
and restoration functions are implemented.

The modifications to the udma driver are as follows:
This patch adds the feature to optimize the reset process.
This includes optimizing the RX functionality for opening
UE and retry process. The driver removal process has also
been optimized.

Signed-off-by: Shi Long <shilong10@h-partners.com>
Signed-off-by: Zhang Lei <zhanglei917@huawei.com>
Signed-off-by: Xu Wang <wangxu341@h-partners.com>
Signed-off-by: Zhipeng Lu <luzhipeng8@h-partners.com>
Signed-off-by: Jiawei Kang <kangjiawei2@huawei.com>

drivers/ub/ubase/ubase_reset.c
@@ -5,6 +5,7 @@
  */
 
 #include <linux/delay.h>
+#include <linux/limits.h>
 #include <ub/ubase/ubase_comm_cmd.h>
 
 #include "debugfs/ubase_debugfs.h"
@@ -16,30 +17,57 @@
 #include "ubase_ubus.h"
 #include "ubase_reset.h"
 
+static void __ubase_reset_task_schedule(struct ubase_dev *udev,
+					unsigned long delay)
+{
+	ubase_info(udev, "schedule reset task(%u), delay = %ums.\n",
+		   udev->reset_stat.reset_retry_cnt, jiffies_to_msecs(delay));
+
+	udev->last_reset_scheduled = jiffies;
+	mod_delayed_work(udev->ubase_reset_wq,
+			 &udev->reset_service_task.service_task, delay);
+}
+
 static void ubase_reset_task_schedule(struct ubase_dev *udev)
 {
-#define RESET_TASK_DELAY_TIME msecs_to_jiffies(10)
+#define RESET_TASK_DELAY_TIME		msecs_to_jiffies(10)
+#define RESET_TASK_DELAY_TIME_5S	msecs_to_jiffies(5000)
+
+	unsigned long delay =
+		test_bit(UBASE_STATE_RST_TIMEOUT_RETRY_B, &udev->state_bits) ?
+			 RESET_TASK_DELAY_TIME_5S : RESET_TASK_DELAY_TIME;
 
 	if (!test_and_set_bit(UBASE_SERVICE_STATE_RESET_SCHED,
-			      &udev->service_task.state)) {
-		udev->last_reset_scheduled = jiffies;
-		mod_delayed_work(udev->ubase_reset_wq,
-				 &udev->reset_service_task.service_task,
-				 RESET_TASK_DELAY_TIME);
-	}
+			      &udev->service_task.state))
+		__ubase_reset_task_schedule(udev, delay);
+}
+
+void ubase_reset_task_schedule_immediately(struct ubase_dev *udev)
+{
+#define RESET_TASK_DELAY_TIME_IM	msecs_to_jiffies(4)
+
+	set_bit(UBASE_SERVICE_STATE_RESET_SCHED, &udev->service_task.state);
+
+	__ubase_reset_task_schedule(udev, RESET_TASK_DELAY_TIME_IM);
 }
 
 static void ubase_reset_err_handle(struct ubase_dev *udev)
 {
+#define UBASE_RST_MAX_RETRY_CNT	5
+
+	u32 reset_max_cnt;
+
 	if (test_bit(UBASE_STATE_REMOVING_B, &udev->state_bits))
 		return;
 
+	reset_max_cnt =
+		test_bit(UBASE_STATE_RST_TIMEOUT_RETRY_B, &udev->state_bits) ?
+			 U32_MAX : UBASE_RST_MAX_RETRY_CNT;
+
 	udev->reset_stat.reset_fail_cnt++;
 	udev->reset_stat.reset_retry_cnt++;
-	if (udev->reset_stat.reset_retry_cnt < UBASE_RST_MAX_RETRY_CNT) {
+	if (udev->reset_stat.reset_retry_cnt < reset_max_cnt) {
 		ubase_reset_task_schedule(udev);
-		ubase_info(udev, "re-schedule reset task(%u).\n",
-			   udev->reset_stat.reset_retry_cnt);
 		return;
 	}
 
@@ -74,7 +102,7 @@ void ubase_reset_service(struct ubase_delay_work *ubase_work)
 			   smp_processor_id());
 
 	ret = ubase_ubus_reset_entry(udev->dev);
-	if (ret)
+	if (ret == -EBUSY)
 		ubase_reset_err_handle(udev);
 }
 
@@ -198,7 +226,7 @@ static int ubase_ue_reset_done_check(struct ubase_dev *udev)
 	}
 
 	ubase_warn(udev, "wait reset done reg time out.\n");
-	return -EBUSY;
+	return -ETIMEDOUT;
 }
 
 static void ubase_reset_done(struct ubase_dev *udev)
@@ -238,10 +266,12 @@ void ubase_suspend(struct ubase_dev *udev)
 		return;
 	}
 
+	clear_bit(UBASE_STATE_RST_TIMEOUT_RETRY_B, &udev->state_bits);
+
 	ubase_notify_all_ue_reset(udev);
 
 	udev->reset_stage = UBASE_RESET_STAGE_DOWN;
-	ubase_suspend_aux_devices(udev);
+	ubase_suspend_aux_devices(udev, UBASE_RESET_STAGE_DOWN);
 	ubase_wait_ue_reset_ready(udev);
 	udev->reset_stage = UBASE_RESET_STAGE_UNINIT;
 
@@ -255,7 +285,7 @@ void ubase_suspend(struct ubase_dev *udev)
 	ubase_flush_workqueue(udev);
 }
 
-void ubase_resume(struct ubase_dev *udev)
+void ubase_resume(struct ubase_dev *udev, int pret)
 {
 	int ret;
 
@@ -267,7 +297,7 @@ void ubase_resume(struct ubase_dev *udev)
 	}
 
 	if (ubase_dev_pmu_supported(udev)) {
-		ubase_ubus_reinit(udev->dev);
+		ubase_ubus_reset_init(udev->dev);
 		__ubase_cmd_enable(udev);
 		udev->reset_stat.reset_done_cnt++;
 		udev->reset_stat.hw_reset_done_cnt++;
@@ -277,22 +307,38 @@ void ubase_resume(struct ubase_dev *udev)
 		return;
 	}
 
+	if (pret == -ETIMEDOUT) {
+		/* Try to restore resource space. Otherwise, if a removing
+		 * is performed immediately after this ELR, issues may arise
+		 * because the CMDQ will be unavailable.
+		 */
+		ubase_ubus_reset_init(udev->dev);
+		goto timeout_resume;
+	}
+
 	clear_bit(UBASE_STATE_RST_WAIT_DEACTIVE_B, &udev->state_bits);
 	udev->reset_stat.hw_reset_done_cnt++;
-	ubase_suspend_aux_devices(udev);
+	ubase_suspend_aux_devices(udev, UBASE_RESET_STAGE_UNINIT);
 	ubase_dev_reset_uninit(udev);
-	ubase_ubus_reinit(udev->dev);
+	ret = ubase_ubus_reset_init(udev->dev);
+	if (ret == -ETIMEDOUT)
+		goto timeout_resume;
 
 	udev->reset_stage = UBASE_RESET_STAGE_NONE;
 	ret = ubase_ue_reset_done_check(udev);
 	if (ret)
-		goto err_resume;
+		goto timeout_resume;
 
 	ret = ubase_dev_reset_init(udev);
-	if (ret)
+	if (ret) {
+		if (test_and_clear_bit(UBASE_STATE_INIT_AGAIN_B, &udev->state_bits))
+			goto timeout_resume;
 		goto err_resume;
+	}
 
-	ubase_resume_aux_devices(udev);
+	ret = ubase_resume_aux_devices(udev, UBASE_RESET_STAGE_INIT);
+	if (ret == -EAGAIN)
+		goto timeout_resume;
 	ubase_reset_done(udev);
 
 	udev->reset_stat.reset_done_cnt++;
@@ -301,8 +347,11 @@ void ubase_resume(struct ubase_dev *udev)
 	clear_bit(UBASE_STATE_DISABLED_B, &udev->state_bits);
 	return;
 
+timeout_resume:
+	udev->reset_stage = UBASE_RESET_STAGE_NONE;
+	set_bit(UBASE_STATE_RST_TIMEOUT_RETRY_B, &udev->state_bits);
 err_resume:
-	ubase_resume_aux_devices(udev);
+	ubase_resume_aux_devices(udev, UBASE_RESET_STAGE_ABORT);
 	clear_bit(UBASE_STATE_RST_HANDLING_B, &udev->state_bits);
 	clear_bit(UBASE_STATE_DISABLED_B, &udev->state_bits);
 	ubase_reset_err_handle(udev);

drivers/net/ub/unic/unic_dev.c
@@ -1102,11 +1102,20 @@ int unic_dev_init(struct auxiliary_device *adev)
 		goto err_unregister_event;
 	}
 
-	unic_query_ip_addr(adev);
+	ret = unic_query_ip_addr(adev);
+	if (ret) {
+		if (ret == -ETIMEDOUT)
+			ubase_update_adev_status(adev, UBASE_ADEV_PROBE_FAIL);
+
+		goto err_unregister_netdev;
+	}
+
 	unic_start_dev_period_task(netdev);
 
 	return 0;
 
+err_unregister_netdev:
+	unregister_netdev(netdev);
 err_unregister_event:
 	unic_unregister_event(adev);
 err_uninit_netdev_priv:

drivers/net/ub/unic/unic_ip.c
@@ -674,7 +674,7 @@ static void unic_update_ip_list(struct unic_vport *vport,
 	spin_unlock_bh(&vport->addr_tbl.ip_list_lock);
 }
 
-void unic_query_ip_addr(struct auxiliary_device *adev)
+int unic_query_ip_addr(struct auxiliary_device *adev)
 {
 #define UNIC_LOOP_COUNT(total_size, size) ((total_size) / (size) + 1)
 
@@ -687,7 +687,7 @@ void unic_query_ip_addr(struct auxiliary_device *adev)
 	int ret;
 
 	if (!unic_dev_ubl_supported(priv))
-		return;
+		return 0;
 
 	set_bit(UNIC_VPORT_STATE_IP_QUERYING, &priv->vport.state);
 	INIT_LIST_HEAD(&tmp_list);
@@ -726,6 +726,8 @@ void unic_query_ip_addr(struct auxiliary_device *adev)
 		list_del(&ip_node->node);
 		kfree(ip_node);
 	}
+
+	return ret;
 }
 
 void unic_uninit_ip_table(struct unic_dev *unic_dev)

drivers/net/ub/unic/unic_ip.h
@@ -75,7 +75,7 @@ static inline void unic_format_masked_ip_addr(char *format_masked_ip_addr,
 void unic_sync_ip_table(struct unic_dev *unic_dev);
 int unic_handle_notify_ip_event(struct auxiliary_device *adev, u8 service_ver,
 				void *data, u16 len, u16 seq);
-void unic_query_ip_addr(struct auxiliary_device *adev);
+int unic_query_ip_addr(struct auxiliary_device *adev);
 void unic_uninit_ip_table(struct unic_dev *unic_dev);
 int unic_add_ip_addr(struct unic_dev *unic_dev, struct sockaddr *addr,
 		     u16 ip_mask);

drivers/net/ub/unic/unic_main.c
@@ -72,6 +72,9 @@ static struct auxiliary_driver unic_drv = {
 	.probe = unic_probe,
 	.remove = unic_remove,
 	.name = "unic",
+	.driver = {
+		.probe_type = PROBE_FORCE_SYNCHRONOUS,
+	},
 	.id_table = unic_id_table,
 };
 

drivers/net/ub/unic/unic_reset.c
@@ -78,20 +78,25 @@ static int unic_dev_resume(struct unic_dev *unic_dev)
 	return ret;
 }
 
-static void unic_reset_init(struct auxiliary_device *adev)
+static int unic_reset_init(struct auxiliary_device *adev)
 {
 	struct unic_dev *priv = (struct unic_dev *)dev_get_drvdata(&adev->dev);
 	struct net_device *netdev = priv->comdev.netdev;
 	int ret;
 
 	if (!test_bit(UNIC_STATE_RESETTING, &priv->state))
-		return;
+		return 0;
+
+	ret = unic_query_ip_addr(adev);
+	if (ret == -ETIMEDOUT) {
+		ret = -EAGAIN;
+		goto err_unic_resume;
+	}
 
 	ret = unic_dev_resume(priv);
 	if (ret)
 		goto err_unic_resume;
 
-	unic_query_ip_addr(adev);
 	unic_start_period_task(netdev);
 
 	clear_bit(UNIC_STATE_RESETTING, &priv->state);
@@ -104,16 +109,31 @@ static void unic_reset_init(struct auxiliary_device *adev)
 
 	unic_info(priv, "unic reset done.\n");
 
-	return;
+	return 0;
 
 err_unic_resume:
 	clear_bit(UNIC_STATE_RESETTING, &priv->state);
 	clear_bit(UNIC_STATE_DISABLED, &priv->state);
+
+	return ret;
 }
 
-void unic_reset_handler(struct auxiliary_device *adev,
-			enum ubase_reset_stage stage)
+static void unic_reset_abort(struct auxiliary_device *adev)
 {
+	struct unic_dev *priv = (struct unic_dev *)dev_get_drvdata(&adev->dev);
+
+	if (!test_bit(UNIC_STATE_RESETTING, &priv->state))
+		return;
+
+	clear_bit(UNIC_STATE_RESETTING, &priv->state);
+	clear_bit(UNIC_STATE_DISABLED, &priv->state);
+}
+
+int unic_reset_handler(struct auxiliary_device *adev,
+		       enum ubase_reset_stage stage)
+{
+	int ret = 0;
+
 	switch (stage) {
 	case UBASE_RESET_STAGE_DOWN:
 		unic_reset_down(adev);
@@ -122,9 +142,14 @@ void unic_reset_handler(struct auxiliary_device *adev,
 		unic_reset_uninit(adev);
 		break;
 	case UBASE_RESET_STAGE_INIT:
-		unic_reset_init(adev);
+		ret = unic_reset_init(adev);
+		break;
+	case UBASE_RESET_STAGE_ABORT:
+		unic_reset_abort(adev);
 		break;
 	default:
 		break;
 	}
+
+	return ret;
 }

drivers/net/ub/unic/unic_reset.h
@@ -9,6 +9,6 @@
 
 #include <ub/ubase/ubase_comm_dev.h>
 
-void unic_reset_handler(struct auxiliary_device *adev, enum ubase_reset_stage stage);
+int unic_reset_handler(struct auxiliary_device *adev, enum ubase_reset_stage stage);
 
 #endif /* __UNIC_RESET_H__ */

drivers/ub/cdma/cdma_cmd.c
@@ -223,8 +223,9 @@ void cdma_cmd_inc(struct cdma_dev *cdev)
 
 void cdma_cmd_dec(struct cdma_dev *cdev)
 {
-	if (atomic_dec_and_test(&cdev->cmdcnt))
-		complete(&cdev->cmddone);
+	if (atomic_add_unless(&cdev->cmdcnt, -1, 1))
+		return;
+	complete(&cdev->cmddone);
 }
 
 void cdma_cmd_flush(struct cdma_dev *cdev)
@@ -242,8 +243,9 @@ void cdma_kcmd_inc(struct cdma_dev *cdev)
 
 void cdma_kcmd_dec(struct cdma_dev *cdev)
 {
-	if (atomic_dec_and_test(&cdev->kcmdcnt))
-		complete(&cdev->kcmddone);
+	if (atomic_add_unless(&cdev->kcmdcnt, -1, 1))
+		return;
+	complete(&cdev->kcmddone);
 }
 
 void cdma_kcmd_flush(struct cdma_dev *cdev)

commit a542cc139929037c91d76f8742c38c342e747144

ub: ubase: fix probabilistic reset failure issue in
drivers inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/9786
CVE: NA

--------------------------------------------------------------------------

The ubase_ubus_reinit flow holds the device lock until reinit
completes. Meanwhile, ub_reset_entity acquires the same lock via
device_trylock, which fails immediately with -EBUSY if the lock is
still held by reinit. Under certain timing conditions this lock
contention causes the reset to fail probabilistically.

To mitigate the contention window:
1. Increase RESET_TASK_DELAY_TIME_IM from 50ms to 100ms to give the
reset task scheduled from the RST_FAILED path a larger window
before attempting to acquire the device lock.
2. Clear reset_retry_cnt in ubase_reset_err_handle() after retry
count reaches the limit.

Fixes: 2368191f131b ("ub: ubase&unic&cdma&udma: Supporting the restart optimization of the management and control process")
Signed-off-by: mufengyan <mufengyan@huawei.com>
Signed-off-by: Zhang lei <zhanglei@huawei.com>

drivers/ub/ubase/ubase_reset.c
@@ -44,7 +44,7 @@ static void ubase_reset_task_schedule(struct ubase_dev *udev)
 
 void ubase_reset_task_schedule_immediately(struct ubase_dev *udev)
 {
-#define RESET_TASK_DELAY_TIME_IM	msecs_to_jiffies(50)
+#define RESET_TASK_DELAY_TIME_IM	msecs_to_jiffies(100)
 
 	set_bit(UBASE_SERVICE_STATE_RESET_SCHED, &udev->service_task.state);
 
@@ -66,6 +66,7 @@ static void ubase_reset_err_handle(struct ubase_dev *udev)
 		return;
 	}
 
+	udev->reset_stat.reset_retry_cnt = 0;
 	ubase_err(udev, "failed to reset, too many attempts.\n");
 }
 