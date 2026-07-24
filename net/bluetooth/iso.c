ec1725198f9fffdcc6e4c12226fd9d24c5b704bd
Pauli Virtanen <pav@iki.fi>
2026-07-24 23:20:32 +0300

    Bluetooth: ISO: ensure no dangling hcon references in iso_conn
    mainline inclusion
    from mainline-v7.2-rc6
    commit aa9f7cb2bd3a2be998ceb739fc9a2f986eba43eb
    category: bugfix
    bugzilla: https://atomgit.com/src-openeuler/kernel/issues/18470
    CVE: CVE-2026-80721
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=aa9f7cb2bd3a2be998ceb739fc9a2f986eba43eb
    
    (cherry-pick from OLK-6.6 commit f6bbe32b4764)
    
    --------------------------------
    
    After iso_conn_del(), ISO sockets should not dereference the hcon any
    more.  Currently, clearing iso_conn::hcon relies on iso_conn_del()
    releasing the last reference to the iso_conn.
    
    Simplify this by explicitly clearing conn->hcon in iso_conn_del(), to
    avoid more complex reasoning on races about who holds the last
    reference.
    
    Signed-off-by: Pauli Virtanen <pav@iki.fi>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Conflicts:
    	net/bluetooth/iso.c
    [Context conflicts]
    Signed-off-by: Yao Yiqi <yaoyiqi3@huawei.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- net/bluetooth/iso.c
+++ net/bluetooth/iso.c
@@ -276,6 +276,7 @@ static bool iso_match_conn_sync_handle(struct sock *sk, void *data)
 }
 
 static void iso_conn_del(struct hci_conn *hcon, int err)
+	__must_hold(&hcon->hdev->lock)
 {
 	struct iso_conn *conn = hcon->iso_data;
 	struct sock *sk;
@@ -291,11 +292,10 @@ static void iso_conn_del(struct hci_conn *hcon, int err)
 	iso_conn_lock(conn);
 	sk = iso_sock_hold(conn);
 	iso_conn_unlock(conn);
-	iso_conn_put(conn);
 
 	if (!sk) {
 		iso_conn_put(conn);
-		return;
+		goto done;
 	}
 
 	lock_sock(sk);
@@ -323,6 +323,14 @@ static void iso_conn_del(struct hci_conn *hcon, int err)
 	iso_sock_kill(sk);
 	sock_put(sk);
 
+done:
+	/* No sk access to conn->hcon any more (lock_sock + hdev->lock) */
+	iso_conn_lock(conn);
+	conn->hcon = NULL;
+	hcon->iso_data = NULL;
+	iso_conn_unlock(conn);
+
+	iso_conn_put(conn);
 }
 
 static int __iso_chan_add(struct iso_conn *conn, struct sock *sk,
@@ -336,6 +344,11 @@ static int __iso_chan_add(struct iso_conn *conn, struct sock *sk,
 	if (conn->sk) {
 		BT_ERR("conn->sk already set");
 		return -EBUSY;
+	}
+
+	if (!conn->hcon) {
+		BT_ERR("conn->hcon missing");
+		return -EIO;
 	}
 
 	iso_pi(sk)->conn = conn;
@@ -2055,6 +2068,7 @@ done:
 }
 
 static void iso_connect_cfm(struct hci_conn *hcon, __u8 status)
+	__must_hold(&hcon->hdev->lock)
 {
 	if (hcon->type != CIS_LINK && hcon->type != BIS_LINK) {
 		if (hcon->type != LE_LINK)
@@ -2065,8 +2079,10 @@ static void iso_connect_cfm(struct hci_conn *hcon, __u8 status)
 			struct hci_link *link, *t;
 
 			list_for_each_entry_safe(link, t, &hcon->link_list,
-						 list)
+						 list) {
+				lockdep_assert_held(&link->conn->hdev->lock);
 				iso_conn_del(link->conn, bt_to_errno(status));
+			}
 
 			return;
 		}
@@ -2096,6 +2112,7 @@ static void iso_connect_cfm(struct hci_conn *hcon, __u8 status)
 }
 
 static void iso_disconn_cfm(struct hci_conn *hcon, __u8 reason)
+	__must_hold(&hcon->hdev->lock)
 {
 	if (hcon->type != CIS_LINK && hcon->type != BIS_LINK)
 		return;
