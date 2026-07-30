fda7f68fd128fe08259cc7b7b9efb12c3f807d12
Amir Goldstein <amir73il@gmail.com>
2026-07-30 21:49:11 +0800

    fs: get mnt_writers count for an open backing file's real path
    mainline inclusion
    from mainline-v6.7-rc1
    commit 83bc1d294130cc471a89ce10770daa281a93fcb0
    category: bugfix
    bugzilla: https://atomgit.com/src-openeuler/kernel/issues/15316
    CVE: CVE-2026-46054
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=83bc1d294130cc471a89ce10770daa281a93fcb0
    
    --------------------------------
    
    A writeable mapped backing file can perform writes to the real inode.
    Therefore, the real path mount must be kept writable so long as the
    writable map exists.
    
    This may not be strictly needed for ovelrayfs private upper mount,
    but it is correct to take the mnt_writers count in the vfs helper.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Link: https://lore.kernel.org/r/20231009153712.1566422-2-amir73il@gmail.com
    Signed-off-by: Christian Brauner <brauner@kernel.org>
    Conflicts:
    	fs/internal.h
    	fs/open.c
    [fs/internal.h: 5.10 use __mnt_drop_write instead of
    mnt_put_write_access.
    fs/open.c: 5.10 use __mnt_want_write instead of mnt_get_write_access.]
    Signed-off-by: Cai Xinchen <caixinchen1@huawei.com>

--- fs/internal.h
+++ fs/internal.h
@@ -111,13 +111,20 @@ struct file *alloc_empty_file(int flags, const struct cred *cred);
 struct file *alloc_empty_file_noaccount(int flags, const struct cred *cred);
 struct file *alloc_empty_backing_file(int flags, const struct cred *cred);
 
+static inline void file_put_write_access(struct file *file)
+{
+	put_write_access(file->f_inode);
+	__mnt_drop_write(file->f_path.mnt);
+	if (unlikely(file->f_mode & FMODE_BACKING))
+		__mnt_drop_write(backing_file_real_path(file)->mnt);
+}
+
 static inline void put_file_access(struct file *file)
 {
 	if ((file->f_mode & (FMODE_READ | FMODE_WRITE)) == FMODE_READ) {
 		i_readcount_dec(file->f_inode);
 	} else if (file->f_mode & FMODE_WRITER) {
-		put_write_access(file->f_inode);
-		__mnt_drop_write(file->f_path.mnt);
+		file_put_write_access(file);
 	}
 }
 
