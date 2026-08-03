11eac461ef43a2b3027a0b54ebd29a50318c173e
Jann Horn <jannh@google.com>
2026-08-03 08:23:32 +0000

    bpf,fork: wipe ->bpf_storage before bailouts that access it
    stable inclusion
    from stable-v6.6.145
    commit c3fd6f28c7ce1142a3b23dbb840eaa4777de1d74
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/9693
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=c3fd6f28c7ce
    
    --------------------------------
    
    commit 9b51a6155d14389876916726430da30eabb1d4ed upstream.
    
    Currently, copy_process() can bail out to free_task() before p->bpf_storage
    has been initialized, with this call graph (shown here for the
    !CONFIG_MEMCG case):
    
    copy_process
      dup_task_struct
        arch_dup_task_struct
          [copies the entire task_struct, including ->bpf_storage member]
      [RLIMIT_NPROC check fails]
      delayed_free_task
        free_task
          bpf_task_storage_free
            rcu_dereference(task->bpf_storage)
            bpf_local_storage_destroy
    
    In this case, the nascent task's ->bpf_storage member that
    bpf_local_storage_destroy() operates on is a plain copy of the parent's
    ->bpf_storage pointer, not a real initialized pointer.
    This leads to badness (kernel hangs, UAF).
    
    This is reachable as long as the process calling fork() has been inserted
    into a task storage map.
    
    Cc: stable@kernel.org
    Fixes: a10787e6d58c ("bpf: Enable task local storage for tracing programs")
    Signed-off-by: Jann Horn <jannh@google.com>
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Conflicts:
    	kernel/fork.c
    [ctx conflicts]
    Signed-off-by: Pu Lehui <pulehui@huawei.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1290,6 +1290,11 @@ static struct task_struct *dup_task_struct(struct task_struct *orig, int node)
 	tsk->migrate_from_cpu = -1;
 #endif
 
+#ifdef CONFIG_BPF_SYSCALL
+	RCU_INIT_POINTER(tsk->bpf_storage, NULL);
+	tsk->bpf_ctx = NULL;
+#endif
+
 #ifdef CONFIG_FAST_SYSCALL
 	tsk->xinfo = NULL;
 #endif
@@ -2607,10 +2612,6 @@ __latent_entropy struct task_struct *copy_process(
 #ifdef CONFIG_BCACHE
 	p->sequential_io	= 0;
 	p->sequential_io_avg	= 0;
-#endif
-#ifdef CONFIG_BPF_SYSCALL
-	RCU_INIT_POINTER(p->bpf_storage, NULL);
-	p->bpf_ctx = NULL;
 #endif
 
 	/* Perform scheduler related setup. Assign this task to a CPU. */
