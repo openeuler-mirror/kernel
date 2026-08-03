1cc0eae9ea4cc430c8dda8884ba99d663dbcab18
Jiashun Wang <wangjiashun@huawei.com>
2026-08-03 09:36:02 +0800

    iommu/ummu: Move ioplb_sync to flush.c
    ummu inclusion
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/9742
    
    ----------------------------------------
    
    Plbi belongs to the flush sub-function and should not be placed in
    the sva sub-function. Therefore, its implementation position should
    be adjusted.
    
    Fixes: 619a7ebff3eb ("iommu/ummu: Add SVA API support to UMMU driver")
    
    Signed-off-by: Jiashun Wang <wangjiashun@huawei.com>
    Signed-off-by: Liming An <anliming1@h-partners.com>

--- drivers/iommu/hisilicon/flush.h
+++ drivers/iommu/hisilicon/flush.h
@@ -43,8 +43,8 @@ void ummu_device_sync_tect(struct ummu_device *ummu, u32 tecte_tag);
 void ummu_sync_tct(struct ummu_device *ummu, u32 tecte_tag, u32 tid,
 		   bool leaf);
 void ummu_sync_tct_all(struct ummu_device *ummu, u32 tecte_tag);
-int ummu_device_flush_plb(struct ummu_device *ummu, u32 tag, u32 tid,
-			  u64 addr, size_t size);
+void ummu_device_ioplb_sync(struct iommu_domain *domain,
+			    struct iommu_plb_gather *plb_gather);
 void ummu_device_flush_plb_all(struct iommu_domain *domain);
 void ummu_device_flush_ioplb_all(struct ummu_device *ummu);
 int ummu_device_check_pa_continuity(struct ummu_device *ummu, u64 addr,
