d8e6d202425e615c546091fd4b3b1778e0a9dafa
Jann Horn <jannh@google.com>
2026-08-12 10:28:25 +0800

    proc: protect ptrace_may_access() with exec_update_lock (part 1)
    stable inclusion
    from stable-v5.10.261
    commit ae1e630bcaac739f625822078edbaea98366930d
    category: bugfix
    bugzilla: https://atomgit.com/src-openeuler/kernel/issues/16666
    CVE: CVE-2026-64279
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=ae1e630bcaac739f625822078edbaea98366930d
    
    --------------------------------
    
    [ Upstream commit 6650527444dadc63d84aa939d14ecba4fadb2f69 ]
    
    Fix the easy cases where procfs currently calls ptrace_may_access() without
    exec_update_lock protection, where the fix is to simply add the extra lock
    or use mm_access():
    
     - do_task_stat(): grab exec_update_lock
     - proc_pid_wchan(): grab exec_update_lock
     - proc_map_files_lookup(): use mm_access() instead of get_task_mm()
     - proc_map_files_readdir(): use mm_access() instead of get_task_mm()
     - proc_ns_get_link(): grab exec_update_lock
     - proc_ns_readlink(): grab exec_update_lock
    
    Fixes: f83ce3e6b02d ("proc: avoid information leaks to non-privileged processes")
    Cc: stable@vger.kernel.org
    Signed-off-by: Jann Horn <jannh@google.com>
    Link: https://patch.msgid.link/20260518-procfs-lockfix-part1-v1-1-5c3d20e0ac33@google.com
    Signed-off-by: Christian Brauner (Amutable) <brauner@kernel.org>
    [ converted NULL return from mm_access() to ERR_PTR(-ESRCH) at call sites since 5.15's mm_access() returns NULL when the task has no mm ]
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Conflicts:
            fs/proc/array.c
    [Context differences.]
    Signed-off-by: Xia Fukun <xiafukun@huawei.com>

--- fs/proc/array.c
+++ fs/proc/array.c
@@ -463,6 +463,11 @@ static int do_task_stat(struct seq_file *m, struct pid_namespace *ns,
 	int exit_code = task->exit_code;
 	struct signal_struct *sig = task->signal;
 	unsigned int seq = 1;
+	int ret;
+
+	ret = down_read_killable(&task->signal->exec_update_lock);
+	if (ret)
+		return ret;
 
 	state = *get_task_state(task);
 	vsize = eip = esp = 0;
@@ -644,6 +649,7 @@ static int do_task_stat(struct seq_file *m, struct pid_namespace *ns,
 		seq_puts(m, " 0");
 
 	seq_putc(m, '\n');
+	up_read(&task->signal->exec_update_lock);
 	if (mm)
 		mmput(mm);
 	return 0;
