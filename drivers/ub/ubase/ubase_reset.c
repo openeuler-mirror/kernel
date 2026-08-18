6e28dbd41f9a4f272196d7e671c1119ad9256f41
Xiongchuan Zhou <zhouxiongchuan@h-partners.com>
2026-08-18 14:49:24 +0800

    ub: ubase: Recover the cmdq queue after reset failure
    drivers inclusion
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/9786
    CVE: NA
    
    (cherry-pick from OLK-6.6 commit 5ab2bd975d7c)
    
    --------------------------------------------------------------------------
    
    In the next generation implementation, if the MUE fails to query the UE
    status in the ubase_suspend phase, the reset procedure is terminated
    directly. Currently, the MUE disables the cmdq in the ubase_suspend phase
    and then enters the ubase_resume phase. If the resume phase fails, the
    cmdq channel is disabled. When the reset retry enters the ubase_suspend
    phase, the cmdq channel is disabled. As a result, the system cannot query
    whether the UE reset is complete and returns a failure message.
    To solve the preceding problem, the cmdq queue needs to be restored after
    the ubase_resume fails.
    
    Fixes: 2368191f131b ("ub: ubase&unic&cdma&udma: Supporting the restart optimization of the management and control process")
    Signed-off-by: Zhenyu Wan <wanzhenyu3@huawei.com>
    Signed-off-by: Xiongchuan Zhou <zhouxiongchuan@h-partners.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- drivers/ub/ubase/ubase_reset.c
+++ drivers/ub/ubase/ubase_reset.c
@@ -250,6 +250,44 @@ int ubase_reset_done(struct ubase_dev *udev)
 	return 0;
 }
 
+static void ubase_pmu_suspend(struct ubase_dev *udev)
+{
+	__ubase_cmd_disable(udev);
+	udev->reset_stat.elr_reset_cnt++;
+}
+
+static void ubase_pmu_resume(struct ubase_dev *udev)
+{
+	ubase_ubus_reset_init(udev->dev);
+	__ubase_cmd_enable(udev);
+	udev->reset_stat.reset_done_cnt++;
+	udev->reset_stat.hw_reset_done_cnt++;
+	clear_bit(UBASE_STATE_RST_WAIT_DEACTIVE_B, &udev->state_bits);
+	clear_bit(UBASE_STATE_RST_HANDLING_B, &udev->state_bits);
+	clear_bit(UBASE_STATE_DISABLED_B, &udev->state_bits);
+}
+
+static void ubase_resume_fail_handle(struct ubase_dev *udev)
+{
+	udev->reset_stat.reset_fail_cnt++;
+	/* The cmdq is restored at the unified egress so that commands can
+	 * be sent through cmdq during the next reset retry. This function
+	 * is executed before reset_stage is set to NONE, which enables
+	 * ubase_cmd_uninit to skip the sleep.
+	 */
+	if (test_bit(UBASE_STATE_CMD_DISABLE, &udev->hw.state)) {
+		ubase_cmd_uninit(udev);
+		if (ubase_cmd_init(udev))
+			ubase_err(udev, "failed to reinit cmdq after reset failure.\n");
+	}
+	udev->reset_stage = UBASE_RESET_STAGE_NONE;
+	ubase_resume_aux_devices(udev, UBASE_RESET_STAGE_ABORT);
+	clear_bit(UBASE_STATE_RST_HANDLING_B, &udev->state_bits);
+	clear_bit(UBASE_STATE_DISABLED_B, &udev->state_bits);
+	set_bit(UBASE_STATE_RST_FAILED_B, &udev->state_bits);
+	ubase_reset_err_handle(udev);
+}
+
 void ubase_suspend(struct ubase_dev *udev)
 {
 	if (!test_bit(UBASE_STATE_INITED_B, &udev->state_bits) ||
@@ -263,8 +301,7 @@ void ubase_suspend(struct ubase_dev *udev)
 	set_bit(UBASE_STATE_RST_WAIT_DEACTIVE_B, &udev->state_bits);
 
 	if (ubase_dev_pmu_supported(udev)) {
-		__ubase_cmd_disable(udev);
-		udev->reset_stat.elr_reset_cnt++;
+		ubase_pmu_suspend(udev);
 		return;
 	}
 
@@ -299,13 +336,7 @@ void ubase_resume(struct ubase_dev *udev, int pret)
 	}
 
 	if (ubase_dev_pmu_supported(udev)) {
-		ubase_ubus_reset_init(udev->dev);
-		__ubase_cmd_enable(udev);
-		udev->reset_stat.reset_done_cnt++;
-		udev->reset_stat.hw_reset_done_cnt++;
-		clear_bit(UBASE_STATE_RST_WAIT_DEACTIVE_B, &udev->state_bits);
-		clear_bit(UBASE_STATE_RST_HANDLING_B, &udev->state_bits);
-		clear_bit(UBASE_STATE_DISABLED_B, &udev->state_bits);
+		ubase_pmu_resume(udev);
 		return;
 	}
 
@@ -338,12 +369,6 @@ void ubase_resume(struct ubase_dev *udev, int pret)
 
 	ret = ubase_dev_reset_init(udev);
 	if (ret) {
-		/* When the reset fails, the cmdq resources are uninitialized.
-		 * To ensure that the subsequent reset is successful,
-		 * need to initialize the cmdq resources again.
-		 */
-		if (ubase_cmd_init(udev))
-			ubase_err(udev, "failed to reinit cmdq after reset failure.\n");
 		if (test_and_clear_bit(UBASE_STATE_INIT_AGAIN_B, &udev->state_bits))
 			goto timeout_resume;
 		goto err_resume;
@@ -369,13 +394,7 @@ void ubase_resume(struct ubase_dev *udev, int pret)
 timeout_resume:
 	set_bit(UBASE_STATE_RST_TIMEOUT_RETRY_B, &udev->state_bits);
 err_resume:
-	udev->reset_stat.reset_fail_cnt++;
-	udev->reset_stage = UBASE_RESET_STAGE_NONE;
-	ubase_resume_aux_devices(udev, UBASE_RESET_STAGE_ABORT);
-	clear_bit(UBASE_STATE_RST_HANDLING_B, &udev->state_bits);
-	clear_bit(UBASE_STATE_DISABLED_B, &udev->state_bits);
-	set_bit(UBASE_STATE_RST_FAILED_B, &udev->state_bits);
-	ubase_reset_err_handle(udev);
+	ubase_resume_fail_handle(udev);
 }
 
 void ubase_errhandle_service_task(struct ubase_delay_work *ubase_work)
