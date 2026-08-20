0a77555eeb21ae3d1caf8009082cfb2579acbee7
Mark Brown <broonie@kernel.org>
2026-08-20 10:16:38 +0800

    arm64/sve: Report FEAT_SVE_B16B16 to userspace
    mainline inclusion
    from mainline-v6.7-rc1
    commit 5d5b4e8c2d9ec12a5cc013b82c1873cd387e0ddf
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8904
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=5d5b4e8c2d9ec12a5cc013b82c1873cd387e0ddf
    
    ----------------------------------------------------------------------
    
    SVE 2.1 introduced a new feature FEAT_SVE_B16B16 which adds instructions
    supporting the BFloat16 floating point format. Report this to userspace
    through the ID registers and hwcap.
    
    Reported-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Link: https://lore.kernel.org/r/20230915-arm64-zfr-b16b16-el0-v1-1-f9aba807bdb5@kernel.org
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Conflicts:
    	arch/arm64/kernel/hwcap_str.h
    	arch/arm64/kernel/cpufeature.c
    	arch/arm64/include/asm/hwcap.h
    	arch/arm64/include/uapi/asm/hwcap.h
    	arch/arm64/kernel/cpuinfo.c
    	Documentation/arch/arm64/elf_hwcaps.rst
    [context conflict]
    Signed-off-by: Qinxin Xia <xiaqinxin@huawei.com>
    Signed-off-by: Hongye Lin <linhongye@h-partners.com>

--- arch/arm64/include/uapi/asm/hwcap.h
+++ arch/arm64/include/uapi/asm/hwcap.h
@@ -104,6 +104,7 @@
 #define HWCAP2_SME_F16F16	(1UL << 42)
 #define HWCAP2_MOPS		(1UL << 43)
 #define HWCAP2_HBC		(1UL << 44)
+#define HWCAP2_SVE_B16B16	(1UL << 45)
 #define HWCAP2_FPMR		(1UL << 48)
 #define HWCAP2_LUT		(1UL << 49)
 #define HWCAP2_FAMINMAX	(1UL << 50)
