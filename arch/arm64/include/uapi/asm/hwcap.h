47ddf194229b97c9f3dc79fbd816e8c6ce075678
Mark Brown <broonie@kernel.org>
2026-08-20 10:16:43 +0800

    arm64/hwcap: Describe 2024 dpISA extensions to userspace
    mainline inclusion
    from mainline-v6.14-rc1
    commit 819935464cb2f72fff8dfbbf95cf2726d4a66388
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8904
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=819935464cb2f72fff8dfbbf95cf2726d4a66388
    
    ----------------------------------------------------------------------
    
    The 2024 dpISA introduces a number of architecture features all of which
    only add new instructions so only require the addition of hwcaps and ID
    register visibility.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Link: https://lore.kernel.org/r/20250107-arm64-2024-dpisa-v5-3-7578da51fc3d@kernel.org
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Qinxin Xia <xiaqinxin@huawei.com>
    Conflicts:
    	arch/arm64/kernel/hwcap_str.h
    	arch/arm64/kernel/cpufeature.c
    	arch/arm64/include/asm/hwcap.h
    	arch/arm64/include/uapi/asm/hwcap.h
    	arch/arm64/kernel/cpuinfo.c
    	Documentation/arch/arm64/elf_hwcaps.rst
    [context conflict]
    Signed-off-by: Hongye Lin <linhongye@h-partners.com>

--- arch/arm64/include/uapi/asm/hwcap.h
+++ arch/arm64/include/uapi/asm/hwcap.h
@@ -55,6 +55,22 @@
 #define HWCAP_SB		(1 << 29)
 #define HWCAP_PACA		(1 << 30)
 #define HWCAP_PACG		(1UL << 31)
+/* bit 32 (HWCAP_GCS) is unused in this kernel */
+#define HWCAP_CMPBR		(1UL << 33)
+#define HWCAP_FPRCVT		(1UL << 34)
+#define HWCAP_F8MM8		(1UL << 35)
+#define HWCAP_F8MM4		(1UL << 36)
+#define HWCAP_SVE_F16MM		(1UL << 37)
+#define HWCAP_SVE_ELTPERM	(1UL << 38)
+#define HWCAP_SVE_AES2		(1UL << 39)
+#define HWCAP_SVE_BFSCALE	(1UL << 40)
+#define HWCAP_SVE2P2		(1UL << 41)
+#define HWCAP_SME2P2		(1UL << 42)
+#define HWCAP_SME_SBITPERM	(1UL << 43)
+#define HWCAP_SME_AES		(1UL << 44)
+#define HWCAP_SME_SFEXPA	(1UL << 45)
+#define HWCAP_SME_STMOP		(1UL << 46)
+#define HWCAP_SME_SMOP4		(1UL << 47)
 
 /*
  * HWCAP2 flags - for AT_HWCAP2
