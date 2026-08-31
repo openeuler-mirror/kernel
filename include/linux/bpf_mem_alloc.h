677e069bcd3d092c56c22ebab7f2a9e755042934
Kumar Kartikeya Dwivedi <memxor@gmail.com>
2026-08-31 07:29:15 +0000

    bpf: Register dtor for freeing special fields
    mainline inclusion
    from mainline-v7.0-rc2
    commit 1df97a7453eec80c1912c2d0360290a3970a7671
    category: bugfix
    bugzilla: https://atomgit.com/src-openeuler/kernel/issues/17960
    CVE: CVE-2026-74314
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=1df97a7453ee
    
    --------------------------------
    
    There is a race window where BPF hash map elements can leak special
    fields if the program with access to the map value recreates these
    special fields between the check_and_free_fields done on the map value
    and its eventual return to the memory allocator.
    
    Several ways were explored prior to this patch, most notably [0] tried
    to use a poison value to reject attempts to recreate special fields for
    map values that have been logically deleted but still accessible to BPF
    programs (either while sitting in the free list or when reused). While
    this approach works well for task work, timers, wq, etc., it is harder
    to apply the idea to kptrs, which have a similar race and failure mode.
    
    Instead, we change bpf_mem_alloc to allow registering destructor for
    allocated elements, such that when they are returned to the allocator,
    any special fields created while they were accessible to programs in the
    mean time will be freed. If these values get reused, we do not free the
    fields again before handing the element back. The special fields thus
    may remain initialized while the map value sits in a free list.
    
    When bpf_mem_alloc is retired in the future, a similar concept can be
    introduced to kmalloc_nolock-backed kmem_cache, paired with the existing
    idea of a constructor.
    
    Note that the destructor registration happens in map_check_btf, after
    the BTF record is populated and (at that point) available for inspection
    and duplication. Duplication is necessary since the freeing of embedded
    bpf_mem_alloc can be decoupled from actual map lifetime due to logic
    introduced to reduce the cost of rcu_barrier()s in mem alloc free path in
    9f2c6e96c65e ("bpf: Optimize rcu_barrier usage between hash map and bpf_mem_alloc.").
    
    As such, once all callbacks are done, we must also free the duplicated
    record. To remove dependency on the bpf_map itself, also stash the key
    size of the map to obtain value from htab_elem long after the map is
    gone.
    
      [0]: https://lore.kernel.org/bpf/20260216131341.1285427-1-mykyta.yatsenko5@gmail.com
    
    Fixes: 14a324f6a67e ("bpf: Wire up freeing of referenced kptr")
    Fixes: 1bfbc267ec91 ("bpf: Enable bpf_timer and bpf_wq in any context")
    Reported-by: Alexei Starovoitov <ast@kernel.org>
    Tested-by: syzbot@syzkaller.appspotmail.com
    Signed-off-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>
    Link: https://lore.kernel.org/r/20260227224806.646888-2-memxor@gmail.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Conflicts:
    	include/linux/bpf_mem_alloc.h
    	kernel/bpf/hashtab.c
    	kernel/bpf/memalloc.c
    [ctx conflicts]
    Signed-off-by: Pu Lehui <pulehui@huawei.com>

--- include/linux/bpf_mem_alloc.h
+++ include/linux/bpf_mem_alloc.h
@@ -13,6 +13,8 @@ struct bpf_mem_alloc {
 	struct bpf_mem_cache __percpu *cache;
 	bool percpu;
 	struct work_struct work;
+	void (*dtor_ctx_free)(void *ctx);
+	void *dtor_ctx;
 };
 
 /* 'size != 0' is for bpf_mem_alloc which manages fixed-size objects.
@@ -24,6 +26,10 @@ struct bpf_mem_alloc {
  */
 int bpf_mem_alloc_init(struct bpf_mem_alloc *ma, int size, bool percpu);
 void bpf_mem_alloc_destroy(struct bpf_mem_alloc *ma);
+void bpf_mem_alloc_set_dtor(struct bpf_mem_alloc *ma,
+			    void (*dtor)(void *obj, void *ctx),
+			    void (*dtor_ctx_free)(void *ctx),
+			    void *ctx);
 
 /* Check the allocation size for kmalloc equivalent allocator */
 int bpf_mem_alloc_check_size(bool percpu, size_t size);
