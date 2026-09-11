1e0d43a9fad43eff138afb663db13c55c6e7f301
hehua10 <hehua10@huawei.com>
2026-09-11 15:19:40 +0800

    KVM: arm64: Don't leave mmu->pgt dangling on kvm_init_stage2_mmu() error
    kvm inclusion
    bugzilla: https://atomgit.com/openeuler/kernel/issues/9932
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=2fc0f3e2b9a9f397554ffe86e8f6eb0e2507ec6e
    
    (cherry-pick from OLK-6.6 commit b7892498d816)
    
    ----------------------------------------------------------------------
    
    If kvm_init_stage2_mmu() fails to allocate 'mmu->last_vcpu_ran', it
    destroys the newly allocated stage-2 page-table before returning ENOMEM.
    
    Unfortunately, it also leaves a dangling pointer in 'mmu->pgt' which
    points at the freed 'kvm_pgtable' structure. This is likely to confuse
    the kvm_vcpu_init_nested() failure path which can double-free the
    structure if it finds it via kvm_free_stage2_pgd().
    
    Ensure that the dangling 'mmu->pgt' pointer is cleared when returning an
    error from kvm_init_stage2_mmu().
    
    Link: https://sashiko.dev/#/patchset/20260327140039.21228-1-will%40kernel.org?patch=12265
    Signed-off-by: Will Deacon <will@kernel.org>
    Link: https://patch.msgid.link/20260327192758.21739-2-will@kernel.org
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Signed-off-by: hehua10 <hehua10@huawei.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>

--- arch/arm64/kvm/mmu.c
+++ arch/arm64/kvm/mmu.c
@@ -951,6 +951,7 @@ int kvm_init_stage2_mmu(struct kvm *kvm, struct kvm_s2_mmu *mmu, unsigned long t
 
 out_destroy_pgtable:
 	kvm_pgtable_stage2_destroy(pgt);
+	mmu->pgt = NULL;
 out_free_pgtable:
 	kfree(pgt);
 	return err;
