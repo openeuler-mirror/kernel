9bf0bef6b15bf98af4131781731e8e99df5dba1b
Mark Rutland <mark.rutland@arm.com>
2026-09-17 23:14:01 +0800

    arm64/fpsimd: signal32: Always save+flush state early
    mainline inclusion
    from mainline-6.16
    commit 3aa4d74438afa1324513a7a6bc30f57e8727ad86
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8904
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=3aa4d74438afa1324513a7a6bc30f57e8727ad86
    
    ----------------------------------------------------------------------
    
    There are several issues with the way the native signal handling code
    manipulates FPSIMD/SVE/SME state. To fix those issues, subsequent
    patches will rework the native signal handling code to always save+flush
    the current task's FPSIMD/SVE/SME state before manipulating that state.
    
    In preparation for those changes, rework the compat signal handling code
    to save+flush the current task's FPSIMD state before manipulating it.
    
    Subsequent patches will remove fpsimd_signal_preserve_current_state()
    and fpsimd_update_current_state(). Compat tasks can only have FPSIMD
    state, and cannot have any SVE or SME state. Thus, the SVE state
    manipulation present in fpsimd_signal_preserve_current_state() and
    fpsimd_update_current_state() is not necessary, and it is safe to
    directly manipulate current->thread.uw.fpsimd_state once it has been
    saved+flushed.
    
    Use fpsimd_save_and_flush_current_state() to save+flush the state for
    both signal delivery and signal return, before the state is manipulated
    in any way. While it would be safe for compat_restore_vfp_context() to
    use fpsimd_flush_task_state(current), there are several extant issues in
    the native signal code resulting from incorrect use of
    fpsimd_flush_task_state(), and for consistency it is preferable to use
    fpsimd_save_and_flush_current_state().
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Marc Zyngier <maz@kernel.org>
    Cc: Mark Brown <broonie@kernel.org>
    Cc: Will Deacon <will@kernel.org>
    Reviewed-by: Mark Brown <broonie@kernel.org>
    Link: https://lore.kernel.org/r/20250409164010.3480271-12-mark.rutland@arm.com
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Conflicts:
    	arch/arm64/kernel/signal32.c
    [Context conflicts]
    Signed-off-by: Qinxin Xia <xiaqinxin@huawei.com>
    Signed-off-by: Hongye Lin <linhongye@h-partners.com>

--- arch/arm64/kernel/signal32.c
+++ arch/arm64/kernel/signal32.c
@@ -82,7 +82,7 @@ static int a32_preserve_vfp_context(struct a32_vfp_sigframe __user *frame)
 	 * Note that this also saves V16-31, which aren't visible
 	 * in AArch32.
 	 */
-	fpsimd_signal_preserve_current_state();
+	fpsimd_save_and_flush_current_state();
 
 	/* Place structure header on the stack */
 	__put_user_error(magic, &frame->magic, err);
@@ -148,14 +148,17 @@ static int a32_restore_vfp_context(struct a32_vfp_sigframe __user *frame)
 	fpsimd.fpsr = fpscr & VFP_FPSCR_STAT_MASK;
 	fpsimd.fpcr = fpscr & VFP_FPSCR_CTRL_MASK;
 
+	if (err)
+		return -EFAULT;
+
 	/*
 	 * We don't need to touch the exception register, so
 	 * reload the hardware state.
 	 */
-	if (!err)
-		fpsimd_update_current_state(&fpsimd);
+	fpsimd_save_and_flush_current_state();
+	current->thread.uw.fpsimd_state = fpsimd;
 
-	return err ? -EFAULT : 0;
+	return 0;
 }
 
 static int a32_restore_sigframe(struct pt_regs *regs,
