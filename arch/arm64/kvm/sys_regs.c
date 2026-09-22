60ccf638d8e733a95bea0368a50b9e6971b703a7
Mark Brown <broonie@kernel.org>
2026-09-22 14:44:24 +0800

    KVM: arm64: Allow control of dpISA extensions in ID_AA64ISAR3_EL1
    mainline inclusion
    from mainline-6.12
    commit fd22af17a458d98c14cebd00091cebf69b954b40
    category: feature
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8904
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=fd22af17a458d98c14cebd00091cebf69b954b40
    
    ----------------------------------------------------------------------
    
    ID_AA64ISAR3_EL1 is currently marked as unallocated in KVM but does have a
    number of bitfields defined in it. Expose FPRCVT and FAMINMAX, two simple
    instruction only extensions to guests.
    
    Reviewed-by: Marc Zyngier <maz@kernel.org>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Link: https://lore.kernel.org/r/20250107-arm64-2024-dpisa-v5-4-7578da51fc3d@kernel.org
    Signed-off-by: Will Deacon <will@kernel.org>
    Conflicts:
    		arch/arm64/kvm/sys_regs.c
    [Context conflicts]
    Signed-off-by: Qinxin Xia <xiaqinxin@huawei.com>
    Signed-off-by: Hongye Lin <linhongye@h-partners.com>

--- arch/arm64/kvm/sys_regs.c
+++ arch/arm64/kvm/sys_regs.c
@@ -1435,6 +1435,9 @@ static u64 __kvm_read_sanitised_id_reg(const struct kvm_vcpu *vcpu,
 			val &= ~ARM64_FEATURE_MASK(ID_AA64ISAR2_EL1_WFxT);
 		val &= ~ARM64_FEATURE_MASK(ID_AA64ISAR2_EL1_MOPS);
 		break;
+	case SYS_ID_AA64ISAR3_EL1:
+		val &= ID_AA64ISAR3_EL1_FAMINMAX | ID_AA64ISAR3_EL1_LSUI;
+		break;
 	case SYS_ID_AA64MMFR2_EL1:
 		val &= ~ID_AA64MMFR2_EL1_CCIDX_MASK;
 		break;
@@ -2523,7 +2526,8 @@ static const struct sys_reg_desc sys_reg_descs[] = {
 					ID_AA64ISAR2_EL1_MOPS |
 					ID_AA64ISAR2_EL1_APA3 |
 					ID_AA64ISAR2_EL1_GPA3)),
-	ID_UNALLOCATED(6,3),
+	ID_WRITABLE(ID_AA64ISAR3_EL1, (ID_AA64ISAR3_EL1_FAMINMAX |
+					ID_AA64ISAR3_EL1_LSUI)),
 	ID_UNALLOCATED(6,4),
 	ID_UNALLOCATED(6,5),
 	ID_UNALLOCATED(6,6),
